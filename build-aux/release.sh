e03608ba1428f5f877b3897af1bb57a5cb6a5b7c
Federico Simoncelli <fsimonce@redhat.com>
2011-07-20 09:46:27 +0000

    Introduce new version system for VDSM
    Change-Id: I9b9efb781a59bc1179bdb1b10b9ec17b7d08fbc1
    Reviewed-on: http://gerrit.usersys.redhat.com/752
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Tested-by: Dan Kenigsberg <danken@redhat.com>

--- build-aux/release.sh
+++ build-aux/release.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
-GIT_RELEASE=`git describe | awk -F- '{print $3 "." $5}' | tr -d '\n'`
-GIT_BRANCH=`git branch | awk '/^*/{print $2}' | sed 's/[^a-zA-Z0-9_.]//g'`
-echo -n $GIT_RELEASE.$GIT_BRANCH
+# tags and output releases:
+#   - v4.9.0   => 0 (upstream clean)
+#   - v4.9.0-1 => 1 (downstream clean)
+#   - v4.9.0-2-g34e62f   => 0.2.g34e62f (upstream dirty)
+#   - v4.9.0-1-2-g34e62f => 1.2.g34e62f (downstream dirty)
+AWK_RELEASE='
+    BEGIN { FS="-"; OFS="." }
+    /^v[0-9]/ {
+      if (NF == 1) print 0
+      else if (NF == 2) print $2
+      else if (NF == 3) print 0, $2, $3
+      else if (NF == 4) print $2, $3, $4
+    }'
+git describe 2> /dev/null \
+    | awk "$AWK_RELEASE" | tr -cd '[:alnum:].'
