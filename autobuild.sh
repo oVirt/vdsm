ed1c0b677c8415a819b473e47e34a65d5c251b13
Daniel P. Berrange <berrange@redhat.com>
2011-08-22 14:39:56 +0100

    Add control script for automated builds
    The http://autobuild.org/ tool is a framework for doing automated
    builds from upstream source, including builds of RPMs. The file
    autobuild.sh is a simple script that integrates with this tool.
    It can also be run directly by developers
    
      export AUTOBUILD_INSTALL_ROOT=$HOME/builder
      ./autobuild.sh
    
    * vdsm.spec.in: Add a unique counter for automated builds
    * .gitignore: Ignore results.log file
    * autobuild.sh: Add script for doing automated builds of source
      and RPM
    
    Change-Id: Idf6f525ad6c0693325f441a8a71865aaf9ab081b
    Reviewed-on: http://gerrit.usersys.redhat.com/839
    Reviewed-by: Federico Simoncelli <fsimonce@redhat.com>
    Tested-by: Federico Simoncelli <fsimonce@redhat.com>
    Reviewed-by: Saggi Mizrahi <smizrahi@redhat.com>

---
+++ autobuild.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+set -e
+set -v
+
+# Make things clean.
+
+test -n "$1" && RESULTS=$1 || RESULTS=results.log
+: ${AUTOBUILD_INSTALL_ROOT=$HOME/builder}
+
+test -f Makefile && make -k distclean || :
+
+./autogen.sh --prefix="$AUTOBUILD_INSTALL_ROOT"
+
+# If the MAKEFLAGS envvar does not yet include a -j option,
+# add -jN where N depends on the number of processors.
+case $MAKEFLAGS in
+  *-j*) ;;
+  *) n=$(getconf _NPROCESSORS_ONLN 2> /dev/null)
+    test "$n" -gt 0 || n=1
+    n=$(expr $n + 1)
+    MAKEFLAGS="$MAKEFLAGS -j$n"
+    export MAKEFLAGS
+    ;;
+esac
+
+make
+make install
+
+# set -o pipefail is a bashism; this use of exec is the POSIX alternative
+exec 3>&1
+st=$(
+  exec 4>&1 >&3
+  { make check 2>&1 3>&- 4>&-; echo $? >&4; } | tee "$RESULTS"
+)
+exec 3>&-
+test "$st" = 0
+
+rm -f *.tar.gz
+make dist
+
+if [ -n "$AUTOBUILD_COUNTER" ]; then
+  EXTRA_RELEASE=".auto$AUTOBUILD_COUNTER"
+else
+  NOW=`date +"%s"`
+  EXTRA_RELEASE=".$USER$NOW"
+fi
+
+if [ -f /usr/bin/rpmbuild ]; then
+  rpmbuild --nodeps \
+     --define "extra_release $EXTRA_RELEASE" \
+     --define "_sourcedir `pwd`" \
+     -ba --clean vdsm.spec
+fi
