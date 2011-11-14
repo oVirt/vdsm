25d25100db8b2966d923ba3592140a3c6c01f3d1
Federico Simoncelli <fsimonce@redhat.com>
2011-11-14 15:57:45 +0000

    Revise the release versioning
    In this patch:
    * revise the release versioning according to fedora guidelines
    
    Change-Id: I1f7b267a3ea9f415219fec0d45d4e4dad8eadb70
    Reviewed-on: http://gerrit.ovirt.org/218
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Tested-by: Dan Kenigsberg <danken@redhat.com>

--- build-aux/release.sh
+++ build-aux/release.sh
@@ -2,15 +2,15 @@
 # tags and output releases:
 #   - v4.9.0   => 0 (upstream clean)
 #   - v4.9.0-1 => 1 (downstream clean)
-#   - v4.9.0-2-g34e62f   => 0.2.g34e62f (upstream dirty)
-#   - v4.9.0-1-2-g34e62f => 1.2.g34e62f (downstream dirty)
+#   - v4.9.0-2-g34e62f1   => 0.2.git34e62f1 (upstream dirty)
+#   - v4.9.0-1-2-g34e62f1 => 1.2.git34e62f1 (downstream dirty)
 AWK_RELEASE='
     BEGIN { FS="-"; OFS="." }
     /^v[0-9]/ {
       if (NF == 1) print 0
       else if (NF == 2) print $2
-      else if (NF == 3) print 0, $2, $3
-      else if (NF == 4) print $2, $3, $4
+      else if (NF == 3) print 0, $2, "git" substr($3, 2)
+      else if (NF == 4) print $2, $3, "git" substr($4, 2)
     }'
 git describe 2> /dev/null \
     | awk "$AWK_RELEASE" | tr -cd '[:alnum:].'
