ce280b09c776b33203847dc7e7ad77ee90a34bc0
Saggi Mizrahi <smizrahi@redhat.com>
2011-11-28 15:16:27 -0500

    Fix getMasterDomain masterVersion handling
    If getMasterDomain() gets -1 as the expected masterVersion it should
    skip the masterVersion equality test and accept whatever version is in
    the MD.
    
    Change-Id: I2e5dd06800e770c098460466fa8642c5bc928e54
    Reviewed-on: http://gerrit.ovirt.org/431
    Tested-by: Saggi Mizrahi <smizrahi@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/storage/sp.py
+++ vdsm/storage/sp.py
@@ -1478,6 +1478,7 @@ class StoragePool:
         'msdUUID' - expected master domain UUID.
         'masterVersion' - expected pool msd version.
         """
+        masterVersion = int(masterVersion)
         try:
             domain = sdCache.produce(msdUUID)
         except se.StorageDomainDoesNotExist:
@@ -1494,7 +1495,7 @@ class StoragePool:
             raise se.StoragePoolWrongMaster(self.spUUID, msdUUID)
 
         version = self._getPoolMD(domain)[PMDK_MASTER_VER]
-        if version != int(masterVersion):
+        if version != masterVersion and masterVersion != -1:
             self.log.error("Requested master domain %s does not have expected version %s it is version %s",
                         msdUUID, masterVersion, version)
             raise se.StoragePoolWrongMaster(self.spUUID, msdUUID)
