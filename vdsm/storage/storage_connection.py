958a5424b1b69c5c5df53e31ea1eb67e50b3b0ca
Saggi Mizrahi <smizrahi@redhat.com>
2011-12-09 11:18:38 -0500

    Rearange code in storage_connection to minimize code duplication
    Change-Id: If3e28c064043f19cefccde13decf6699d2718634
    Reviewed-on: http://gerrit.ovirt.org/556
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Tested-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/storage/storage_connection.py
+++ vdsm/storage/storage_connection.py
@@ -97,13 +97,37 @@ class StorageServerConnection:
         conParams = self.__validateConnectionParams(domType, conList)
 
         if domType == sd.NFS_DOMAIN:
-            return self.__connectFileServer(conParams)
+            func = self.__connectFileServer
         elif domType == sd.LOCALFS_DOMAIN:
-            return self.__connectLocalConnection(conParams)
+            func = self.__connectLocalConnection
         elif domType in sd.BLOCK_DOMAIN_TYPES:
-            return self.__connectiSCSIServer(conParams)
+            func = self.__connectiSCSIServer
         else:
             raise se.InvalidParameterException("type", domType)
+
+        return self.__processConnections(func, conParams,
+                se.StorageServerConnectionError.code)
+
+
+    def __processConnections(self, func, conParams,
+            defaultErrCode=se.StorageServerConnectionError.code):
+        conStatus = []
+        for con in conParams:
+            try:
+                rc = func(con)
+            # We should return error status instead of exception itself
+            except se.StorageException, ex:
+                rc = ex.code
+                self.log.error("Error during storage connection operation:", exc_info=True)
+            except Exception, ex:
+                rc = defaultErrCode
+                self.log.error("Error during storage connection operation:", exc_info=True)
+
+            conStatus.append({'id':con['cid'], 'status': rc})
+
+        return conStatus
+
+
 
     def disconnect(self, domType, conList):
         """
@@ -113,13 +137,16 @@ class StorageServerConnection:
         conParams = self.__validateConnectionParams(domType, conList)
 
         if domType == sd.NFS_DOMAIN:
-            return self.__disconnectFileServer(conParams, mount.VFS_NFS)
+            func = self.__disconnectFileServer
         elif domType == sd.LOCALFS_DOMAIN:
-            return self.__disconnectLocalConnection(conParams)
+            func = self.__disconnectLocalConnection
         elif domType in sd.BLOCK_DOMAIN_TYPES:
-            return self.__disconnectiSCSIServer(conParams)
+            func = self.__disconnectiSCSIServer
         else:
             raise se.InvalidParameterException("type", domType)
+
+        return self.__processConnections(func, conParams,
+                se.StorageServerDisconnectionError.code)
 
     def validate(self, domType, conList):
         """
@@ -129,206 +156,140 @@ class StorageServerConnection:
         conParams = self.__validateConnectionParams(domType, conList)
 
         if domType == sd.NFS_DOMAIN:
-            return self.__validateFileServer(conParams)
+            func = self.__validateFileServer
         elif domType == sd.LOCALFS_DOMAIN:
-            return self.__validateLocalConnection(conParams)
+            func = self.__validateLocalConnection
         elif domType in sd.BLOCK_DOMAIN_TYPES:
-            return self.__validateiSCSIConnection(conParams)
+            func = self.__validateiSCSIConnection
         else:
             raise se.InvalidParameterException("type", domType)
 
-    def __connectFileServer(self, conParams):
+        return self.__processConnections(func, conParams,
+                se.StorageServerValidationError.code)
+
+    def __connectFileServer(self, con):
         """
         Connect to a storage low level entity.
         """
-        conStatus = []
         localPath = os.path.join(self.storage_repository, sd.DOMAIN_MNT_POINT)
         fileUtils.createdir(localPath)
 
-        for con in conParams:
-            try:
-                mntPoint = fileUtils.transformPath(con['rp'])
-                mntPath = os.path.join(localPath, mntPoint)
+        mntPoint = fileUtils.transformPath(con['rp'])
+        mntPath = os.path.join(localPath, mntPoint)
 
-                rc = 0
-                mnt = mount.Mount(con['rp'], mntPath)
+        rc = 0
+        mnt = mount.Mount(con['rp'], mntPath)
 
-                # Stale handle usually resolves itself when doing directory lookups
-                # BUT if someone deletes the export on the servers side. We will keep
-                # getting stale handles and this is unresolvable unless you umount and
-                # remount.
-                if getProcPool().fileUtils.isStaleHandle(mnt.fs_file):
-                    # A VM might be holding a stale handle, we have to umount
-                    # but we can't umount as long as someone is holding a handle
-                    # even if it's stale. We use lazy so we can at least recover.
-                    # Processes having an open file handle will not recover until
-                    # they reopen the files.
-                    mnt.umount(lazy=True, timeout=CON_TIMEOUT)
+        # Stale handle usually resolves itself when doing directory lookups
+        # BUT if someone deletes the export on the servers side. We will keep
+        # getting stale handles and this is unresolvable unless you umount and
+        # remount.
+        if getProcPool().fileUtils.isStaleHandle(mnt.fs_file):
+            # A VM might be holding a stale handle, we have to umount
+            # but we can't umount as long as someone is holding a handle
+            # even if it's stale. We use lazy so we can at least recover.
+            # Processes having an open file handle will not recover until
+            # they reopen the files.
+            mnt.umount(lazy=True)
 
-                fileUtils.createdir(mnt.fs_file)
+        fileUtils.createdir(mnt.fs_file)
 
-                try:
-                    if not mnt.isMounted():
-                        mnt.mount(fileUtils.NFS_OPTIONS, mount.VFS_NFS, timeout=CON_TIMEOUT)
-                except mnt.MountError:
-                    self.log.error("Error during storage connection", exc_info=True)
-                    rc = se.StorageServerConnectionError.code
+        try:
+            if not mnt.isMounted():
+                mnt.mount(fileUtils.NFS_OPTIONS, mount.VFS_NFS, timeout=CON_TIMEOUT)
+        except mnt.MountError:
+            self.log.error("Error during storage connection", exc_info=True)
+            rc = se.StorageServerConnectionError.code
 
-                try:
-                    validateDirAccess(mnt.fs_file)
-                except se.StorageServerAccessPermissionError:
-                    self.log.debug("Unmounting file system %s "
-                        "(not enough access permissions)", con['rp'])
-                    mnt.umount(timeout=CON_TIMEOUT)
-                    raise
+        try:
+            validateDirAccess(mnt.fs_file)
+        except se.StorageServerAccessPermissionError:
+            self.log.debug("Unmounting file system %s "
+                "(not enough access permissions)", con['rp'])
+            mnt.umount(timeout=CON_TIMEOUT)
+            raise
 
-            # We should return error status instead of exception itself
-            except se.StorageException, ex:
-                rc = ex.code
-                self.log.error("Error during storage connection", exc_info=True)
-            except Exception:
-                rc = se.StorageServerConnectionError.code
-                self.log.error("Error during storage connection", exc_info=True)
+        return rc
 
-            conStatus.append({'id':con['cid'], 'status': rc})
-        return conStatus
-
-    def __connectLocalConnection(self, conParams):
+    def __connectLocalConnection(self, con):
         """
         Connect to a storage low level entity.
         """
-        conStatus = []
         localPath = os.path.join(self.storage_repository, sd.DOMAIN_MNT_POINT)
         fileUtils.createdir(localPath)
 
-        for con in conParams:
-            rc = 0
-            try:
-                if os.path.exists(con['rp']):
-                    lnPoint = fileUtils.transformPath(con['rp'])
-                    lnPath = os.path.join(localPath, lnPoint)
-                    if not os.path.lexists(lnPath):
-                        os.symlink(con['rp'], lnPath)
-                else:
-                    self.log.error("Path %s does not exists.", con['rp'])
-                    rc = se.StorageServerConnectionError.code
-            except se.StorageException, ex:
-                rc = ex.code
-                self.log.error("Error during storage connection: %s", str(ex), exc_info=True)
-            except Exception, ex:
-                rc = se.StorageServerConnectionError.code
-                self.log.error("Error during storage connection: %s", str(ex), exc_info=True)
+        rc = 0
+        if os.path.exists(con['rp']):
+            lnPoint = fileUtils.transformPath(con['rp'])
+            lnPath = os.path.join(localPath, lnPoint)
+            if not os.path.lexists(lnPath):
+                os.symlink(con['rp'], lnPath)
+        else:
+            self.log.error("Path %s does not exists.", con['rp'])
+            rc = se.StorageServerConnectionError.code
 
-            conStatus.append({'id':con['cid'], 'status': rc})
-        return conStatus
+        return rc
 
-    def __connectiSCSIServer(self, conParams):
+    def __connectiSCSIServer(self, con):
         """
         Connect to a storage low level entity (server).
         """
-        conStatus = []
+        if not con['iqn']:
+            iscsi.addiSCSIPortal(con['ip'], con['port'],
+                con['initiatorName'], con['user'], con['password'])[0]
+        else:
+            iscsi.addiSCSINode(con['ip'], con['port'], con['iqn'],
+                con['tpgt'], con['initiatorName'],
+                con['user'], con['password'])
+        return 0
 
-        for con in conParams:
-            try:
-                if not con['iqn']:
-                    iscsi.addiSCSIPortal(con['ip'], con['port'],
-                        con['initiatorName'], con['user'], con['password'])[0]
-                else:
-                    iscsi.addiSCSINode(con['ip'], con['port'], con['iqn'],
-                        con['tpgt'], con['initiatorName'],
-                        con['user'], con['password'])
-
-                rc = 0
-            # We should return error status instead of exception itself
-            except se.StorageException, ex:
-                rc = ex.code
-                self.log.error("Error during storage connection: %s", str(ex), exc_info=True)
-            except Exception, ex:
-                rc = se.StorageServerConnectionError.code
-                self.log.error("Error during storage connection: %s", str(ex), exc_info=True)
-
-            conStatus.append({'id':con['cid'], 'status':rc})
-
-        return conStatus
-
-    def __validateFileServer(self, conParams):
+    def __validateFileServer(self, con):
         """
         Validate that we can connect to a storage server.
         """
-        conStatus = []
-
-        for con in conParams:
+        rc = 0
+        mountpoint = tempfile.mkdtemp()
+        mnt = mount.Mount(con['rp'], mountpoint)
+        try:
             try:
-                rc = 0
-                mountpoint = tempfile.mkdtemp()
-                mnt = mount.Mount(con['rp'], mountpoint)
-                try:
-                    try:
-                        mnt.mount(fileUtils.NFS_OPTIONS, mount.VFS_NFS, timeout=CON_TIMEOUT)
-                    except mount.MountError:
-                        self.log.error("Error during storage connection validation", exc_info=True)
-                        rc = se.StorageServerValidationError.code
-                        continue
+                mnt.mount(fileUtils.NFS_OPTIONS, mount.VFS_NFS)
+            except mount.MountError:
+                self.log.error("Error during storage connection validation", exc_info=True)
+                return se.StorageServerValidationError.code
 
-                    try:
-                        validateDirAccess(mountpoint)
-                    except se.StorageServerAccessPermissionError, ex:
-                        rc = ex.code
-                finally:
-                    try:
-                        if mnt.isMounted():
-                            mnt.umount(timeout=CON_TIMEOUT)
-                    except mount.MountError:
-                        self.log.error("Error cleaning mount %s", mnt)
-                    finally:
-                        getProcPool().os.rmdir(mnt.fs_file)
-
-            except se.StorageException, ex:
+            try:
+                validateDirAccess(mountpoint)
+            except se.StorageServerAccessPermissionError, ex:
                 rc = ex.code
-                self.log.error("Error during storage connection validation: %s", str(ex), exc_info=True)
-            except Exception, ex:
-                rc = se.StorageServerValidationError.code
-                self.log.error("Error during storage connection validation: %s", str(ex), exc_info=True)
+        finally:
+            try:
+                if mnt.isMounted():
+                    mnt.umount(timeout=CON_TIMEOUT)
+            except mount.MountError:
+                self.log.error("Error cleaning mount %s", mnt)
             finally:
-                conStatus.append({'id':con['cid'], 'status': rc})
-        return conStatus
+                getProcPool().os.rmdir(mnt.fs_file)
 
-    def __validateLocalConnection(self, conParams):
+        return rc
+
+    def __validateLocalConnection(self, con):
         """
         Validate that we can connect to a storage server.
         """
-        conStatus = []
+        rc = 0
+        if os.path.exists(con['rp']):
+            validateDirAccess(con['rp'])
+        else:
+            self.log.error("Path %s does not exists.", con['rp'])
+            rc = se.StorageServerValidationError.code
 
-        for con in conParams:
-            rc = 0
-            try:
-                if os.path.exists(con['rp']):
-                    validateDirAccess(con['rp'])
-                else:
-                    self.log.error("Path %s does not exists.", con['rp'])
-                    rc = se.StorageServerValidationError.code
-            except se.StorageException, ex:
-                rc = ex.code
-                self.log.error("Error during storage connection validation: %s", str(ex), exc_info=True)
-            except Exception, ex:
-                rc = se.StorageServerValidationError.code
-                self.log.error("Error during storage connection validation: %s", str(ex), exc_info=True)
+        return rc
 
-            conStatus.append({'id':con['cid'], 'status': rc})
-
-        return conStatus
-
-    def __validateiSCSIConnection(self, conParams):
+    def __validateiSCSIConnection(self, con):
         """
         Validate that we can connect to a storage server.
         """
-        conStatus = []
-
-        for con in conParams:
-            #iscsi.addiSCSIPortal('localhost')  ##FIXME
-            conStatus.append({'id':con['cid'], 'status':0})
-
-        return conStatus
+        return 0
 
     def __getConnectionDictMount(self, con):
         localPath = os.path.join(self.storage_repository, sd.DOMAIN_MNT_POINT)
@@ -337,89 +298,49 @@ class StorageServerConnection:
 
         return mount.Mount(con['rp'], mntPath)
 
-    def __disconnectFileServer(self, conParams, fsType):
+    def __disconnectFileServer(self, con):
         """
         Disconnect from a storage low level entity (server).
         """
-        conStatus = []
+        try:
+            rc = 0
+            mnt = self.__getConnectionDictMount(con)
+            if mnt.isMounted():
+                mnt.umount(timeout=CON_TIMEOUT)
+        except mount.MountError:
+            self.log.error("Error during storage disconnection", exc_info=True)
+            return se.StorageServerDisconnectionError.code
 
-        for con in conParams:
-            try:
-                try:
-                    rc = 0
-                    mnt = self.__getConnectionDictMount(con)
-                    if mnt.isMounted():
-                        mnt.umount(timeout=CON_TIMEOUT)
-                except mount.MountError:
-                    self.log.error("Error during storage disconnection", exc_info=True)
-                    rc = se.StorageServerDisconnectionError.code
-                    continue
+        try:
+            os.rmdir(mnt.fs_file)
+        except OSError:
+            # Report the error to the log, but keep going,
+            # afterall we succeeded in disconnecting the NFS server
+            self.log.warning("Cannot remove mountpoint after umount()", exc_info=True)
 
-                try:
-                    os.rmdir(mnt.fs_file)
-                except OSError:
-                    # Report the error to the log, but keep going,
-                    # afterall we succeeded in disconnecting the NFS server
-                    self.log.warning("Cannot remove mountpoint after umount()", exc_info=True)
+        return rc
 
-            # We should return error status instead of exception itself
-            except se.StorageException, ex:
-                rc = ex.code
-                self.log.error("Error during storage disconnection:", exc_info=True)
-            except Exception, ex:
-                rc = se.StorageException.code
-                self.log.error("Error during storage disconnection:", exc_info=True)
-            finally:
-                conStatus.append({'id':con['cid'], 'status': rc})
 
-        return conStatus
-
-    def __disconnectLocalConnection(self, conParams):
+    def __disconnectLocalConnection(self, con):
         """
         Disconnect from a storage low level entity (server).
         """
-        conStatus = []
         localPath = os.path.join(self.storage_repository, sd.DOMAIN_MNT_POINT)
 
-        for con in conParams:
-            rc = 0
-            try:
-                lnPoint = fileUtils.transformPath(con['rp'])
-                lnPath = os.path.join(localPath, lnPoint)
-                if os.path.lexists(lnPath):
-                    os.unlink(lnPath)
-            except se.StorageException, ex:
-                rc = ex.code
-                self.log.error("Error during storage disconnection: %s", str(ex), exc_info=True)
-            except Exception, ex:
-                rc = se.StorageServerDisconnectionError.code
-                self.log.error("Error during storage disconnection: %s", str(ex), exc_info=True)
+        lnPoint = fileUtils.transformPath(con['rp'])
+        lnPath = os.path.join(localPath, lnPoint)
+        if os.path.lexists(lnPath):
+            os.unlink(lnPath)
 
-            conStatus.append({'id':con['cid'], 'status': rc})
-        return conStatus
+        return 0
 
-    def __disconnectiSCSIServer(self, conParams):
+    def __disconnectiSCSIServer(self, con):
         """
         Disconnect from a storage low level entity (server).
         """
-        conStatus = []
+        if not con['iqn']:
+            iscsi.remiSCSIPortal(con['ip'], con['port'])
+        else:
+            iscsi.remiSCSINode(con['ip'], con['port'], con['iqn'], con['tpgt'])
+        return 0
 
-        for con in conParams:
-            try:
-                if not con['iqn']:
-                    iscsi.remiSCSIPortal(con['ip'], con['port'])
-                else:
-                    iscsi.remiSCSINode(con['ip'], con['port'], con['iqn'], con['tpgt'])
-
-                rc = 0
-            # We should return error status instead of exception itself
-            except se.StorageException, ex:
-                rc = ex.code
-                self.log.error("Error during storage disconnection: %s", str(ex), exc_info=True)
-            except Exception, ex:
-                rc = se.StorageException.code
-                self.log.error("Error during storage disconnection: %s", str(ex), exc_info=True)
-
-            conStatus.append({'id':con['cid'], 'status':rc})
-
-        return conStatus
