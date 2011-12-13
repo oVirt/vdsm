oVirt/vdsm

commit 5ac3213e3ab08765759d5d5b082ba8030fbd0449

caps.py: include oVirt Node to OSName
Add oVirt Node as a supported

Change-Id: I8e59867abaf4cd6e9f142356b05fca15992ae300
Reviewed-on: http://gerrit.ovirt.org/454
Tested-by: Douglas Schilling Landgraf <dougsland@redhat.com>
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/caps.py
@@ -42,8 +42,9 @@ import storage.hba
 
 class OSName:
     UNKNOWN = 'unknown'
-    OVIRT = 'RHEV Hypervisor'
+    OVIRT = 'oVirt Node'
     RHEL = 'RHEL'
+    RHEVH = 'RHEV Hypervisor'
 
 class CpuInfo(object):
     def __init__(self):
@@ -125,6 +126,8 @@ def _getIscsiIniName():
 
 def getos():
     if os.path.exists('/etc/rhev-hypervisor-release'):
+        return OSName.RHEV
+    elif os.path.exists('/etc/ovirt-node-image-release'):
         return OSName.OVIRT
     elif os.path.exists('/etc/redhat-release'):
         return OSName.RHEL
@@ -141,7 +144,7 @@ def osversion():
 
     osname = getos()
     try:
-        if osname == OSName.OVIRT:
+        if osname == OSName.RHEVH:
             d = _parseKeyVal( file('/etc/default/version') )
             version = d.get('VERSION', '')
             release = d.get('RELEASE', '')

vdsm/clientIF.py
@@ -1009,7 +1009,8 @@ class clientIF:
                 for entry in entries:
                     if entry.getAttribute("name") == "product":
                         prod = entry.firstChild.data
-                        if prod == caps.OSName.RHEL or prod == caps.OSName.OVIRT:
+                        if prod in (caps.OSName.RHEL, caps.OSName.OVIRT, \
+                                caps.OSName.RHEVH):
                             return True
         return False
 

vdsm/libvirtvm.py
@@ -993,7 +993,7 @@ class LibvirtVm(vm.Vm):
 
         osd = caps.osversion()
         domxml.appendSysinfo(
-            osname=caps.OSName.OVIRT,
+            osname=caps.OSName.RHEVH,
             osversion=osd.get('version', '') + '-' + osd.get('release', ''),
             hostUUID=utils.getHostUUID() )
 

commit dfdbfc211a06b04b2b7ff281003aa4ed1c899d05

caps: fix typo
typo introduced in commit 5ac3213e3ab087

Change-Id: I267018885464edd2a7cbddda2e06570a55ac4418
Reviewed-on: http://gerrit.ovirt.org/580
Reviewed-by: Douglas Schilling Landgraf <dougsland@redhat.com>
Reviewed-by: Dan Kenigsberg <danken@redhat.com>
Tested-by: Dan Kenigsberg <danken@redhat.com>

vdsm/caps.py
@@ -126,7 +126,7 @@ def _getIscsiIniName():
 
 def getos():
     if os.path.exists('/etc/rhev-hypervisor-release'):
-        return OSName.RHEV
+        return OSName.RHEVH
     elif os.path.exists('/etc/ovirt-node-image-release'):
         return OSName.OVIRT
     elif os.path.exists('/etc/redhat-release'):