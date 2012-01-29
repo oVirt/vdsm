oVirt/vdsm

commit c0766ddb0852fd391d15424ae24761bb37ebbda3

Add before_vm_destroy hook
Run a hook directory when a Vm is asked to die. Note that a Vm may die
without being asked to, were the hook would not run.

This hook is currently missing from Vdsm since I was not sure what
should be its exact semantic. However in hindsight, we'd better have
something (even awkward) and improve after feedback from the real world.

Change-Id: Ide2c143921e58944df97c3dc74cd20cd85b1dae8
Reviewed-on: http://gerrit.ovirt.org/1175
Reviewed-by: Dan Kenigsberg <danken@redhat.com>
Tested-by: Dan Kenigsberg <danken@redhat.com>

vdsm/libvirtvm.py
@@ -1572,6 +1572,7 @@ class LibvirtVm(vm.Vm):
 
     def destroy(self):
         self.log.debug('destroy Called')
+        hooks.before_vm_destroy(self._dom.XMLDesc(0), self.conf)
         self.destroyed = True
 
         response = self.releaseVm()

vdsm.spec.in
@@ -528,6 +528,7 @@ exit 0
 %dir %{_libexecdir}/%{vdsm_name}/hooks/after_vm_migrate_source
 %dir %{_libexecdir}/%{vdsm_name}/hooks/before_vm_migrate_destination
 %dir %{_libexecdir}/%{vdsm_name}/hooks/after_vm_migrate_destination
+%dir %{_libexecdir}/%{vdsm_name}/hooks/before_vm_destroy
 %dir %{_libexecdir}/%{vdsm_name}/hooks/after_vm_destroy
 %dir %{_libexecdir}/%{vdsm_name}/hooks/before_vdsm_start
 %dir %{_libexecdir}/%{vdsm_name}/hooks/after_vdsm_stop

vdsm/hooks.py
@@ -123,8 +123,13 @@ def after_vm_dehibernate(domxml, vmconf={}):
     return _runHooksDir(domxml, 'after_vm_hibernate', vmconf=vmconf,
                         raiseError=False)
 
+def before_vm_destroy(domxml, vmconf={}):
+    return _runHooksDir(None, 'before_vm_destroy', vmconf=vmconf,
+                        raiseError=False)
+
 def after_vm_destroy(domxml, vmconf={}):
-    return _runHooksDir(domxml, 'after_vm_destroy', vmconf=vmconf, raiseError=False)
+    return _runHooksDir(domxml, 'after_vm_destroy', vmconf=vmconf,
+                        raiseError=False)
 
 def before_vdsm_start():
     return _runHooksDir(None, 'before_vdsm_start', raiseError=False)

vdsm/vdsmd.8.in
@@ -45,7 +45,7 @@ Currently-supported hooks have self-explanatory names:
     before_vm_dehibernate, after_vm_dehibernate,
     before_vm_migrate_source, after_vm_migrate_source,
     before_vm_migrate_destination, after_vm_migrate_destination,
-    after_vm_destroy,
+    before_vm_destroy, after_vm_destroy,
     before_vdsm_start, after_vdsm_stop.
 
 Each hook executes the scripts under
@@ -128,6 +128,7 @@ reserved.
 
 .TP
 If a before_<action> hook fails, the <action> would be aborted.
+However, before_vm_destroy's failure does not abort destroy().
 
 .SH FILES
 .PD 0

vdsm_hooks/Makefile.am
@@ -70,6 +70,7 @@ VDSMHOOKS = \
 	after_vm_migrate_source \
 	before_vm_migrate_destination \
 	after_vm_migrate_destination \
+	before_vm_destroy \
 	after_vm_destroy \
 	before_vdsm_start \
 	after_vdsm_stop

commit 144e3c5672dd889ec2b303416cd0fcc89257b82f

destroy may be called when the Vm is already Down
This patch fixes an error introduced in c0766ddb0852fd391d15424ae24761.

Change-Id: I50e4ca55262e64b13081cc832fba932e56aafd49
Reviewed-on: http://gerrit.ovirt.org/1321
Reviewed-by: Dan Kenigsberg <danken@redhat.com>
Tested-by: Dan Kenigsberg <danken@redhat.com>

vdsm/libvirtvm.py
@@ -1589,7 +1589,7 @@ class LibvirtVm(vm.Vm):
 
     def destroy(self):
         self.log.debug('destroy Called')
-        hooks.before_vm_destroy(self._dom.XMLDesc(0), self.conf)
+        hooks.before_vm_destroy(self._lastXMLDesc, self.conf)
         self.destroyed = True
 
         response = self.releaseVm()