15faa5487e1759d98227f16b4f68ee844773a877
Saggi Mizrahi <smizrahi@redhat.com>
2012-04-02 16:54:41 -0400

    Integrate nosetests to vdsm testing
    This patch integrates nosetests into the vdsm testing pipeline. This
    gives us a more advanced framework to write tests around.
    
    `make check` still works as expected. In order to run tests
    individually or with different options use /tests/run_tests.sh.
    To learn more about nose tests you could run
    `./run_tests.sh -h` or go to the nose documentation.
    
    http://readthedocs.org/docs/nose/en/latest/#
    
    This also introduces the VdsmTestCase base class. There is no need to
    inherit from it but it will set up a logger for you that in the future
    will be used to log out intermidate test information for long tests.
    
    Change-Id: I4c8d761ac0c03adf25324ea04e948b98faa82e39
    Reviewed-on: http://gerrit.ovirt.org/3257
    Reviewed-by: Peter V. Saveliev <peet@redhat.com>
    Tested-by: Peter V. Saveliev <peet@redhat.com>
    Reviewed-by: Federico Simoncelli <fsimonce@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Tested-by: Dan Kenigsberg <danken@redhat.com>

---
+++ tests/testrunner.py
@@ -0,0 +1,162 @@
+#
+# Copyright 2012 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+import logging
+import sys
+import os
+import unittest
+
+from nose import config
+from nose import core
+from nose import result
+
+
+class TermColor(object):
+    black = 30
+    red = 31
+    green = 32
+    yellow = 33
+    blue = 34
+    magenta = 35
+    cyan = 36
+    white = 37
+
+
+def colorWrite(stream, text, color):
+    stream.write('\x1b[%s;1m%s\x1b[0m' % (color, text))
+
+
+class VdsmTestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
+        self.log = logging.getLogger(self.__class__.__name__)
+
+
+class VdsmTestResult(result.TextTestResult):
+    def __init__(self, *args, **kwargs):
+        result.TextTestResult.__init__(self, *args, **kwargs)
+        self._last_case = None
+
+    def getDescription(self, test):
+        return str(test)
+
+    def _writeResult(self, test, long_result, color, short_result, success):
+        if self.showAll:
+            colorWrite(self.stream, long_result, color)
+            self.stream.writeln()
+        elif self.dots:
+            self.stream.write(short_result)
+            self.stream.flush()
+
+    def addSuccess(self, test):
+        unittest.TestResult.addSuccess(self, test)
+        self._writeResult(test, 'OK', TermColor.green, '.', True)
+
+    def addFailure(self, test, err):
+        unittest.TestResult.addFailure(self, test, err)
+        self._writeResult(test, 'FAIL', TermColor.red, 'F', False)
+
+    def addSkip(self, test, reason):
+        # 2.7 skip compat
+        from nose.plugins.skip import SkipTest
+        if SkipTest in self.errorClasses:
+            storage, label, isfail = self.errorClasses[SkipTest]
+            storage.append((test, reason))
+            self._writeResult(test, 'SKIP : %s' % reason, TermColor.blue, 'S',
+                    True)
+
+    def addError(self, test, err):
+        stream = getattr(self, 'stream', None)
+        ec, ev, tb = err
+        try:
+            exc_info = self._exc_info_to_string(err, test)
+        except TypeError:
+            # 2.3 compat
+            exc_info = self._exc_info_to_string(err)
+        for cls, (storage, label, isfail) in self.errorClasses.items():
+            if result.isclass(ec) and issubclass(ec, cls):
+                if isfail:
+                    test.passed = False
+                storage.append((test, exc_info))
+                # Might get patched into a streamless result
+                if stream is not None:
+                    if self.showAll:
+                        message = [label]
+                        detail = result._exception_detail(err[1])
+                        if detail:
+                            message.append(detail)
+                        stream.writeln(": ".join(message))
+                    elif self.dots:
+                        stream.write(label[:1])
+                return
+        self.errors.append((test, exc_info))
+        test.passed = False
+        if stream is not None:
+            self._writeResult(test, 'ERROR', TermColor.red, 'E', False)
+
+    def startTest(self, test):
+        unittest.TestResult.startTest(self, test)
+        current_case = test.test.__class__.__name__
+
+        if self.showAll:
+            if current_case != self._last_case:
+                self.stream.writeln(current_case)
+                self._last_case = current_case
+
+            self.stream.write(
+                '    %s' % str(test.test._testMethodName).ljust(60))
+            self.stream.flush()
+
+
+class VdsmTestRunner(core.TextTestRunner):
+    def __init__(self, *args, **kwargs):
+        core.TextTestRunner.__init__(self, *args, **kwargs)
+
+    def _makeResult(self):
+        return VdsmTestResult(self.stream,
+                              self.descriptions,
+                              self.verbosity,
+                              self.config)
+
+    def run(self, test):
+        result_ = core.TextTestRunner.run(self, test)
+        return result_
+
+
+def run():
+    argv = sys.argv
+    stream = sys.stdout
+    verbosity = 3
+    testdir = os.path.dirname(os.path.abspath(__file__))
+
+    conf = config.Config(stream=stream,
+                      env=os.environ,
+                      verbosity=verbosity,
+                      workingDir=testdir,
+                      plugins=core.DefaultPluginManager())
+
+    runner = VdsmTestRunner(stream=conf.stream,
+                            verbosity=conf.verbosity,
+                            config=conf)
+
+    sys.exit(not core.run(config=conf, testRunner=runner, argv=argv))
+
+
+if __name__ == '__main__':
+    run()
