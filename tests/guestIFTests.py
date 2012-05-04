e4ddc6130589acc51fb6ffe0c3de2e8d5a8fb986
Mark Wu <wudxw@linux.vnet.ibm.com>
2012-05-04 09:44:03 +0800

    Add a test case for _handleMessage()
    Change-Id: Iec467cdef122999a28b3e47cd24e25192df10c21
    Signed-off-by: Mark Wu <wudxw@linux.vnet.ibm.com>
    Reviewed-on: http://gerrit.ovirt.org/5819
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Tested-by: Dan Kenigsberg <danken@redhat.com>

--- tests/guestIFTests.py
+++ tests/guestIFTests.py
@@ -18,7 +18,8 @@
 #
 # Refer to the README and COPYING files for full details of the license
 #
-
+import logging
+from collections import namedtuple
 import guestIF
 
 from testrunner import VdsmTestCase as TestCaseBase
@@ -31,3 +32,71 @@ class TestGuestIF(TestCaseBase):
         self.assertEqual(ALL_LEGAL, guestIF._filterXmlChars(ALL_LEGAL))
         TM = u"\u2122".encode('utf8')
         self.assertNotEqual(TM, guestIF._filterXmlChars(TM))
+
+    def test_handleMessage(self):
+        logging.TRACE = 5
+        fakeGuestAgent = guestIF.GuestAgent(None,
+                                            None, self.log, connect=False)
+        testCase = namedtuple('testCase', 'msgType, message, assertDict')
+
+        msgTypes = ['heartbeat', 'host-name', 'os-version',
+                    'network-interfaces', 'applications', 'disks-usage']
+
+        inputs = [
+            {'free-ram': 1024000,
+             'memory-stat': {'swap_out': 0, 'majflt': 0, 'mem_free': 4466104,
+                             'swap_in': 0, 'pageflt': 0, 'mem_total': 8059320,
+                             'mem_unused': 2733832}},
+
+            {'name': 'example.ovirt.org'},
+            {'version': '2.6.32-71.el6.x86_64'},
+
+            {'interfaces':[
+                {'hw': '00:21:cc:68:d7:38', 'name': 'eth0', 'inet':
+                 ['9.115.122.77'], 'inet6': ['fe80::221:ccff:fe68:d738']},
+                {'hw': 'a0:88:b4:f0:ce:a0', 'name': 'wlan0', 'inet':
+                 ['9.115.126.23'], 'inet6': ['fe80::a288:b4ff:fef0:cea0']},
+                {'hw': '52:54:00:5b:3f:e1', 'name': 'virbr0', 'inet':
+                 ['192.168.122.1'], 'inet6': []}]},
+
+            {'applications':
+                ['kernel-2.6.32-71.7.1.el6', 'kernel-2.6.32-220.el6']},
+
+            {'disks':[
+                {'total': 130062397440, 'path': '/', 'fs': 'ext4',
+                 'used':76402614272},
+                {'total': 203097088, 'path': '/boot', 'fs': 'ext4',
+                 'used': 153149440}]}]
+
+        outputs = [
+            {'memUsage': 1024000, 'memoryStats':
+                {'swap_out': '0', 'majflt': '0', 'mem_free':
+                 '4466104', 'swap_in': '0', 'pageflt': '0',
+                 'mem_total': '8059320', 'mem_unused': '2733832'}},
+
+            {'guestName': 'example.ovirt.org'},
+            {'guestOs': '2.6.32-71.el6.x86_64'},
+
+            {'netIfaces': [
+                {'hw': '00:21:cc:68:d7:38', 'name': 'eth0', 'inet':
+                 ['9.115.122.77'], 'inet6': ['fe80::221:ccff:fe68:d738']},
+                {'hw': 'a0:88:b4:f0:ce:a0', 'name': 'wlan0', 'inet':
+                 ['9.115.126.23'], 'inet6': ['fe80::a288:b4ff:fef0:cea0']},
+                {'hw': '52:54:00:5b:3f:e1', 'name': 'virbr0', 'inet':
+                 ['192.168.122.1'], 'inet6': []}],
+             'guestIPs': '9.115.122.77 9.115.126.23 192.168.122.1'},
+
+            {'appsList':
+                ['kernel-2.6.32-71.7.1.el6', 'kernel-2.6.32-220.el6']},
+
+            {'disksUsage': [
+                {'total': '130062397440', 'path': '/', 'fs': 'ext4',
+                 'used': '76402614272'},
+                {'total': '203097088', 'path': '/boot', 'fs': 'ext4',
+                 'used': '153149440'}]}]
+
+        for t in zip(msgTypes, inputs, outputs):
+            t = testCase(*t)
+            fakeGuestAgent._handleMessage(t.msgType, t.message)
+            for (k, v) in t.assertDict.iteritems():
+                self.assertEqual(fakeGuestAgent.guestInfo[k], v)
