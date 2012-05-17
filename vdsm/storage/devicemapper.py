2634b6647926bdfbabf4ac176952ea53937633c4
ShaoHe Feng <shaohef@linux.vnet.ibm.com>
2012-05-17 22:02:46 +0800

    Make devicemapper.py PEP8 clean
    Change-Id: I6579a3364f7fccd827238e9e243f8892b383388b
    Signed-off-by: ShaoHe Feng <shaohef@linux.vnet.ibm.com>
    Reviewed-on: http://gerrit.ovirt.org/4796
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Tested-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/storage/devicemapper.py
+++ vdsm/storage/devicemapper.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
 # Refer to the README and COPYING files for full details of the license
 #
@@ -29,30 +29,38 @@ from vdsm.constants import EXT_DMSETUP
 
 DMPATH_FORMAT = "/dev/mapper/%s"
 
+
 def getDmId(deviceMultipathName):
     devlinkPath = DMPATH_FORMAT % deviceMultipathName
     try:
         devStat = os.stat(devlinkPath)
     except OSError:
-        raise OSError(errno.ENODEV, "Could not find dm device named `%s`" % deviceMultipathName)
+        raise OSError(errno.ENODEV, "Could not find dm device named `%s`" %
+                                    deviceMultipathName)
 
     return "dm-%d" % os.minor(devStat.st_rdev)
 
+
 def findDev(major, minor):
-    return os.path.basename(os.path.realpath('/sys/dev/block/%d:%d' % (major, minor)))
+    return os.path.basename(os.path.realpath('/sys/dev/block/%d:%d' %
+                                             (major, minor)))
+
 
 def getSysfsPath(devName):
     if "/" in devName:
-        raise ValueError("devName has an illegal format. Parameter is not a devname `%s`" % devName)
+        raise ValueError("devName has an illegal format. "
+                         "Parameter is not a devname `%s`" % devName)
 
     sysfsPath = os.path.join("/sys/block/", devName)
     if sysfsPath != os.path.abspath(sysfsPath):
-        raise ValueError("devName has an illegal format. Parameter is not a devname `%s`" % devName)
+        raise ValueError("devName has an illegal format. "
+                         "Parameter is not a devname `%s`" % devName)
 
     if not os.path.exists(sysfsPath):
         raise OSError(errno.ENOENT, "device `%s` doesn't exists" % devName)
 
     return sysfsPath
+
 
 def _parseDevFile(devFilePath):
     with open(devFilePath, "r") as f:
@@ -60,20 +68,24 @@ def _parseDevFile(devFilePath):
 
     return (int(mj), int(mn))
 
+
 def getSlaves(deviceName):
     mpName = resolveDevName(deviceName)
     sysfsPath = getSysfsPath(mpName)
     return os.listdir(os.path.join(sysfsPath, "slaves"))
+
 
 def getDevName(dmId):
     nameFilePath = os.path.join(getSysfsPath(dmId), "dm", "name")
     with open(nameFilePath, "r") as f:
         return f.readline().rstrip("\n")
 
+
 def getDevUuid(dmId):
     nameFilePath = os.path.join(getSysfsPath(dmId), "dm", "uuid")
     with open(nameFilePath, "r") as f:
         return f.readline().rstrip("\n")
+
 
 def resolveDevName(devName):
     try:
@@ -87,9 +99,11 @@ def resolveDevName(devName):
     except Exception:
         raise OSError(errno.ENODEV, "No such block device `%s`" % devName)
 
+
 def isVirtualDevice(devName):
     devName = resolveDevName(devName)
     return os.path.exists(os.path.join("/sys/devices/virtual/block/", devName))
+
 
 def isBlockDevice(devName):
     try:
@@ -98,9 +112,11 @@ def isBlockDevice(devName):
     except OSError:
         return False
 
+
 def isDmDevice(devName):
     devName = resolveDevName(devName)
     return os.path.exists("/sys/block/%s/dm" % devName)
+
 
 def isPartitioned(devName):
     dmName = resolveDevName(devName)
@@ -112,13 +128,13 @@ def isPartitioned(devName):
     if not isVirtualDevice(devName) or not isDmDevice(devName):
         return False
 
-
     mpathName = getDevName(dmName)
     for holder in getHolders(dmName):
         if resolveDevName(holder).startswith(mpathName):
             return True
 
     return False
+
 
 def getAllSlaves():
     deps = {}
@@ -127,20 +143,24 @@ def getAllSlaves():
 
     return deps
 
+
 def getDevNum(deviceName):
     mpName = resolveDevName(deviceName)
     sysfsPath = getSysfsPath(mpName)
     devFile = os.path.join(sysfsPath, "dev")
     return _parseDevFile(devFile)
 
+
 def removeMapping(deviceName):
     return getProxy().removeDeviceMapping(deviceName)
+
 
 def _removeMapping(deviceName):
     cmd = [EXT_DMSETUP, "remove", deviceName]
     rc = misc.execCmd(cmd, sudo=False)[0]
     if rc != 0:
         raise Exception("Could not remove mapping `%s`" % deviceName)
+
 
 def getAllMappedDevices():
     devices = glob("/sys/devices/virtual/block/dm-*")
@@ -151,10 +171,12 @@ def getAllMappedDevices():
 
     return tuple(names)
 
+
 def getHolders(slaveName):
     slaveName = resolveDevName(slaveName)
     holdersDir = os.path.join(getSysfsPath(slaveName), "holders")
     return os.listdir(holdersDir)
+
 
 def removeMappingsHoldingDevice(slaveName):
     holders = getHolders(slaveName)
@@ -162,6 +184,8 @@ def removeMappingsHoldingDevice(slaveName):
         removeMapping(getDevName(holder))
 
 PATH_STATUS_RE = re.compile(r"(?P<devnum>\d+:\d+)\s+(?P<status>[AF])")
+
+
 def _getPathsStatus():
     cmd = [EXT_DMSETUP, "status"]
     rc, out, err = misc.execCmd(cmd, sudo=False)
@@ -178,6 +202,6 @@ def _getPathsStatus():
 
     return res
 
+
 def getPathsStatus():
     return getProxy().getPathsStatus()
-
