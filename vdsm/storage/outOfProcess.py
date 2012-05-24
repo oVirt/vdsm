b1f726165706e8d0adabfaea5952f65d2b838cea
Cao,Bing Bu <mars@linux.vnet.ibm.com>
2012-05-24 14:25:54 +0800

    make vdsm/storage/outOfProcess.py PEP8 clean
    Change-Id: Icddb21d4b9f72d6d1e944d244a1fd519474c12a0
    Signed-off-by: Cao,Bing Bu <mars@linux.vnet.ibm.com>
    Reviewed-on: http://gerrit.ovirt.org/5698
    Tested-by: Zhou Zheng Sheng <zhshzhou@linux.vnet.ibm.com>
    Reviewed-by: Zhou Zheng Sheng <zhshzhou@linux.vnet.ibm.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/storage/outOfProcess.py
+++ vdsm/storage/outOfProcess.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
 # Refer to the README and COPYING files for full details of the license
 #
@@ -36,34 +36,42 @@ HELPERS_PER_DOMAIN = config.getint("irs", "process_pool_max_slots_per_domain")
 
 _multiplexerLock = threading.Lock()
 _multiplexer = [None]
+
+
 def getProcessPool(clientName):
     if _multiplexer[0] is None:
         with _multiplexerLock:
             if _multiplexer[0] is None:
                 pool = ProcessPool(MAX_HELPERS, GRACE_PERIOD, DEFAULT_TIMEOUT)
-                _multiplexer[0] = ProcessPoolMultiplexer(pool, HELPERS_PER_DOMAIN)
+                _multiplexer[0] = ProcessPoolMultiplexer(pool,
+                                                         HELPERS_PER_DOMAIN)
 
     return OopWrapper(_multiplexer[0][clientName])
+
 
 def getGlobalProcPool():
     return getProcessPool("Global")
 
+
 def _simpleWalk(top, topdown=True, onerror=None, followlinks=False):
-    # We need this _simpleWalk wrapper because of regular os.walk return iterator
-    # and we can't use it in oop.
+    # We need this _simpleWalk wrapper because of regular os.walk return
+    #iterator and we can't use it in oop.
     filesList = []
     for base, dirs, files in mod_os.walk(top, topdown, onerror, followlinks):
         for f in files:
-            filesList.append(mod_os.path.join(base,f))
+            filesList.append(mod_os.path.join(base, f))
     return filesList
+
 
 def _directReadLines(path):
     with open_ex(path, "dr") as f:
         return f.readlines()
 
+
 def _directWriteLines(path, lines):
     with open_ex(path, "dw") as f:
         return f.writelines(lines)
+
 
 def _createSparseFile(path, size, mode=None):
     with open(path, "w") as f:
@@ -71,13 +79,16 @@ def _createSparseFile(path, size, mode=None):
             mod_os.chmod(path, mode)
         f.truncate(size)
 
+
 def _readLines(path):
     with open(path, "r") as f:
         return f.readlines()
 
+
 def _writeLines(path, lines):
     with open(path, "w") as f:
         return f.writelines(lines)
+
 
 class _ModuleWrapper(types.ModuleType):
     def __init__(self, wrappedModule, procPool):
@@ -87,7 +98,9 @@ class _ModuleWrapper(types.ModuleType):
     def __getattr__(self, name):
         return self._procPool.wrapFunction(getattr(self._wrappedModule, name))
 
+
 class OopWrapper(object):
+
     def __init__(self, procPool):
         self._processPool = procPool
         self._registerFunctions()
@@ -95,9 +108,12 @@ class OopWrapper(object):
 
     def _registerFunctions(self):
         self.simpleWalk = self._processPool.wrapFunction(_simpleWalk)
-        self.directReadLines = self._processPool.wrapFunction(_directReadLines)
-        self.directWriteLines = self._processPool.wrapFunction(_directWriteLines)
-        self.createSparseFile = self._processPool.wrapFunction(_createSparseFile)
+        self.directReadLines = (self._processPool
+                                    .wrapFunction(_directReadLines))
+        self.directWriteLines = (self._processPool
+                                    .wrapFunction(_directWriteLines))
+        self.createSparseFile = (self._processPool
+                                    .wrapFunction(_createSparseFile))
         self.readLines = self._processPool.wrapFunction(_readLines)
         self.writeLines = self._processPool.wrapFunction(_writeLines)
 
@@ -105,4 +121,5 @@ class OopWrapper(object):
         self.glob = _ModuleWrapper(mod_glob, self._processPool)
         self.fileUtils = _ModuleWrapper(mod_fileUtils, self._processPool)
         self.os = _ModuleWrapper(mod_os, self._processPool)
-        setattr(self.os, 'path', _ModuleWrapper(mod_os.path, self._processPool))
+        setattr(self.os, 'path',
+                _ModuleWrapper(mod_os.path, self._processPool))
