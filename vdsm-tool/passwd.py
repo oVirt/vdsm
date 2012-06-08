5bc4f0a5155e84ad37191d4aff36edc4d6f8ac61
Lei Li <lilei@linux.vnet.ibm.com>
2012-06-08 17:29:50 +0800

    Add passwd command to vdsm-tool
    Change since v3
        - Automate this process by calling vdsm-tool
          accordingly.
    
    Change since v2
        - Processes problem fixs and report the error
          for missing file suggested by Saggi.
    
    Change since v1
        - Small fix suggested by Mark.
    
    Move and encapsulate set-saslpasswd from post
    section into vdsm-tool function.
    
    Change-Id: I76b5aee4c2f11a7680c675c171ff35a463a70402
    Signed-off-by: Lei Li <lilei@linux.vnet.ibm.com>
    Reviewed-on: http://gerrit.ovirt.org/4528
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Tested-by: Dan Kenigsberg <danken@redhat.com>

---
+++ vdsm-tool/passwd.py
@@ -0,0 +1,46 @@
+# Copyright 2012 IBM, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import errno
+import subprocess
+from vdsm import constants
+from vdsm.tool import vdsm_tool_expose
+
+
+@vdsm_tool_expose("set-saslpasswd")
+def set_saslpasswd():
+    """
+    Set vdsm password for libvirt connection
+    """
+    script = ['/usr/sbin/saslpasswd2', '-p', '-a', 'libvirt',
+              constants.SASL_USERNAME]
+
+    try:
+        f = open(constants.P_VDSM_LIBVIRT_PASSWD, 'r')
+    except IOError as e:
+        if e.errno == errno.ENOENT:
+            return
+        raise
+
+    p = subprocess.Popen(
+            script, stdin=f, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE, close_fds=True)
+    output, err = p.communicate()
+    if p.returncode != 0:
+        raise Exception("Set password failed: %s" % (err, ))
