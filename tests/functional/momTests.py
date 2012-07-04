5c8440f7b51a73a39996333370c97e021a43db31
Mark Wu <wudxw@linux.vnet.ibm.com>
2012-07-04 17:46:45 +0800

    Add a MOM functional test case for KSM
    Change-Id: Ibeda253c8395a68d98171fad1e6bdbb5a55c3555
    Signed-off-by: Mark Wu <wudxw@linux.vnet.ibm.com>
    Reviewed-on: http://gerrit.ovirt.org/5070
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ tests/functional/momTests.py
@@ -0,0 +1,47 @@
+#
+# Copyright IBM Corp. 2012
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+import random
+import time
+import testValidation
+from testrunner import VdsmTestCase as TestCaseBase
+from vdsm import vdscli
+
+
+class MOMTest(TestCaseBase):
+
+    @testValidation.ValidateRunningAsRoot
+    def testKSM(self):
+        run = 1
+        pages_to_scan = random.randint(100, 200)
+
+        # Set a simple MOM policy to change KSM paramters unconditionally.
+        testPolicyStr = """
+            (Host.Control "ksm_run" %d)
+            (Host.Control "ksm_pages_to_scan" %d)""" % \
+            (run, pages_to_scan)
+        s = vdscli.connect()
+        s.setMOMPolicy(testPolicyStr)
+
+        # Wait for the policy taking effect
+        time.sleep(10)
+
+        hostStats = s.getVdsStats()['info']
+        self.assertEqual(bool(run), hostStats['ksmState'])
+        self.assertEqual(pages_to_scan, hostStats['ksmPages'])
