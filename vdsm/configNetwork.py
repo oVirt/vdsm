f1792cfb33bff5ef5c9a8c1f938ff92b00d192a8
Douglas Schilling Landgraf <dougsland@redhat.com>
2012-07-09 17:20:35 -0400

    BZ#837443: removeBridge() drop/remove interface
    Move ifdown() and brctl delbr to removeBridge(). We need a generic
    function to be called multiple times. For example, removing a bridge listed
    or not listed in libvirt database.
    
    Change-Id: Iea798b2ddf6413b58dbfc3d8d11dc4bf54c592fc
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/5914
    Reviewed-by: Igor Lvovsky <ilvovsky@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/6204

--- vdsm/configNetwork.py
+++ vdsm/configNetwork.py
@@ -321,6 +321,8 @@ class ConfigWriter(object):
         self._removeFile(self.NET_CONF_PREF + bonding)
 
     def removeBridge(self, bridge):
+        ifdown(bridge)
+        subprocess.call([constants.EXT_BRCTL, 'delbr', bridge])
         self._backup(self.NET_CONF_PREF + bridge)
         self._removeFile(self.NET_CONF_PREF + bridge)
 
@@ -775,8 +777,6 @@ def delNetwork(network, vlan=None, bonding=None, nics=None, force=False,
                 "delNetwork: bridge %s still exists" % network)
 
     if network and bridged:
-        ifdown(network)
-        subprocess.call([constants.EXT_BRCTL, 'delbr', network])
         configWriter.removeBridge(network)
 
     if vlan:
