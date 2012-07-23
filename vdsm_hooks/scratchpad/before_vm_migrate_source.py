0cb220f45176d8601882c8113351bf6a637342ae
Wenyi Gao <wenyi@linux.vnet.ibm.com>
2012-07-23 15:21:36 +0800

    make vdsm_hooks/scratchpad/before_vm_migrate_source.py PEP 8 clean
    Change-Id: I270cef856bce2d39c4173aab05125df6d66e3691
    Signed-off-by: Wenyi Gao <wenyi@linux.vnet.ibm.com>
    Reviewed-on: http://gerrit.ovirt.org/6533
    Tested-by: Zhou Zheng Sheng <zhshzhou@linux.vnet.ibm.com>
    Reviewed-by: Zhou Zheng Sheng <zhshzhou@linux.vnet.ibm.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm_hooks/scratchpad/before_vm_migrate_source.py
+++ vdsm_hooks/scratchpad/before_vm_migrate_source.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
-if os.environ.has_key('scratchpad'):
-    sys.stderr.write('scratchpad bevort_vm_migrate_source: cannot migrate VM with scratchpad devices\n')
+if 'scratchpad' in os.environ:
+    sys.stderr.write('scratchpad bevort_vm_migrate_source: '
+                     'cannot migrate VM with scratchpad devices\n')
     sys.exit(2)
