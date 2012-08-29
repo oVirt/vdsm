oVirt/vdsm

commit c3e2272fd8ce2f9297698ec9c94c9a7a0095d202

extract a method for appending a child node with a text node to self.dom in libvirtvm.py
In vdsm/libvirtvm.py, there are some duplicated code. It creates an
xml element node, then creates a text node and appends it to the element,
at last appends the element to self.dom. This can be done in a function.

Change-Id: Ie05d8f0f9869080b34d5cafb5c672d4034f09934
Signed-off-by: Zhou Zheng Sheng <zhshzhou@linux.vnet.ibm.com>
Reviewed-on: http://gerrit.ovirt.org/6846
Tested-by: oVirt Jenkins CI Server
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/libvirtvm.py
@@ -523,20 +523,21 @@ class _DomXML:
 
         self.doc.appendChild(self.dom)
 
-        self.dom.appendChild(self.doc.createElement('name')) \
-           .appendChild(self.doc.createTextNode(self.conf['vmName']))
-        self.dom.appendChild(self.doc.createElement('uuid')) \
-           .appendChild(self.doc.createTextNode(self.conf['vmId']))
+        self.appendChildWithText('name', self.conf['vmName'])
+        self.appendChildWithText('uuid', self.conf['vmId'])
         memSizeKB = str(int(self.conf.get('memSize', '256')) * 1024)
-        self.dom.appendChild(self.doc.createElement('memory')) \
-           .appendChild(self.doc.createTextNode(memSizeKB))
-        self.dom.appendChild(self.doc.createElement('currentMemory')) \
-           .appendChild(self.doc.createTextNode(memSizeKB))
-        self.dom.appendChild(self.doc.createElement('vcpu')) \
-           .appendChild(self.doc.createTextNode(self.conf['smp']))
+        self.appendChildWithText('memory', memSizeKB)
+        self.appendChildWithText('currentMemory', memSizeKB)
+        self.appendChildWithText('vcpu', self.conf['smp'])
 
         self._devices = self.doc.createElement('devices')
         self.dom.appendChild(self._devices)
+
+    def appendChildWithText(self, childName, text):
+        childNode = self.doc.createElement(childName)
+        textNode = self.doc.createTextNode(text)
+        childNode.appendChild(textNode)
+        self.dom.appendChild(childNode)
 
     def appendConsole(self):
         """
@@ -670,8 +671,7 @@ class _DomXML:
         <features/>
         """
         if utils.tobool(self.conf.get('acpiEnable', 'true')):
-            self.dom.appendChild(self.doc.createElement('features')) \
-               .appendChild(self.doc.createElement('acpi'))
+            self.appendChildWithText('features', 'acpi')
 
     def appendCpu(self):
         """

commit 0696d4237d76c7a6c05b931f1eb91be31d450617

libvirtvm: allow acpi again
Commit c3e2272f has mistakenly changed

        <features>
                <acpi/>
        </features>

into

        <features>
                acpi
        </features>

which caused VMs to start up silently with no acpi support. This patch
reverts the offending part of the above commit.

Bug initially reported by Itzik Brown.

Change-Id: I591c40678f030f1bcd4cb7cb42c21907420f7cf1
Signed-off-by: Dan Kenigsberg <danken@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/7533
Reviewed-by: Zhou Zheng Sheng <zhshzhou@linux.vnet.ibm.com>
Tested-by: Zhou Zheng Sheng <zhshzhou@linux.vnet.ibm.com>
Reviewed-by: Igor Lvovsky <ilvovsky@redhat.com>

vdsm/libvirtvm.py
@@ -726,7 +726,8 @@ class _DomXML:
         <features/>
         """
         if utils.tobool(self.conf.get('acpiEnable', 'true')):
-            self.appendChildWithText('features', 'acpi')
+            self.dom.appendChild(self.doc.createElement('features')) \
+               .appendChild(self.doc.createElement('acpi'))
 
     def appendCpu(self):
         """