60d7e63bbee9e52bbed2b462d676d9d970a76409
Dan Kenigsberg <danken@redhat.com>
2012-11-08 14:30:12 +0200

    storage/threadPool: fix copyright notice
    Change-Id: Ib6a06dc60f53f12793a93bb71cd3d5bfec4cc0d1
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/9119
    Reviewed-by: Ayal Baron <abaron@redhat.com>

--- vdsm/storage/threadPool.py
+++ vdsm/storage/threadPool.py
@@ -1,21 +1,9 @@
 #
-# Copyright 2011 Red Hat, Inc.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-#
-# Refer to the README and COPYING files for full details of the license
+# Adapted by Red Hat from
+# http://code.activestate.com/recipes/203871-a-generic-programming-thread-pool/
+# Author: Tim Lesher
+# License: PSF License
+# http://wiki.python.org/moin/PythonSoftwareFoundationLicenseV2Easy
 #
 
 import threading
