84b767f903a105b06b9c6a177ccb8b29cb0b1c48
Federico Simoncelli <fsimonce@redhat.com>
2012-11-13 11:02:10 -0500

    hooks: add the nested virtualization hook
    If the nested virtualization is enabled in your kvm module this hook
    will expose it to the guests.
    
    Change-Id: I08c24c1b932370577e9687d75e292cd7c40f8e5d
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/9225
    Reviewed-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    Tested-by: Ohad Basan <obasan@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ vdsm_hooks/nestedvt/before_vm_start.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+#
+# Copyright 2012 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import hooking
+
+cpu_nested_features = {
+    "kvm_intel": "vmx",
+    "kvm_amd": "svm",
+}
+
+for kvm_mod in ("kvm_intel", "kvm_amd"):
+    kvm_mod_path = "/sys/module/%s/parameters/nested" % kvm_mod
+    try:
+        with file(kvm_mod_path) as f:
+            if f.readline().strip() == "Y":
+                break
+    except IOError:
+        pass
+else:
+    kvm_mod = None
+
+if kvm_mod:
+    domxml = hooking.read_domxml()
+    feature_vmx = domxml.createElement("feature")
+    feature_vmx.setAttribute("name", cpu_nested_features[kvm_mod])
+    feature_vmx.setAttribute("policy", "require")
+    domxml.getElementsByTagName("cpu")[0].appendChild(feature_vmx)
+    hooking.write_domxml(domxml)
