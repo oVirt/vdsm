d59d9dfaa5a4cc510d31f6ff7627bb86f031fd5c
Dan Kenigsberg <danken@redhat.com>
2012-11-22 22:58:30 +0200

    libvirtev.py: move to site_packages/vdsm
    Until we drop this little module, it has to sit within the vdsm package,
    or else `from vdsm import libvirtconnection` would fail.
    
    Bug-Url: https://bugzilla.redhat.com/870985
    Change-Id: Ieffbc359880e87a7159906579a18b2e5c63bd763
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/9435
    Tested-by: Yaniv Bronhaim <ybronhei@redhat.com>
    Reviewed-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- vdsm/libvirtvm.py
+++ vdsm/libvirtvm.py
@@ -29,8 +29,7 @@ from vdsm.define import ERROR, doneCode, errCode
 from vdsm import utils
 from vdsm import constants
 import guestIF
-import libvirtev
-from vdsm import libvirtconnection
+from vdsm import libvirtconnection, libvirtev
 from vdsm.config import config
 import hooks
 import caps
