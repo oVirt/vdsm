5a2e2c08ebe5afeabd04f60165bb1c18c62073b7
Assaf Muller <amuller@redhat.com>
2013-06-03 19:20:23 +0300

    iproute2 binary wrapper
    Initially to be used by the upcoming multiple gateways
    feature. Only the needed commands were implemented.
    
    Patch Set 5:
      * Changed Route and Rule parsing to Toni's more
        robust method
      * Route class: Previously there was a distinction
        between a default route and a normal route. I removed
        that distinction and the word 'default' in a route
        is interpreted as '0.0.0.0/0'
    
    Patch Sets 6-9:
      * Empty
    
    Patch Set 10:
      * Added ipwrapper.py to spec and make files
    
    PatchSet 11:
      * Made changes proposed in earlier reviews.
    
    PatchSet 12:
      * Parsing unit tests.
    
    PatchSet 13:
      * Added forgotten parsing unit tests.
    
    PatchSet 15:
      * Changed name of routeForDevice to reflect that it only returns the single
        route for the link network of the device.
    
    PatchSet 16:
      * Removed unnecessary output from action performing methods.
    
    PatchSet 17:
      * Added python-netaddr to the build requirements.
    
    PatchSet 18:
      * Rebased to get the benefits from markin jsonrpc tests as broken.
    
    PatchSet 19:
      * Provided more descriptive docstrings for fromText.
    
    Change-Id: I1d315c3294fd7f058cdc840dea329d91a658a304
    Signed-off-by: Assaf Muller <amuller@redhat.com>
    Signed-off-by: Antoni S. Puimedon <asegurap@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/15335
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ tests/ipwrapperTests.py
@@ -0,0 +1,66 @@
+# Copyright 2013 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301  USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from ipwrapper import Route
+from ipwrapper import Rule
+
+from testrunner import VdsmTestCase as TestCaseBase
+
+
+class TestIpwrapper(TestCaseBase):
+    def testRouteFromText(self):
+        _getRouteAttrs = lambda x: (x.network, x.ipaddr, x.device, x.table)
+        good_routes = {
+            'default via 192.168.99.254 dev eth0':
+            ('0.0.0.0/0', '192.168.99.254', 'eth0', None),
+            'default via 192.168.99.254 dev eth0 table foo':
+            ('0.0.0.0/0', '192.168.99.254', 'eth0', 'foo'),
+            '200.100.50.0/16 via 11.11.11.11 dev eth2 table foo':
+            ('200.100.50.0/16', '11.11.11.11', 'eth2', 'foo')}
+        for text, attributes in good_routes.iteritems():
+            route = Route.fromText(text)
+            self.assertEqual(_getRouteAttrs(route), attributes)
+
+        bad_routes = ['default via 192.168.99.257 dev eth0 table foo',
+                      '200.100.50.0/16 dev eth2 table foo',
+                      '288.100.23.9/43 via 192.168.99.254 dev eth0 table foo',
+                      '200.100.50.0/16 via 192.168.99.254 table foo']
+        for text in bad_routes:
+            self.assertRaises(ValueError, Route.fromText, text)
+
+    def testRuleFromText(self):
+        _getRuleAttrs = lambda x: (x.table, x.source, x.destination)
+        good_rules = {
+            '32766:    from all lookup main':
+            ('main', None, None),
+            '32767:    from 10.0.0.0/8 to 20.0.0.0/8 lookup table_100':
+            ('table_100', '10.0.0.0/8', '20.0.0.0/8'),
+            '32768:    from all to 8.8.8.8 lookup table_200':
+            ('table_200', None, '8.8.8.8')}
+        for text, attributes in good_rules.iteritems():
+            rule = Rule.fromText(text)
+            self.assertEqual(_getRuleAttrs(rule), attributes)
+
+        bad_rules = ['32766:    from all lookup main foo',
+                     '2766:    lookup main',
+                     '276:    from 8.8.8.8'
+                     '32:    from 10.0.0.0/8 to 264.0.0.0/8 lookup table_100']
+        for text in bad_rules:
+            self.assertRaises(ValueError, Rule.fromText, text)
---
+++ vdsm/ipwrapper.py
@@ -0,0 +1,234 @@
+# Copyright 2013 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from netaddr.core import AddrFormatError
+from netaddr import IPAddress
+from netaddr import IPNetwork
+
+from vdsm.utils import CommandPath
+from vdsm.utils import execCmd
+
+_IP_BINARY = CommandPath('ip', '/sbin/ip')
+
+
+def _isValid(ip, verifier):
+    try:
+        verifier(ip)
+    except (AddrFormatError, ValueError):
+        return False
+
+    return True
+
+
+class Route(object):
+    def __init__(self, network, ipaddr=None, device=None, table=None):
+        if not _isValid(network, IPNetwork):
+            raise ValueError('network is not properly defined')
+
+        if ipaddr and not _isValid(ipaddr, IPAddress):
+            raise ValueError('ipaddr is not properly defined')
+
+        self.network = network
+        self.ipaddr = ipaddr
+        self.device = device
+        self.table = table
+
+    @classmethod
+    def fromText(cls, text):
+        """
+            Creates a Route object from a textual representation. For the vdsm
+            use case we require the network IP address and interface to reach
+            the network to be provided in the text.
+
+            Examples:
+            'default via 192.168.99.254 dev eth0':
+            '0.0.0.0/0 via 192.168.99.254 dev eth0 table foo':
+            '200.100.50.0/16 via 11.11.11.11 dev eth2 table foo':
+        """
+        route = text.split()
+
+        '''
+        The network / first column is required, followed by key+value pairs.
+        Thus, the length of a route must be odd.
+        '''
+        if len(route) % 2 == 0:
+            raise ValueError('The length of the textual representation of a '
+                             'route must be odd')
+
+        network, parameters = route[0], route[1:]
+        if network == 'default':
+            network = '0.0.0.0/0'
+
+        data = dict(parameters[i:i + 2] for i in range(0, len(parameters), 2))
+        try:
+            ipaddr = data['via']
+        except KeyError:
+            raise ValueError('Routes require an IP address.')
+        try:
+            device = data['dev']
+        except KeyError:
+            raise ValueError('Routes require a device.')
+        table = data.get('table')
+
+        return cls(network, ipaddr=ipaddr, device=device, table=table)
+
+    def __str__(self):
+        str = '%s via %s dev %s' % (self.network, self.ipaddr, self.device)
+
+        if self.table:
+            str += ' table %s' % self.table
+
+        return str
+
+    def __iter__(self):
+        for word in str(self).split():
+            yield word
+
+
+class Rule(object):
+    def __init__(self, table, source=None, destination=None):
+        if source:
+            if not (_isValid(source, IPAddress) or
+                    _isValid(source, IPNetwork)):
+                raise ValueError('Invalid source: Not an ip address '
+                                 'or network')
+
+        if destination:
+            if not (_isValid(destination, IPAddress) or
+                    _isValid(destination, IPNetwork)):
+                raise ValueError('Invalid destination: Not an ip address '
+                                 'or network')
+
+        self.table = table
+        self.source = source
+        self.destination = destination
+
+    @classmethod
+    def fromText(cls, text):
+        """
+            Creates a Rule object from a textual representation. Since it is
+            used for source routing, the source network specified in "from" and
+            the table "lookup" that shall be used for the routing must be
+            specified.
+
+            Examples:
+            32766:    from all lookup main
+            32767:    from 10.0.0.0/8 to 20.0.0.0/8 lookup table_100
+            32768:    from all to 8.8.8.8 lookup table_200
+        """
+
+        rule = text.split()
+        parameters = rule[1:]
+
+        if len(rule) % 2 == 0:
+            raise ValueError('The length of a textual representation of a '
+                             'rule must be odd')
+
+        data = dict(parameters[i:i + 2] for i in range(0, len(parameters), 2))
+        try:
+            table = data['lookup']
+        except KeyError:
+            raise ValueError('Rules require "lookup" information.')
+        try:
+            source = data['from']
+        except KeyError:
+            raise ValueError('Rules require "from" information.')
+
+        destination = data.get('to')
+        if source == 'all':
+            source = None
+        if destination == 'all':
+            destination = None
+
+        return cls(table, source=source, destination=destination)
+
+    def __str__(self):
+        str = 'from '
+        if self.source:
+            str += self.source
+        else:
+            str += 'all'
+        if self.destination:
+            str += ' to %s' % self.destination
+
+        str += ' table %s' % self.table
+
+        return str
+
+    def __iter__(self):
+        for word in str(self).split():
+            yield word
+
+
+class IPRoute2Error(Exception):
+    pass
+
+
+def _execCmd(command):
+    returnCode, output, error = execCmd(command)
+
+    if returnCode:
+        raise IPRoute2Error(error)
+
+    return output
+
+
+def routeList():
+    command = [_IP_BINARY.cmd, 'route']
+    return _execCmd(command)
+
+
+def routeShowTable(table):
+    command = [_IP_BINARY.cmd, 'route', 'show', 'table', table]
+    return _execCmd(command)
+
+
+def routeAdd(route):
+    command = [_IP_BINARY.cmd, 'route', 'add']
+    command += route
+    _execCmd(command)
+
+
+def routeDel(route):
+    command = [_IP_BINARY.cmd, 'route', 'del']
+    command += route
+    _execCmd(command)
+
+
+def routeLinkNetForDevice(device):
+    command = [_IP_BINARY.cmd, 'route', 'show', 'dev', device, 'scope', 'link',
+               'proto', 'kernel']
+    return _execCmd(command)
+
+
+def ruleList():
+    command = [_IP_BINARY.cmd, 'rule']
+    return _execCmd(command)
+
+
+def ruleAdd(rule):
+    command = [_IP_BINARY.cmd, 'rule', 'add']
+    command += rule
+    _execCmd(command)
+
+
+def ruleDel(rule):
+    command = [_IP_BINARY.cmd, 'rule', 'del']
+    command += rule
+    _execCmd(command)
