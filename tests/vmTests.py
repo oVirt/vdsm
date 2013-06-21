d3af124325f77d9bf07d1d79d3d354d657813d48
Dan Kenigsberg <danken@redhat.com>
2013-06-21 18:09:53 +0300

    vmTests: split data to another module
    As suggested by Maciej and promised to Vinzenz in
    http://gerrit.ovirt.org/14111
    Also, bury the turm "libvirtvm".
    
    Change-Id: I2685d296784e315b8705349893894992a615b2bd
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/15981
    Reviewed-by: Giuseppe Vallarelli <gvallare@redhat.com>
    Reviewed-by: Maciej Lichon <maciej.lichon.wroclaw@gmail.com>

--- tests/vmTests.py
+++ tests/vmTests.py
@@ -29,71 +29,7 @@ import caps
 from vdsm import utils
 from vdsm import libvirtconnection
 from monkeypatch import MonkeyPatch
-
-_CONF_TO_DOMXML = [({
-    'vmId': '9ffe28b6-6134-4b1e-8804-1185f49c436f',
-    'smp': '8', 'memSize': '1024',
-    'displayPort': '-1', 'vmName': 'testVm',
-    'display': 'vnc', 'emulatedMachine': 'pc',
-    'boot': '', 'timeOffset': 0, 'tdf': True,
-    'acpiEnable': 'true', 'cpuType': 'qemu64',
-    'smpCoresPerSocket': 1, 'smpThreadsPerCore': 1,
-    'smp': '1', 'cpuPinning': {},
-    'vmchannel': 'true', 'qgaEnable': 'true',
-    'tabletEnable': False,
-    'displayNetwork': 'mydisp', 'custom': {}},
-
-    """<?xml version="1.0" encoding="utf-8"?>
-        <domain type="kvm">
-            <name>testVm</name>
-            <uuid>%(vmId)s</uuid>
-            <memory>1048576</memory>
-            <currentMemory>1048576</currentMemory>
-            <vcpu>1</vcpu>
-            <devices>
-                <channel type="unix">
-                    <target name="com.redhat.rhevm.vdsm" type="virtio"/>
-                    <source mode="bind"
-       path="/var/lib/libvirt/qemu/channels/%(vmId)s.com.redhat.rhevm.vdsm"/>
-                </channel>
-                <channel type="unix">
-                    <target name="org.qemu.guest_agent.0" type="virtio"/>
-                    <source mode="bind"
-       path="/var/lib/libvirt/qemu/channels/%(vmId)s.org.qemu.guest_agent.0"/>
-                </channel>
-                <input bus="ps2" type="mouse"/>
-                <graphics autoport="yes" passwd="*****"
-                passwdValidTo="1970-01-01T00:00:01" port="-1" type="vnc">
-                <listen network="vdsm-mydisp" type="network"/>
-                </graphics>
-                </devices>
-                <os>
-                    <type arch="x86_64" machine="pc">hvm</type>
-                    <smbios mode="sysinfo"/>
-                </os>
-                <sysinfo type="smbios">
-                    <system>
-                        <entry name="manufacturer">oVirt</entry>
-                        <entry name="product">oVirt Node</entry>
-                        <entry name="version">18-1</entry>
-      <entry name="serial">fc25cbbe-5520-4f83-b82e-1541914753d9</entry>
-                        <entry name="uuid">%(vmId)s</entry>
-                    </system>
-                </sysinfo>
-                <clock adjustment="0" offset="variable">
-                    <timer name="rtc" tickpolicy="catchup"/>
-                </clock>
-                <features>
-                    <acpi/>
-                </features>
-                <cputune/>
-                <cpu match="exact">
-                    <model>qemu64</model>
-                    <feature name="svm" policy="disable"/>
-                    <topology cores="1" sockets="1" threads="1"/>
-                </cpu>
-            </domain>
-""", )]
+from vmTestsData import CONF_TO_DOMXML
 
 
 class TestVm(TestCaseBase):
@@ -552,7 +488,7 @@ class TestVm(TestCaseBase):
         oldVdsmRun = constants.P_VDSM_RUN
         constants.P_VDSM_RUN = tempfile.mkdtemp()
         try:
-            for conf, expectedXML in _CONF_TO_DOMXML:
+            for conf, expectedXML in CONF_TO_DOMXML:
 
                 expectedXML = expectedXML % conf
 
