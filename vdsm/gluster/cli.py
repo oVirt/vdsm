e00899831db6085b5ba34c4f6d19e861b8c3ca08
Bala.FA <barumuga@redhat.com>
2013-09-30 16:27:23 +0530

    gluster: interpret and use correct xml elements
    xml output of rebalance and remove-brick status returns filesSkipped
    in <skipped> tag and value of <statusStr> tag cleaned up to use it as
    proper enum.
    
    Change-Id: I9b90283ac2b9f4b54acfccffa02a30e38e5e9d85
    Signed-off-by: Bala.FA <barumuga@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/19674
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/gluster/cli.py
+++ vdsm/gluster/cli.py
@@ -641,27 +641,31 @@ def _parseVolumeRebalanceRemoveBrickStatus(xmltree, mode):
     else:
         return
 
+    st = tree.find('aggregate/statusStr').text
+    statusStr = st.replace(' ', '_').replace('-', '_')
     status = {
         'summary': {
             'runtime': tree.find('aggregate/runtime').text,
             'filesScanned': tree.find('aggregate/lookups').text,
             'filesMoved': tree.find('aggregate/files').text,
             'filesFailed': tree.find('aggregate/failures').text,
-            'filesSkipped': tree.find('aggregate/failures').text,
+            'filesSkipped': tree.find('aggregate/skipped').text,
             'totalSizeMoved': tree.find('aggregate/size').text,
-            'status': tree.find('aggregate/statusStr').text.upper()},
+            'status': statusStr.upper()},
         'hosts': []}
 
     for el in tree.findall('node'):
+        st = el.find('statusStr').text
+        statusStr = st.replace(' ', '_').replace('-', '_')
         status['hosts'].append({'name': el.find('nodeName').text,
                                 'id': el.find('id').text,
                                 'runtime': el.find('runtime').text,
                                 'filesScanned': el.find('lookups').text,
                                 'filesMoved': el.find('files').text,
                                 'filesFailed': el.find('failures').text,
-                                'filesSkipped': el.find('failures').text,
+                                'filesSkipped': el.find('skipped').text,
                                 'totalSizeMoved': el.find('size').text,
-                                'status': el.find('statusStr').text.upper()})
+                                'status': statusStr.upper()})
 
     return status
 
