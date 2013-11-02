8086bf15e11d7e7cec26ba33cfccc95448214e01
Dan Kenigsberg <danken@redhat.com>
2013-11-02 21:37:33 +0000

    sampling.ImagePathStatus: drop unused code
    In pre-historic rhev-2.1 days, ImagePathStatus thread was used to
    collect and cache information about the image repository accessibility.
    However, this code was never used by ovirt-3.y.
    
    By default, images_check_times is set to 0, meaning that the
    sampling.ImagePathStatus thread is never started. It is highly unlikely
    that there's a secret vdsm client out there that explicitly turns on
    this monitoring thread and read the 'storageDomains' element out of
    getVdsStats.
    
    Change-Id: I682d9730ac087e0a24ba5c72dbac912c87a6452f
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/20812
    Reviewed-by: Petr Šebek <psebek@redhat.com>
    Tested-by: Petr Šebek <psebek@redhat.com>
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>

--- vdsm/sampling.py
+++ vdsm/sampling.py
@@ -36,7 +36,6 @@ import ethtool
 from vdsm import utils
 from vdsm import netinfo
 from vdsm.constants import P_VDSM_RUN
-from vdsm.config import config
 
 _THP_STATE_PATH = '/sys/kernel/mm/transparent_hugepage/enabled'
 if not os.path.exists(_THP_STATE_PATH):
@@ -365,7 +364,7 @@ class HostStatsThread(threading.Thread):
     AVERAGING_WINDOW = 5
     SAMPLE_INTERVAL_SEC = 2
 
-    def __init__(self, cif, log):
+    def __init__(self, log):
         self.startTime = time.time()
 
         threading.Thread.__init__(self)
@@ -377,12 +376,10 @@ class HostStatsThread(threading.Thread):
         self._lineRate = (sum(self._ifrates) or 1000) * (10 ** 6) / 8
         self._lastSampleTime = time.time()
 
-        self._imagesStatus = ImagePathStatus(cif)
         self._pid = os.getpid()
         self._ncpus = max(os.sysconf('SC_NPROCESSORS_ONLN'), 1)
 
     def stop(self):
-        self._imagesStatus.stop()
         self._stopEvent.set()
 
     def _updateIfidsIfrates(self):
@@ -418,19 +415,7 @@ class HostStatsThread(threading.Thread):
     def get(self):
         stats = self._getInterfacesStats()
         stats['cpuSysVdsmd'] = stats['cpuUserVdsmd'] = 0.0
-        stats['storageDomains'] = {}
-        if self._imagesStatus._cif.irs:
-            self._imagesStatus._refreshStorageDomains()
         now = time.time()
-        for sd, d in self._imagesStatus.storageDomains.iteritems():
-            stats['storageDomains'][sd] = {
-                'code': d['code'],
-                'delay': d['delay'],
-                'lastCheck': d['lastCheck'],
-                'valid': d['valid'],
-                'version': d['version'],
-                'acquired': d['acquired'],
-            }
         stats['elapsedTime'] = int(now - self.startTime)
         if len(self._samples) < 2:
             return stats
@@ -519,41 +504,7 @@ class HostStatsThread(threading.Thread):
             stats['txRate'] = min(stats['txRate'], 100.0)
             stats['rxRate'] = min(stats['rxRate'], 100.0)
             logging.debug(stats)
-#        stats['rxBps'] = float(rx) / interval
-#        stats['txBps'] = float(tx) / interval
         stats['rxDropped'] = rxDropped
         stats['txDropped'] = txDropped
 
         return stats
-
-
-class ImagePathStatus(threading.Thread):
-    def __init__(self, cif, interval=None):
-        if interval is None:
-            interval = config.getint('irs', 'images_check_times')
-        self._interval = interval
-        self._cif = cif
-        self.storageDomains = {}
-        self._stopEvent = threading.Event()
-        threading.Thread.__init__(self, name='ImagePathStatus')
-        if self._interval > 0:
-            self.start()
-
-    def stop(self):
-        self._stopEvent.set()
-
-    def _refreshStorageDomains(self):
-        self.storageDomains = self._cif.irs.repoStats()
-        del self.storageDomains["status"]
-        if "args" in self.storageDomains:
-            del self.storageDomains["args"]
-
-    def run(self):
-        try:
-            while not self._stopEvent.isSet():
-                if self._cif.irs:
-                    self._refreshStorageDomains()
-                self._stopEvent.wait(self._interval)
-        except:
-            logging.error("Error while refreshing storage domains",
-                          exc_info=True)
