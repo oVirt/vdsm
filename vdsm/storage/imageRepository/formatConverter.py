oVirt/vdsm

commit 2c9f0d66db7795ff73d473c15c795817beab28a8

upgrade: fix v3ResetMetaVolSize argument
In commit c072945 (one shot prepare) the v3ResetMetaVolSize argument
was changed from volume object to volume uuid (string) triggering
attribute errors during the execution (e.g. getMetaParam, etc.).
This patch fixes the issue reintroducing the volume objects and the
relevant exception handling.

Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1022975
Change-Id: Iddffa996652300e5a5daa19bef5bd7873f39bb15
Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/20721
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/storage/imageRepository/formatConverter.py
@@ -274,13 +274,25 @@ def v3DomainConverter(repoPath, hostId, domain, isMsd):
                     log.error("Image %s can't be activated.",
                               imgUUID, exc_info=True)
 
-                for vol in imgVolumes:
+                for volUUID in imgVolumes:
                     try:
-                        v3ResetMetaVolSize(vol)  # BZ#811880
+                        v3ResetMetaVolSize(  # BZ#811880
+                            domain.produceVolume(imgUUID, volUUID))
                     except qemuImg.QImgError:
                         log.error("It is not possible to read the volume %s "
                                   "using qemu-img, the content looks damaged",
-                                  vol.volUUID, exc_info=True)
+                                  volUUID, exc_info=True)
+
+            except se.VolumeDoesNotExist:
+                log.error("It is not possible to prepare the image %s, the "
+                          "volume chain looks damaged", imgUUID,
+                          exc_info=True)
+
+            except se.MetaDataKeyNotFoundError:
+                log.error("It is not possible to prepare the image %s, the "
+                          "volume metadata looks damaged", imgUUID,
+                          exc_info=True)
+
             finally:
                 try:
                     domain.deactivateImage(imgUUID)

commit 036d5e85ba3983c6d76dfdde126dc4e9a24f1ed4

pep8: Fix indentation to comply with pep8 1.3.3
Commit 2c9f0d66db7795 introduced a pep8 indentation regression, breaking
the build on RHEL using vdsm provided pep8. This patch fixes the
indentation by breaking long statement to multiple lines.

Change-Id: I3431742f9dbfd86a9816c7b3c6de90e282dbd415
Relates-To: https://bugzilla.redhat.com/1022975
Signed-off-by: Nir Soffer <nsoffer@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/21055
Reviewed-by: Antoni Segura Puimedon <asegurap@redhat.com>
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/storage/imageRepository/formatConverter.py
@@ -276,8 +276,8 @@ def v3DomainConverter(repoPath, hostId, domain, isMsd):
 
                 for volUUID in imgVolumes:
                     try:
-                        v3ResetMetaVolSize(  # BZ#811880
-                            domain.produceVolume(imgUUID, volUUID))
+                        vol = domain.produceVolume(imgUUID, volUUID)
+                        v3ResetMetaVolSize(vol)  # BZ#811880
                     except qemuImg.QImgError:
                         log.error("It is not possible to read the volume %s "
                                   "using qemu-img, the content looks damaged",