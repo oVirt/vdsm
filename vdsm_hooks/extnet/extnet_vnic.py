3dac283ecd3df6c4863dc53bdd931f9d3ba8eb4b
Dan Kenigsberg <danken@redhat.com>
2013-12-08 09:55:18 +0000

    hook: extnet: attach a vNIC to a libvirt network defined outside oVirt
    This patch adds a hook which allows to connect a vNIC to a libvirt
    network that is managed outside of oVirt. To use this hook, you should
    first define and activate such a network, as documented in
    http://libvirt.org/formatnetwork.html, on each host where the hook is
    deployed.
    
    For example, to use an openvswitch bridge, first virsh net-define
        <network>
          <name>ovs-net</name>
          <forward mode='bridge'/>
          <bridge name='ovsbr0'/>
          <virtualport type='openvswitch'/>
        </network>
    
    This preperation was not required in a previous version for the hook
    (named forcebridge). However, this version allows to benefit of
    non-bridged libvirt networks, too.
    
    Change-Id: I70bc74a9ac4a6eb88d089baa86918275c61f5190
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Signed-off-by: Humble Chirammal <hchiramm@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/22124
    Reviewed-by: Antoni Segura Puimedon <asegurap@redhat.com>
    Reviewed-by: Assaf Muller <amuller@redhat.com>

---
+++ vdsm_hooks/extnet/extnet_vnic.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+"""
+Tweak an interface defintion so that its source is forced to be a specified
+network. Applied on a per vnic basis, it gets triggered and used by two
+different events:
+    * before_device_create
+    * before_nic_hotplug
+
+This hook can be used to force a VM to use a libvirt network that is managed
+outside of ovirt, such as an openvswitch network, or libvirt's default network.
+"""
+
+
+import os
+import sys
+import traceback
+import xml.dom
+
+import hooking
+
+
+def replaceSource(interface, newnet):
+    source, = interface.getElementsByTagName('source')
+    source.removeAttribute('bridge')
+    source.setAttribute('network', newnet)
+    interface.setAttribute('type', 'network')
+
+
+def main():
+    newnet = os.environ.get('extnet')
+    if newnet is not None:
+        doc = hooking.read_domxml()
+        interface, = doc.getElementsByTagName('interface')
+        replaceSource(interface, newnet)
+        hooking.write_domxml(doc)
+
+
+def test():
+
+    interface = xml.dom.minidom.parseString("""
+    <interface type="bridge">
+        <address bus="0x00" domain="0x0000" function="0x0" slot="0x03"\
+                                            type="pci"/>
+        <mac address="00:1a:4a:16:01:b0"/>
+        <model type="virtio"/>
+        <source bridge="ovirtmgmt"/>
+        <filterref filter="vdsm-no-mac-spoofing"/>
+        <link state="up"/>
+        <boot order="1"/>
+    </interface>
+    """).getElementsByTagName('interface')[0]
+
+    print "Interface before forcing network: %s" % \
+        interface.toxml(encoding='UTF-8')
+
+    replaceSource(interface, 'yipee')
+    print "Interface after forcing network: %s" % \
+        interface.toxml(encoding='UTF-8')
+
+
+if __name__ == '__main__':
+    try:
+        if '--test' in sys.argv:
+            test()
+        else:
+            main()
+    except:
+        hooking.exit_hook('extnet hook: [unexpected error]: %s\n' %
+                          traceback.format_exc())
