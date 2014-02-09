4d20a3f638588a8fb82ccf12783485a24deccd23
Daniel Erez <derez@redhat.com>
2014-02-09 12:56:30 +0200

    vm: Update LUN size when starting a vm
    In order to handle resize of DirectLUN disks
    (i.e. when a LUN volume is extended by user),
    relevant data should be retrieved when invoking getVmStats.
    
    Hence, added the following information to disks stats for
    DirectLUN disks: truesize, apparentsize and lunGUID
    (the sizes values already exist but are zeroed).
    Using these values, the engine could keep the DB updated.
    
    The following changes are introduced:
    * hsm:
      appropriateDevice method now fetches LUN's device size
      using 'multipath -> getDeviceSize'.
    * clientIF:
      For LUN devices, updated drive's truesize/apparentsize
      using hsm's getVolumeSize method.
    * vm:
      _getDiskStats -> in order to identify each LUN
      (e.g. on engine side), added 'lunGUID' to dStats
      when vmDrive.GUID exists (applicable for DirectLUN disks).
    
    Change-Id: Ia4988212f7f96078e774d2a4e7b5cd1681383cb0
    Bug-Url: https://bugzilla.redhat.com/1026868
    Signed-off-by: Daniel Erez <derez@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/24235
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-by: Federico Simoncelli <fsimonce@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/24527
    Reviewed-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- vdsm/vm.py
+++ vdsm/vm.py
@@ -635,6 +635,8 @@ class VmStatsThread(sampling.AdvancedStatsThread):
                           'apparentsize': str(vmDrive.apparentsize)}
                 if isVdsmImage(vmDrive):
                     dStats['imageID'] = vmDrive.imageID
+                elif "GUID" in vmDrive:
+                    dStats['lunGUID'] = vmDrive.GUID
                 dStats['readRate'] = ((eInfo[dName][1] - sInfo[dName][1]) /
                                       sampleInterval)
                 dStats['writeRate'] = ((eInfo[dName][3] - sInfo[dName][3]) /
