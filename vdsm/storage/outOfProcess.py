576428e876c2a347d20a9b1a694ab5a330897967
Yeela Kaplan <ykaplan@redhat.com>
2014-04-08 20:24:46 +0300

    ioprocess implementation
    change-Id: I132129315c73e880d998a13f84e822a9d4fec2a6
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1083771
    Signed-off-by: Yeela Kaplan <ykaplan@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/26967
    Reviewed-by: Antoni Segura Puimedon <asegurap@redhat.com>
    Reviewed-by: Yaniv Bronhaim <ybronhei@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/storage/outOfProcess.py
+++ vdsm/storage/outOfProcess.py
@@ -34,6 +34,7 @@ except ImportError:
     pass
 
 from remoteFileHandler import RemoteFileHandlerPool
+import storage_exception as se
 
 RFH = 'rfh'
 IOPROC = 'ioprocess'
@@ -46,6 +47,8 @@ HELPERS_PER_DOMAIN = config.getint("irs", "process_pool_max_slots_per_domain")
 
 _procLock = threading.Lock()
 _proc = {}
+
+log = logging.getLogger('oop')
 
 
 def setDefaultImpl(impl):
@@ -76,6 +79,197 @@ def getGlobalProcPool():
     return getProcessPool(GLOBAL)
 
 
+class _IOProcessGlob(object):
+    def __init__(self, iop):
+        self._iop = iop
+
+    def glob(self, pattern):
+        return self._iop.glob(pattern)
+
+
+class _IOProcessFileUtils(object):
+    def __init__(self, iop):
+        self._iop = iop
+
+    def fsyncPath(self, path):
+        self._iop.fsyncPath(path)
+
+    def cleanupdir(self, path, ignoreErrors=True):
+        cleanupdir_errors = []
+
+        try:
+            files = self._iop.listdir(path)
+        except OSError:
+            if not ignoreErrors:
+                raise
+        else:
+            for f in files:
+                fullpath = os.path.join(path, f)
+                if _IOProcessOs(self._iop).path.isdir(fullpath):
+                    try:
+                        self.cleanupdir(fullpath, ignoreErrors)
+                    except OSError as e:
+                        cleanupdir_errors.append(e)
+                else:
+                    try:
+                        self._iop.unlink(fullpath)
+                    except Exception as e:
+                        cleanupdir_errors.append('%s: %s' % ("unlink", e))
+            try:
+                self._iop.rmdir(path)
+            except Exception as e:
+                cleanupdir_errors.append('%s: %s' % ("rmdir", e))
+
+        if not ignoreErrors and cleanupdir_errors:
+            raise se.MiscDirCleanupFailure("%s %s" % (path, cleanupdir_errors))
+
+    def copyUserModeToGroup(self, path):
+        mode = _IOProcessOs(self._iop).stat(path).st_mode
+        userMode = mode & 0o700  # user mode mask
+        newGroupMode = userMode >> 3
+        if (mode & 0o070) != newGroupMode:  # group mode mask
+            # setting the new group mode masking out the original one
+            newMode = (mode & 0o707) | newGroupMode
+            log.debug("Changing mode for %s to %#o", path, newMode)
+            _IOProcessOs(self._iop).chmod(path, newMode)
+
+    def createdir(self, path, mode=None):
+        parts = path.split("/")
+        tmpPath = ""
+        for part in parts:
+            tmpPath = os.path.join(tmpPath, part)
+            if tmpPath == "":
+                tmpPath = "/"
+
+            try:
+                if mode:
+                    self._iop.mkdir(tmpPath, mode)
+                else:
+                    self._iop.mkdir(tmpPath)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+                else:
+                    if tmpPath == path and mode is not None:
+                        statinfo = self._iop.stat(path)
+                        curMode = statinfo[stat.ST_MODE]
+                        if curMode != mode:
+                            raise OSError(errno.EPERM,
+                                          ("Existing %s "
+                                           "permissions %s are not as "
+                                           "requested %s") % (path,
+                                                              oct(curMode),
+                                                              oct(mode)))
+
+    def validateAccess(self, targetPath, perms=(os.R_OK | os.W_OK | os.X_OK)):
+        if not self._iop.access(targetPath, perms):
+            log.warning("Permission denied for directory: %s with permissions:"
+                        "%s", targetPath, perms)
+            raise OSError(errno.EACCES, os.strerror(errno.EACCES))
+
+    def pathExists(self, filename, writable=False):
+        return self._iop.pathExists(filename, writable)
+
+
+class _IOProcessOs(object):
+    def __init__(self, iop):
+        self._iop = iop
+        self.path = _IOProcessOs.Path(iop)
+
+    def access(self, path, perms):
+        return self._iop.access(path, perms)
+
+    def chmod(self, path, mode):
+        self._iop.chmod(path, mode)
+
+    def mkdir(self, path, mode=None):
+        if mode is not None:
+            self._iop.mkdir(path, mode)
+        else:
+            self._iop.mkdir(path)
+
+    def remove(self, path):
+        self._iop.unlink(path)
+
+    def rename(self, oldpath, newpath):
+        '''
+        WARNING: Renaming a directory is not an atomic op.
+        Supported in the same manner as Python's os.rename.
+        '''
+        log.warning("renaming a directory is not an atomic operation")
+        try:
+            return self._iop.rename(oldpath, newpath)
+        except OSError as e:
+            if e.errno != errno.ENOTEMPTY:
+                raise
+
+        _IOProcessFileUtils(self._iop).cleanupdir(newpath, False)
+        self.mkdir(newpath)
+        for fname in self.listdir(oldpath):
+            src = os.path.join(oldpath, fname)
+            dst = os.path.join(newpath, fname)
+            self.rename(src, dst)
+
+        self._iop.rmdir(oldpath)
+
+    def rmdir(self, path):
+        self._iop.rmdir(path)
+
+    def stat(self, path):
+        return self._iop.stat(path)
+
+    def statvfs(self, path):
+        return self._iop.statvfs(path)
+
+    def unlink(self, path):
+        return self._iop.unlink(path)
+
+    class Path(object):
+        def __init__(self, iop):
+            self._iop = iop
+
+        def isdir(self, path):
+            res = self._iop.stat(path)
+            return stat.S_ISDIR(res.st_mode)
+
+        def islink(self, path):
+            res = self._iop.stat(path)
+            return stat.S_ISLNK(res.st_mode)
+
+        def lexists(self, path):
+            return self._iop.lexists(path)
+
+        def exists(self, path):
+            return self._iop.pathExists(path, False)
+
+
+def directReadLines(ioproc, path):
+    fileStr = ioproc.readfile(path, direct=True)
+    return fileStr.splitlines(True)
+
+
+def readLines(ioproc, path):
+    return ioproc.readlines(path)
+
+
+def writeLines(ioproc, path, lines):
+    data = ''.join(lines)
+    return ioproc.writefile(path, data)
+
+
+def simpleWalk(ioproc, path):
+    files = []
+    for f in ioproc.listdir(path):
+        fullpath = os.path.join(path, f)
+        osPath = _IOProcessOs(ioproc).path
+        if osPath.isdir(fullpath) and not osPath.islink(fullpath):
+            files.extend(simpleWalk(ioproc, fullpath))
+        else:
+            files.append(fullpath)
+
+    return files
+
+
 class _ModuleWrapper(types.ModuleType):
     def __init__(self, modName, procPool, ioproc, timeout, subModNames=()):
         '''
@@ -99,6 +293,35 @@ class _ModuleWrapper(types.ModuleType):
                                    DEFAULT_TIMEOUT,
                                    subSubModNames)
                     )
+
+        if ioproc:
+            self.glob = _IOProcessGlob(ioproc)
+            self.fileUtils.cleanupdir = \
+                _IOProcessFileUtils(ioproc).cleanupdir
+            self.fileUtils.createdir = \
+                _IOProcessFileUtils(ioproc).createdir
+            self.fileUtils.validateAccess = \
+                _IOProcessFileUtils(ioproc).validateAccess
+            self.fileUtils.pathExists = \
+                _IOProcessFileUtils(ioproc).pathExists
+            self.fileUtils.fsyncPath = \
+                _IOProcessFileUtils(ioproc).fsyncPath
+            self.fileUtils.copyUserModeToGroup = \
+                _IOProcessFileUtils(ioproc).copyUserModeToGroup
+            self.os.chmod = _IOProcessOs(ioproc).chmod
+            self.os.statvfs = _IOProcessOs(ioproc).statvfs
+            self.os.rename = _IOProcessOs(ioproc).rename
+            self.os.unlink = _IOProcessOs(ioproc).unlink
+            self.os.path = _IOProcessOs(ioproc).path
+            self.os.mkdir = _IOProcessOs(ioproc).mkdir
+            self.os.remove = _IOProcessOs(ioproc).remove
+            self.os.rmdir = _IOProcessOs(ioproc).rmdir
+            self.os.stat = _IOProcessOs(ioproc).stat
+            self.os.access = _IOProcessOs(ioproc).access
+            self.directReadLines = partial(directReadLines, ioproc)
+            self.writeLines = partial(writeLines, ioproc)
+            self.simpleWalk = partial(simpleWalk, ioproc)
+            self.readLines = partial(readLines, ioproc)
 
     def __getattr__(self, name):
         # Root modules is fake, we need to remove it
