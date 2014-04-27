674337d8f9e0e98432ba2b0778c1c3a66dc32491
Nir Soffer <nsoffer@redhat.com>
2014-04-28 00:19:10 +0300

    multipath: Rescan also FC devices
    When rescanning devices, we used to update ISCSI devices, and then run
    multipath to update device mapping. However, we never rescanned FC
    devices.
    
    We used to believe that "multipath -r" was triggering a rescan of FC
    devices and solving this issue. This may have been true in older
    versions but certainly does not work with current RHEL release, and was
    removed in the previous patch.
    
    This patch adds the missing FC rescan using the issue_lip sysfs api,
    recommended in the RHEL Storage Administration Guide.
    
    Bug-Url: https://bugzilla.redhat.com/1071654
    Change-Id: I7699504f9771232ee0b880f9c83a51fd5b90f40e
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/27122
    Tested-by: Elad Ben Aharon <eladba1990@gmail.com>
    Reviewed-by: Allon Mureinik <amureini@redhat.com>
    Reviewed-by: Xavi Francisco <xfrancis@redhat.com>
    Reviewed-by: Federico Simoncelli <fsimonce@redhat.com>

--- vdsm/storage/hba.py
+++ vdsm/storage/hba.py
@@ -36,6 +36,31 @@ PORT_NAME = "port_name"
 NODE_NAME = "node_name"
 
 
+def rescan():
+    """
+    Rescan HBAs connections, updating available devices.
+
+    This operation performs a Loop Initialization Protocol (LIP) and then
+    scans the interconnect and causes the SCSI layer to be updated to reflect
+    the devices currently on the bus. A LIP is, essentially, a bus reset, and
+    will cause device addition and removal.
+
+    Bear in mind that issue_lip is an asynchronous operation. The command may
+    complete before the entire scan has completed.
+
+    Note: Must be executed as root.
+    TODO: Some drivers do not support this operation.
+    """
+    log.info("Rescanning HBAs")
+    for path in glob.glob(FC_HOST_MASK + '/issue_lip'):
+        log.debug("Issuing lip %s", path)
+        try:
+            with open(path, 'w') as f:
+                f.write('1')
+        except IOError as e:
+            logging.error("Error issuing lip: %s", e)
+
+
 def getiSCSIInitiators():
     """
     Get iSCSI initiator name from the default location.
