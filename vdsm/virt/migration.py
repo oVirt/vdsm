08d547a915c377168626828cfafd9e3716841c11
Vinzenz Feenstra <vfeenstr@redhat.com>
2014-04-28 10:33:30 +0200

    virt: Capture migration start time after the semaphore was accquired
    Previously the migration start time was captured at the start of the
    MigrationSourceThread which leads to aborting migrations when the VMs
    had too long to wait for the accquisition of the migration semaphore.
    
    This patch captures the start time in the moment the migration really
    begins. Additionally this patch logs the time the migration took to
    accquire the migration semaphore to improve debugging of the logs.
    
    Change-Id: I209023f5f4a3ccee5db4d43b17627b09ca55bef8
    Bug-Url: https://bugzilla.redhat.com/1090109
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/27135
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Reviewed-by: Michal Skrivanek <michal.skrivanek@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/virt/migration.py
+++ vdsm/virt/migration.py
@@ -233,7 +233,8 @@ class SourceThread(threading.Thread):
             try:
                 if self._migrationCanceledEvt:
                     self._raiseAbortError()
-                self.log.debug("migration semaphore acquired")
+                self.log.debug("migration semaphore acquired after %d seconds",
+                               time.time() - startTime)
                 self._vm.conf['_migrationParams'] = {
                     'dst': self._dst,
                     'mode': self._mode,
@@ -241,7 +242,7 @@ class SourceThread(threading.Thread):
                     'dstparams': self._dstparams,
                     'dstqemu': self._dstqemu}
                 self._vm.saveState()
-                self._startUnderlyingMigration(startTime)
+                self._startUnderlyingMigration(time.time())
                 self._finishSuccessfully()
             except libvirt.libvirtError as e:
                 if e.get_error_code() == libvirt.VIR_ERR_OPERATION_ABORTED:
