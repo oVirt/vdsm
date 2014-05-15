af65e97bce33e7f08e90bbabd873ed71d46145cb
Robert Cernak <robocernak@gmail.com>
2014-05-15 17:42:14 +0200

    Pyroute2 network configurator
    Network configurator used for setting network devices.
    Configuration is done using pyroute2 library.
    
    Change-Id: Ife1ba6fca28f04bc1b171313bfb57bf630f8bc7a
    Signed-off-by: Robert Cernak <robocernak@gmail.com>
    Reviewed-on: http://gerrit.ovirt.org/27745
    Reviewed-by: Antoni Segura Puimedon <asegurap@redhat.com>
    Reviewed-by: Peter V. Saveliev <svinota.saveliev@gmail.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Tested-by: Dan Kenigsberg <danken@redhat.com>

---
+++ vdsm/network/configurators/pyroute_two.py
@@ -0,0 +1,147 @@
+# Copyright 2014 Robert Cernak
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import logging
+
+from vdsm import netinfo
+from vdsm import ipwrapper
+from vdsm.netconfpersistence import RunningConfig
+from vdsm.utils import CommandPath
+from . import libvirt
+from .dhclient import DhcpClient
+from pyroute2 import IPDB
+from .iproute2 import Iproute2
+
+
+class PyrouteTwo(Iproute2):
+    def __init__(self, inRollback=False):
+        super(Iproute2, self).__init__(ConfigApplier(), inRollback)
+        self.runningConfig = RunningConfig()
+
+    def commit(self):
+        self.configApplier.releaseSocket()
+        self.configApplier = None
+        self.runningConfig.save()
+        self.runningConfig = None
+
+
+class ConfigApplier(object):
+    def __init__(self):
+        self.ip = IPDB()
+
+    def _setIpConfig(self, iface):
+        ipConfig = iface.ipConfig
+        if ipConfig.ipaddr:
+            self.removeIpConfig(iface)
+            with self.ip.interfaces[iface.name] as i:
+                i.add_ip('%s/%s' % (ipConfig.ipaddr, ipConfig.netmask))
+            if ipConfig.gateway and ipConfig.defaultRoute:
+                ipwrapper.routeAdd(['default', 'via', ipConfig.gateway])
+
+    def removeIpConfig(self, iface):
+        ipwrapper.addrFlush(iface.name)
+
+    def setIfaceMtu(self, iface, mtu):
+        with self.ip.interfaces[iface] as i:
+            i['mtu'] = int(mtu)
+
+    def setBondingMtu(self, iface, mtu):
+        self.setIfaceMtu(iface, mtu)
+
+    def ifup(self, iface):
+        with self.ip.interfaces[iface.name] as i:
+            i.up()
+        if iface.ipConfig.bootproto == 'dhcp':
+            dhclient = DhcpClient(iface.name)
+            dhclient.start(iface.ipConfig.async)
+
+    def ifdown(self, iface):
+        with self.ip.interfaces[iface.name] as i:
+            i.down()
+        dhclient = DhcpClient(iface.name)
+        dhclient.shutdown()
+
+    def setIfaceConfigAndUp(self, iface):
+        if iface.ip:
+            self._setIpConfig(iface)
+        if iface.mtu:
+            self.setIfaceMtu(iface.name, iface.mtu)
+        self.ifup(iface)
+
+    def addBridge(self, bridge):
+        self.ip.create(kind='bridge', ifname=bridge.name).commit()
+
+    def addBridgePort(self, bridge):
+        with self.ip.interfaces[bridge.name] as i:
+            i.add_port(self.ip.interfaces[bridge.port.name])
+
+    def removeBridge(self, bridge):
+        with self.ip.interfaces[bridge.name] as i:
+            i.remove()
+
+    def removeBridgePort(self, bridge):
+        with self.ip.interfaces[bridge.name] as i:
+            i.del_port(self.ip.interfaces[bridge.port.name])
+
+    def addVlan(self, vlan):
+        link = self.ip.interfaces[vlan.device.name].index
+        self.ip.create(kind='vlan', ifname=vlan.name,
+                       link=link, vlan_id=vlan.tag).commit()
+
+    def removeVlan(self, vlan):
+        with self.ip.interfaces[vlan.name] as i:
+            i.remove()
+
+    def addBond(self, bond):
+        if bond.name not in netinfo.bondings():
+            self.ip.create(kind='bond', ifname=bond.name).commit()
+
+    def removeBond(self, bond):
+        if bond.name not in netinfo.REQUIRED_BONDINGS:
+            with self.ip.interfaces[bond.name] as i:
+                i.remove()
+
+    def addBondSlave(self, bond, slave):
+        self.ifdown(slave)
+        with self.ip.interfaces[bond.name] as i:
+            i.add_port(self.ip.interfaces[slave.name])
+        self.ifup(slave)
+
+    def removeBondSlave(self, bond, slave):
+        with self.ip.interfaces[bond.name] as i:
+            i.del_port(self.ip.interfaces[slave.name])
+
+    def addBondOptions(self, bond):
+        logging.debug('Add bond options %s', bond.options)
+        for option in bond.options.split():
+            key, value = option.split('=')
+            with open(netinfo.BONDING_OPT % (bond.name, key), 'w') as f:
+                f.write(value)
+
+    def createLibvirtNetwork(self, network, bridged, iface, qosInbound=None,
+                             qosOutbound=None):
+        netXml = libvirt.createNetworkDef(network, bridged, iface,
+                                          qosInbound, qosOutbound)
+        libvirt.createNetwork(netXml)
+
+    def removeLibvirtNetwork(self, network):
+        libvirt.removeNetwork(network)
+
+    def releaseSocket(self):
+        self.ip.release()
