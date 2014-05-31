oVirt/vdsm

commit 9d15a8cf3b8a732d0fa1823392aab6a7f0fb9418

volume: unify cow format check on creation
The same check performed both in fileVolume and blockVolume has been
moved to the base volume class. Now validateCreateVolumeParams in
fileVolume doesn't contain anything specific and it has been removed.

Change-Id: I78401df47278cdae34d824379acd0f2d088fa7b1
Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/28126
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/storage/volume.py
@@ -367,6 +367,10 @@ class Volume(object):
         if preallocate not in VOL_TYPE:
             raise se.IncorrectType(type2name(preallocate))
 
+        # Volumes with a parent must be cow
+        if srcVolUUID != volume.BLANK_UUID and volFormat != volume.COW_FORMAT:
+            raise se.IncorrectFormat(srcVolUUID)
+
     @classmethod
     def create(cls, repoPath, sdUUID, imgUUID, size, volFormat, preallocate,
                diskType, volUUID, desc, srcImgUUID, srcVolUUID):

vdsm/storage/blockVolume.py
@@ -127,10 +127,6 @@ class BlockVolume(volume.Volume):
         if preallocate == volume.SPARSE_VOL and volFormat == volume.RAW_FORMAT:
             raise se.IncorrectFormat(srcVolUUID)
 
-        # Snapshot should be COW volume
-        if srcVolUUID != volume.BLANK_UUID and volFormat != volume.COW_FORMAT:
-            raise se.IncorrectFormat(srcVolUUID)
-
     @classmethod
     def createVolumeMetadataRollback(cls, taskObj, sdUUID, offs):
         cls.log.info("Metadata rollback for sdUUID=%s offs=%s", sdUUID, offs)

vdsm/storage/fileVolume.py
@@ -90,21 +90,6 @@ class FileVolume(volume.Volume):
             oop.getProcessPool(sdUUID).os.unlink(volPath)
 
     @classmethod
-    def validateCreateVolumeParams(cls, volFormat, preallocate, srcVolUUID):
-        """
-        Validate create volume parameters.
-        'srcVolUUID' - backing volume UUID
-        'volFormat' - volume format RAW/QCOW2
-        'preallocate' - sparse/preallocate
-        """
-        volume.Volume.validateCreateVolumeParams(volFormat, preallocate,
-                                                 srcVolUUID)
-
-        # Snapshot should be COW volume
-        if srcVolUUID != volume.BLANK_UUID and volFormat != volume.COW_FORMAT:
-            raise se.IncorrectFormat(srcVolUUID)
-
-    @classmethod
     def createVolumeMetadataRollback(cls, taskObj, volPath):
         cls.log.info("createVolumeMetadataRollback: volPath=%s" % (volPath))
         metaPath = cls.__metaVolumePath(volPath)

commit 2d43cd98170ec8b4c09e9dc20dd9e95eb20ae5b4

volume: fix constants in validateCreateVolumeParams
This patch solves a problem related to the use of the volume constants.
The issue, introduced in 9d15a8c, was hid by subsequent patches in the
same series.

Change-Id: I2d809f51f68935e3c1d4670ee1556e262dd1b10f
Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/28250

vdsm/storage/volume.py
@@ -368,7 +368,7 @@ class Volume(object):
             raise se.IncorrectType(type2name(preallocate))
 
         # Volumes with a parent must be cow
-        if srcVolUUID != volume.BLANK_UUID and volFormat != volume.COW_FORMAT:
+        if srcVolUUID != BLANK_UUID and volFormat != COW_FORMAT:
             raise se.IncorrectFormat(srcVolUUID)
 
     @classmethod