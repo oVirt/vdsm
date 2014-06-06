439db6e06e697840da6cfad8291639753ecf51b4
Antoni S. Puimedon <asegurap@redhat.com>
2014-06-06 17:14:15 +0200

    ifcfg: remove and re-create bonds when they are not to be destroyed
    In order to make sure that bonds that should be left undestroyed,
    but do not have users, are set with the default options and devoid
    of any previous configuration, we delete the bond and its ifcfg
    file and create new ones.
    
    Bug-Url: https://bugzilla.redhat.com/1105645
    Change-Id: If897e1ad96737916a6e440664295694790e2226a
    Signed-off-by: Antoni S. Puimedon <asegurap@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/28062
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Signed-off-by: Antoni S. Puimedon <asegurap@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/28439

--- vdsm/netconf/ifcfg.py
+++ vdsm/netconf/ifcfg.py
@@ -180,19 +180,15 @@ class Ifcfg(Configurator):
         _netinfo = netinfo.NetInfo()
         to_be_removed = self._ifaceDownAndCleanup(bonding, _netinfo)
         if to_be_removed:
+            self.configApplier.removeBonding(bonding.name)
             if bonding.destroyOnMasterRemoval:
-                self.configApplier.removeBonding(bonding.name)
                 for slave in bonding.slaves:
                     slave.remove()
                 if self.unifiedPersistence:
                     self.runningConfig.removeBonding(bonding.name)
-            else:
-                self.configApplier.setBondingMtu(bonding.name,
-                                                 netinfo.DEFAULT_MTU)
-                if bonding.bridge is not None:
-                    self.configApplier._updateConfigValue(
-                        netinfo.NET_CONF_PREF + bonding.name, 'BRIDGE', None)
-                ifup(bonding.name)
+            else:  # Recreate the bond with ip and master info cleared
+                bonding.ip = bonding.master = None
+                bonding.configure()
         else:
             set_mtu = self._setNewMtu(
                 bonding, _netinfo.getVlanDevsForIface(bonding.name))
