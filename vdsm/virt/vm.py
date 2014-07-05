oVirt/vdsm

commit 02741ce7a6dff687c8ff5ba70a92dd5d89279644

virt: Specify snapshot type
Libvirt 1.2.2 introduced the 'type' attribute to the source element in a
domainsnapshot XML document.  This attribute allows you to specify
'block' as the type (which we should be doing on block domains).
Leaving this out creates a snapshot of type 'file' which has some
troubling side effects.  For one, it changes the disk type from block to
file.  This interferes with libvirt's write watermark detection causing
the wrong value to be returned.  In turn, vdsm will extend the drive
until it consumes the entire block storage domain.  See bug 1091094 [1].

[1] https://bugzilla.redhat.com/show_bug.cgi?id=1091094

Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1091094
Change-Id: I940ef0fb702708bf2158739eeed7be43ccce78d6
Signed-off-by: Adam Litke <alitke@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/28531
Reviewed-by: Federico Simoncelli <fsimonce@redhat.com>

vdsm/virt/vm.py
@@ -3715,11 +3715,17 @@ class Vm(object):
     def snapshot(self, snapDrives, memoryParams):
         """Live snapshot command"""
 
-        def _diskSnapshot(vmDev, newPath):
+        def _diskSnapshot(vmDev, newPath, sourceType):
             """Libvirt snapshot XML"""
 
-            disk = XMLElement('disk', name=vmDev, snapshot='external')
-            disk.appendChildWithArgs('source', file=newPath)
+            disk = XMLElement('disk', name=vmDev, snapshot='external',
+                              type=sourceType)
+            if sourceType == 'block':
+                args = {'dev': newPath}
+            elif sourceType == 'file':
+                args = {'file': newPath}
+            args['type'] = sourceType
+            disk.appendChildWithArgs('source', **args)
             return disk
 
         def _normSnapDriveParams(drive):
@@ -3784,6 +3790,7 @@ class Vm(object):
         snap = xml.dom.minidom.Element('domainsnapshot')
         disks = xml.dom.minidom.Element('disks')
         newDrives = {}
+        snapTypes = {}
 
         if self.isMigrating():
             return errCode['migInProgress']
@@ -3823,6 +3830,7 @@ class Vm(object):
             newDrives[vmDevName]["poolID"] = vmDrive.poolID
             newDrives[vmDevName]["name"] = vmDevName
             newDrives[vmDevName]["format"] = "cow"
+            snapTypes[vmDevName] = ('file', 'block')[vmDrive.blockDev]
 
         # If all the drives are the current ones, return success
         if len(newDrives) == 0:
@@ -3845,7 +3853,8 @@ class Vm(object):
                 _rollbackDrives(preparedDrives)
                 return errCode['snapshotErr']
 
-            snapelem = _diskSnapshot(vmDevName, newDrives[vmDevName]["path"])
+            snapelem = _diskSnapshot(vmDevName, newDrives[vmDevName]["path"],
+                                     snapTypes[vmDevName])
             disks.appendChild(snapelem)
 
         snap.appendChild(disks)

commit c49bb59300fca63828e3b0cb1443c4a1d3d685a3

virt: Restore BC for block type live snapshots
Commit 02741ce introduced a regression in older hosts running libvirt <
1.2.2 where libvirt rejects the type='block' and source/dev combination.
Eric Blake suggested that this could be worked around by supplying the
old format and new format in the domainshapshot XML document.  This
union would continue to work as before on old hosts but would pick up
the correct block snapshot behavior on newer versions.  Basically, we
still express the disk type but we repeat the newPath inside both a dev
and file attribute.

Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1115126
Change-Id: I85290f17842ada6c04695bf45ffd1b1108c63d55
Signed-off-by: Adam Litke <alitke@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/29567
Reviewed-by: Francesco Romani <fromani@redhat.com>
Reviewed-by: Federico Simoncelli <fsimonce@redhat.com>
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/virt/vm.py
@@ -4043,11 +4043,14 @@ class Vm(object):
 
             disk = XMLElement('disk', name=vmDev, snapshot='external',
                               type=sourceType)
+            # Libvirt versions before 1.2.2 do not understand 'type' and treat
+            # all snapshots as if they are type='file'.  In order to ensure
+            # proper handling of block snapshots in modern libvirt versions,
+            # we specify type='block' and dev=path for block volumes but we
+            # always speficy the file=path for backwards compatibility.
+            args = {'type': sourceType, 'file': newPath}
             if sourceType == 'block':
-                args = {'dev': newPath}
-            elif sourceType == 'file':
-                args = {'file': newPath}
-            args['type'] = sourceType
+                args['dev'] = newPath
             disk.appendChildWithArgs('source', **args)
             return disk
 