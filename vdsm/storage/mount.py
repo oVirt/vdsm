d2c53b8fa323b4bb55b211b993dc27221b2738f7
Saggi Mizrahi <smizrahi@redhat.com>
2014-07-07 22:24:01 +0300

    mount: Remove a redundant call to speed things up
    This way we query sysfs less times and everyone is happy.
    
    Also update the copyright message it appear our lawyer overlords.
    
    Change-Id: I2febde44cfc9be25b42c36ddb2e54283f7898288
    Signed-off-by: Saggi Mizrahi <smizrahi@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/29679
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/storage/mount.py
+++ vdsm/storage/mount.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2011 Red Hat, Inc.
+# Copyright 2011-2014 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -121,10 +121,13 @@ def _resolveLoopDevice(path):
     loopdir = os.path.join(_SYS_DEV_BLOCK_PATH,
                            '%d:%d' % (major, minor),
                            'loop')
-    if os.path.exists(loopdir):
+    try:
         with open(loopdir + "/backing_file", "r") as f:
             # Remove trailing newline
             return f.read()[:-1]
+    except IOError as e:
+        if e.errno != errno.ENOENT:
+            raise
 
     # Old kernels might not have the sysfs entry, this is a bit slower and does
     # not work on hosts that do support the above method.
