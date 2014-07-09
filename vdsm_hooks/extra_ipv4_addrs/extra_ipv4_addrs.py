8bf7a6f1ae74db8437d56029070656da8b0c1608
Antoni S. Puimedon <asegurap@redhat.com>
2014-07-09 11:22:18 +0200

    extra_ipv4_addrs_hook: Add a hook for configuring secondary addrs
    Sometimes it might be useful to set up extra addresses for the top
    network device. This patch creates an after_network_setup hook that
    allows just that, thanks to the custom network properties.
    
    Change-Id: Id465870df6274552a68f6c9c0b907a843f08bf58
    Signed-off-by: Antoni S. Puimedon <asegurap@redhat.com>
    Signed-off-by: Ido Barkan <ibarkan@redhat.com>
    Signed-off-by: Edward Haas <edwardh@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/29738
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ vdsm_hooks/extra_ipv4_addrs/extra_ipv4_addrs.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# Copyright 2014 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+from __future__ import print_function
+import sys
+import hooking
+import traceback
+
+from vdsm import ipwrapper
+from vdsm import netinfo
+from vdsm import utils
+
+
+def test():
+    options = 'addr/prefix,\taddr2/prefix2,addrN/prefixN '
+    commands = ['/sbin/ip -4 addr add dev super_device addr/prefix',
+                '/sbin/ip -4 addr add dev super_device addr2/prefix2',
+                '/sbin/ip -4 addr add dev super_device addrN/prefixN']
+    print(commands)
+    assert list(' '.join(cmd) for cmd in _generate_commands(
+        options, 'super_device')) == commands
+
+
+def main():
+    """Read ipv4_addrs from the network 'custom' properties and apply them
+    to the network's top device"""
+    setup_nets_config = hooking.read_json()
+    for network, attrs in setup_nets_config['request']['networks'].items():
+        if 'remove' in attrs:
+            continue
+
+        if 'custom' in attrs:
+            _process_network(network, attrs)
+
+
+def _process_network(network, attrs):
+    """Applies extra ipv4 addresses to the network if necessary"""
+    options = attrs['custom'].get('ipv4_addrs')
+    if options is not None:
+        top_dev = _top_dev(network, attrs)
+        for cmd in _generate_commands(options, top_dev):
+            hooking.execCmd(cmd, sudo=True)
+
+
+def _generate_commands(options, top_level_device):
+    for addr in options.split(','):
+        yield [ipwrapper._IP_BINARY.cmd, '-4', 'addr', 'add', 'dev',
+               top_level_device, addr.strip()]
+
+
+def _top_dev(network, attrs):
+    if utils.tobool(attrs.get('bridged')):
+        return network
+    # bridgeless
+    nics, vlan, _, bonding = netinfo.NetInfo().\
+        getNicsVlanAndBondingForNetwork(network)
+    return vlan or bonding or nics[0]
+
+
+if __name__ == '__main__':
+    try:
+        if '--test' in sys.argv:
+            test()
+        else:
+            main()
+    except:
+        hooking.exit_hook('extra ipv4 addrs hook: [unexpected error]: %s\n' %
+                          traceback.format_exc())
