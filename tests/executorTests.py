855e2f98c774b5e14df592d91b005d89e3b5952d
Francesco Romani <fromani@redhat.com>
2014-07-11 09:49:55 +0200

    executor: introduce the executor library
    Executor is a thread pool augmented with the
    capability of discard blocked worker threads,
    by replacing them with fresh ones.
    
    This is needed to accomodate the needs of the sampling code.
    The sampling needs to deal with possibly blocking libvirt
    calls which needs to enter into the QEMU monitor,
    thus can get stuck due to QEMU being in D state.
    
    Change-Id: Ic06da1ba57868dc2c7db67a1868ad10087a1cff2
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/29191
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Signed-off-by: pkliczewski <piotr.kliczewski@gmail.com>
    Bug-Url: https://bugzilla.redhat.com/1279950
    Reviewed-on: https://gerrit.ovirt.org/48196
    Continuous-Integration: Jenkins CI

---
+++ tests/executorTests.py
@@ -0,0 +1,145 @@
+#
+# Copyright 2014 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import threading
+import time
+
+from vdsm import executor
+from vdsm import schedule
+
+from testValidation import slowtest
+from testrunner import VdsmTestCase as TestCaseBase
+
+
+class ExecutorTests(TestCaseBase):
+
+    def setUp(self):
+        self.scheduler = schedule.Scheduler()
+        self.scheduler.start()
+        self.executor = executor.Executor('test',
+                                          workers_count=10, max_tasks=20,
+                                          scheduler=self.scheduler)
+        self.executor.start()
+        time.sleep(0.1)  # Give time to start all threads
+
+    def tearDown(self):
+        self.executor.stop()
+        self.scheduler.stop()
+
+    def test_dispatch_not_running(self):
+        self.executor.stop()
+        self.assertRaises(executor.NotRunning,
+                          self.executor.dispatch,
+                          Task())
+
+    def test_start_twice(self):
+        self.assertRaises(executor.AlreadyStarted,
+                          self.executor.start)
+
+    def test_dispatch(self):
+        task = Task()
+        self.executor.dispatch(task)
+        task.executed.wait(0.1)
+        self.assertTrue(task.executed.is_set())
+
+    def test_dispatch_after_fault(self):
+        faulty_task = Task(error=RuntimeError("fake error"))
+        self.executor.dispatch(faulty_task)
+        faulty_task.executed.wait(0.1)
+        task = Task()
+        self.executor.dispatch(task)
+        task.executed.wait(0.1)
+        self.assertTrue(task.executed.is_set())
+
+    @slowtest
+    def test_dispatch_with_timeout(self):
+        task = Task(wait=0.2)
+        self.executor.dispatch(task, 0.1)
+        task.executed.wait(0.3)
+        self.assertTrue(task.executed.is_set())  # task must have executed!
+
+    def test_too_many_tasks(self):
+        tasks = [Task(wait=0.1) for n in xrange(31)]
+        with self.assertRaises(executor.TooManyTasks):
+            for task in tasks:
+                self.executor.dispatch(task)
+
+    @slowtest
+    def test_concurrency(self):
+        tasks = [Task(wait=0.1) for n in xrange(20)]
+        for task in tasks:
+            self.executor.dispatch(task, 1.0)
+        time.sleep(0.3)
+        for task in tasks:
+            self.assertTrue(task.executed.is_set())
+
+    @slowtest
+    def test_blocked_workers(self):
+        slow_tasks = [Task(wait=0.4) for n in xrange(5)]
+        for task in slow_tasks:
+            self.executor.dispatch(task, 1.0)
+        # Slow tasks block half of the workers
+        tasks = [Task(wait=0.1) for n in xrange(20)]
+        for task in tasks:
+            self.executor.dispatch(task, 1.0)
+        time.sleep(0.5)
+        for task in tasks:
+            self.assertTrue(task.executed.is_set())
+        for task in slow_tasks:
+            self.assertTrue(task.executed.is_set())
+
+    @slowtest
+    def test_discarded_workers(self):
+        slow_tasks = [Task(wait=0.4) for n in xrange(10)]
+        for task in slow_tasks:
+            self.executor.dispatch(task, 0.1)
+        # All workers are blocked on slow tasks
+        time.sleep(0.1)
+        # Blocked workers should be replaced with new workers
+        tasks = [Task(wait=0.1) for n in xrange(20)]
+        for task in tasks:
+            self.executor.dispatch(task, 1.0)
+        time.sleep(0.3)
+        for task in tasks:
+            self.assertTrue(task.executed.is_set())
+        for task in slow_tasks:
+            self.assertTrue(task.executed.is_set())
+        # Discarded workers should exit, executor should operate normally
+        tasks = [Task(wait=0.1) for n in xrange(20)]
+        for task in tasks:
+            self.executor.dispatch(task, 1.0)
+        time.sleep(0.3)
+        for task in tasks:
+            self.assertTrue(task.executed.is_set())
+
+
+class Task(object):
+
+    def __init__(self, wait=None, error=None):
+        self.wait = wait
+        self.error = error
+        self.executed = threading.Event()
+
+    def __call__(self):
+        if self.wait:
+            time.sleep(self.wait)
+        self.executed.set()
+        if self.error:
+            raise self.error
