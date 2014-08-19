oVirt/vdsm

commit 24c8e242df3a000a9cd8ffdbc86963350e5708f6

connectivity log: report if Engine has been seen recently
Vdsm monitors when it has been accessed by a client (most probably
Engine). If Vdsm finds out that it has not been accessed for the last 30
seconds (twice the Enginei's polling rate), "recent_client:False" is
logged to the connectivity.log. When polling is restored,
"recent_client:True" is reported.

Bug-Url: https://bugzilla.redhat.com/1111234
Change-Id: Ice088421d82f60e59ada6e743aeac2704c44cb7f
Signed-off-by: Dan Kenigsberg <danken@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/30226
Reviewed-by: Antoni Segura Puimedon <asegurap@redhat.com>
Reviewed-by: Francesco Romani <fromani@redhat.com>

vdsm/virt/sampling.py
@@ -37,7 +37,7 @@ import re
 from vdsm import utils
 from vdsm import netinfo
 from vdsm.ipwrapper import getLinks
-from vdsm.constants import P_VDSM_RUN
+from vdsm.constants import P_VDSM_RUN, P_VDSM_CLIENT_LOG
 
 import caps
 
@@ -279,11 +279,16 @@ class HostSample(TimedSample):
             self.thpState = 'never'
         self.cpuCores = CpuCoreSample()
         self.numaNodeMem = NumaNodeMemorySample()
+        ENGINE_DEFAULT_POLL_INTERVAL = 15
+        self.recentClient = (
+            self.timestamp - os.stat(P_VDSM_CLIENT_LOG).st_mtime <
+            2 * ENGINE_DEFAULT_POLL_INTERVAL)
 
     def to_connlog(self):
-        return ', '.join(
+        text = ', '.join(
             ('%s:(%s)' % (ifid, ifacesample.to_connlog()))
             for (ifid, ifacesample) in self.interfaces.iteritems())
+        return ('recent_client:%s, ' % self.recentClient) + text
 
     def connlog_diff(self, other):
         text = ''
@@ -294,6 +299,10 @@ class HostSample(TimedSample):
                     text += '%s:(%s) ' % (ifid, diff)
             else:
                 text += 'new %s:(%s) ' % (ifid, sample.to_connlog())
+
+        if self.recentClient != other.recentClient:
+            text += 'recent_client:%s' % self.recentClient
+
         return text
 
 

commit 9533d02707951164d80702a85afd40c26069160c

virt: keep on sampling before /var/run/vdsm/client.log exists
My commit 24c8e24 has introduced a serious bug: if vdsm is started
before /var/run/vdsm/client.log is created (which is the common case for
new installations), the host-sampling thread would stop when it fails to
find it.

This patch explicitly handles the case of a missing
/var/run/vdsm/client.log on the two locations that attempted to read its
time.

Change-Id: Ibb7d380882f5a93e5c75eda80598f382744b178c
Bug-Url: https://bugzilla.redhat.com/1111234
Signed-off-by: Dan Kenigsberg <danken@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/31619
Reviewed-by: Antoni Segura Puimedon <asegurap@redhat.com>
Reviewed-by: Francesco Romani <fromani@redhat.com>

vdsm/network/api.py
@@ -18,6 +18,7 @@
 #
 
 from functools import wraps
+import errno
 import inspect
 import sys
 import os
@@ -472,8 +473,14 @@ def delNetwork(network, vlan=None, bonding=None, nics=None, force=False,
 def clientSeen(timeout):
     start = time.time()
     while timeout >= 0:
-        if os.stat(constants.P_VDSM_CLIENT_LOG).st_mtime > start:
-            return True
+        try:
+            if os.stat(constants.P_VDSM_CLIENT_LOG).st_mtime > start:
+                return True
+        except OSError as e:
+            if e.errno == errno.ENOENT:
+                pass  # P_VDSM_CLIENT_LOG is not yet there
+            else:
+                raise
         time.sleep(1)
         timeout -= 1
     return False

vdsm/virt/sampling.py
@@ -280,9 +280,15 @@ class HostSample(TimedSample):
         self.cpuCores = CpuCoreSample()
         self.numaNodeMem = NumaNodeMemorySample()
         ENGINE_DEFAULT_POLL_INTERVAL = 15
-        self.recentClient = (
-            self.timestamp - os.stat(P_VDSM_CLIENT_LOG).st_mtime <
-            2 * ENGINE_DEFAULT_POLL_INTERVAL)
+        try:
+            self.recentClient = (
+                self.timestamp - os.stat(P_VDSM_CLIENT_LOG).st_mtime <
+                2 * ENGINE_DEFAULT_POLL_INTERVAL)
+        except OSError as e:
+            if e.errno == errno.ENOENT:
+                self.recentClient = False
+            else:
+                raise
 
     def to_connlog(self):
         text = ', '.join(