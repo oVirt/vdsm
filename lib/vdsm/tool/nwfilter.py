7cdae61ddcf7eea31a183d4e70ca1f8cde2ffba2
Petr Horáček <phoracek@redhat.com>
2014-09-09 13:29:56 +0200

    revert: libvirt is happy with redefinition of an existing nwfilter
    Change-Id: If7ab71951ae4edef750be9a4f944e3180d568c91
    Bug-Url: https://bugzilla.redhat.com/1138807
    Signed-off-by: Petr Horáček <phoracek@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/32691
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- lib/vdsm/tool/nwfilter.py
+++ lib/vdsm/tool/nwfilter.py
@@ -21,6 +21,8 @@
 
 import logging
 
+import libvirt
+
 from .. import libvirtconnection
 from . import expose, ExtraArgsError
 
@@ -56,6 +58,14 @@ class NwFilter(object):
         """
         define vdsm network filter on libvirt to control VM traffic
         """
+
+        try:
+            conn.nwfilterLookupByName(self.filterName).undefine()
+        except libvirt.libvirtError:
+            # Ignore failure if filter isn't exists or if failed to remove.
+            # Failure might occur when attempting to remove a filter which
+            # is being used by running VMs
+            pass
 
         nwFilter = conn.nwfilterDefineXML(self.buildFilterXml())
         logging.debug("Filter %s was defined", nwFilter.name())
