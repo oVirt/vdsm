2ce53585ba7b1c462394dab9b3bac6ba9eb6491f
Federico Simoncelli <fsimonce@redhat.com>
2014-10-01 13:23:03 +0000

    net: backport cmdutils.systemd_run
    This is a minimal backport of cmdutils to 3.5. cmdutils is only a
    partial version of what was already included in the master branch. This
    was done to avoid meeting many dependencies just in order to fix a bug
    in the stable branch.
    
    Change-Id: I7686e80c0880dcc28fa735d1dd3ab658136889f9
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1228322
    Reviewed-on: https://gerrit.ovirt.org/43852
    Tested-by: Ido Barkan <ibarkan@redhat.com>
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ lib/vdsm/cmdutils.py
@@ -0,0 +1,37 @@
+#
+# Copyright 2014 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+"""This is only a partial version of the module that exists in the master
+branch. It was backported here only to support systemd_run for the use of
+network configurators."""
+
+from . import constants
+
+
+def systemd_run(cmd, scope=False, unit=None, slice=None):
+    # this is supported only on OS versions that use systemd (el7 and later).
+    command = [constants.EXT_SYSTEMD_RUN]
+    if scope:
+        command.append('--scope')
+    if unit:
+        command.append('--unit=%s' % unit)
+    if slice:
+        command.append('--slice=%s' % slice)
+    command.extend(cmd)
+    return command
---
+++ tests/cmdutilsTests.py
@@ -0,0 +1,47 @@
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from vdsm import cmdutils
+from vdsm import constants
+
+from testrunner import VdsmTestCase
+
+
+class SystemdRunTests(VdsmTestCase):
+
+    def test_defaults(self):
+        cmd = cmdutils.systemd_run(['a', 'b'])
+        res = [constants.EXT_SYSTEMD_RUN, 'a', 'b']
+        self.assertEqual(cmd, res)
+
+    def test_scope(self):
+        cmd = cmdutils.systemd_run(['a', 'b'], scope=True)
+        res = [constants.EXT_SYSTEMD_RUN, '--scope', 'a', 'b']
+        self.assertEqual(cmd, res)
+
+    def test_unit(self):
+        cmd = cmdutils.systemd_run(['a', 'b'], unit='unit')
+        res = [constants.EXT_SYSTEMD_RUN, '--unit=unit', 'a', 'b']
+        self.assertEqual(cmd, res)
+
+    def test_slice(self):
+        cmd = cmdutils.systemd_run(['a', 'b'], slice='slice')
+        res = [constants.EXT_SYSTEMD_RUN, '--slice=slice', 'a', 'b']
+        self.assertEqual(cmd, res)
