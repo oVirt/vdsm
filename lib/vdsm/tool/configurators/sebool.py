c8d72a6071ba2d152271cf8f79d2779e7705055a
Mooli Tayer <mtayer@redhat.com>
2014-10-27 18:35:50 +0200

    tool: sebool is-configured when selinux is disabled will always be MAYBE
    Otherwise if selinux is disabled, we get an exception[1]
    
    [1]:
    >>> sebool_obj = seobject.booleanRecords()
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
      File "/usr/lib64/python2.6/site-packages/seobject.py", line 2032, in __init__
        semanageRecords.__init__(self, store)
      File "/usr/lib64/python2.6/site-packages/seobject.py", line 240, in __init__
        self.sh = self.get_handle(store)
      File "/usr/lib64/python2.6/site-packages/seobject.py", line 267, in get_handle
        raise ValueError(_("SELinux policy is not managed or store cannot be accessed."))
    ValueError: SELinux policy is not managed or store cannot be accessed.
    
    Change-Id: I8d728fac39be454600235d80c8c24641d6c309a4
    Signed-off-by: Mooli Tayer <mtayer@redhat.com>
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/34516
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/34585

--- lib/vdsm/tool/configurators/sebool.py
+++ lib/vdsm/tool/configurators/sebool.py
@@ -20,7 +20,8 @@
 from .import \
     CONFIGURED, \
     ModuleConfigure, \
-    NOT_CONFIGURED
+    NOT_CONFIGURED, \
+    NOT_SURE
 
 from ... import utils
 
@@ -69,7 +70,9 @@ class SEBool(ModuleConfigure):
         True all selinux booleans in the list above are set properly
         """
         ret = CONFIGURED
-        if utils.get_selinux_enforce_mode() > -1:
+        if utils.get_selinux_enforce_mode() == -1:
+            ret = NOT_SURE
+        else:
             import seobject
             sebool_obj = seobject.booleanRecords()
             sebool_status = sebool_obj.get_all()
