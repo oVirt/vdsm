d5256c6b223d6a2d83c5c011520d13c8e665f1eb
Nir Soffer <nsoffer@redhat.com>
2014-10-29 23:17:20 +0200

    tests: Start DomainDescriptor tests
    Adding some tests before fixing hashing implementation.
    
    Change-Id: I3502d0db927abbc8a8fdd90f74db07eeb517e457
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/34620
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Reviewed-by: Vinzenz Feenstra <vfeenstr@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ tests/domainDescriptorTests.py
@@ -0,0 +1,81 @@
+#
+# Copyright 2014 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301  USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from virt.domain_descriptor import DomainDescriptor
+from testlib import VdsmTestCase
+from testValidation import brokentest
+
+NO_DEVICES = """
+<domain>
+    <uuid>xyz</uuid>
+</domain>
+"""
+
+EMPTY_DEVICES = """
+<domain>
+    <uuid>xyz</uuid>
+    <devices/>
+</domain>
+"""
+
+SOME_DEVICES = """
+<domain>
+    <uuid>xyz</uuid>
+    <devices>
+        <device name="foo"/>
+        <device name="bar"/>
+    </devices>
+</domain>
+"""
+
+REORDERED_DEVICES = """
+<domain>
+    <uuid>xyz</uuid>
+    <devices>
+        <device name="bar"/>
+        <device name="foo"/>
+    </devices>
+</domain>
+"""
+
+
+class DevicesHashTests(VdsmTestCase):
+
+    def test_no_devices(self):
+        desc1 = DomainDescriptor(NO_DEVICES)
+        desc2 = DomainDescriptor(EMPTY_DEVICES)
+        self.assertNotEqual(desc1.devicesHash, desc2.devicesHash)
+
+    def test_different_devices(self):
+        desc1 = DomainDescriptor(EMPTY_DEVICES)
+        desc2 = DomainDescriptor(SOME_DEVICES)
+        self.assertNotEqual(desc1.devicesHash, desc2.devicesHash)
+
+    def test_different_order(self):
+        desc1 = DomainDescriptor(SOME_DEVICES)
+        desc2 = DomainDescriptor(REORDERED_DEVICES)
+        self.assertNotEqual(desc1.devicesHash, desc2.devicesHash)
+
+    @brokentest("hashing is not stable")
+    def test_stable_hash(self):
+        desc1 = DomainDescriptor(SOME_DEVICES)
+        desc2 = DomainDescriptor(SOME_DEVICES)
+        self.assertEqual(desc1.devicesHash, desc2.devicesHash)
