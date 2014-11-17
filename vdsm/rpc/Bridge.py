55df4a7f1c6cfefdc70b439d0d5a3f735f6bbd4b
pkliczewski <piotr.kliczewski@gmail.com>
2014-11-17 10:53:51 +0100

    jsonrpc: diskSizeExtend missing in command_info
    Bug-Url: https://bugzilla.redhat.com/1161115
    Change-Id: I099b83defd9fc3f0280ddeb28f3c4b706f8998c2
    Signed-off-by: pkliczewski <piotr.kliczewski@gmail.com>
    Reviewed-on: http://gerrit.ovirt.org/35218
    Reviewed-by: Oved Ourfali <oourfali@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/rpc/Bridge.py
+++ vdsm/rpc/Bridge.py
@@ -451,6 +451,7 @@ command_info = {
     'VM_changeFloppy': {'ret': 'vmList'},
     'VM_create': {'ret': 'vmList'},
     'VM_cont': {'ret': VM_running_state_change_Ret},
+    'VM_diskSizeExtend': {'ret': 'size'},
     'VM_getInfo': {'call': VM_getInfo_Call, 'ret': VM_getInfo_Ret},
     'VM_getIoTunePolicy': {'ret': 'ioTunePolicyList'},
     'VM_getStats': {'ret': 'statsList'},
