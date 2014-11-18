c2acc19c65d5ad5a5e99b76741bd8246415c387e
Francesco Romani <fromani@redhat.com>
2014-11-18 14:11:31 +0100

    utils: add monotonic_time function
    We currently use time.time() to get timestamp
    with (near) second precision.
    We we just need the difference between two instants
    in time, os.times() provide safer alternative
    because it is more resilient with respect to
    system clock adjustments, and can never go
    backwards.
    
    This function add a simple monotonic_time() function
    based on os.times().
    
    The drawback of this function is lower resolution.
    The actual resolution depend on the kernel configuration,
    but a commonly found value is ten milliseconds.
    
    Change-Id: I65c7d6041ed89fc8c497115b2d70341e82936b26
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/35350
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- lib/vdsm/utils.py
+++ lib/vdsm/utils.py
@@ -1199,3 +1199,21 @@ def picklecopy(obj):
     https://docs.python.org/2/library/pickle.html
     """
     return pickle.loads(pickle.dumps(obj, pickle.HIGHEST_PROTOCOL))
+
+
+def monotonic_time():
+    """
+    Return the amount of time, in secs, elapsed since a fixed
+    arbitrary point in time in the past.
+    This function is useful if the client just
+    needs to use the difference between two given time points.
+
+    With repect to time.time():
+    * The resolution of this function is lower. On Linux,
+      the resolution is 1/_SC_CLK_TCK, which in turn depend on
+      the value of HZ configured in the kernel. A commonly
+      found resolution is 10 (ten) ms.
+    * This functions is resilient with respect to system clock
+      adjustments.
+    """
+    return os.times()[4]
