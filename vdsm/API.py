eda3ff204deb61ca689d9eecf8a66e95813dfecf
pkliczewski <piotr.kliczewski@gmail.com>
2014-11-25 13:34:00 +0100

    jsonrpc: Image_download
    Align naming between API.py and schema.
    
    Change-Id: Ia36f8278e78ed46eba7876ea80c52c268ac935a1
    Signed-off-by: pkliczewski <piotr.kliczewski@gmail.com>
    Reviewed-on: http://gerrit.ovirt.org/35528
    Reviewed-by: Adam Litke <alitke@redhat.com>

--- vdsm/API.py
+++ vdsm/API.py
@@ -880,9 +880,9 @@ class Image(APIBase):
         return self._irs.uploadImage(
             methodArgs, self._spUUID, self._sdUUID, self._UUID, volUUID)
 
-    def download(self, methodArgs, volUUID=None):
+    def download(self, methodArgs, volumeID=None):
         return self._irs.downloadImage(
-            methodArgs, self._spUUID, self._sdUUID, self._UUID, volUUID)
+            methodArgs, self._spUUID, self._sdUUID, self._UUID, volumeID)
 
     def prepare(self, volUUID=None):
         return self._irs.prepareImage(
