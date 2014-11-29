cf200a2b0c43f8ffc7729e5ff56d71c6b091b18d
Antoni S. Puimedon <asegurap@redhat.com>
2014-11-29 03:07:59 +0100

    net_func_tests: add calling method name to setupNetworks
    It is usually difficult to know which test originated the
    setupNetworks calls in the logs. This patch makes it easier to debug
    
    Change-Id: Id5f07be1f2cef54a773deb8dd73967dc55bf2cf0
    Signed-off-by: Antoni S. Puimedon <asegurap@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/35711
    Reviewed-by: Ondřej Svoboda <osvoboda@redhat.com>
    Tested-by: Ondřej Svoboda <osvoboda@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- tests/functional/utils.py
+++ tests/functional/utils.py
@@ -18,6 +18,7 @@
 #
 from contextlib import contextmanager
 from functools import wraps
+import inspect
 import socket
 import time
 import threading
@@ -152,6 +153,8 @@ class VdsProxy(object):
 
     @netinfo_altering
     def setupNetworks(self, networks, bonds, options):
+        stack = inspect.stack()
+        options['_caller'] = stack[2][3]  # add calling method for logs
         result = self.vdscli.setupNetworks(networks, bonds, options)
         return result['status']['code'], result['status']['message']
 
