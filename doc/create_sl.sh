2fcb5d8e209ec616e9500c877183b8202eb42b98
Dima Kuznetsov <dkuznets@redhat.com>
2014-12-07 15:45:30 +0200

    doc: Remove create_sl.sh
    sphinx does not require this.
    
    Change-Id: I941bb9456f1de0748787738258539ec08ca630a5
    Signed-off-by: Dima Kuznetsov <dkuznets@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/35916
    Reviewed-by: Saggi Mizrahi <smizrahi@redhat.com>
    Reviewed-by: Yeela Kaplan <ykaplan@redhat.com>
    Reviewed-by: Yaniv Bronhaim <ybronhei@redhat.com>
    Reviewed-by: mooli tayer <mtayer@redhat.com>

--- doc/create_sl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Adds a symoblic link for vdscli in vdsm if not present.
-VDSCLI=$(readlink -f "../vdsm/vdscli.py")
-if [ ! -e "$VDSCLI" ] ; then
-   ln -s ../client/vdscli.py "$VDSCLI"
-fi
