a6424acef3153f0a71ac334d8d9cac6830a40249
Francesco Romani <fromani@redhat.com>
2015-01-05 17:51:20 +0100

    virt: add ExpiringCache
    ExpiringCache is a simple cache with expiring time attached
    to keys. This class implements parts of the dict() interface,
    but do not aims to be a full replacement for dict()s.
    ExpiringCache is thread safe.
    
    Change-Id: I51e38cea6a23b7abe2375780e6ece0ff90831b6d
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/36716
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ tests/vmUtilsTests.py
@@ -0,0 +1,95 @@
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from virt import utils
+
+from testlib import VdsmTestCase as TestCaseBase
+
+
+class ExpiringCacheOperationTests(TestCaseBase):
+    def setUp(self):
+        self.cache = utils.ExpiringCache(ttl=20)
+
+    def test_setitem_getitem_same_key(self):
+        self.cache['the answer'] = 42
+        self.assertEqual(42, self.cache['the answer'])
+
+    def test_setitem_get_same_key(self):
+        self.cache['the answer'] = 42
+        self.assertEqual(42, self.cache.get('the answer'))
+
+    def test_setitem_get_same_key_with_default(self):
+        self.cache['the answer'] = 42
+        self.assertEqual(42, self.cache.get('the answer', 'default'))
+
+    def test_setitem_get_different_key_with_default(self):
+        value = self.cache.get('a different answer', 'default')
+        self.assertEqual(value, 'default')
+
+    def test_get_key_without_explicit_default(self):
+        self.assertEqual(None, self.cache.get('a key noone added'))
+
+    def test_getitem_missing_key(self):
+        self.assertRaises(KeyError,
+                          lambda key: self.cache[key],
+                          'FIZZBUZZ')
+
+    def test_delitem_existing_key(self):
+        self.cache['the answer'] = 42
+        del self.cache['the answer']
+        self.assertEquals(self.cache.get('the answer'), None)
+
+    def test_delitem_missing_key(self):
+        def _del(key):
+            del self.cache[key]
+        self.assertRaises(KeyError,
+                          _del,
+                          'this key does not exist')
+
+    def test_clear(self):
+        ITEMS = 10
+        for i in xrange(ITEMS):
+            self.cache[i] = 'foobar-%d' % i
+
+        self.cache.clear()
+
+        for i in xrange(ITEMS):
+            self.cache.get(i) is None
+
+
+class FakeClock(object):
+    def __init__(self, now):
+        self.now = now
+
+    def __call__(self):
+        return self.now
+
+
+class ExpirationTests(TestCaseBase):
+    def test_key_expiration(self):
+        clock = FakeClock(0.0)
+        cache = utils.ExpiringCache(ttl=1.0, clock=clock)
+        cache['the answer'] = 42
+        clock.now = 0.999999
+        self.assertEqual(42, cache['the answer'])
+        clock.now = 1.0
+        self.assertEqual(None, cache.get('the answer'))
+        clock.now = 1.000001
+        self.assertEqual(None, cache.get('the answer'))
--- vdsm/virt/utils.py
+++ vdsm/virt/utils.py
@@ -22,6 +22,10 @@
 shared utilities and common code for the virt package
 """
 
+import threading
+
+from vdsm.utils import monotonic_time
+
 
 def isVdsmImage(drive):
     """
@@ -33,3 +37,64 @@ def isVdsmImage(drive):
     """
     required = ('domainID', 'imageID', 'poolID', 'volumeID')
     return all(k in drive for k in required)
+
+
+class ItemExpired(KeyError):
+    pass
+
+
+class ExpiringCache(object):
+    """
+    ExpiringCache behaves like a dict, but an expiration time
+    is attached to each key. Thread safe.
+
+    Parameters:
+    ttl: items will expire ttl seconds after they were inserted.
+    clock: time.time() or monotonic_time()-like callable.
+
+    Expired keys are removed on the first attempt to access them.
+    """
+    def __init__(self, ttl, clock=monotonic_time):
+        self._ttl = ttl
+        self._clock = clock
+        self._items = {}
+        self._lock = threading.Lock()
+
+    def get(self, key, default=None):
+        try:
+            return self._get_live(key)
+        except KeyError:
+            return default
+
+    def clear(self):
+        with self._lock:
+            self._items.clear()
+
+    def __setitem__(self, key, value):
+        item = self._clock() + self._ttl, value
+        with self._lock:
+            self._items[key] = item
+
+    def __getitem__(self, key):
+        """
+        If no value is stored for key, raises KeyError.
+        If an item expired, raises ItemExpired (a subclass of KeyError).
+        """
+        return self._get_live(key)
+
+    def __delitem__(self, key):
+        with self._lock:
+            del self._items[key]
+
+    # private
+
+    def _get_live(self, key):
+        now = self._clock()
+        with self._lock:
+            expiration, value = self._items[key]
+
+            if expiration <= now:
+                del self._items[key]
+                raise ItemExpired
+
+            return value
