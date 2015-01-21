oVirt/vdsm

commit d6d7db4ca24606d35acafe6e92b1ee031c0c8fe1

teach delNetworks to remove a network entity under a bridge
By leaving the bridge intact, VDSM will be able to allow
partial network editing while VMs are connected to it.
Inter-VM networking on the bridge is also preserved during
network editing.
This commit does not yet change VDSM's behavior.

Change-Id: Ia6bb891f23ef4550f90c6c7b057aef6302cf435c
Signed-off-by: ibarkan <ibarkan@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/35921
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/network/api.py
@@ -410,7 +410,7 @@ def _delNonVdsmNetwork(network, vlan, bonding, nics, _netinfo, configurator):
 @_alterRunningConfig
 def _delNetwork(network, vlan=None, bonding=None, nics=None, force=False,
                 configurator=None, implicitBonding=True, _netinfo=None,
-                **options):
+                keep_bridge=False, **options):
     if _netinfo is None:
         _netinfo = netinfo.NetInfo()
 
@@ -428,7 +428,8 @@ def _delNetwork(network, vlan=None, bonding=None, nics=None, force=False,
     bridged = _netinfo.networks[network]['bridged']
 
     logging.info("Removing network %s with vlan=%s, bonding=%s, nics=%s,"
-                 "options=%s" % (network, vlan, bonding, nics, options))
+                 "keep_bridge=%s options=%s", network, vlan, bonding,
+                 nics, keep_bridge, options)
 
     if not utils.tobool(force):
         _validateDelNetwork(network, vlan, bonding, nics, bridged, _netinfo)
@@ -439,12 +440,27 @@ def _delNetwork(network, vlan=None, bonding=None, nics=None, force=False,
                                  implicitBonding=implicitBonding)
     net_ent.ip.bootproto = _netinfo.networks[network]['bootproto4']
 
+    if bridged and keep_bridge:
+        # we now leave the bridge intact but delete everything underneath it
+        net_ent_to_remove = net_ent.port
+        if net_ent_to_remove is not None:
+            # the configurator will not allow us to remove a bridge interface
+            # (be it vlan, bond or nic) unless it is not used anymore. Since
+            # we are interested to leave the bridge here, we have to disconnect
+            # it from the device so that the configurator will allow its
+            # removal.
+            ipwrapper.linkSet(net_ent_to_remove.name, ['nomaster'])
+    else:
+        net_ent_to_remove = net_ent
+
     # We must first remove the libvirt network and then the network entity.
     # Otherwise if we first remove the network entity while the libvirt
     # network is still up, the network entity (In some flows) thinks that
     # it still has users and thus does not allow its removal
     configurator.removeLibvirtNetwork(network)
-    net_ent.remove()
+    if net_ent_to_remove is not None:
+        logging.info('Removing network entity %s', net_ent_to_remove)
+        net_ent_to_remove.remove()
     # We must remove the QoS last so that no devices nor networks mark the
     # QoS as used
     backing_device = hierarchy_backing_device(net_ent)

commit e5b08c0c2e55695c09e604c9d8e0ba109c0fd1e4

disconnect bridge port on rhel 6
This fixes a bug introduced in d6d7db4ca24606d35acafe6e92b1ee031c0c8fe1
Where del network learned how to disconnect a bridge port.  Since rhel6 ip link
command does not support 'nomaster', we also try brctl.

Change-Id: Ieaa6be3f4d5a202ef79704f3af76c8968765f54c
Signed-off-by: Ido Barkan <ibarkan@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/37157
Reviewed-by: Ondřej Svoboda <osvoboda@redhat.com>
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/network/api.py
@@ -430,6 +430,22 @@ def _delNonVdsmNetwork(network, vlan, bonding, nics, _netinfo, configurator):
                                  network)
 
 
+def _disconnect_bridge_port(bridge, port):
+    try:
+        ipwrapper.linkSet(port, ['nomaster'])
+    except ipwrapper.IPRoute2Error:
+        # REQUIRED_FOR: el6
+        # rhel6 ip link command does not support 'nomaster'
+        rc, out, err = utils.execCmd(
+            [constants.EXT_BRCTL, 'delif', bridge, port])
+        if rc != 0:
+            raise ConfigNetworkError(
+                ne.ERR_FAILED_IFDOWN,
+                'Failed to disconnect {0} from {1}. error: {2}'.format(
+                    port, bridge, err
+                ))
+
+
 @_alterRunningConfig
 def _delNetwork(network, vlan=None, bonding=None, nics=None, force=False,
                 configurator=None, implicitBonding=True, _netinfo=None,
@@ -473,7 +489,7 @@ def _delNetwork(network, vlan=None, bonding=None, nics=None, force=False,
             # we are interested to leave the bridge here, we have to disconnect
             # it from the device so that the configurator will allow its
             # removal.
-            ipwrapper.linkSet(net_ent_to_remove.name, ['nomaster'])
+            _disconnect_bridge_port(net_ent.name, net_ent_to_remove.name)
     else:
         net_ent_to_remove = net_ent
 