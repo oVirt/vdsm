oVirt/vdsm

commit 3951f078d840f6c99d3e72b8ef95a94c6fad4094

be smarter about forcing bridge removal
Change-Id: I2b68cae78370ab357dbdbc74d3d7c0311b28cf15
Signed-off-by: ibarkan <ibarkan@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/35926
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/network/api.py
@@ -702,6 +702,16 @@ def _apply_hook(bondings, networks, options):
     return bondings, networks, options
 
 
+def _should_keep_bridge(marked_for_removal, currently_bridged,
+                        should_be_bridged):
+    if marked_for_removal:
+        return False
+    if currently_bridged and not should_be_bridged:
+        return False
+
+    return True
+
+
 def setupNetworks(networks, bondings, **options):
     """Add/Edit/Remove configuration for networks and bondings.
 
@@ -767,8 +777,14 @@ def setupNetworks(networks, bondings, **options):
         for network, attrs in networks.items():
             if network in _netinfo.networks:
                 logger.debug("Removing network %r", network)
+                keep_bridge = _should_keep_bridge(
+                    marked_for_removal='remove' in attrs,
+                    currently_bridged=_netinfo.networks[network]['bridged'],
+                    should_be_bridged=attrs.get('bridged'))
+
                 _delNetwork(network, configurator=configurator, force=force,
-                            implicitBonding=False, _netinfo=_netinfo)
+                            implicitBonding=False, _netinfo=_netinfo,
+                            keep_bridge=keep_bridge)
                 _netinfo.updateDevices()
                 del _netinfo.networks[network]
             elif network in libvirt_nets:

tests/functional/networkTests.py
@@ -41,7 +41,7 @@ from utils import SUCCESS, VdsProxy
 from vdsm.ipwrapper import (routeExists, ruleExists, addrFlush, LinkType,
                             getLinks, routeShowTable)
 
-from vdsm.constants import EXT_BRCTL
+from vdsm.constants import EXT_BRCTL, EXT_IFUP, EXT_IFDOWN
 from vdsm.utils import RollbackContext, execCmd, running
 from vdsm.netinfo import (bridges, operstate, prefix2netmask, getRouteDeviceTo,
                           getDhclientIfaces)
@@ -51,6 +51,8 @@ from vdsm.utils import pgrep
 import caps
 from network import errors
 from network import tc
+from network import api
+from network.configurators.ifcfg import Ifcfg
 
 
 NETWORK_NAME = 'test-network'
@@ -973,6 +975,44 @@ class NetworkTest(TestCaseBase):
 
             for net in nets_to_clean:
                 self.assertNetworkDoesntExist(net)
+
+    @cleanupNet
+    @RequireDummyMod
+    @ValidateRunningAsRoot
+    def testSetupNetworksDoesNotDeleteTheBridge(self):
+        def get_bridge_index():
+            link = ipwrapper.getLink(NETWORK_NAME)
+            return link.index
+
+        with dummyIf(2) as nics:
+            first, second = nics
+            STANDARD = 1500
+            first_net = {NETWORK_NAME: dict(bridged=True, nic=first,
+                                            mtu=STANDARD)}
+            status, msg = self.vdsm_net.setupNetworks(first_net, {}, NOCHK)
+            self.assertEquals(status, SUCCESS, msg)
+            self.assertMtu(STANDARD, NETWORK_NAME, first)
+            bridge_index = get_bridge_index()
+
+            BIG = 2000
+            second_net = {NETWORK_NAME: dict(bridged=True, nic=second,
+                                             mtu=BIG)}
+            status, msg = self.vdsm_net.setupNetworks(second_net, {}, NOCHK)
+            self.assertEquals(status, SUCCESS, msg)
+            self.assertEquals(bridge_index, get_bridge_index())
+            # the kernel bridge driver automatically updates the bridge to the
+            # new minimum MTU of all of its connected interfaces
+            self.assertMtu(BIG, NETWORK_NAME, second)
+
+            if api.ConfiguratorClass == Ifcfg:
+                # verify that the ifcfg configuration files are also updated
+                # with the new MTU
+                rc, _, _ = execCmd([EXT_IFDOWN, NETWORK_NAME])
+                self.assertEquals(rc, 0, 'ifdown failed: rc=%s' % (rc,))
+                rc, _, _ = execCmd([EXT_IFUP, NETWORK_NAME])
+                self.assertEquals(rc, 0, 'ifup failed: rc=%s' % (rc,))
+                self.vdsm_net.refreshNetinfo()
+                self.assertMtu(BIG, NETWORK_NAME, second)
 
     @cleanupNet
     @permutations([[True], [False]])

commit f44b412dbe28a8823e42a0e28381abbb1abbfd9a

Handle bridge reconfiguration
This fixes a regression that was introduced in
3951f078d840f6c99d3e72b8ef95a94c6fad4094. When a bridged network is
reconfigured, for example, changing its IP or its bootproto, vdsm would change
nothing, since it would try to leave the bridge intact. Comparing the required
configuration (setupNetworks arguments) to the running configuration solves
this.

Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1184497
Change-Id: Ia461004eb59d10283ac893dc8b5858388d5c4a8c
Signed-off-by: Ido Barkan <ibarkan@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/37059
Reviewed-by: Petr Horáček <phoracek@redhat.com>
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/network/api.py
@@ -721,11 +721,30 @@ def _apply_hook(bondings, networks, options):
     return bondings, networks, options
 
 
-def _should_keep_bridge(marked_for_removal, currently_bridged,
-                        should_be_bridged):
+def _should_keep_bridge(network_attrs, currently_bridged,
+                        network_running_config):
+    marked_for_removal = 'remove' in network_attrs
     if marked_for_removal:
         return False
+
+    should_be_bridged = network_attrs.get('bridged')
     if currently_bridged and not should_be_bridged:
+        return False
+
+    # check if the user wanted to only reconfigure bridge itself (mtu is a
+    # special case as it is handled automatically by the os)
+    def _bridge_only_config(conf):
+        return dict(
+            (k, v) for k, v in conf.iteritems()
+            if k not in ('bonding', 'nic', 'mtu'))
+
+    def _bridge_reconfigured(required_conf, current_conf):
+        return (_bridge_only_config(required_conf) !=
+                _bridge_only_config(current_conf))
+
+    if currently_bridged and _bridge_reconfigured(network_running_config,
+                                                  network_attrs):
+        logging.debug("the bridge is being reconfigured")
         return False
 
     return True
@@ -796,10 +815,12 @@ def setupNetworks(networks, bondings, **options):
         for network, attrs in networks.items():
             if network in _netinfo.networks:
                 logger.debug("Removing network %r", network)
+                running_conf = configurator.runningConfig.networks.get(network)
                 keep_bridge = _should_keep_bridge(
-                    marked_for_removal='remove' in attrs,
+                    network_attrs=attrs,
                     currently_bridged=_netinfo.networks[network]['bridged'],
-                    should_be_bridged=attrs.get('bridged'))
+                    network_running_config=running_conf
+                )
 
                 _delNetwork(network, configurator=configurator, force=force,
                             implicitBonding=False, _netinfo=_netinfo,

tests/functional/networkTests.py
@@ -1857,6 +1857,48 @@ class NetworkTest(TestCaseBase):
                     dhcp.delete_dhclient_leases(
                         NETWORK_NAME if bridged else right, dhcpv4, dhcpv6)
 
+    @cleanupNet
+    @RequireVethMod
+    def testSetupNetworksReconfigureBridge(self):
+        def setup_test_network(dhcp=True):
+            network_params = {'nic': right, 'bridged': True}
+            if dhcp:
+                network_params.update(
+                    {'bootproto': 'dhcp', 'blockingdhcp': True})
+            else:
+                network_params.update(
+                    {'ipaddr': IP_ADDRESS_IN_NETWORK, 'netmask': IP_MASK,
+                     'gateway':  IP_GATEWAY})
+
+            status, msg = self.vdsm_net.setupNetworks(
+                {NETWORK_NAME: network_params}, {}, NOCHK)
+            self.assertEqual(status, SUCCESS, msg)
+            self.assertNetworkExists(NETWORK_NAME)
+
+            test_net = self.vdsm_net.netinfo.networks[NETWORK_NAME]
+            self.assertEqual(test_net['dhcpv4'], dhcp)
+
+            ifcfg_bootproto = 'dhcp' if dhcp else 'none'
+            self.assertEqual(test_net['cfg']['BOOTPROTO'], ifcfg_bootproto)
+
+            bridges = self.vdsm_net.netinfo.bridges
+            self.assertIn(NETWORK_NAME, bridges)
+            self.assertEqual(
+                bridges[NETWORK_NAME]['cfg']['BOOTPROTO'],
+                ifcfg_bootproto)
+            self.assertEqual(bridges[NETWORK_NAME]['dhcpv4'], dhcp)
+
+        with vethIf() as (left, right):
+            veth.setIP(left, IP_ADDRESS, IP_CIDR)
+            veth.setLinkUp(left)
+            with dnsmasqDhcp(left, _system_is_el6()):
+                try:
+                    setup_test_network(dhcp=True)
+                    dhcp.delete_dhclient_leases(NETWORK_NAME, dhcpv4=True)
+                    setup_test_network(dhcp=False)
+                finally:
+                    dhcp.delete_dhclient_leases(NETWORK_NAME, dhcpv4=True)
+
     @permutations([[(4, 'default')], [(4, 'local')], [(6, 'not applicable')]])
     @cleanupNet
     @RequireVethMod