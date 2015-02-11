oVirt/vdsm

commit 7f577ab94b35f2cb2560d3ae49d6b0f753588474

virt: cleaner getAllVmStats
the api.Global.getAllVmStats method was implemented reusing
api.Global.getVMList and api.VM.getStats().

This lead to unnecessary work, and a lot of layering traversal
with objects being wrapped in responses, which in turn are
promptly discarded and rewrapped again to obtain the final
result. All of this without any clear benefit.

This patch drops all the layering and use simple and lean
methods to obtain the same result doing much less work.
Unfortunately, total performance gains are expected
to be negligible because this function was already
very low in the profiling data.

Related-To: https://bugzilla.redhat.com/1177634
Change-Id: If1ed3f49c236086c6184973762d9909d7f79bf7e
Signed-off-by: Francesco Romani <fromani@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/35140
Reviewed-by: Nir Soffer <nsoffer@redhat.com>
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/virt/vm.py
@@ -1770,6 +1770,7 @@ class Vm(object):
         but can change as result as interaction with libvirt (display*)
         """
         stats = {
+            'vmId': self.conf['vmId'],
             'pid': self.conf['pid'],
             'vmType': self.conf['vmType'],
             'kvmEnable': self._kvmEnable,

vdsm/API.py
@@ -368,7 +368,6 @@ class VM(APIBase):
         if runHooks:
             hooks.before_get_vm_stats()
         stats = v.getStats().copy()
-        stats['vmId'] = self._UUID
         if runHooks:
             stats = hooks.after_get_vm_stats([stats])[0]
         return {'status': doneCode, 'statsList': [stats]}
@@ -1306,12 +1305,7 @@ class Global(APIBase):
         Get statistics of all running VMs.
         """
         hooks.before_get_all_vm_stats()
-        vms = self.getVMList()
-        statsList = []
-        for s in vms['vmList']:
-            response = VM(s['vmId'])._getStats(runHooks=False)
-            if response:
-                statsList.append(response['statsList'][0])
+        statsList = self._cif.getAllVmStats()
         statsList = hooks.after_get_all_vm_stats(statsList)
         return {'status': doneCode, 'statsList': statsList}
 

vdsm/clientIF.py
@@ -389,6 +389,9 @@ class clientIF(object):
         vm.run()
         return {'status': doneCode, 'vmList': vm.status()}
 
+    def getAllVmStats(self):
+        return [v.getStats() for v in self.vmContainer.values()]
+
     @utils.traceback()
     def _recoverThread(self):
         # Trying to run recover process until it works. During that time vdsm

commit a07ba734455741a10e2ef8948b5f5d5df7d665dd

vm: add missing vm id to get stats of down vm
The stats that are returned for a VM which is down does not contains the
id of the VM.

This was broken by 7f577ab94b35f2cb2560d3ae49d6b0f753588474, which failed
to put the vmId in the 'Exited Vm Stats', but only in the 'Running Vm Stats'

That causes a regression since the engine uses this id,
without it hibernated VM is not destroyed, but remains down on the host.
Thus, adding the vm-id back.

Change-Id: I9534f2105b9a32ebd4b28dae42e265094a3655b9
Bug-Url: https://bugzilla.redhat.com/1191405
Signed-off-by: Arik Hadas <ahadas@redhat.com>
Reviewed-on: http://gerrit.ovirt.org/37694
Reviewed-by: Francesco Romani <fromani@redhat.com>
Reviewed-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/virt/vm.py
@@ -1756,6 +1756,7 @@ class Vm(object):
 
     def _getExitedVmStats(self):
         stats = {
+            'vmId': self.conf['vmId'],
             'exitCode': self.conf['exitCode'],
             'status': self.lastStatus,
             'exitMessage': self.conf['exitMessage'],