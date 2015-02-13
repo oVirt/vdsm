920eeb95f225c7790619423a69a904da70a8231e
Nir Soffer <nsoffer@redhat.com>
2015-02-14 01:08:46 +0200

    utils: Add memoized invalidation
    We learned in the hard way that memoizing and monkey-patching do work
    nicely together. However, disabling memoizing during the tests means
    that we do not test the same code in the application.
    
    This patch adds invalidate() method to memoized functions, so tests can
    clean up properly after they modify memozied data.
    
    This can also be used by application code to invalidate memoized
    functions in certain conditions.
    
    Change-Id: I3053b8ecc567f7c3154f7473dfd6b587823313ae
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1267851
    Reviewed-on: http://gerrit.ovirt.org/37788
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Reviewed-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/40029
    Tested-by: Francesco Romani <fromani@redhat.com>
    Continuous-Integration: Francesco Romani <fromani@redhat.com>

--- tests/utilsTests.py
+++ tests/utilsTests.py
@@ -18,6 +18,7 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
+import collections
 import os.path
 import contextlib
 import errno
@@ -712,3 +713,63 @@ import vmTestsData
         self.assertTrue(
             hack < base/2,
             "picklecopy [%f] not faster than deepcopy [%f]" % (hack, base))
+
+
+@expandPermutations
+class MemoizedTests(TestCaseBase):
+
+    def setUp(self):
+        self.values = {}
+        self.accessed = collections.defaultdict(int)
+
+    @permutations([[()], [("a",)], [("a", "b")]])
+    def test_memoized_method(self, args):
+        self.values[args] = 42
+        self.assertEqual(self.accessed[args], 0)
+        self.assertEqual(self.memoized_method(*args), 42)
+        self.assertEqual(self.accessed[args], 1)
+        self.assertEqual(self.memoized_method(*args), 42)
+        self.assertEqual(self.accessed[args], 1)
+
+    @permutations([[()], [("a",)], [("a", "b")]])
+    def test_memoized_function(self, args):
+        self.values[args] = 42
+        self.assertEqual(self.accessed[args], 0)
+        self.assertEqual(memoized_function(self, *args), 42)
+        self.assertEqual(self.accessed[args], 1)
+        self.assertEqual(memoized_function(self, *args), 42)
+        self.assertEqual(self.accessed[args], 1)
+
+    def test_key_error(self):
+        self.assertRaises(KeyError, self.memoized_method)
+        self.assertRaises(KeyError, self.memoized_method, "a")
+        self.assertRaises(KeyError, self.memoized_method, "a", "b")
+
+    def test_invalidate_method(self):
+        args = ("a",)
+        self.values[args] = 42
+        self.assertEqual(self.memoized_method(*args), 42)
+        self.memoized_method.invalidate()
+        self.assertEqual(self.memoized_method(*args), 42)
+        self.assertEqual(self.accessed[args], 2)
+
+    def test_invalidate_function(self):
+        args = ("a",)
+        self.values[args] = 42
+        self.assertEqual(memoized_function(self, *args), 42)
+        memoized_function.invalidate()
+        self.assertEqual(memoized_function(self, *args), 42)
+        self.assertEqual(self.accessed[args], 2)
+
+    @utils.memoized
+    def memoized_method(self, *args):
+        return self.get(args)
+
+    def get(self, key):
+        self.accessed[key] += 1
+        return self.values[key]
+
+
+@utils.memoized
+def memoized_function(test, *args):
+    return test.get(args)
