dcb2ca275cb440a11c1fd043085b9e19e8e92a5c
Francesco Romani <fromani@redhat.com>
2015-02-19 13:58:05 +0100

    tests: fix return value
    fix tests to conform to the new internal API.
    
    Change-Id: I6064c8a4ae363bae99691a0c596a0a473b50205f
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    Reviewed-on: http://gerrit.ovirt.org/37960
    Reviewed-by: Shahar Havivi <shavivi@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- tests/v2vTests.py
+++ tests/v2vTests.py
@@ -94,7 +94,7 @@ class v2vTests(TestCaseBase):
             raise SkipTest('v2v is not supported current os version')
 
         vms = v2v.get_external_vms('esx://mydomain', 'user',
-                                   'password')
+                                   'password')['vmList']
         self.assertEquals(len(vms), 1)
         vm = vms[0]
         self.assertEquals(vm['vmId'], '564d7cb4-8e3d-06ec-ce82-7b2b13c6a611')
