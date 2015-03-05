9ba66c903083e32cdabebb11ed59725e84b22253
Vinzenz Feenstra <vfeenstr@redhat.com>
2015-03-05 14:18:21 +0100

    virt: Additional reporting of CPU usage in ns
    As per request, we're now additionally reporting the VM CPU usage
    in nanoseconds
    
    Change-Id: I31fb4305511f0c2174b217ac24f0d96767c31803
    Bug-Url: https://bugzilla.redhat.com/1172153
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/38412
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/virt/vmstats.py
+++ vdsm/virt/vmstats.py
@@ -75,6 +75,8 @@ def cpu(stats, first_sample, last_sample, interval):
     if first_sample is None or last_sample is None:
         return
 
+    stats['cpuUsage'] = str(last_sample['system_time']
+                            + last_sample['user_time'])
     try:
         stats['cpuSys'] = _usage_percentage(
             _diff(last_sample, first_sample, 'cpu.user') +
