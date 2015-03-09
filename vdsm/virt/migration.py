33fdb823aa2b5dd2eacc6457c91d6d10b7e14e6a
Vinzenz Feenstra <vfeenstr@redhat.com>
2015-03-09 14:30:12 +0100

    virt: Log the the duration time of the migration
    For post mortem analysis in regards to migration times, this patchset
    will additionally log the time the successful migration took.
    This will help us in future to determine how long migrations took on a
    certain system.
    
    Change-Id: I6415dd159c55633e6ed5fd395f6fac38aca69a63
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/38482
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/virt/migration.py
+++ vdsm/virt/migration.py
@@ -321,6 +321,9 @@ class SourceThread(threading.Thread):
                     # we need to support python 2.6, so two nested with-s.
                     self._perform_migration(duri, muri)
 
+            self.log.info("migration took %d seconds to complete",
+                          (time.time() - startTime) + destCreationTime)
+
     def _perform_migration(self, duri, muri):
         if self._vm.hasSpice and self._vm.conf.get('clientIp'):
             SPICE_MIGRATION_HANDOVER_TIME = 120
