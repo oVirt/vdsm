cf7248fdfe93aa08d686a87435d9c9a826e4ce28
Martin Sivak <msivak@redhat.com>
2015-03-31 17:26:05 +0200

    Fix the CPU quota MOM policy computations
    This changes the way we compute period and quota to what
    is described in the documentation:
    
    The maximum CPU load percentage is related to the total computing
    power the host has.
    
    Two issues are also fixed by this:
    
    - the period could get below 1000 with low enough percentage
      or high enough vCPU count and libvirt won't accept that
    
    - the quota number was higher than period when vCPU count was
      lower than the number of physical CPUs, effectively disabling
      the CPU limits
    
    An additional improvement in performance should be visible for
    VMs that do not use cpu limiting (using 100% as the limit),
    because the policy disables cgroups limit tracking in that case.
    
    Change-Id: Ia1f014d1b4058ef447a962cbda3336115e8610dc
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1213438
    Signed-off-by: Martin Sivak <msivak@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/39411
    Reviewed-by: Adam Litke <alitke@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/40311
    Reviewed-by: Yaniv Bronhaim <ybronhei@redhat.com>
    Continuous-Integration: Yaniv Bronhaim <ybronhei@redhat.com>

---
+++ tests/momPolicyTests.py
@@ -0,0 +1,139 @@
+import os.path
+import ConfigParser
+
+from mom.Policy.Policy import Policy
+from mom.Entity import Entity
+from mom.Monitor import Monitor
+from unittest import TestCase
+
+# This is a very hacky way of implementing the test scenario
+# we should update MOM to offer test capability and use it here.
+# Unfortunately bug #1207610 has very high priority and won't
+# wait for MOM to appear in Fedora repositories
+# TODO replace with proper test API once it appears
+
+
+class MomPolicyTests(TestCase):
+    def _getPolicyContent(self, name):
+        path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                            "../vdsm/mom.d",
+                            name)
+        return open(path, "r").read()
+
+    def _loadPolicyFile(self, policy, filename):
+        """Load MOM policy from vdsm/mom.d/+filename and apply it
+           under the 'basename without extension' policy name.
+
+           Example:
+           00-constants.policy is loaded from vdsm/mom.d/00-constants.policy
+           and inserted as 00-costants policy.
+        """
+
+        policy_string = self._getPolicyContent(filename)
+        policy_name = os.path.splitext(os.path.basename(filename))[0]
+        self.assertTrue(policy.set_policy(policy_name, policy_string))
+
+    def _prepareEntity(self, name, data):
+        cfg = ConfigParser.SafeConfigParser()
+        cfg.add_section("__int__")
+        cfg.set("__int__", "plot-subdir", "")
+
+        ent = Entity(Monitor(cfg, name))
+        ent.statistics.append(data)
+        ent.monitor.fields = set(ent.statistics[-1].keys())
+        ent.monitor.optional_fields = []
+        ent._finalize()
+
+        return ent
+
+    def testCpuTuneBasicTest(self):
+        p = Policy()
+
+        host = self._prepareEntity("host", {
+            "cpu_count": 1
+        })
+
+        vm = self._prepareEntity("vm", {
+            "vcpu_count": 1,
+            "vcpu_user_limit": 50,
+
+            "vcpu_quota": None,
+            "vcpu_period": None
+        })
+
+        self._loadPolicyFile(p, "00-defines.policy")
+        self._loadPolicyFile(p, "04-cputune.policy")
+
+        p.evaluate(host, [vm])
+
+        self.assertEqual(vm.controls["vcpu_quota"], 50000)
+        self.assertEqual(vm.controls["vcpu_period"], 100000)
+
+    def testCpuTuneHundredCpus(self):
+        p = Policy()
+
+        host = self._prepareEntity("host", {
+            "cpu_count": 120
+        })
+
+        vm = self._prepareEntity("vm", {
+            "vcpu_count": 100,
+            "vcpu_user_limit": 50,
+
+            "vcpu_quota": None,
+            "vcpu_period": None
+        })
+
+        self._loadPolicyFile(p, "00-defines.policy")
+        self._loadPolicyFile(p, "04-cputune.policy")
+
+        p.evaluate(host, [vm])
+
+        self.assertEqual(vm.controls["vcpu_quota"], 60000)
+        self.assertEqual(vm.controls["vcpu_period"], 100000)
+
+    def testCpuTuneNoLimit(self):
+        p = Policy()
+
+        host = self._prepareEntity("host", {
+            "cpu_count": 120
+        })
+
+        vm = self._prepareEntity("vm", {
+            "vcpu_count": 100,
+            "vcpu_user_limit": 100,
+
+            "vcpu_quota": None,
+            "vcpu_period": None
+        })
+
+        self._loadPolicyFile(p, "00-defines.policy")
+        self._loadPolicyFile(p, "04-cputune.policy")
+
+        p.evaluate(host, [vm])
+
+        self.assertEqual(vm.controls["vcpu_quota"], -1)
+        self.assertEqual(vm.controls["vcpu_period"], 100000)
+
+    def testCpuTuneTooSmall(self):
+        p = Policy()
+
+        host = self._prepareEntity("host", {
+            "cpu_count": 1
+        })
+
+        vm = self._prepareEntity("vm", {
+            "vcpu_count": 100,
+            "vcpu_user_limit": 10,
+
+            "vcpu_quota": None,
+            "vcpu_period": None
+        })
+
+        self._loadPolicyFile(p, "00-defines.policy")
+        self._loadPolicyFile(p, "04-cputune.policy")
+
+        p.evaluate(host, [vm])
+
+        self.assertEqual(vm.controls["vcpu_quota"], 1100)
+        self.assertEqual(vm.controls["vcpu_period"], 1100000)
