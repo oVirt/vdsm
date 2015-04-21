oVirt/vdsm

commit b25dbdcce417f366e0654c2c8456e8f0d0e3059e

Have the protocol detector use the Reactor
The current implementation duplicates a lot of the code in the reactor.
This removes a lot of the duplicate code and prepares the ground for a
single reactor for VDSM.

Change-Id: If1cc2a205cdad335e8c3af4a77ed49c8977a79ba
Signed-off-by: Saggi Mizrahi <smizrahi@redhat.com>
Signed-off-by: pkliczewski <piotr.kliczewski@gmail.com>
Reviewed-on: https://gerrit.ovirt.org/37098
Reviewed-by: Yaniv Bronhaim <ybronhei@redhat.com>
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/protocoldetector.py
@@ -18,17 +18,33 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
-import errno
 import logging
-import os
-import select
 import socket
-import time
-
-from M2Crypto import SSL
 
 from vdsm.utils import traceback
 import vdsm.infra.filecontrol as filecontrol
+from yajsonrpc.betterAsyncore import (
+    Dispatcher,
+    Reactor,
+)
+
+from vdsm.utils import monotonic_time
+from vdsm.sslutils import SSLHandshakeDispatcher
+
+
+def _create_socket(host, port):
+    addr = socket.getaddrinfo(host, port, socket.AF_INET,
+                              socket.SOCK_STREAM)
+    if not addr:
+        raise socket.error("Could not translate address '%s:%s'"
+                           % (host, str(port)))
+
+    server_socket = socket.socket(addr[0][0], addr[0][1], addr[0][2])
+    filecontrol.set_close_on_exec(server_socket.fileno())
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(addr[0][4])
+
+    return server_socket
 
 
 def _is_handshaking(sock):
@@ -36,6 +52,75 @@ def _is_handshaking(sock):
         return False
 
     return sock.is_handshaking
+
+
+class _AcceptorImpl(object):
+    log = logging.getLogger("ProtocolDetector.AcceptorImpl")
+
+    def __init__(self, dispatcher_factory):
+        self._dispatcher_factory = dispatcher_factory
+
+    def readable(self, dispatcher):
+        return True
+
+    def handle_accept(self, dispatcher):
+        try:
+            client, _ = dispatcher.socket.accept()
+        except socket.error:
+            pass
+        else:
+            client.setblocking(0)
+            self.log.info("Accepting connection from %s:%d",
+                          *client.getpeername())
+            self._dispatcher_factory(client)
+
+
+class _ProtocolDetector(object):
+    log = logging.getLogger("ProtocolDetector.Detector")
+
+    def __init__(self, detectors, timeout=None):
+        self._detectors = detectors
+        self._required_size = max(h.REQUIRED_SIZE for h in self._detectors)
+        self.log.debug("Using required_size=%d", self._required_size)
+        self._give_up_at = monotonic_time() + timeout
+
+    def readable(self, dispatcher):
+        return True
+
+    def next_check_interval(self):
+        return min(self._give_up_at - monotonic_time(), 0)
+
+    def handle_read(self, dispatcher):
+        sock = dispatcher.socket
+        try:
+            data = sock.recv(self._required_size, socket.MSG_PEEK)
+        except socket.error, why:
+            if why.args[0] == socket.EWOULDBLOCK:
+                return
+            dispatcher.handle_error()
+            return
+
+        if len(data) < self._required_size:
+            return
+
+        if monotonic_time() > self._give_up_at:
+            self.log.debug("Timed out while waiting for data")
+            dispatcher.close()
+
+        for detector in self._detectors:
+            if detector.detect(data):
+                host, port = sock.getpeername()
+                self.log.info(
+                    "Detected protocol %s from %s:%d",
+                    detector.NAME,
+                    host,
+                    port
+                )
+                detector.handle_dispatcher(dispatcher, (host, port))
+                break
+        else:
+            self.log.warning("Unrecognized protocol: %r", data)
+            dispatcher.close()
 
 
 class MultiProtocolAcceptor:
@@ -60,95 +145,70 @@ class MultiProtocolAcceptor:
             Given first bytes read from the connection, try to detect the
             protocol. Returns True if protocol is detected.
 
-        def handleSocket(self, client_socket, socket_address):
+        def handle_dispatcher(self, client_dispatcher, socket_address):
             Called after detect() succeeded. The detector owns the socket and
-            is responsible for closing it.
+            is responsible for closing it or changing the implementation.
     """
     log = logging.getLogger("vds.MultiProtocolAcceptor")
 
-    CLEANUP_INTERVAL = 30.0
-
-    def __init__(self, host, port, sslctx=None):
+    def __init__(
+        self,
+        host,
+        port,
+        sslctx=None,
+        ssl_hanshake_timeout=SSLHandshakeDispatcher.SSL_HANDSHAKE_TIMEOUT,
+    ):
         self._sslctx = sslctx
-        self._host = host
-        self._port = port
-
-        self._read_fd, self._write_fd = os.pipe()
-        filecontrol.set_non_blocking(self._read_fd)
-        filecontrol.set_close_on_exec(self._read_fd)
-        filecontrol.set_non_blocking(self._write_fd)
-        filecontrol.set_close_on_exec(self._write_fd)
-
-        self._socket = self._create_socket(host, port)
-        self._poller = select.poll()
-        self._poller.register(self._socket, select.POLLIN)
-        self._poller.register(self._read_fd, select.POLLIN)
-        self._pending_connections = {}
+        self._reactor = Reactor()
+        sock = _create_socket(host, port)
+        self._host, self._port = sock.getsockname()
+        self.log.info("Listening at %s:%d", self._host, self._port)
+        self._acceptor = Dispatcher(
+            _AcceptorImpl(
+                self.handle_accept
+            ),
+            sock,
+        )
+        self._acceptor.listen(5)
+        self._reactor.add_dispatcher(self._acceptor)
         self._handlers = []
-        # Initialize *before* starting to serve, to make it easier to test the
-        # cleanup logic.
-        self._next_cleanup = time.time() + self.CLEANUP_INTERVAL
-        self._required_size = None
+        self.TIMEOUT = ssl_hanshake_timeout
+
+    def handle_accept(self, client):
+        if self._sslctx is None:
+            self._reactor.add_dispatcher(
+                self._register_protocol_detector(
+                    Dispatcher(
+                        sock=client,
+                    ),
+                ),
+            )
+        else:
+            self._reactor.add_dispatcher(
+                Dispatcher(
+                    SSLHandshakeDispatcher(
+                        self._sslctx,
+                        self._register_protocol_detector,
+                        self.TIMEOUT,
+                    ),
+                    client,
+                ),
+            )
+
+    def _register_protocol_detector(self, dispatcher):
+        dispatcher.switch_implementation(
+            _ProtocolDetector(
+                self._handlers,
+                self.TIMEOUT,
+            ),
+        )
+
+        return dispatcher
 
     @traceback(on=log.name)
     def serve_forever(self):
         self.log.debug("Running")
-        self._required_size = max(h.REQUIRED_SIZE for h in self._handlers)
-        self.log.debug("Using required_size=%d", self._required_size)
-        try:
-            while True:
-                try:
-                    self._process_events()
-                except _Stopped:
-                    break
-                except Exception:
-                    self.log.exception("Unhandled exception")
-        finally:
-            self._cleanup()
-
-    def _process_events(self):
-        seconds = max(self._next_cleanup - time.time(), 0)
-        events = self._poller.poll(seconds * 1000)
-
-        for fd, event in events:
-            if event & select.POLLIN:
-                if fd is self._read_fd:
-                    self._maybe_stop()
-                elif fd is self._socket.fileno():
-                    self._accept_connection()
-                else:
-                    self._handle_connection_read(fd)
-            if fd in self._pending_connections and event & select.POLLOUT:
-                self._handle_connection_write(fd)
-
-        now = time.time()
-        if now > self._next_cleanup:
-            self._next_cleanup += self.CLEANUP_INTERVAL
-            self._cleanup_pending_connections()
-
-    def _cleanup(self):
-        self.log.debug("Cleaning up")
-
-        for _, client_socket in self._pending_connections.values():
-            self._close_connection(client_socket)
-
-        self._poller.unregister(self._socket)
-        self._poller.unregister(self._read_fd)
-        self._socket.close()
-        os.close(self._read_fd)
-        os.close(self._write_fd)
-
-    def _cleanup_pending_connections(self):
-        now = time.time()
-        for accepted, client_socket in self._pending_connections.values():
-            if now - accepted > self.CLEANUP_INTERVAL:
-                self._close_connection(client_socket)
-
-    def detect_protocol(self, data):
-        for handler in self._handlers:
-            if handler.detect(data):
-                return handler
-        raise _CannotDetectProtocol()
+        self._reactor.process_requests()
 
     def add_detector(self, detector):
         self.log.debug("Adding detector %s", detector)
@@ -156,127 +216,7 @@ class MultiProtocolAcceptor:
 
     def stop(self):
         self.log.debug("Stopping Acceptor")
-        while True:
-            try:
-                os.write(self._write_fd, "1")
-            except OSError as e:
-                if e.errno in (errno.EPIPE, errno.EBADF):
-                    # Detector already stopped
-                    return
-                if e.errno != errno.EINTR:
-                    raise
-            else:
-                break
-
-    def _maybe_stop(self):
-        try:
-            if os.read(self._read_fd, 1) == '1':
-                raise _Stopped()
-        except OSError as e:
-            if e.errno not in (errno.EAGAIN, errno.EWOULDBLOCK):
-                raise
-
-    def _accept_connection(self):
-        client_socket, address = self._socket.accept()
-        if self._sslctx:
-            client_socket = self._sslctx.wrapSocket(client_socket)
-            # Older versions of M2Crypto ignore nbio and retry internally
-            # if timeout is set.
-            client_socket.settimeout(None)
-            client_socket.address = address
-            try:
-                client_socket.setup_ssl()
-                client_socket.set_accept_state()
-            except SSL.SSLError as e:
-                self.log.warning("Error setting up ssl: %s", e)
-                client_socket.close()
-                return
-
-            client_socket.is_handshaking = True
-
-        self._add_connection(client_socket)
-
-    def _add_connection(self, socket):
-        host, port = socket.getpeername()
-        self.log.debug("Adding connection %s:%d", host, port)
-        socket.setblocking(0)
-        self._pending_connections[socket.fileno()] = (time.time(),
-                                                      socket)
-        if _is_handshaking(socket):
-            self._poller.register(socket, select.POLLIN | select.POLLOUT)
-        else:
-            self._poller.register(socket, select.POLLIN)
-
-    def _remove_connection(self, socket):
-        self._poller.unregister(socket)
-        del self._pending_connections[socket.fileno()]
-        socket.setblocking(1)
-        host, port = socket.getpeername()
-        self.log.debug("Removing connection %s:%d", host, port)
-
-    def _close_connection(self, socket):
-        self._remove_connection(socket)
-        socket.close()
-
-    def _process_handshake(self, socket):
-        try:
-            socket.is_handshaking = (socket.accept_ssl() == 0)
-        except Exception as e:
-            self.log.debug("Error during handshake: %s", e)
-            self._close_connection(socket)
-        else:
-            if not socket.is_handshaking:
-                self._poller.modify(socket, select.POLLIN)
-
-    def _handle_connection_write(self, fd):
-        _, client_socket = self._pending_connections[fd]
-        if _is_handshaking(client_socket):
-            self._process_handshake(client_socket)
-
-    def _handle_connection_read(self, fd):
-        _, client_socket = self._pending_connections[fd]
-        if _is_handshaking(client_socket):
-            self._process_handshake(client_socket)
-            return
-
-        try:
-            data = client_socket.recv(self._required_size, socket.MSG_PEEK)
-        except socket.error as e:
-            if e.errno not in (errno.EAGAIN, errno.EWOULDBLOCK):
-                self.log.warning("Unable to read data: %s", e)
-                self._close_connection(client_socket)
-            return
-
-        if data is None:
-            # None is returned when ssl socket needs to read more data
-            return
-
-        try:
-            handler = self.detect_protocol(data)
-        except _CannotDetectProtocol:
-            self.log.warning("Unrecognized protocol: %r", data)
-            self._close_connection(client_socket)
-        else:
-            host, port = client_socket.getpeername()
-            self.log.debug("Detected protocol %s from %s:%d",
-                           handler.NAME, host, port)
-            self._remove_connection(client_socket)
-            handler.handleSocket(client_socket, (host, port))
-
-    def _create_socket(self, host, port):
-        addr = socket.getaddrinfo(host, port, socket.AF_INET,
-                                  socket.SOCK_STREAM)
-        if not addr:
-            raise Exception("Could not translate address '%s:%s'"
-                            % (self._host, str(self._port)))
-        server_socket = socket.socket(addr[0][0], addr[0][1], addr[0][2])
-        filecontrol.set_close_on_exec(server_socket.fileno())
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind(addr[0][4])
-        server_socket.listen(5)
-
-        server_socket.setblocking(0)
-        return server_socket
+        self._reactor.stop()
 
 
 class _CannotDetectProtocol(Exception):

tests/protocoldetectorTests.py
@@ -23,21 +23,12 @@ import socket
 import ssl
 import threading
 import time
-from contextlib import contextmanager, closing
-
-import protocoldetector
+from contextlib import contextmanager
 
 from vdsm import sslutils
+from protocoldetector import MultiProtocolAcceptor
 from sslhelper import KEY_FILE, CRT_FILE
 from testlib import VdsmTestCase, expandPermutations, permutations
-
-
-class TestingAcceptor(protocoldetector.MultiProtocolAcceptor):
-    """
-    Acceptor with shorter cleanup interval to make it practical to test the
-    cleanup logic.
-    """
-    CLEANUP_INTERVAL = 1.0
 
 
 class Detector(object):
@@ -53,15 +44,19 @@ class Detector(object):
     def detect(self, data):
         return data.startswith(self.NAME)
 
-    def handleSocket(self, client_socket, socket_address):
+    def handle_dispatcher(self, dispatcher, socket_address):
+        client_socket = dispatcher.socket
+        dispatcher.del_channel()
+
         def run():
             try:
-                assert client_socket.gettimeout() is None
-                rfile = client_socket.makefile('rb', -1)
-                with closing(rfile):
-                    request = rfile.readline()
-                    response = self.response(request)
-                    client_socket.sendall(response)
+                request = ""
+                while "\n" not in request:
+                    request += dispatcher.recv(1024)
+
+                response = self.response(request)
+                client_socket.setblocking(1)
+                client_socket.sendall(response)
             finally:
                 client_socket.shutdown(socket.SHUT_RDWR)
                 client_socket.close()
@@ -179,14 +174,14 @@ class AcceptorTests(VdsmTestCase):
 
     def check_slow_client(self, use_ssl):
         with self.connect(use_ssl) as client:
-            time.sleep(self.acceptor.CLEANUP_INTERVAL - self.GRACETIME)
+            time.sleep(self.acceptor.TIMEOUT - self.GRACETIME)
             data = "echo let me in\n"
             client.sendall(data)
             self.assertEqual(client.recv(self.BUFSIZE), data)
 
     def check_very_slow_client(self, use_ssl):
         with self.connect(use_ssl) as client:
-            time.sleep(self.acceptor.CLEANUP_INTERVAL * 2 + self.GRACETIME)
+            time.sleep(self.acceptor.TIMEOUT * 2 + self.GRACETIME)
             client.sendall("echo too slow probably\n")
             self.check_disconnected(client)
 
@@ -201,11 +196,12 @@ class AcceptorTests(VdsmTestCase):
     # Helpers
 
     def start_acceptor(self, use_ssl):
-        self.acceptor = TestingAcceptor(
+        self.acceptor = MultiProtocolAcceptor(
             '127.0.0.1', 0, sslctx=self.SSLCTX if use_ssl else None)
+        self.acceptor.TIMEOUT = 1
         self.acceptor.add_detector(Echo())
         self.acceptor.add_detector(Uppercase())
-        self.acceptor_address = self.acceptor._socket.getsockname()
+        self.acceptor_address = self.acceptor._acceptor.socket.getsockname()
         t = threading.Thread(target=self.acceptor.serve_forever)
         t.deamon = True
         t.start()

lib/vdsm/sslutils.py
@@ -18,9 +18,13 @@
 # Refer to the README and COPYING files for full details of the license
 #
 import httplib
+import logging
 import socket
 import ssl
 import xmlrpclib
+from vdsm.utils import (
+    monotonic_time,
+)
 
 from M2Crypto import SSL, X509, threading
 
@@ -36,7 +40,7 @@ threading.init()
 class SSLSocket(object):
     def __init__(self, connection):
         self.connection = connection
-        self._data = None
+        self._data = ''
 
     def gettimeout(self):
         return self.connection.socket.gettimeout()
@@ -58,12 +62,14 @@ class SSLSocket(object):
     def read(self, size=4096, flag=None):
         result = None
         if flag == socket.MSG_PEEK:
-            self._data = self.connection.read(size)
+            bytes_left = size - len(self._data)
+            if bytes_left > 0:
+                self._data += self.connection.read(bytes_left)
             result = self._data
         else:
             if self._data:
                 result = self._data
-                self._data = None
+                self._data = ''
             else:
                 result = self.connection.read(size)
         return result
@@ -253,3 +259,75 @@ class VerifyingHTTPS(httplib.HTTPS):
         # here for compatibility with post-1.5.2 CVS.
         self.key_file = key_file
         self.cert_file = cert_file
+
+
+class SSLHandshakeDispatcher(object):
+    """
+    SSLHandshakeDispatcher is dispatcher implementation to process ssl
+    handshake in asynchronous way. Once we are done with handshaking we
+    we need to swap our dispatcher implementation with message processing
+    dispatcher. We use handshake_finished_handler function to perform
+    swapping. The handler implementation need to invoke
+
+    dispatcher.switch_implementation()
+
+    where we provide message processing dispatcher as parameter.
+    """
+    log = logging.getLogger("ProtocolDetector.SSLHandshakeDispatcher")
+    SSL_HANDSHAKE_TIMEOUT = 10
+
+    def __init__(
+        self,
+        sslctx,
+        handshake_finished_handler,
+        handshake_timeout=SSL_HANDSHAKE_TIMEOUT,
+    ):
+        self._give_up_at = monotonic_time() + handshake_timeout
+        self._has_been_set_up = False
+        self._is_handshaking = True
+        self._sslctx = sslctx
+        self._handshake_finished_handler = handshake_finished_handler
+
+    def _set_up_socket(self, dispatcher):
+        client_socket = dispatcher.socket
+        client_socket = self._sslctx.wrapSocket(client_socket)
+        # Older versions of M2Crypto ignore nbio and retry internally
+        # if timeout is set.
+        client_socket.settimeout(None)
+        client_socket.address = client_socket.getpeername()
+        try:
+            client_socket.setup_ssl()
+            client_socket.set_accept_state()
+        except SSL.SSLError as e:
+            self.log.error("Error setting up ssl: %s", e)
+            dispatcher.close()
+            return
+
+        dispatcher.socket = client_socket
+
+    def next_check_interval(self):
+        return min(self._give_up_at - monotonic_time(), 0)
+
+    def readable(self, dispatcher):
+        if self.has_expired():
+            dispatcher.close()
+            return False
+
+        return True
+
+    def has_expired(self):
+        return monotonic_time() > self._give_up_at
+
+    def handle_read(self, dispatcher):
+        if not self._has_been_set_up:
+            self._set_up_socket(dispatcher)
+
+        if self._is_handshaking:
+            try:
+                self._is_handshaking = (dispatcher.socket.accept_ssl() == 0)
+            except Exception as e:
+                self.log.error("Error during handshake: %s", e)
+                dispatcher.close()
+
+        if not self._is_handshaking:
+            self._handshake_finished_handler(dispatcher)

lib/yajsonrpc/__init__.py
@@ -22,7 +22,7 @@ from vdsm.compat import json
 
 from vdsm.utils import traceback
 
-__all__ = ["tcpReactor"]
+__all__ = ["betterAsyncore", "stompReactor", "stomp"]
 
 _STATE_INCOMING = 1
 _STATE_OUTGOING = 2

lib/yajsonrpc/betterAsyncore.py
@@ -25,17 +25,10 @@ from vdsm.infra.eventfd import EventFD
 
 
 class Dispatcher(asyncore.dispatcher):
-    def __init__(self, impl, sock=None, map=None):
-        self.__impl = impl
+    def __init__(self, impl=None, sock=None, map=None):
         asyncore.dispatcher.__init__(self, sock=sock, map=map)
-
-        try:
-            impl.init(self)
-        except AttributeError:
-            # impl.init() is optional.
-            pass
-
-        self._bind_implementation()
+        if impl is not None:
+            self.switch_implementation(impl)
 
     def _bind_implementation(self):
         for attr_name in (
@@ -68,6 +61,14 @@ class Dispatcher(asyncore.dispatcher):
                 )
             )
 
+    def switch_implementation(self, impl):
+        self.__impl = impl
+
+        if hasattr(impl, 'init'):
+            impl.init(self)
+
+        self._bind_implementation()
+
     def next_check_interval(self):
         """
         Return the relative timeout wanted between poller refresh checks
@@ -79,7 +80,6 @@ class Dispatcher(asyncore.dispatcher):
         Note that this value is a recommendation only.
         """
         default_func = lambda: None
-
         return getattr(self.__impl, "next_check_interval", default_func)()
 
     def handle_read_event(self):
@@ -96,7 +96,7 @@ class Dispatcher(asyncore.dispatcher):
         # we need to check whether there is pending function on
         # our fd because we use eventfds which do not provide it
         if hasattr(self.socket, "pending"):
-            while self.socket.pending() > 0:
+            while self.socket.pending() > 0 and self.connected:
                 self.handle_read()
 
     def recv(self, buffer_size):
@@ -111,7 +111,9 @@ class Dispatcher(asyncore.dispatcher):
                 return data
         except socket.error, why:
             # winsock sometimes raises ENOTCONN
-            if why.args[0] in asyncore._DISCONNECTED:
+            if why.args[0] == EWOULDBLOCK:
+                return None
+            elif why.args[0] in asyncore._DISCONNECTED:
                 self.handle_close()
                 return ''
             else:
@@ -180,6 +182,7 @@ class Reactor(object):
 
     def add_dispatcher(self, disp):
         disp.add_channel(self._map)
+        disp._map = self._map
 
     def remove_dispatcher(self, disp):
         disp.del_channel(self._map)
@@ -201,7 +204,7 @@ class Reactor(object):
 
     def _get_timeout(self, map):
         timeout = None
-        for disp in map.itervalues():
+        for disp in self._map.values():
             if hasattr(disp, "next_check_interval"):
                 interval = disp.next_check_interval()
                 if interval is not None and interval >= 0:

lib/yajsonrpc/stompReactor.py
@@ -273,6 +273,8 @@ class StompDetector():
     def detect(self, data):
         return data.startswith(stomp.COMMANDS)
 
-    def handleSocket(self, client_socket, socket_address):
+    def handle_dispatcher(self, dispatcher, socket_address):
+        client_socket = dispatcher.socket
+        dispatcher.del_channel()
         self.json_binding.add_socket(self._reactor, client_socket)
         self.log.debug("Stomp detected from %s", socket_address)

tests/jsonRpcHelper.py
@@ -24,7 +24,6 @@ import threading
 
 from xmlrpclib import Transport, dumps, Fault
 from contextlib import contextmanager
-from functools import partial
 from itertools import product
 from M2Crypto import SSL
 from rpc.BindingXMLRPC import BindingXMLRPC, XmlDetector
@@ -110,9 +109,12 @@ def constructClient(log, bridge, ssl, type):
             t.start()
             client = cpool.createClient
 
-        _, port = acceptor._socket.getsockname()
-        clientFactory = partial(client, create_socket(sslctx, acceptor._host,
-                                                      port))
+        def clientFactory():
+            return client(create_socket(
+                sslctx,
+                acceptor._host,
+                acceptor._port
+            ))
 
         yield clientFactory
 

vdsm/rpc/BindingXMLRPC.py
@@ -1194,6 +1194,8 @@ class XmlDetector():
         return (data.startswith("PUT /") or data.startswith("GET /") or
                 data.startswith("POST /"))
 
-    def handleSocket(self, client_socket, socket_address):
+    def handle_dispatcher(self, dispatcher, socket_address):
+        client_socket = dispatcher.socket
+        dispatcher.del_channel()
         self.xml_binding.add_socket(client_socket, socket_address)
         self.log.debug("xml over http detected from %s", socket_address)

commit 5b35dbf3375a2a264b6008b744543cd69ca13930

protocoldetector: Make socket blocking after detection
Commit b25dbdcce4 (Have the protocol detector use the Reactor)
introduced a regression, passing non-blocking sockets to the xmlrpc
server and the tests.

Before this commit, a socket was set to non-blocking mode when entering
to the protocol detector, and was set to blocking mode when exiting the
protocol detector.

The xmlrpc server used to work by accident, because we use a timeout in
the request handler, which set timeout on a socket, switching it to
non-blocking mode. The jsonrpc server needs non-blocking socket, but it
makes the socket non-blocking when accepting a socket.

This patch restores the previous behavior, setting the socket to
blocking mode when protocol detector pass the socket to the detector.

Change-Id: I25b1bc6aa8e06cbda63b2ba2a113e7db7b245f2b
Signed-off-by: Nir Soffer <nsoffer@redhat.com>
Reviewed-on: https://gerrit.ovirt.org/39746
Reviewed-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>
Reviewed-by: Dan Kenigsberg <danken@redhat.com>

vdsm/protocoldetector.py
@@ -120,6 +120,7 @@ class _ProtocolDetector(object):
                     port
                 )
                 dispatcher.del_channel()
+                sock.setblocking(1)
                 detector.handle_socket(sock, (host, port))
                 break
         else:

tests/protocoldetectorTests.py
@@ -59,7 +59,6 @@ class Detector(object):
                     request += chunk
 
                 response = self.response(request)
-                client_socket.setblocking(1)
                 client_socket.sendall(response)
             finally:
                 client_socket.shutdown(socket.SHUT_RDWR)