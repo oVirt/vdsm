c1c23a03bd3a3688f0cc5cbebf590cc6e8614624
Nir Soffer <nsoffer@redhat.com>
2015-05-08 12:28:05 +0300

    properties: Add properties module
    Properties are reusable objects similar to builtin property() function,
    adding input validation and initialization guarantees.
    
    Examples
    
        class Foo(properties.Owner):
    
            uuid = properties.UUID(required=True)
            format = properties.Enum(values=("cow", "raw"), required=True)
            size = properties.Integer(minval=0)
            name = properties.String()
    
            def __init__(self, uuid, format, size=0):
                self.uuid = uuid
                self.format = format
                self.size = size
    
    Creating an object with invalid properties or with uninitialized
    required properties would raise:
    
        Foo("not-a-uuid", "raw")
        Foo("49d8842d-43e8-4c33-b588-b5538df4ed8a", "invalid format")
        Foo("49d8842d-43e8-4c33-b588-b5538df4ed8a", "raw", size=-1)
    
    Attempting to set invalid value would raise:
    
        foo = Foo("49d8842d-43e8-4c33-b588-b5538df4ed8a", "raw")
        foo.size = -1  # Raises ValueError
    
    Accessing uninitialized properties does not raise AttributeError.
    Instead, the default value is returned:
    
        foo.name  # None
    
    Change-Id: I62175172dddce7f84319e3c8669747994e06a697
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/40822
    Continuous-Integration: Jenkins CI
    Tested-by: Adam Litke <alitke@redhat.com>
    Reviewed-by: Adam Litke <alitke@redhat.com>

---
+++ lib/vdsm/properties.py
@@ -0,0 +1,264 @@
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+"""
+properties - reusable properties
+
+Properties are reusable objects similar to builtin property() function, adding
+input validation and initialization grantees.
+
+
+This module provides the following properties:
+
+  Boolean       keeps a boolean value
+  Enum          keeps one of values from the values list
+  Float         keeps a floating point value within limits
+  Integer       keeps integer value within limits
+  String        keeps string value
+  UUID          keeps a UUID string
+  Password      keeps a ProtectedPassword object
+
+
+Common property attributes:
+
+  required      Ensure that a property is initialized in __init__
+  default       Default value if property is not set (None)
+  doc           Property docstring, accessible via help()
+
+
+Examples
+
+    class Foo(properties.Owner):
+
+        uuid = properties.UUID(required=True)
+        format = properties.Enum(values=("cow", "raw"), required=True)
+        size = properties.Integer(minval=0)
+        name = properties.String()
+
+        def __init__(self, uuid, format, size=0):
+            self.uuid = uuid
+            self.format = format
+            self.size = size
+
+Note that you must inherit from properties.Owner, which implements the required
+magic for naming properties and checking required properties after an object is
+created.
+
+Given Foo definition, it cannot be created with missing or invalid properties.
+The following expression would raise ValueError:
+
+    Foo("not-a-uuid", "raw")
+    Foo("49d8842d-43e8-4c33-b588-b5538df4ed8a", "other")
+    Foo("49d8842d-43e8-4c33-b588-b5538df4ed8a", "raw", size=-1)
+
+After creation, you cannot set invalid values:
+
+    f = Foo("49d8842d-43e8-4c33-b588-b5538df4ed8a", "raw")
+    f.size = -1  # Will raise ValueError
+
+Note that "name" was not initialized in __init__, but it is defined as
+property, so the attribute exists, returning None:
+
+    f = Foo("49d8842d-43e8-4c33-b588-b5538df4ed8a", "raw")
+    f.name  # None
+
+"""
+
+from __future__ import absolute_import
+
+import base64
+import uuid
+
+from vdsm.password import ProtectedPassword
+
+
+class Property(object):
+
+    def __init__(self, required=False, default=None, doc=None):
+        self.name = None
+        self.required = required
+        self.default = default
+        self.__doc__ = doc
+
+    def __get__(self, obj, objtype=None):
+        """
+        Called when getting a property value.
+        """
+        if obj is None:
+            return self  # Call from the class
+        return obj.__dict__.get(self.name, self.default)
+
+    def __set__(self, obj, value):
+        """
+        Called when assigning a value to a property.
+        """
+        if self.required and value is None:
+            raise ValueError("Property %s is required" % self.name)
+        if value is not None:
+            value = self.validate(value)
+        obj.__dict__[self.name] = value
+
+    def check(self, obj):
+        """
+        Called after an object is initialized to detect unset required
+        properties.
+        """
+        if self.required and obj.__dict__.get(self.name) is None:
+            raise ValueError("Property %s is required" % self.name)
+
+    def validate(self, value):
+        """
+        Should be implemented by subclass to validate value.
+
+        Called when assigning a value to a property.
+        """
+        return value
+
+
+class Enum(Property):
+
+    def __init__(self, required=False, default=None, doc=None, values=()):
+        if not required and default not in values:
+            raise ValueError("Default value %s not in allowed values %s" %
+                             (default, values))
+        super(Enum, self).__init__(required=required, default=default, doc=doc)
+        self.values = values
+
+    def validate(self, value):
+        if value not in self.values:
+            raise ValueError("Invalid value %r for property %s" %
+                             (value, self.name))
+        return value
+
+
+class String(Property):
+
+    def validate(self, value):
+        if not isinstance(value, basestring):
+            raise ValueError("Invalid value %r for string property %s" %
+                             (value, self.name))
+        return value
+
+
+class _Number(Property):
+
+    def __init__(self, required=False, default=None, doc=None, minval=None,
+                 maxval=None):
+        if minval is not None and default is not None and default < minval:
+            raise ValueError("Invalid default %s < %s" % (default, minval))
+        if maxval is not None and default is not None and default > maxval:
+            raise ValueError("Invalid default %s > %s" % (default, maxval))
+        super(_Number, self).__init__(required=required, default=default,
+                                      doc=doc)
+        self.minval = minval
+        self.maxval = maxval
+
+    def validate(self, value):
+        if self.minval is not None and value < self.minval:
+            raise ValueError("Invalid value %s < %s for property %s" %
+                             (value, self.minval, self.name))
+        if self.maxval is not None and value > self.maxval:
+            raise ValueError("Invalid value %s > %s for property %s" %
+                             (value, self.maxval, self.name))
+        return value
+
+
+class Integer(_Number):
+
+    def validate(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Invalid value %r for integer property %s" %
+                             (value, self.name))
+        return super(Integer, self).validate(value)
+
+
+class Float(_Number):
+
+    def validate(self, value):
+        if not isinstance(value, float):
+            raise ValueError("Invalid value %r for float property %s" %
+                             (value, self.name))
+        return super(Float, self).validate(value)
+
+
+class Boolean(Property):
+
+    def validate(self, value):
+        if value not in (True, False):
+            raise ValueError("Invalid value %r for boolean property %s" %
+                             (value, self.name))
+        return value
+
+
+class UUID(Property):
+
+    def validate(self, value):
+        return str(uuid.UUID(value))
+
+
+class Password(Property):
+
+    def __init__(self, required=False, default=None, doc=None, decode=None):
+        super(Password, self).__init__(required=required, default=default,
+                                       doc=doc)
+        self.decode = decode
+
+    def validate(self, password):
+        if not isinstance(password, ProtectedPassword):
+            raise ValueError("Not a ProtectedPassword %s" % type(password))
+        if self.decode:
+            password.value = self.decode(password.value)
+        return password
+
+
+def decode_base64(value):
+    try:
+        return base64.b64decode(value)
+    except TypeError as e:
+        raise ValueError("Unable to decode base64 value %s" % e)
+
+
+class OwnerType(type):
+
+    def __new__(cls, name, bases, dct):
+        # Name properties used by this class
+        for name, obj in dct.items():
+            if isinstance(obj, Property):
+                obj.name = name
+        return type.__new__(cls, name, bases, dct)
+
+    def __call__(self, *args, **kw):
+        # Check properties after  object is initialized
+        instance = super(OwnerType, self).__call__(*args, **kw)
+        for name, obj in instance.__class__.__dict__.items():
+            if isinstance(obj, Property):
+                obj.check(instance)
+        return instance
+
+
+class Owner(object):
+    """
+    Base class for classes using properties
+
+    Inheriting from this class, all properties on this class and subclasses
+    will be automatically named and required properties will raise ValueError
+    if not initialized.
+    """
+    __metaclass__ = OwnerType
---
+++ tests/properties_test.py
@@ -0,0 +1,480 @@
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import base64
+from testlib import VdsmTestCase
+from vdsm.password import ProtectedPassword
+from vdsm import properties
+
+
+class PropertyTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Property()
+
+    def test_default(self):
+        obj = self.Cls()
+        self.assertEqual(obj.value, None)
+
+    def test_set(self):
+        obj = self.Cls()
+        obj.value = "value"
+        self.assertEqual(obj.value, "value")
+
+    def test_value_per_object(self):
+        obj1 = self.Cls()
+        obj2 = self.Cls()
+        obj1.value = "1"
+        obj2.value = "2"
+        self.assertNotEqual(obj1.value, obj2.value)
+
+
+class PropertyDocTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Property(doc="description")
+
+    def test_doc(self):
+        self.Cls()
+        self.assertEqual(self.Cls.value.__doc__, "description")
+
+
+class PropertyRequiredTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Property(required=True)
+
+    def test_required(self):
+        self.assertRaises(ValueError, self.Cls)
+
+
+class PropertyRequiredNoneTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Property(required=True)
+
+        def __init__(self, value=None):
+            self.value = value
+
+    def test_required(self):
+        # Required property set to None is considered unset
+        self.assertRaises(ValueError, self.Cls)
+
+    def test_assign_raises(self):
+        obj = self.Cls(42)
+        self.assertRaises(ValueError, setattr, obj, 'value', None)
+
+
+class PropertyDefaultTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Property(default="default")
+
+    def test_default(self):
+        obj = self.Cls()
+        self.assertEqual("default", obj.value)
+
+
+class PropertyDefaultRequiredTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Property(required=True, default="default")
+
+    def test_required(self):
+        self.assertRaises(ValueError, self.Cls)
+
+
+class EnumDefaultTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Enum(values=("1", "2", "3"), default="2")
+
+    def test_invalid_default(self):
+        def invalid_default():
+            class Cls(properties.Owner):
+                value = properties.Enum(values=("1", "2", "3"), default="4")
+        self.assertRaises(ValueError, invalid_default)
+
+    def test_default(self):
+        obj = self.Cls()
+        self.assertEqual(obj.value, "2")
+
+    def test_allowed(self):
+        for value in ("1", "2", "3"):
+            obj = self.Cls()
+            obj.value = value
+            self.assertEqual(obj.value, value)
+
+    def test_forbidden(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "value", "4")
+
+    def test_none(self):
+        obj = self.Cls()
+        obj.value = None
+        self.assertEqual(None, obj.value)
+
+
+class EnumMixedTypesTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Enum(values=(1, "2", 3.0), default="2")
+
+    def test_types(self):
+        # This is bad idea, but we don't forbidd this for now.
+        obj = self.Cls()
+        obj.value = 1
+        obj.value = "2"
+        obj.value = 3.0
+
+
+class EnumRequiredTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Enum(required=True)
+
+        def __init__(self, value=None):
+            self.value = value
+
+    def test_required(self):
+        self.assertRaises(ValueError, self.Cls)
+
+
+class StringTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.String()
+
+    def test_default(self):
+        obj = self.Cls()
+        self.assertEqual(None, obj.value)
+
+    def test_str(self):
+        obj = self.Cls()
+        obj.value = "value"
+        self.assertEqual(obj.value, "value")
+
+    def test_unicode(self):
+        obj = self.Cls()
+        obj.value = u"\u05d0"  # Alef
+        self.assertEqual(obj.value, u"\u05d0")
+
+    def test_invalid(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "value", 1)
+
+    def test_none(self):
+        obj = self.Cls()
+        obj.value = None
+        self.assertEqual(None, obj.value)
+
+
+class StringRequiredTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.String(required=True)
+
+        def __init__(self, value=None):
+            self.value = value
+
+    def test_required(self):
+        self.assertRaises(ValueError, self.Cls)
+
+    def test_empty(self):
+        obj = self.Cls("")
+        self.assertEqual("", obj.value)
+
+
+class IntegerTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Integer()
+
+    def test_default(self):
+        obj = self.Cls()
+        self.assertEqual(None, obj.value)
+
+    def test_valid(self):
+        obj = self.Cls()
+        obj.value = 7
+        self.assertEqual(7, obj.value)
+
+    def test_invalid(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "value", 3.14)
+
+    def test_none(self):
+        obj = self.Cls()
+        obj.value = None
+        self.assertEqual(None, obj.value)
+
+
+class IntegerMinValueTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Integer(minval=0)
+
+    def test_valid(self):
+        obj = self.Cls()
+        obj.value = 7
+        self.assertEqual(7, obj.value)
+
+    def test_too_small(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "value", -1)
+
+
+class IntegerMaxValueTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Integer(maxval=100)
+
+    def test_valid(self):
+        obj = self.Cls()
+        obj.value = 7
+        self.assertEqual(7, obj.value)
+
+    def test_too_large(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "value", 101)
+
+
+class IntegerRequiredTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Integer(required=True)
+
+        def __init__(self, value=None):
+            self.value = value
+
+    def test_required(self):
+        self.assertRaises(ValueError, self.Cls)
+
+    def test_zero(self):
+        obj = self.Cls(0)
+        self.assertEqual(0, obj.value)
+
+
+class FloatTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Float()
+
+    def test_default(self):
+        obj = self.Cls()
+        self.assertEqual(None, obj.value)
+
+    def test_valid(self):
+        obj = self.Cls()
+        obj.value = 3.14
+        self.assertEqual(obj.value, 3.14)
+
+    def test_invalid(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "value", "not a float")
+
+    def test_int(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "value", 3)
+
+    def test_none(self):
+        obj = self.Cls()
+        obj.value = None
+        self.assertEqual(None, obj.value)
+
+
+class FloatMinValueTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Float(minval=1.0)
+
+    def test_valid(self):
+        obj = self.Cls()
+        obj.value = 1.0
+        self.assertEqual(1.0, obj.value)
+
+    def test_too_small(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "value", 0.999999999)
+
+
+class FloatMaxValueTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Float(maxval=1.0)
+
+    def test_valid(self):
+        obj = self.Cls()
+        obj.value = 0.2
+        self.assertEqual(0.2, obj.value)
+
+    def test_too_large(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "value", 1.000000001)
+
+
+class FloatRequiredTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Float(required=True)
+
+        def __init__(self, value=None):
+            self.value = value
+
+    def test_required(self):
+        self.assertRaises(ValueError, self.Cls)
+
+    def test_zero(self):
+        obj = self.Cls(0.0)
+        self.assertEqual(0.0, obj.value)
+
+
+class BooleanTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Boolean()
+
+    def test_not_set(self):
+        obj = self.Cls()
+        self.assertEqual(None, obj.value)
+
+    def test_true(self):
+        obj = self.Cls()
+        obj.value = True
+        self.assertEqual(True, obj.value)
+
+    def test_false(self):
+        obj = self.Cls()
+        obj.value = False
+        self.assertEqual(False, obj.value)
+
+    def test_invalid(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "value", "not a bool")
+
+    def test_none(self):
+        obj = self.Cls()
+        obj.value = None
+        self.assertEqual(None, obj.value)
+
+
+class BooleanDefaultTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Boolean(default=False)
+
+    def test_default(self):
+        obj = self.Cls()
+        self.assertEqual(False, obj.value)
+
+
+class BooleanRequiredTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.Boolean(required=True)
+
+    def test_required(self):
+        self.assertRaises(ValueError, self.Cls)
+
+
+class UUIDTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.UUID(default="00000000-0000-0000-0000-000000000000")
+
+    def test_default(self):
+        obj = self.Cls()
+        self.assertEqual("00000000-0000-0000-0000-000000000000", obj.value)
+
+    def test_valid(self):
+        obj = self.Cls()
+        obj.value = "774229a2-9300-474d-9d95-ab8423df94e1"
+        self.assertEqual("774229a2-9300-474d-9d95-ab8423df94e1", obj.value)
+
+    def test_invalid(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "value", "not-a-uuid")
+
+    def test_none(self):
+        obj = self.Cls()
+        obj.value = None
+        self.assertEqual(None, obj.value)
+
+
+class UUIDRequiredTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        value = properties.UUID(required=True)
+
+    def test_required(self):
+        self.assertRaises(ValueError, self.Cls)
+
+
+class PasswordTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        password = properties.Password()
+
+    def test_default(self):
+        obj = self.Cls()
+        self.assertEqual(None, obj.password)
+
+    def test_valid(self):
+        obj = self.Cls()
+        obj.password = ProtectedPassword("12345678")
+        self.assertEqual("12345678", obj.password.value)
+
+    def test_invalid(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "password",
+                          "bare password")
+
+    def test_none(self):
+        obj = self.Cls()
+        obj.password = None
+        self.assertEqual(None, obj.password)
+
+
+class PasswordRequiredTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        password = properties.Password(required=True)
+
+        def __init__(self, password=None):
+            self.password = password
+
+    def test_required(self):
+        self.assertRaises(ValueError, self.Cls)
+
+
+class PasswordDecodeTests(VdsmTestCase):
+
+    class Cls(properties.Owner):
+        password = properties.Password(decode=properties.decode_base64)
+
+    def test_decode(self):
+        obj = self.Cls()
+        obj.password = ProtectedPassword(base64.b64encode("12345678"))
+        self.assertEqual("12345678", obj.password.value)
+
+    def test_invalid(self):
+        obj = self.Cls()
+        self.assertRaises(ValueError, setattr, obj, "password",
+                          ProtectedPassword("not base64 value"))
