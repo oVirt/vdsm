f3ee692bc2c3646a09d09f8c2b778f775f4a43bf
Nir Soffer <nsoffer@redhat.com>
2015-05-11 23:17:01 +0300

    ceph: Add libvirt secret testing infrastructure
    New vmfakelib.Error function create a libvirt.libvirtError with specific
    error code, for testing code that must handle certain libvirt errors.
    
    vmfakelib.Connection supports now secret related methods:
    
    - secretDefineXML
    - secretLookupByUUIDString
    
    New vmfakelibTests module added to verify that the fakes are behaving
    like the real libvirt objects.
    
    Change-Id: Iac2d4ebaf8cadeff2818b2eac593b8d28b5d6706
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/40789
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Reviewed-by: Adam Litke <alitke@redhat.com>
    Continuous-Integration: Jenkins CI
    Reviewed-by: Federico Simoncelli <fsimonce@redhat.com>

--- tests/vmfakelib.py
+++ tests/vmfakelib.py
@@ -39,9 +39,40 @@ from testlib import namedTemporaryDir
 from monkeypatch import MonkeyPatchScope
 
 
-class Connection:
+def Error(code, msg="fake error"):
+    e = libvirt.libvirtError(msg)
+    e.err = [code, None, msg]
+    return e
+
+
+class Connection(object):
+
     def __init__(self, *args):
-        pass
+        self.secrets = {}
+
+    def secretDefineXML(self, xml):
+        uuid, usage_type, usage_id, description = parse_secret(xml)
+        if uuid in self.secrets:
+            # If a secret exists, we cannot change its usage_id
+            # See libvirt/src/secret/secret_driver.c:782
+            sec = self.secrets[uuid]
+            if usage_id != sec.usage_id:
+                raise Error(libvirt.VIR_ERR_INTERNAL_ERROR)
+            sec.usage_type = usage_type
+            sec.description = description
+        else:
+            # (usage_type, usage_id) pair must be unique
+            for sec in self.secrets.values():
+                if sec.usage_type == usage_type and sec.usage_id == usage_id:
+                    raise Error(libvirt.VIR_ERR_INTERNAL_ERROR)
+            sec = Secret(self, uuid, usage_type, usage_id, description)
+            self.secrets[uuid] = sec
+        return sec
+
+    def secretLookupByUUIDString(self, uuid):
+        if uuid not in self.secrets:
+            raise Error(libvirt.VIR_ERR_NO_SECRET)
+        return self.secrets[uuid]
 
     def domainEventRegisterAny(self, *arg):
         pass
@@ -68,6 +99,48 @@ class Connection:
             device_xml = device_xml_file.read()
 
         return VirNodeDeviceStub(device_xml)
+
+
+class Secret(object):
+
+    def __init__(self, con, uuid, usage_type, usage_id, description):
+        self.con = con
+        self.uuid = uuid
+        self.usage_type = usage_type
+        self.usage_id = usage_id
+        self.description = description
+        self.value = None
+
+    def undefine(self):
+        del self.con.secrets[self.uuid]
+
+    def UUIDString(self):
+        return self.uuid
+
+    def usageID(self):
+        return self.usage_id
+
+    def setValue(self, value):
+        self.value = value
+
+
+def parse_secret(xml):
+    root = etree.fromstring(xml)
+    uuid = root.find("./uuid").text
+    usage_type = root.find("./usage/[@type]").get("type")
+    if usage_type == "volume":
+        usage_id = root.find("./usage/volume").text
+    elif usage_type == "ceph":
+        usage_id = root.find("./usage/name").text
+    elif usage_type == "iscsi":
+        usage_id = root.find("./usage/target").text
+    else:
+        raise Error(libvirt.VIR_ERR_INTERNAL_ERROR)
+    try:
+        description = root.find("./description").text
+    except AttributeError:
+        description = None
+    return uuid, usage_type, usage_id, description
 
 
 class ClientIF(clientIF.clientIF):
---
+++ tests/vmfakelibTests.py
@@ -0,0 +1,160 @@
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import libvirt
+from testlib import VdsmTestCase
+import vmfakelib
+
+
+class SecretsTests(VdsmTestCase):
+
+    def test_define_new(self):
+        con = vmfakelib.Connection()
+        xml = """
+        <secret>
+            <uuid>uuid</uuid>
+            <usage type="ceph">
+                <name>name</name>
+            </usage>
+        </secret>
+        """
+        con.secretDefineXML(xml)
+        sec = con.secrets['uuid']
+        self.assertEqual(sec.uuid, "uuid")
+        self.assertEqual(sec.usage_type, "ceph")
+        self.assertEqual(sec.usage_id, "name")
+        self.assertEqual(sec.description, None)
+
+    def test_define_new_with_description(self):
+        con = vmfakelib.Connection()
+        xml = """
+        <secret>
+            <description>description</description>
+            <uuid>uuid</uuid>
+            <usage type="ceph">
+                <name>name</name>
+            </usage>
+        </secret>
+        """
+        con.secretDefineXML(xml)
+        sec = con.secrets['uuid']
+        self.assertEqual(sec.description, "description")
+
+    def test_define_replace(self):
+        con = vmfakelib.Connection()
+        xml1 = """
+        <secret>
+            <description>old description</description>
+            <uuid>uuid</uuid>
+            <usage type="ceph">
+                <name>name</name>
+            </usage>
+        </secret>
+        """
+        xml2 = """
+        <secret>
+            <description>new description</description>
+            <uuid>uuid</uuid>
+            <usage type="ceph">
+                <name>name</name>
+            </usage>
+        </secret>
+        """
+        con.secretDefineXML(xml1)
+        con.secretDefineXML(xml2)
+        sec = con.secrets['uuid']
+        self.assertEqual(sec.description, "new description")
+
+    def test_define_cannot_change_usage_id(self):
+        con = vmfakelib.Connection()
+        xml1 = """
+        <secret>
+            <uuid>uuid</uuid>
+            <usage type="ceph">
+                <name>name 1</name>
+            </usage>
+        </secret>
+        """
+        xml2 = """
+        <secret>
+            <uuid>uuid</uuid>
+            <usage type="ceph">
+                <name>name 2</name>
+            </usage>
+        </secret>
+        """
+        con.secretDefineXML(xml1)
+        try:
+            con.secretDefineXML(xml2)
+        except libvirt.libvirtError as e:
+            self.assertEqual(e.get_error_code(),
+                             libvirt.VIR_ERR_INTERNAL_ERROR)
+        else:
+            self.fail("libvirtError was not raised")
+
+    def test_define_usage_not_unique(self):
+        con = vmfakelib.Connection()
+        xml1 = """
+        <secret>
+            <uuid>uuid 1</uuid>
+            <usage type="ceph">
+                <name>name</name>
+            </usage>
+        </secret>
+        """
+        xml2 = """
+        <secret>
+            <uuid>uuid 2</uuid>
+            <usage type="ceph">
+                <name>name</name>
+            </usage>
+        </secret>
+        """
+        con.secretDefineXML(xml1)
+        try:
+            con.secretDefineXML(xml2)
+        except libvirt.libvirtError as e:
+            self.assertEqual(e.get_error_code(),
+                             libvirt.VIR_ERR_INTERNAL_ERROR)
+        else:
+            self.fail("libvirtError was not raised")
+
+    def test_lookup(self):
+        con = vmfakelib.Connection()
+        xml = """
+        <secret>
+            <uuid>uuid</uuid>
+            <usage type="ceph">
+                <name>name</name>
+            </usage>
+        </secret>
+        """
+        con.secretDefineXML(xml)
+        sec = con.secretLookupByUUIDString('uuid')
+        self.assertEqual(sec.usage_id, "name")
+
+    def test_lookup_error(self):
+        con = vmfakelib.Connection()
+        try:
+            con.secretLookupByUUIDString('no-such-uuid')
+        except libvirt.libvirtError as e:
+            self.assertEqual(e.get_error_code(), libvirt.VIR_ERR_NO_SECRET)
+        else:
+            self.fail("libvirtError was not raised")
