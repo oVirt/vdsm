c0aad9e622125db44c989f229d1ffaa5aaf2d42b
Francesco Romani <fromani@redhat.com>
2015-05-18 11:12:30 +0200

    lib: pthread: allow to set system thread names
    Python allow to set user-friendly names for threads,
    but the given thread name will be visible only at python level,
    not at system level.
    The lack of thread names make hard to follow the resource
    consumption using system tools (htop, top, ps).
    
    This feature was requested for python library, but rejected:
    http://bugs.python.org/issue15500
    
    VDSM makes large use of threads until upcoming 4.17, and even
    in 4.17 there is still a fair amount of threads.
    
    This patch adds additional facilities to set the thread name at
    system level, to improve troubleshooting and performance analysis.
    
    Change-Id: Ib96bd0471a07449d2a0a0328137e8437aefad901
    Bug-Url: https://bugzilla.redhat.com/1141422
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/41052
    Continuous-Integration: Jenkins CI
    Reviewed-by: Yaniv Bronhaim <ybronhei@redhat.com>
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ lib/vdsm/pthread.py
@@ -0,0 +1,89 @@
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import ctypes
+import logging
+import threading
+
+
+NAME_MAX_LENGTH = 15
+
+
+_LIBPTHREAD = ctypes.CDLL("libpthread.so.0", use_errno=True)
+
+_pthread_setname_np_proto = ctypes.CFUNCTYPE(
+    ctypes.c_int, ctypes.c_ulong, ctypes.c_char_p)
+_pthread_getname_np_proto = ctypes.CFUNCTYPE(
+    ctypes.c_int, ctypes.c_ulong, ctypes.c_char_p, ctypes.c_size_t)
+
+try:
+    _pthread_setname_np = _pthread_setname_np_proto(('pthread_setname_np',
+                                                    _LIBPTHREAD))
+
+    _pthread_getname_np = _pthread_getname_np_proto(('pthread_getname_np',
+                                                    _LIBPTHREAD))
+except AttributeError:
+    def _pthread_setname_np(ident, name):
+        pass
+
+    def _pthread_getname_np(ident):
+        return ""
+
+    logging.warning(
+        'pthread_{set,get}name_np unavailable. '
+        'System thread names will not be set.')
+
+
+def setname(name):
+    """
+    Set a system-wide thread name.
+
+    The most common use of this function is inside a thread target function:
+
+        def run():
+            pthread.setname("vdsm-cleanup")
+            ...
+
+        Thread(target=run).start()
+
+    The name is limited to 15 ASCII characters - see pthread_setname_np(3).
+    """
+
+    name = name.encode("ascii")
+    if len(name) > NAME_MAX_LENGTH:
+        raise ValueError("Expecting up to %d bytes for the name" % (
+            NAME_MAX_LENGTH))
+
+    thread = threading.current_thread()
+    _pthread_setname_np(thread.ident, name)
+
+
+def getname():
+    """
+    Get the system-wide name of the current thread.
+
+    Return empty string if the thread has no system name.
+    """
+    bufsize = NAME_MAX_LENGTH + 1
+    buf = ctypes.create_string_buffer('\0' * bufsize)
+
+    thread = threading.current_thread()
+    _pthread_getname_np(thread.ident, buf, bufsize)
+    return buf.value
---
+++ tests/pthreadTests.py
@@ -0,0 +1,70 @@
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import threading
+
+from vdsm import pthread
+
+from testlib import VdsmTestCase as TestCaseBase
+
+
+class PthreadNameTests(TestCaseBase):
+
+    def setUp(self):
+        self.ready = threading.Event()
+        self.done = threading.Event()
+
+    def test_name_too_long(self):
+        self.assertRaises(ValueError,
+                          pthread.setname,
+                          "any name longer than fifteen ASCII characters")
+
+    def test_name_is_set(self):
+        NAME = "test-name"
+        names = [None]
+
+        def run():
+            pthread.setname(NAME)
+            names[0] = pthread.getname()
+            self.ready.set()
+            self.done.wait()
+
+        threading.Thread(target=run).start()
+        self.ready.wait()
+
+        self.assertEqual(names[0], NAME)
+
+        self.done.set()
+
+    def test_name_set_doesnt_change_parent(self):
+        HELPER_NAME = "helper-name"
+        parent_name = pthread.getname()
+
+        def run():
+            pthread.setname(HELPER_NAME)
+            self.ready.set()
+            self.done.wait()
+
+        threading.Thread(target=run).start()
+        self.ready.wait()
+
+        self.assertEqual(parent_name, pthread.getname())
+
+        self.done.set()
