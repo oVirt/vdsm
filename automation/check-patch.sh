25aa6b2cf9a5cb1c124026e7176c781976b98216
David Caro <dcaroest@redhat.com>
2015-06-04 11:08:07 +0200

    Adding check-patch ci script
    For now only runs the vdsm unit tests
    
    Change-Id: Ife4cd928b480469a0b1ef4a583efc0d0e0b06989
    Signed-off-by: David Caro <dcaroest@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/45114
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Tested-by: Francesco Romani <fromani@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Continuous-Integration: Dan Kenigsberg <danken@redhat.com>

---
+++ automation/check-patch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+#
+# Run on each patch to gerrit, should be faster than check-meged and require
+# less resources but thorough enough to provide relevant feedback
+
+# Nose 1.3.0 and later segatult with this flag
+#export NOSE_WITH_XUNIT=1
+
+export NOSE_SKIP_STRESS_TESTS=1
+export NOSE_EXCLUDE=
+# disable pep8 checks on unit test, since we have seperate job on it.
+export PEP8=$(which true)
+
+# really ugly and hopefully temporary fix
+# https://bugzilla.redhat.com/show_bug.cgi?id=1255142
+[[ -e /dev/net/tun ]] \
+|| {
+    [[ -e /dev/net ]] || mkdir /dev/net
+    mknod /dev/net/tun c 10 200
+}
+
+sh -x autogen.sh --system
+make all
+make check
