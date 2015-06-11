e56a79468e20a1f1d31222b2c9742502460bdd52
Ido Barkan <ibarkan@redhat.com>
2015-06-11 08:39:35 +0300

    net: missing Exception formatting value
    Change-Id: I1e3bb7067c5c7ec70c7120515c7fb1231e9cabdc
    Signed-off-by: Ido Barkan <ibarkan@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1203422
    Reviewed-on: https://gerrit.ovirt.org/42195
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/42955

--- lib/vdsm/netinfo.py
+++ lib/vdsm/netinfo.py
@@ -294,7 +294,7 @@ def getaddr(dev):
 def prefix2netmask(prefix):
     if not 0 <= prefix <= 32:
         raise ValueError('%s is not a valid prefix value. It must be between '
-                         '0 and 32')
+                         '0 and 32' % prefix)
     return socket.inet_ntoa(
         struct.pack("!I", int('1' * prefix + '0' * (32 - prefix), 2)))
 
