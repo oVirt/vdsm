24328c712f7131752db873b0736fcb545f28c8bf
Ido Barkan <ibarkan@redhat.com>
2015-06-15 12:15:48 +0300

    net: ifcfg: ONBOOT=yes on all persisted network files.
    This will cause network service to configure those networks long
    before vdsm starts. after https://gerrit.ovirt.org/#/c/42355/
    vdsm will only restore changed networks.
    
    Change-Id: Ia942827745945f0f2e2a0e1691996824511cc111
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1203422
    Signed-off-by: Ido Barkan <ibarkan@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/42362
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/42962

--- vdsm/network/configurators/ifcfg.py
+++ vdsm/network/configurators/ifcfg.py
@@ -626,10 +626,7 @@ class ConfigWriter(object):
         if bridge.stp is not None:
             conf += 'STP=%s\n' % ('on' if bridge.stp else 'off')
         ipconfig = bridge.ipConfig
-        if not self.unifiedPersistence or ipconfig.defaultRoute:
-            conf += 'ONBOOT=%s\n' % 'yes'
-        else:
-            conf += 'ONBOOT=%s\n' % 'no'
+        conf += 'ONBOOT=%s\n' % 'yes'
         defaultRoute = ConfigWriter._toIfcfgFormat(ipconfig.defaultRoute)
         ipconfig = ipconfig._replace(defaultRoute=defaultRoute)
 
@@ -643,10 +640,7 @@ class ConfigWriter(object):
         opts['hotplug'] = 'no'  # So that udev doesn't trigger an ifup
         if vlan.bridge:
             conf += 'BRIDGE=%s\n' % pipes.quote(vlan.bridge.name)
-        if not self.unifiedPersistence or vlan.serving_default_route:
-            conf += 'ONBOOT=%s\n' % 'yes'
-        else:
-            conf += 'ONBOOT=%s\n' % 'no'
+        conf += 'ONBOOT=%s\n' % 'yes'
         ipconfig = vlan.ipConfig
         defaultRoute = ConfigWriter._toIfcfgFormat(ipconfig.defaultRoute)
         ipconfig = ipconfig._replace(defaultRoute=defaultRoute)
@@ -658,10 +652,7 @@ class ConfigWriter(object):
         opts['hotplug'] = 'no'  # So that udev doesn't trigger an ifup
         if bond.bridge:
             conf += 'BRIDGE=%s\n' % pipes.quote(bond.bridge.name)
-        if not self.unifiedPersistence or bond.serving_default_route:
-            conf += 'ONBOOT=%s\n' % 'yes'
-        else:
-            conf += 'ONBOOT=%s\n' % 'no'
+        conf += 'ONBOOT=%s\n' % 'yes'
 
         ipconfig, mtu = self._getIfaceConfValues(bond)
         self._createConfFile(conf, bond.name, ipconfig, mtu, **opts)
@@ -684,10 +675,7 @@ class ConfigWriter(object):
             conf += 'BRIDGE=%s\n' % pipes.quote(nic.bridge.name)
         if nic.bond:
             conf += 'MASTER=%s\nSLAVE=yes\n' % pipes.quote(nic.bond.name)
-        if not self.unifiedPersistence or nic.serving_default_route:
-            conf += 'ONBOOT=%s\n' % 'yes'
-        else:
-            conf += 'ONBOOT=%s\n' % 'no'
+        conf += 'ONBOOT=%s\n' % 'yes'
 
         ethtool_opts = getEthtoolOpts(nic.name)
         if ethtool_opts:
