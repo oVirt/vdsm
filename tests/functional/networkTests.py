42bc93b93d8d4d1a2c01b17b52a9cde8e2f370f2
Dan Kenigsberg <danken@redhat.com>
2015-06-16 11:42:08 +0100

    make pyflakes-0.7.3 happy
    commit c087fc4 imported vdsm.config, which triggered a bug in Fedora
    20's pyflakes.
    
    Change-Id: I5a976baa2dd5afc1aa6737c15035eb2fb33a5889
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/42414
    Reviewed-by: Sandro Bonazzola <sbonazzo@redhat.com>
    Reviewed-by: Ido Barkan <ibarkan@redhat.com>
    Continuous-Integration: Jenkins CI
    Reviewed-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- tests/functional/networkTests.py
+++ tests/functional/networkTests.py
@@ -77,16 +77,16 @@ NOCHK = {'connectivityCheck': False}
 
 def setupModule():
     """Persists network configuration."""
-    vdsm = VdsProxy()
-    vdsm.save_config()
+    v = VdsProxy()
+    v.save_config()
     for _ in range(DUMMY_POOL_SIZE):
         dummyPool.add(dummy.create())
 
 
 def tearDownModule():
     """Restores the network configuration previous to running tests."""
-    vdsm = VdsProxy()
-    vdsm.restoreNetConfig()
+    v = VdsProxy()
+    v.restoreNetConfig()
     for nic in dummyPool:
         dummy.remove(nic)
 
