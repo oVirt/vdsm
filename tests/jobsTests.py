5ff13ed0552513cfe667c4488cf5572e3c947319
Adam Litke <alitke@redhat.com>
2015-08-14 15:46:40 -0400

    Refactor v2v jobs for reusability
    The jobs management infrastructure in v2v.py is general purpose and
    reusable.  For example, storage would like to use the exact same thing
    to manage long-running storage operations.  In order to make this
    possible, factor out the job management parts of v2v.py into a new
    module jobs.py and create a generic Job class which contains the
    complete interface needed by the job manager.  In addition, add some
    basic tests for Job objects and their management.
    
    Change-Id: Ida6b1c460c5030c820c540e836e423d4632410df
    Signed-off-by: Adam Litke <alitke@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/44857
    Continuous-Integration: Jenkins CI
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>

---
+++ tests/jobsTests.py
@@ -0,0 +1,138 @@
+# Copyright 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import uuid
+
+import jobs
+from vdsm import response
+
+from testlib import VdsmTestCase, expandPermutations, permutations
+
+
+class TestingJob(jobs.Job):
+    _JOB_TYPE = 'testing'
+
+    def __init__(self):
+        jobs.Job.__init__(self, str(uuid.uuid4()))
+        self._progress = 0
+        self._aborted = False
+
+    @property
+    def progress(self):
+        return self._progress
+
+    def _abort(self):
+        self._aborted = True
+
+
+class FooJob(TestingJob):
+    _JOB_TYPE = 'foo'
+
+
+class BarJob(TestingJob):
+    _JOB_TYPE = 'bar'
+
+
+@expandPermutations
+class JobsTests(VdsmTestCase):
+    TIMEOUT = 1
+
+    def setUp(self):
+        jobs._clear()
+
+    def test_job_initial_state(self):
+        job = TestingJob()
+        self.assertEqual(jobs.STATUS.RUNNING, job.status)
+        self.assertEqual('', job.description)
+        self.assertEqual('testing', job.job_type)
+
+    def test_job_info(self):
+        job = TestingJob()
+        self.assertEqual({'status': 'running', 'progress': 0,
+                          'job_type': 'testing', 'description': ''},
+                         job.info())
+
+    def test_add_job(self):
+        job = TestingJob()
+        jobs.add(job)
+        self.assertEqual(1, len(jobs._jobs))
+
+    def test_add_existing_job(self):
+        job = TestingJob()
+        jobs.add(job)
+        self.assertRaises(jobs.JobExistsError, jobs.add, job)
+
+    def test_get_job(self):
+        job = TestingJob()
+        jobs.add(job)
+        self.assertEqual(job.id, jobs.get(job.id).id)
+
+    def test_get_unknown_job(self):
+        self.assertRaises(jobs.NoSuchJob, jobs.get, 'foo')
+
+    def test_get_jobs_info(self):
+        foo = FooJob()
+        jobs.add(foo)
+        bar = BarJob()
+        jobs.add(bar)
+        self.assertEqual({foo.id: foo.info(), bar.id: bar.info()},
+                         jobs.info())
+
+    def test_get_jobs_info_empty(self):
+        self.assertEqual({}, jobs.info())
+
+    def test_get_jobs_info_filter(self):
+        foo = FooJob()
+        jobs.add(foo)
+        bar = BarJob()
+        jobs.add(bar)
+        self.assertEqual({bar.id: bar.info()},
+                         jobs.info(bar.job_type))
+
+    def test_abort_job(self):
+        job = TestingJob()
+        jobs.add(job)
+        jobs.abort(job.id)
+        self.assertEqual(jobs.STATUS.ABORTED, job.status)
+        self.assertTrue(job._aborted)
+
+    def test_abort_unknown_job(self):
+        self.assertEqual(response.error(jobs.NoSuchJob.name),
+                         jobs.abort('foo'))
+
+    @permutations([
+        [jobs.STATUS.ABORTED],
+        [jobs.STATUS.DONE],
+        [jobs.STATUS.FAILED]
+    ])
+    def test_delete_job(self, status):
+        job = TestingJob()
+        job._status = status
+        jobs.add(job)
+        self.assertEqual(response.success(), jobs.delete(job.id))
+
+    def test_delete_running_job(self):
+        job = TestingJob()
+        jobs.add(job)
+        self.assertEqual(response.error(jobs.JobNotDone.name),
+                         jobs.delete(job.id))
+
+    def test_delete_unknown_job(self):
+        self.assertEqual(response.error(jobs.NoSuchJob.name),
+                         jobs.delete('foo'))
---
+++ vdsm/jobs.py
@@ -0,0 +1,171 @@
+# Copyright 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import logging
+import threading
+
+from vdsm import response
+
+
+_lock = threading.Lock()
+_jobs = {}
+
+
+class STATUS:
+    '''
+    RUNNING: Job is running
+    ABORTED: Job aborted by user request
+    FAILED: Job failed and no longer running
+    DONE: Job has finished successfully
+    '''
+    RUNNING = 'running'
+    ABORTED = 'aborted'
+    FAILED = 'error'
+    DONE = 'done'
+
+
+class ClientError(Exception):
+    ''' Base class for client error '''
+    name = None
+
+
+class JobExistsError(ClientError):
+    ''' Job already exists in _jobs collection '''
+    name = 'JobExistsError'
+
+
+class NoSuchJob(ClientError):
+    ''' Job does not exist in _jobs collection '''
+    name = 'NoSuchJob'
+
+
+class JobNotDone(ClientError):
+    ''' Job still in progress '''
+    name = 'JobNotDone'
+
+
+class Job(object):
+    _JOB_TYPE = None
+
+    def __init__(self, job_id, description=''):
+        self._id = job_id
+        self._status = STATUS.RUNNING
+        self._description = description
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def status(self):
+        return self._status
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def progress(self):
+        raise NotImplementedError()
+
+    @property
+    def job_type(self):
+        return self._JOB_TYPE
+
+    def info(self):
+        return {
+            'status': self.status,
+            'description': self.description,
+            'progress': self.progress,
+            'job_type': self.job_type
+        }
+
+    def abort(self):
+        self._status = STATUS.ABORTED
+        logging.info('Job %r aborting...', self._id)
+        self._abort()
+
+    def validate_done(self):
+        if self.status != STATUS.DONE:
+            raise JobNotDone("Job %r is %s" % (self.id, self.status))
+
+    def validate_not_running(self):
+        if self.status not in (STATUS.DONE, STATUS.FAILED, STATUS.ABORTED):
+            raise JobNotDone("Job %r is %s" % (self.id, self.status))
+
+    def _abort(self):
+        """
+        May be implemented by child class
+        """
+
+
+# This helper should only be called by test code.  Everything else should be
+# using the public APIs.
+def _clear():
+    with _lock:
+        _jobs.clear()
+
+
+def delete(job_id):
+    try:
+        job = get(job_id)
+        job.validate_not_running()
+        _delete(job_id)
+    except ClientError as e:
+        logging.info('Cannot delete job, error: %s', e)
+        return response.error(e.name)
+    return response.success()
+
+
+def abort(job_id):
+    try:
+        job = get(job_id)
+        job.abort()
+    except ClientError as e:
+        logging.info('Cannot abort job, error: %s', e)
+        return response.error(e.name)
+    return response.success()
+
+
+def info(type_filter=None):
+    with _lock:
+        jobs = _jobs.values()
+    return {job.id: job.info() for job in jobs
+            if not type_filter or job.job_type == type_filter}
+
+
+def add(job):
+    with _lock:
+        if job.id in _jobs:
+            raise JobExistsError("Job %r exists" % job.id)
+        _jobs[job.id] = job
+
+
+def get(job_id):
+    with _lock:
+        if job_id not in _jobs:
+            raise NoSuchJob("No such job %r" % job_id)
+        return _jobs[job_id]
+
+
+def _delete(job_id):
+    with _lock:
+        if job_id not in _jobs:
+            raise NoSuchJob("No such job %r" % job_id)
+        del _jobs[job_id]
