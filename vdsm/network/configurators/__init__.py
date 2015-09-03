3154996d73e176672f36a6342bdcd37f6060a281
Ondřej Svoboda <osvoboda@redhat.com>
2015-09-03 11:46:47 +0200

    network.api: EL7's dhclient doesn't support -df so stop using it there
    This is a quick fix only. A proper solution for the affected bug will
    have to fall back to -lf (a copy of a lease file).
    
    Change-Id: Ia096d42d24b00e7ef075f1a2dde7e3a951c0b81c
    Bug-Url: https://bugzilla.redhat.com/1219429
    Signed-off-by: Ondřej Svoboda <osvoboda@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/45713
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Tested-by: Martin Sivák <msivak@redhat.com>
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/45754
    Tested-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/network/configurators/__init__.py
+++ vdsm/network/configurators/__init__.py
@@ -176,7 +176,7 @@ def getEthtoolOpts(name):
 
 def runDhclient(iface, family=4, default_route=False):
     dhclient = DhcpClient(iface.name, family, default_route, iface.duid_source)
-    rc = dhclient.start(iface.blockingdhcp)
+    rc, _, _ = dhclient.start(iface.blockingdhcp)
     if iface.blockingdhcp and rc:
         raise ConfigNetworkError(ERR_FAILED_IFUP, 'dhclient%s failed' % family)
 
