7ce1af8dfd0edb11300f4df63e5ccfde81cc9918
David Caro <dcaroest@redhat.com>
2015-09-29 19:13:23 +0200

    CI: On fc22 trying to install a src.rpm fails
    Change-Id: Id4ceb237465361a7fd6dfb7af33204fe13f8b942
    Signed-off-by: David Caro <dcaroest@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/46812
    Continuous-Integration: Jenkins CI
    Reviewed-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -23,7 +23,9 @@ make check
 
 ./automation/build-artifacts.sh
 
+# enable complex globs
+shopt -s extglob
 # if specfile was changed, try to install all created packages
 if git diff-tree --no-commit-id --name-only -r HEAD | grep --quiet 'vdsm.spec.in' ; then
-  yum -y install exported-artifacts/*.rpm
+    yum -y install exported-artifacts/!(*.src).rpm
 fi
