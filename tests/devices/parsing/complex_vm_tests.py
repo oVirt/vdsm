3476258bf95b024ed769b09e43c5c4e78b445ebf
Ido Barkan <ibarkan@redhat.com>
2015-10-25 09:04:57 +0200

    net: Report correct (guest) pci address fot hostdev interfaces.
    For network interfaces, the address is a direct child of the
    interface node. For host device interfaces, the host address may
    appear before the guest address and be reported instead.
    
    Change-Id: I4542dd9e0a70db4b9a6526442118d610c1b2c615
    Signed-off-by: Ido Barkan <ibarkan@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1261352
    Reviewed-on: https://gerrit.ovirt.org/47696
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- tests/devices/parsing/complex_vm_tests.py
+++ tests/devices/parsing/complex_vm_tests.py
@@ -3,6 +3,7 @@ import os
 from testlib import XMLTestCase
 
 from virt import domain_descriptor
+from virt.vmdevices import hwclass
 import vmfakelib as fake
 
 import verify
@@ -55,3 +56,58 @@ class TestVmDevicesXmlParsing(XMLTestCase, verify.DeviceMixin):
             vm._domain = domain_descriptor.DomainDescriptor(domain)
             vm._getUnderlyingVmDevicesInfo()
             self.verifyDevicesConf(vm.conf['devices'])
+
+
+class TestSRiovXmlParsing(XMLTestCase, verify.DeviceMixin):
+
+    def test_sriov_vm(self):
+        params = {
+            'nicModel': 'rtl8139,pv', 'name': 'SRiovVm',
+            'displaySecurePort': '-1', 'memSize': '256', 'displayPort': '-1',
+            'display': 'qxl'}
+
+        devices = [{'device': 'virtio-serial', 'type': 'controller'},
+                   {'device': 'memballoon', 'specParams': {'model': 'virtio'},
+                    'type': 'balloon'},
+                   {'device': 'bridge', 'nicModel': 'virtio',
+                    'macAddr': '52:54:00:59:FF:FF', 'type': 'interface',
+                    'network': ''},
+                   {'device': 'hostdev', 'type': hwclass.NIC,
+                    'alias': 'hostdev2', 'hostdev': 'pci_0000_05_00_1',
+                    'deviceId': '6940d5e7-9814-4ae0-94ef-f78e68229e76',
+                    'macAddr': '00:00:00:00:00:43',
+                    'specParams': {'vlanid': 12}},
+                   ]
+
+        test_path = os.path.realpath(__file__)
+        dir_name = os.path.split(test_path)[0]
+        api_path = os.path.join(
+            dir_name, '..', 'data', 'testSRiovVm.xml')
+
+        domain = None
+        with open(api_path, 'r') as domxml:
+            domain = domxml.read()
+        with fake.VM(params=params, devices=devices,
+                     create_device_objects=True) as vm:
+            vm._domain = domain_descriptor.DomainDescriptor(domain)
+            vm._getUnderlyingVmDevicesInfo()
+            self.verifyDevicesConf(vm.conf['devices'])
+            self._assert_guest_device_adress_is_reported(vm)
+            self._assert_host_address_is_reported(devices, vm)
+
+    def _assert_host_address_is_reported(self, devices, vm):
+        reported = _reported_host_device(vm)
+        self.assertEqual(reported['hostdev'], devices[3]['hostdev'])
+
+    def _assert_guest_device_adress_is_reported(self, vm):
+        reported = _reported_host_device(vm)
+        self.assertEqual(
+            reported['address'],
+            {'slot': '0x07', 'bus': '0x99', 'domain': '0x0000', 'type': 'pci',
+             'function': '0x0'})
+
+
+def _reported_host_device(vm):
+    for dev in vm.conf['devices']:
+        if dev['device'] == 'hostdev':
+            return dev
