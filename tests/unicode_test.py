5a821ce952fa21c0b30cdc1cde95854f78f4f7ad
Nir Soffer <nsoffer@redhat.com>
2015-11-17 12:34:26 +0200

    startup: Change system default encoding to utf8
    In Python 2, the system default encoding is 'ascii'. This causes mixing
    of unicode and non-ascii strings (e.g. utf8 encoded) to fail with
    UnicodeDecodeError or UnicodeEncodeError. The trigger for this failures
    is starting using the built-in json library, that returns all values as
    unicode strings, even if the value is ascii.
    
    For example:
    
    >>> u'ascii' + '\xd7\x90'
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
    UnicodeDecodeError: 'ascii' codec can't decode byte 0xd7 in position 0:
    ordinal not in range(128)
    
    Python tries to decode the second value implicitly, and fails since this
    is a utf8 encoded string.
    
    To avoid such issues, the entire application must be changed to use only
    strings or only unicode internally, and never mix these types.  In the
    distant future, when we run on Python 3, this will be true. For now, the
    only way to prevent these issues systematically is to change the system
    default encoding to 'utf8'.
    
    Changing the default encoding is done with sys.setdefaultencoding(), but
    this function exists only during startup, and can be called only from
    sitecustomize module.
    
    We keep now sitecustomize.py module, and change PYTHONPATH so it will be
    loaded on startup.
    
    This change is not needed in Python 3 since the default encoding is
    already utf8, and Python does not do any implicit decoding or encoding
    when mixing 'str' and 'bytes' types.
    
    Change-Id: Icc3f072a499ba4034bdbedd09eb60d7a3a32f9c8
    Bug-Url: https://bugzilla.redhat.com/1281940
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/48661
    Continuous-Integration: Jenkins CI
    Reviewed-by: Milan Zamazal <mzamazal@redhat.com>
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Reviewed-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>

---
+++ tests/unicode_test.py
@@ -0,0 +1,67 @@
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from testlib import VdsmTestCase
+from testlib import permutations, expandPermutations
+
+ENCODE = [
+    # value, encoded (utf8)
+    (u'\u05d0', '\xd7\x90'),
+    ('\xd7\x90', '\xd7\x90'),
+    (u'ascii', 'ascii'),
+    ('ascii', 'ascii'),
+]
+
+DECODE = [
+    # value (utf8), decoded
+    ('\xd7\x90', u'\u05d0'),
+    (u'\u05d0', u'\u05d0'),
+    ('ascii', u'ascii'),
+    (u'ascii', u'ascii'),
+]
+
+
+@expandPermutations
+class TestUnicode(VdsmTestCase):
+
+    @permutations(ENCODE)
+    def test_encode(self, value, encoded):
+        self.assertEquals(value.encode("utf8"), encoded)
+
+    @permutations(ENCODE)
+    def test_str(self, value, encoded):
+        self.assertEqual(str(value), encoded)
+
+    @permutations(DECODE)
+    def test_decode(self, value, decoded):
+        self.assertEquals(value.decode("utf8"), decoded)
+
+    @permutations(DECODE)
+    def test_unicode(self, value, decoded):
+        self.assertEquals(unicode(value), decoded)
+
+    def test_mix_add(self):
+        self.assertEquals(u'\u05d0' + '\xd7\x91', u'\u05d0\u05d1')
+
+    def test_mix_format_str(self):
+        self.assertEquals(u'\u05d0%s' % '\xd7\x91', u'\u05d0\u05d1')
+
+    def test_mix_format_unicode(self):
+        self.assertEquals('\xd7\x90%s' % u'\u05d1', u'\u05d0\u05d1')
