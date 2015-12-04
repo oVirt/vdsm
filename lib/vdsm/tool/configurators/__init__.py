3c43e8a447ec7cf2558f4ccf74549ccd75ec5081
Petr Horáček <phoracek@redhat.com>
2015-12-04 14:46:39 +0100

    licenses: rename Hat and RedHat to Red Hat
    Tell me if Hat is another company.
    
    Change-Id: Ib719089dd9a8172e8005de3b536020f9f7344ad2
    Signed-off-by: Petr Horáček <phoracek@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/49718
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Tested-by: Dan Kenigsberg <danken@redhat.com>

--- lib/vdsm/tool/configurators/__init__.py
+++ lib/vdsm/tool/configurators/__init__.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2014 Hat, Inc.
+# Copyright 2014 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- lib/vdsm/tool/configurators/passwd.py
+++ lib/vdsm/tool/configurators/passwd.py
@@ -1,4 +1,4 @@
-# Copyright 2015 RedHat, Inc.
+# Copyright 2015 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
