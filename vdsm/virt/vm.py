0f1656379ae9887532cf1c236b326767385b4933
Francesco Romani <fromani@redhat.com>
2016-01-13 16:08:32 +0100

    vm: delete recovery file only after destroy()
    We must not re-create a recovery file after it
    is cancelled, because this will create an orphan
    recovery file, and ghost VMs (see BZ below).
    
    But we should also do not delete the recovery
    file too early, because Engine is in charge
    of collecting down VMs, and Vdsm is not really
    done with VMs until that moment.
    
    So, we move the cleanup of the recovery file
    in deleteVm. We kill the recovery file only
    when we wipe out the VM from the system, not earlier.
    
    Change-Id: I9513a72041e9514fd3aed8b2292f2ebdea4a03ae
    Bug-Url: https://bugzilla.redhat.com/1253043
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/51799
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/52517
    Reviewed-by: Milan Zamazal <mzamazal@redhat.com>

--- vdsm/virt/vm.py
+++ vdsm/virt/vm.py
@@ -824,8 +824,9 @@ class Vm(object):
 
                 os.rename(f.name, self._recoveryFile)
             else:
-                self.log.debug('saveState after cleanup for status=%s',
-                               self.lastStatus)
+                self.log.debug(
+                    'saveState after cleanup for status=%s destroyed=%s',
+                    self.lastStatus, self._destroyed)
 
         try:
             self._updateDomainDescriptor()
@@ -1689,7 +1690,6 @@ class Vm(object):
         self._cleanupDrives()
         self._cleanupFloppy()
         self._cleanupGuestAgent()
-        self._cleanupRecoveryFile()
         cleanup_guest_socket(self._qemuguestSocketFile)
         self._reattachHostDevices()
         self._cleanupStatsCache()
@@ -3877,6 +3877,7 @@ class Vm(object):
         except KeyError:
             self.log.exception("Failed to delete VM %s", self.id)
         else:
+            self._cleanupRecoveryFile()
             self.log.debug("Total desktops after destroy of %s is %d",
                            self.conf['vmId'], len(self.cif.vmContainer))
 
