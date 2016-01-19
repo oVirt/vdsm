ff3bf7861c69de920cfa37d3fb462b36a7a6a00e
Francesco Romani <fromani@redhat.com>
2016-01-19 10:51:50 +0100

    utils: add weakmethod helper
    The libvirtconnection module adds wrapping around virDomain
    objects, to detect and react libvirt issues, like timeouts and
    connection errors.
    
    This patch adds a helper function to wrap instance methods
    avoiding circular references which in turn may render some objects
    uncollectable, causing leaks.
    
    Note: this patch previously included tests, which were
    stripped to another patch, because of unrelated failures
    made them failed constantly.
    
    Change-Id: I9f26aa314e26142122e9f594275406cf7fbade98
    Bug-Url: https://bugzilla.redhat.com/1283999
    Backport-To: 3.6
    Backport-To: 3.5
    Reported-By: Eldad Marciano <emarcian@redhat.com>
    Reported-By: Milan Zamazal <mzamazal@redhat.com>
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/51865
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-by: Martin Polednik <mpolednik@redhat.com>
    Continuous-Integration: Jenkins CI
    Reviewed-on: https://gerrit.ovirt.org/52422
    Reviewed-on: https://gerrit.ovirt.org/52557

--- lib/vdsm/utils.py
+++ lib/vdsm/utils.py
@@ -52,6 +52,7 @@ import signal
 import stat
 import threading
 import time
+import weakref
 import zombiereaper
 
 from cpopen import CPopen
@@ -1386,3 +1387,29 @@ def stopwatch(message, log=logging.getLogger('vds.stopwatch')):
         log.debug("%s: %.2f seconds", message, elapsed)
     else:
         yield
+
+
+class InvalidatedWeakRef(Exception):
+    """
+    Stale weakref, the object was deallocated
+    """
+
+
+def weakmethod(meth):
+    """
+    Return a weakly-referenced wrapper for an instance method.
+    Use this function when you want to decorate an instance method
+    from the outside, to avoid reference cycles.
+    Raise InvalidatedWeakRef if the related instance was collected,
+    so the wrapped method is no longer usable.
+    """
+    func = meth.__func__
+    ref = weakref.ref(meth.__self__)
+
+    def wrapper(*args, **kwargs):
+        inst = ref()
+        if inst is None:
+            raise InvalidatedWeakRef()
+        return func(inst, *args, **kwargs)
+
+    return wrapper
