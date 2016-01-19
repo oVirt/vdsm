24a91e364c76b9d07211277c856556e96b154a7f
Francesco Romani <fromani@redhat.com>
2016-01-19 10:53:24 +0100

    tests: lib: add tests for utils.weakmethod
    This patch adds tests for utils.weakmethod().
    The tests were previously bundled with
    the same patch which added utils.weakmethod(),
    but they were failing for unrelated failures,
    like other bug or noisy test environment.
    
    The tests were stripped off to not needlessly
    stall the utils.weakmethod() patch, needed to
    plug a memory leak.
    
    Change-Id: I7f0b8642f3fafef28a76ee9cfaa481a73ddaf6aa
    Related-To: https://bugzilla.redhat.com/1283999
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/52408
    Continuous-Integration: Jenkins CI
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>

--- tests/utilsTests.py
+++ tests/utilsTests.py
@@ -18,12 +18,14 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
+from __future__ import print_function
 import collections
 import contextlib
 import copy
 import cpopen
 import errno
 import fcntl
+import gc
 import logging
 import operator
 import os
@@ -894,6 +896,61 @@ class StopwatchTests(TestCaseBase):
         with utils.stopwatch("message", log=log):
             pass
         self.assertEqual(log.messages, [])
+
+
+class ObjectWithDel(object):
+
+    def public(self, *args, **kw):
+        return 'public', args, kw
+
+    def __del__(self):
+        print('__del__', self.__class__.__name__)
+
+
+class TestWeakmethod(TestCaseBase):
+
+    def setUp(self):
+        self.saved_flags = gc.get_debug()
+        gc.disable()
+        gc.set_debug(0)
+
+    def tearDown(self):
+        gc.collect()
+        for obj in gc.garbage:
+            if type(obj) is ObjectWithDel:
+                obj.public = None
+                gc.garbage.remove(obj)
+        gc.set_debug(self.saved_flags)
+        gc.enable()
+
+    def test_with_reference_cycle(self):
+        def _leaking_wrapper(meth):
+            def wrapper(*args, **kwargs):
+                return meth(*args, **kwargs)
+            return wrapper
+
+        obj = ObjectWithDel()
+        obj.public = _leaking_wrapper(obj.public)
+        self.assertEquals(obj.public(), ("public", (), {}))
+        del obj
+        gc.collect()
+        self.assertIn(ObjectWithDel, [type(obj) for obj in gc.garbage])
+
+    def test_without_reference_cycle(self):
+        obj = ObjectWithDel()
+        obj.public = utils.weakmethod(obj.public)
+        self.assertEquals(obj.public(), ("public", (), {}))
+        del obj
+        gc.collect()
+        self.assertNotIn(ObjectWithDel, [type(obj) for obj in gc.garbage])
+
+    def test_raise_on_invalid_weakref(self):
+        obj = ObjectWithDel()
+        method = utils.weakmethod(obj.public)
+        obj.public = method
+        self.assertEquals(obj.public(), ("public", (), {}))
+        del obj
+        self.assertRaises(utils.InvalidatedWeakRef, method)
 
 
 class NoIntrPollTests(TestCaseBase):
