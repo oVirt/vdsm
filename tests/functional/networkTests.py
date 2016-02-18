84bffd45017a60cc8b1df5d9a87cc1e380299196
Dan Kenigsberg <danken@redhat.com>
2016-02-18 17:47:14 +0200

    net tests: make functional tests run again with persistence=ifcfg
    We have multiple users that use net_persistence=ifcfg. We must make sure
    that Vdsm does not inadvertently breaks them.
    
    This patch only makes the tests run again, after recent changes assuming
    unified persistence.
    
    Change-Id: I849172cdab80e760a65ac19d367064b1dbf81b03
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1310410
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Signed-off-by: Edward Haas <edwardh@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/53713
    Continuous-Integration: Jenkins CI
    Reviewed-on: https://gerrit.ovirt.org/53775
    Reviewed-by: Francesco Romani <fromani@redhat.com>

--- tests/functional/networkTests.py
+++ tests/functional/networkTests.py
@@ -212,8 +212,6 @@ class NetworkTest(TestCaseBase):
     def assertNetworkExists(self, networkName, bridged=None, bridgeOpts=None,
                             hostQos=None, assert_in_running_conf=True):
         netinfo = self.vdsm_net.netinfo
-        config = self.vdsm_net.config
-        network_config = config.networks[networkName]
         network_netinfo = netinfo.networks[networkName]
         self.assertIn(networkName, netinfo.networks)
         if bridged is not None:
@@ -230,8 +228,14 @@ class NetworkTest(TestCaseBase):
             reported_qos = network_netinfo['hostQos']
             _cleanup_qos_definition(reported_qos)
             self.assertEqual(reported_qos, hostQos)
-        if assert_in_running_conf and config is not None:
-            self.assertIn(networkName, config.networks)
+
+        if not vdsm.config.config.get('vars', 'net_persistence') == 'unified':
+            return
+
+        running_config = self.vdsm_net.config
+        network_config = running_config.networks[networkName]
+        if assert_in_running_conf:
+            self.assertIn(networkName, running_config.networks)
             if bridged is not None:
                 self.assertEqual(network_config.get('bridged'), bridged)
         physical_iface = (network_config.get('bonding') or
