ed9b42b0ba40071e69bfa977af36529471ef51e1
Francesco Romani <fromani@redhat.com>
2016-02-24 17:06:24 +0100

    virt: adding virt basic api module to supervdsm
    Change-Id: I9acf5a2f8fd14ac172bb6ea70b8888bc2aff8caf
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/54004
    Continuous-Integration: Jenkins CI
    Reviewed-by: Yaniv Bronhaim <ybronhei@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ vdsm/supervdsm_api/virt.py
@@ -0,0 +1,68 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+from __future__ import absolute_import
+
+import os
+import re
+import stat
+
+from storage.fileUtils import resolveGid
+from vdsm.constants import P_LIBVIRT_VMCHANNELS, P_OVIRT_VMCONSOLES
+import numaUtils
+
+from . import expose
+
+
+@expose
+def prepareVmChannel(socketFile, group=None):
+    if (socketFile.startswith(P_LIBVIRT_VMCHANNELS) or
+       socketFile.startswith(P_OVIRT_VMCONSOLES)):
+        fsinfo = os.stat(socketFile)
+        mode = fsinfo.st_mode | stat.S_IWGRP
+        os.chmod(socketFile, mode)
+        if group is not None:
+            os.chown(socketFile,
+                     fsinfo.st_uid,
+                     resolveGid(group))
+    else:
+        raise Exception("Incorporate socketFile")
+
+
+@expose
+def getVmPid(vmName):
+    pidFile = "/var/run/libvirt/qemu/%s.pid" % vmName
+    with open(pidFile) as pid:
+        return pid.read()
+
+
+@expose
+def getVcpuNumaMemoryMapping(vmName):
+    vmPid = getVmPid(vmName)
+    vCpuPids = numaUtils.getVcpuPid(vmName)
+    vCpuIdxToNode = {}
+    for vCpuIndex, vCpuPid in vCpuPids.iteritems():
+        numaMapsFile = "/proc/%s/task/%s/numa_maps" % (vmPid, vCpuPid)
+        try:
+            with open(numaMapsFile, 'r') as f:
+                mappingNodes = map(
+                    int, re.findall('N(\d+)=\d+', f.read()))
+                vCpuIdxToNode[vCpuIndex] = list(set(mappingNodes))
+        except IOError:
+            continue
+    return vCpuIdxToNode
