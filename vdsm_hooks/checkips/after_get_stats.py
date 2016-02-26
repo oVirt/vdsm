cb76c68eaf07f106d8aab6076a1634100129b441
Lukianov Artyom <alukiano@redhat.com>
2016-02-27 00:36:12 +0200

    hooks:checkips: add checkips hook
    Give possibilty to check connectivity between host and given addresses.
    Update network stats with state of addresses.
    
    Bug-Url: https://bugzilla.redhat.com/994170
    Change-Id: I53cec37310f0f1844d6fe244419fd8c10e9b7ebb
    Signed-off-by: Lukianov Artyom <alukiano@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/54102
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ vdsm_hooks/checkips/after_get_stats.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+import os
+import six
+import sys
+import tempfile
+import time
+import traceback
+
+import checkips_utils
+import hooking
+from vdsm import constants
+import vdsm.netconfpersistence as persist_net
+
+HELP_ARG = '-h'
+TEST_ARG = '-t'
+HELP_TEXT = """usage: %(prog)s [%(help)s] [%(test)s]
+
+Check connectivity from host to specific addresses via ping
+
+optional arguments:
+  %(help)s  show this help message and exit
+  %(test)s  run a dry test for the hook
+""" % {
+    'help': HELP_ARG,
+    'test': TEST_ARG,
+    'prog': sys.argv[0]
+}
+
+CONNECTIVITY_TIMEOUT = 60
+
+
+def _is_network_accessible(net, stats_dir):
+    file_path = os.path.join(stats_dir, net)
+    if os.path.exists(file_path):
+        return (
+            time.time() - os.stat(file_path).st_mtime <=
+            CONNECTIVITY_TIMEOUT
+        )
+    return False
+
+
+def update_networks_state(stats_json, networks, stats_dir):
+    for net, net_attrs in six.iteritems(networks):
+        ping_addresses = checkips_utils.get_ping_addresses(net_attrs)
+        if ping_addresses and not _is_network_accessible(net, stats_dir):
+            net_if = None
+            if 'nic' in net_attrs:
+                net_if = net_attrs['nic']
+            elif 'bonding' in net_attrs:
+                net_if = net_attrs['bonding']
+            if net_if:
+                stats_json['network'][net_if]['state'] = 'down'
+
+
+def test():
+    temp_dir = tempfile.mkdtemp()
+    checkips_utils.touch('check_ipv4', temp_dir)
+    checkips_utils.touch('check_ipv6', temp_dir)
+    try:
+        networks = {
+            'check_ipv4':
+                {
+                    'nic': 'eth0_ipv4',
+                    'custom': {
+                        'checkipv4': '127.0.0.1'
+                    }
+                },
+            'check_ipv6':
+                {
+                    'bonding': 'bond_ipv6',
+                    'custom': {
+                        'checkipv6': '::1'
+                    }
+                },
+            'check_fqdn':
+                {
+                    'nic': 'eth_fqdn',
+                    'custom': {
+                        'checkipv4': 'test.test'
+                    }
+                }
+        }
+        network_stats = {
+            'network': {
+                'eth0_ipv4': {
+                    'state': 'up'
+                },
+                'bond_ipv6': {
+                    'state': 'up'
+                },
+                'eth_fqdn': {
+                    'state': 'up'
+                }
+            }
+        }
+        update_networks_state(network_stats, networks, temp_dir)
+        expected_states = {
+            'eth0_ipv4': 'up',
+            'bond_ipv6': 'up',
+            'eth_fqdn': 'down'
+        }
+        for interface, state in six.iteritems(expected_states):
+            test_msg = 'pass'
+            if network_stats['network'][interface]['state'] != state:
+                test_msg = 'fail'
+            print(
+                'test %s: interface %s has state %s' %
+                (test_msg, interface, state)
+            )
+    finally:
+        os.unlink(os.path.join(temp_dir, 'check_ipv4'))
+        os.unlink(os.path.join(temp_dir, 'check_ipv6'))
+        os.rmdir(temp_dir)
+
+
+def main():
+    stats_json = hooking.read_json()
+    networks = persist_net.PersistentConfig().networks
+    update_networks_state(stats_json, networks, constants.P_VDSM_RUN)
+    hooking.write_json(stats_json)
+
+
+if __name__ == '__main__':
+    if HELP_ARG in sys.argv:
+        hooking.exit_hook(HELP_TEXT)
+
+    try:
+        if TEST_ARG in sys.argv:
+            test()
+        else:
+            main()
+    except:
+        hooking.exit_hook(
+            'checkips hook: [unexpected error]: %s\n' %
+            traceback.format_exc()
+        )
---
+++ vdsm_hooks/checkips/checkips_utils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+#
+# Copyright 2008-2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+import os
+
+CHECKIPV4 = 'checkipv4'
+CHECKIPV6 = 'checkipv6'
+
+
+def touch(net, file_dir):
+    file_path = os.path.join(file_dir, net)
+    with open(file_path, 'a'):
+        os.utime(file_path, None)
+
+
+def get_ping_addresses(net_attrs):
+    ping_addresses = []
+    if 'custom' in net_attrs:
+        for address_type in (CHECKIPV4, CHECKIPV6):
+            if address_type in net_attrs['custom']:
+                ping_addresses.append(
+                    (
+                        address_type, net_attrs['custom'][address_type]
+                    )
+                )
+    return ping_addresses
