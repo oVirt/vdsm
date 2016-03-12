7b183967af53ea5373465df12d6443393064a6ad
Nir Soffer <nsoffer@redhat.com>
2016-03-12 03:59:12 +0200

    tests: Simplify DirectFile tests
    For diretio we support only full blocks, so using generated data aligned
    to block size simplifies the tests.
    
    Use permutations to test combinations that were not tested before.
    
    Change-Id: Ic86b0fe22d5d5f1c9252b658ad76a13626de8246
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/54678
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Continuous-Integration: Jenkins CI

--- tests/fileUtilTests.py
+++ tests/fileUtilTests.py
@@ -25,111 +25,53 @@ import testValidation
 from testlib import VdsmTestCase as TestCaseBase
 from testlib import temporaryPath
 from testlib import namedTemporaryDir
+from testlib import permutations, expandPermutations
 
 
+@expandPermutations
 class DirectFileTests(TestCaseBase):
 
-    def testRead(self):
-        data = """Vestibulum. Libero leo nostra, pede nunc eu. Pellentesque
-        platea lacus morbi nisl montes ve. Ac. A, consectetuer erat, justo eu.
-        Elementum et, phasellus fames et rutrum donec magnis eu bibendum. Arcu,
-        ante aliquam ipsum ut facilisis ad."""
-        with temporaryPath(data=data) as srcPath, \
-                fileUtils.open_ex(srcPath, "dr") as f:
-            self.assertEquals(f.read(), data)
+    DATA = "a" * 512 + "b" * 512
 
-    def testSeekRead(self):
-        data = """
-        Habitasse ipsum at fusce litora metus, placerat dui purus aenean ante,
-        ve. Pede hymenaeos ut primis cum, rhoncus, lectus, nunc. Vestibulum
-        curabitur vitae etiam magna auctor velit, mi tempus vivamus orci eros.
-        Pellentesque curabitur risus fermentum eget. Elementum curae, donec
-        nisl egestas ve, ut odio eu nunc elit felis primis id. Ridiculus metus
-        morbi nulla erat, amet nisi. Amet ligula nisi, id penatibus risus in.
-        Purus velit duis. Aenean eget, pellentesque eu rhoncus arcu et
-        consectetuer laoreet, augue nisi dictum lacinia urna. Fermentum
-        torquent. Ut interdum vivamus duis. Felis consequat nec pede. Orci
-        sollicitudin parturient orci felis. Enim, diam velit sapien
-        condimentum fames semper nibh. Integer at, egestas pede consectetuer
-        ac augue pharetra dolor non placerat quisque id cursus ultricies.
-        Ligula mi senectus sit. Habitasse. Integer sollicitudin dapibus cum
-        quam.
-        """
-        self.assertTrue(len(data) > 512)
-        with temporaryPath(data=data) as srcPath, \
+    @permutations([[0], [512], [1024], [1024 + 512]])
+    def testRead(self, size):
+        with temporaryPath(data=self.DATA) as srcPath, \
                 fileUtils.open_ex(srcPath, "dr") as f:
-            f.seek(512)
-            self.assertEquals(f.read(), data[512:])
+            self.assertEquals(f.read(size), self.DATA[:size])
+
+    @permutations([[512], [1024]])
+    def testSeekRead(self, offset):
+        with temporaryPath(data=self.DATA) as srcPath, \
+                fileUtils.open_ex(srcPath, "dr") as f:
+            f.seek(offset)
+            self.assertEquals(f.read(), self.DATA[offset:])
 
     def testWrite(self):
-        data = """In ut non platea egestas, quisque magnis nunc nostra ac etiam
-        suscipit nec integer sociosqu. Fermentum. Ante orci luctus, ipsum
-        ullamcorper enim arcu class neque inceptos class. Ut, sagittis
-        torquent, commodo facilisi."""
         with temporaryPath() as srcPath, fileUtils.open_ex(srcPath, "dw") as f:
-            f.write(data)
+            f.write(self.DATA)
             with fileUtils.open_ex(srcPath, "r") as f:
-                self.assertEquals(f.read(len(data)), data)
+                self.assertEquals(f.read(), self.DATA)
 
     def testSmallWrites(self):
-        data = """
-        Aliquet habitasse tellus. Fringilla faucibus tortor parturient
-        consectetuer sodales, venenatis platea habitant. Hendrerit nostra nunc
-        odio. Primis porttitor consequat enim ridiculus. Taciti nascetur,
-        nibh, convallis sit, cum dis mi. Nonummy justo odio cursus, ac hac
-        curabitur nibh. Tellus. Montes, ut taciti orci ridiculus facilisis
-        nunc. Donec. Risus adipiscing habitant donec vehicula non vitae class,
-        porta vitae senectus. Nascetur felis laoreet integer, tortor ligula.
-        Pellentesque vestibulum cras nostra. Ut sollicitudin posuere, per
-        accumsan curabitur id, nisi fermentum vel, eget netus tristique per,
-        donec, curabitur senectus ut fusce. A. Mauris fringilla senectus et
-        eni facilisis magna inceptos eu, cursus habitant fringilla neque.
-        Nibh. Elit facilisis sed, elit, nostra ve torquent dictumst, aenean
-        sapien quam, habitasse in. Eu tempus aptent, diam, nisi risus
-        pharetra, ac, condimentum orci, consequat mollis. Cras lacus augue
-        ultrices proin fermentum nibh sed urna. Ve ipsum ultrices curae,
-        feugiat faucibus proin et elementum vivamus, lectus. Torquent. Tempus
-        facilisi. Cras suspendisse euismod consectetuer ornare nostra. Fusce
-        amet cum amet diam.
-        """
-        self.assertTrue(len(data) > 512)
-
         with temporaryPath() as srcPath, \
                 fileUtils.open_ex(srcPath, "dw") as f:
-            f.write(data[:512])
-            f.write(data[512:])
+            f.write(self.DATA[:512])
+            f.write(self.DATA[512:])
 
             with fileUtils.open_ex(srcPath, "r") as f:
-                self.assertEquals(f.read(len(data)), data)
+                self.assertEquals(f.read(), self.DATA)
 
     def testUpdateRead(self):
-        data = """
-        Aliquet. Aliquam eni ac nullam iaculis cras ante, adipiscing. Enim
-        eget egestas pretium. Ultricies. Urna cubilia in, hac. Curabitur.
-        Nibh. Purus ridiculus natoque sed id. Feugiat lacus quam, arcu
-        maecenas nec egestas. Hendrerit duis nunc eget dis lacus porttitor per
-        sodales class diam condimentum quisque condimentum nisi ligula.
-        Dapibus blandit arcu nam non ac feugiat diam, dictumst. Ante eget
-        fames eu penatibus in, porta semper accumsan adipiscing tellus in
-        sagittis. Est parturient parturient mi fermentum commodo, per
-        fermentum. Quis duis velit at quam risus mi. Facilisi id fames.
-        Turpis, conubia rhoncus. Id. Elit eni tellus gravida, ut, erat morbi.
-        Euismod, enim a ante vestibulum nibh. Curae curae primis vulputate
-        adipiscing arcu ipsum suspendisse quam hymenaeos primis accumsan
-        vestibulum.
-        """
-        self.assertTrue(len(data) > 512)
-
         with temporaryPath() as srcPath, \
                 fileUtils.open_ex(srcPath, "wd") as f:
-            f.write(data[:512])
+            f.write(self.DATA[:512])
 
             with fileUtils.open_ex(srcPath, "r+d") as f:
                 f.seek(512)
-                f.write(data[512:])
+                f.write(self.DATA[512:])
 
             with fileUtils.open_ex(srcPath, "r") as f:
-                self.assertEquals(f.read(len(data)), data)
+                self.assertEquals(f.read(), self.DATA)
 
 
 class CreatedirTests(TestCaseBase):
