d7d0f52e88c7db0326d800c95421509787a9db9a
Nir Soffer <nsoffer@redhat.com>
2016-03-15 12:21:44 +0200

    monitoring: Introduce the check module
    This module provides event loop based infrastructure for scalable
    storage health monitoring. We plan to have a dedicated thread running
    all storage domain checkers, protected from blocking tasks performed on
    the storage domain monitor threads.
    
    Change-Id: If22fe38b8b29116270f9012b75895506adc48852
    Bug-Url: https://bugzilla.redhat.com/1081962
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/54757
    Continuous-Integration: Jenkins CI
    Reviewed-by: Freddy Rolland <frolland@redhat.com>

---
+++ lib/vdsm/storage/check.py
@@ -0,0 +1,387 @@
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+"""
+This module provides event loop based infrastructure for scalable storage
+health monitoring:
+
+CheckService     entry point for starting and stopping path checkers.
+
+DirectioChecker  checker using dd process for file or block based
+                 volumes.
+
+CheckResult      result object provided to user callback on each check.
+"""
+
+from __future__ import absolute_import
+
+import logging
+import re
+import threading
+
+import cpopen
+
+from vdsm import cmdutils
+from vdsm import concurrent
+from vdsm import constants
+from vdsm.storage import asyncevent
+from vdsm.storage import exception
+
+EXEC_ERROR = 127
+
+_log = logging.getLogger("storage.check")
+
+
+class CheckService(object):
+    """
+    Provides path checking service.
+
+    This object is a simple thread safe entry point for starting and stopping
+    path checkers, keeping the internals decoupled from client code.
+
+    Usage:
+
+        # Start the service
+
+        service = CheckService()
+        service.start()
+
+        # Start checking path
+
+        service.start_checking(path, complete)
+
+        # Stop checking path, waiting up to 30 seconds
+
+        service.stop_checking(path, timeout=30)
+
+        # Stop the service
+
+        service.stop()
+
+    """
+
+    def __init__(self):
+        self._lock = threading.Lock()
+        self._loop = asyncevent.EventLoop()
+        self._thread = concurrent.thread(self._loop.run_forever)
+        self._checkers = {}
+
+    def start(self):
+        """
+        Start the service thread.
+        """
+        _log.info("Starting check service")
+        self._thread.start()
+
+    def stop(self):
+        """
+        Stop all checkers and the service thread.
+
+        Do not wait for running check processes since the application is
+        shutting down. To wait for all processes, stop all checkers and wait
+        for them before stoping.
+        """
+        if not self._thread.is_alive():
+            return
+        _log.info("Stopping check service")
+        with self._lock:
+            for checker in self._checkers.values():
+                self._loop.call_soon_threadsafe(checker.stop)
+            self._checkers.clear()
+            self._loop.call_soon_threadsafe(self._loop.stop)
+            self._thread.join()
+            self._loop.close()
+
+    def start_checking(self, path, complete, interval=10.0):
+        """
+        Start checking path every interval secconds. On check, invoke the
+        complete callback with a CheckResult instance.
+
+        Note that the complete callback is invoked in the check thread, and
+        must not block, as it will block all other checkers.
+        """
+        _log.info("Start checking %r", path)
+        with self._lock:
+            if path in self._checkers:
+                raise RuntimeError("Already checking path %r" % path)
+            checker = DirectioChecker(self._loop, path, complete,
+                                      interval=interval)
+            self._checkers[path] = checker
+        self._loop.call_soon_threadsafe(checker.start)
+
+    def stop_checking(self, path, timeout=None):
+        """
+        Stop checking path. If timeout is set, wait until the checker has
+        stopped, or the timeout has expired.
+        """
+        _log.info("Stop checking %r", path)
+        with self._lock:
+            checker = self._checkers.pop(path)
+        self._loop.call_soon_threadsafe(checker.stop)
+        if timeout:
+            return checker.wait(timeout)
+
+    def is_checking(self, path):
+        return path in self._checkers
+
+
+# Checker state
+IDLE = "idle"
+RUNNING = "running"
+STOPPING = "stopping"
+
+
+class DirectioChecker(object):
+    """
+    Check path availability using direct I/O.
+
+    DirectioChecker is created with a complete callback.  Each time a check
+    cycle is completed, the complete callback will be invoked with a
+    CheckResult instance.
+
+    CheckResult provides a delay() method returning the read delay in
+    seconds. If the check failed, the delay() method will raise the
+    appropriate exception that can be reported to engine.
+
+    Note that the complete callback must not block as it will block the entire
+    event loop thread.
+
+    The checker runs exactly every interval seconds. If a check did not
+    complete before the next check is scheduled, the next check will be delayed
+    to the next interval.
+
+    Checker is not thread safe. Use EventLoop.call_soon_threadsafe() to start
+    or stop a checker. The only thread safe method is wait().
+
+    Usage:
+
+        # Start the event loop thread
+
+        loop = asyncevent.EventLoop()
+        concurrent.thread(loop.run_forever).start()
+
+        # The complete callback
+
+        def complete(result):
+            try:
+                check_delay = result.delay()
+            except Exception as e:
+                check_error = e
+            check_time = time.time()
+
+        # Start a checker on the event loop thread
+
+        checker = DirectioChecker(loop, path, complete)
+        loop.call_soon_threadsafe(checker.start)
+
+        ...
+
+        # Stop a checker from another thread
+
+        loop.call_soon_threadsafe(checker.stop)
+
+        # If needed, wait until a checker actually stopped.
+
+        checker.wait(30)
+
+    """
+
+    log = logging.getLogger("storage.directiochecker")
+
+    def __init__(self, loop, path, complete, interval=10.0):
+        self._loop = loop
+        self._path = path
+        self._complete = complete
+        self._interval = interval
+        self._next_check = None
+        self._check_time = None
+        self._timer = None
+        self._proc = None
+        self._reader = None
+        self._reaper = None
+        self._err = None
+        self._state = IDLE
+        self._stopped = threading.Event()
+
+    def start(self):
+        """
+        Start the checker.
+
+        Raises RuntimeError if the checker is running.
+        """
+        if self._state is not IDLE:
+            raise RuntimeError("Checker is %s", self._state)
+        self._state = RUNNING
+        _log.debug("Checker %r started", self._path)
+        self._stopped.clear()
+        self._next_check = self._loop.time()
+        self._check()
+
+    def stop(self):
+        """
+        Stop the checker.
+
+        If the checker is waiting for the next check, the next check will be
+        cancelled. If the checker is in the middle of a check, it will stop
+        when the check completes.
+
+        If the checker is not running, the call is ignored silently.
+        """
+        if self._state is not RUNNING:
+            return
+        _log.debug("Checker %r stopping", self._path)
+        self._state = STOPPING
+        if self._timer:
+            self._timer.cancel()
+            self._timer = None
+        if self._proc is None:
+            self._stop_completed()
+
+    def wait(self, timeout=None):
+        """
+        Wait until a checker has stopped.
+
+        Returns True if checker has stopped, False if timeout expired.
+        """
+        return self._stopped.wait(timeout)
+
+    def is_running(self):
+        return self._state is not IDLE
+
+    def _stop_completed(self):
+        self._state = IDLE
+        _log.debug("Checker %r stopped", self._path)
+        self._stopped.set()
+
+    def _check(self):
+        """
+        Called when starting the checker, and then every interval seconds until
+        the checker is stopped.
+        """
+        assert self._state is RUNNING
+        self._timer = None
+        self._check_time = self._loop.time()
+        try:
+            self._start_process()
+        except Exception as e:
+            self._err = "Error starting process: %s" % e
+            self._check_completed(EXEC_ERROR)
+
+    def _start_process(self):
+        """
+        Starts a dd process performing direct I/O to path, reading the process
+        stderr. When stderr has closed, _read_completed will be called.
+        """
+        cmd = [constants.EXT_DD, "if=%s" % self._path, "of=/dev/null",
+               "bs=4096", "count=1", "iflag=direct"]
+        cmd = cmdutils.wrap_command(cmd)
+        _log.debug("START check %r cmd=%s delay=%.2f",
+                   self._path, cmd, self._check_time - self._next_check)
+        self._proc = cpopen.CPopen(cmd, stdin=None, stdout=None,
+                                   stderr=cpopen.PIPE)
+        self._reader = self._loop.create_dispatcher(
+            asyncevent.BufferedReader, self._proc.stderr, self._read_completed)
+
+    def _read_completed(self, data):
+        """
+        Called when dd process has closed stderr. At this point the process may
+        be still running.
+        """
+        assert self._state is not IDLE
+        self._reader = None
+        self._err = data
+        rc = self._proc.poll()
+        # About 95% of runs, the process has terminated at this point. If not,
+        # start the reaper to wait for it.
+        if rc is None:
+            self._reaper = asyncevent.Reaper(self._loop, self._proc,
+                                             self._check_completed)
+            return
+        self._check_completed(rc)
+
+    def _check_completed(self, rc):
+        """
+        Called when the dd process has exited with exit code rc.
+        """
+        assert self._state is not IDLE
+        now = self._loop.time()
+        elapsed = now - self._check_time
+        _log.debug("FINISH check %r rc=%s err=%r elapsed=%.02f",
+                   self._path, rc, self._err, elapsed)
+        self._reaper = None
+        self._proc = None
+        if self._state is STOPPING:
+            self._stop_completed()
+            return
+        self._schedule_next_check(now)
+        result = CheckResult(self._path, rc, self._err, self._check_time,
+                             elapsed)
+        self._complete(result)
+
+    def _schedule_next_check(self, now):
+        while self._next_check <= now:
+            self._next_check += self._interval
+        self._timer = self._loop.call_at(self._next_check, self._check)
+
+    def __repr__(self):
+        info = [self.__class__.__name__,
+                self._path,
+                self._state]
+        if self._state is RUNNING:
+            info.append("next_check=%.2f" % self._next_check)
+        return "<%s at 0x%x>" % (" ".join(info), id(self))
+
+
+class CheckResult(object):
+
+    _PATTERN = re.compile(
+        r"\d+ bytes? \([\de\-.]+ [kMGT]*B\) copied, "
+        r"([\de\-.]+) s, "
+        r"(?:[\de\-.]+|Infinity) [kMGT]*B/s")
+
+    def __init__(self, path, rc, err, time, elapsed):
+        self.path = path
+        self.rc = rc
+        self.err = err
+        self.time = time
+        self.elapsed = elapsed
+
+    def delay(self):
+        # TODO: Raising MiscFileReadException for all errors to keep the old
+        # behavior. Should probably use StorageDomainAccessError.
+        if self.rc != 0:
+            raise exception.MiscFileReadException(self.path, self.rc, self.err)
+        if not self.err:
+            raise exception.MiscFileReadException(self.path, "no stats")
+        stats = self.err.splitlines()[-1]
+        match = self._PATTERN.match(stats)
+        if not match:
+            raise exception.MiscFileReadException(self.path,
+                                                  "no match: %r" % stats)
+        seconds = match.group(1)
+        try:
+            return float(seconds)
+        except ValueError as e:
+            raise exception.MiscFileReadException(self.path, e)
+
+    def __repr__(self):
+        return "<%s path=%s rc=%d err=%r time=%.2f elapsed=%.2f at 0x%x>" % (
+            self.__class__.__name__, self.path, self.rc, self.err, self.time,
+            self.elapsed, id(self))
---
+++ tests/storage_check_test.py
@@ -0,0 +1,379 @@
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import print_function
+
+import os
+import pprint
+import threading
+import time
+from contextlib import contextmanager
+
+from monkeypatch import MonkeyPatch
+from monkeypatch import MonkeyPatchScope
+from testValidation import slowtest
+from testlib import VdsmTestCase
+from testlib import expandPermutations, permutations
+from testlib import temporaryPath
+
+from vdsm import concurrent
+from vdsm import constants
+from vdsm.storage import check
+from vdsm.storage import asyncevent
+from vdsm.storage import exception
+
+
+@expandPermutations
+class TestDirectioChecker(VdsmTestCase):
+
+    def setUp(self):
+        self.loop = asyncevent.EventLoop()
+        self.results = []
+        self.checks = 1
+
+    def tearDown(self):
+        self.loop.close()
+
+    def complete(self, result):
+        self.results.append(result)
+        if len(self.results) == self.checks:
+            self.loop.stop()
+
+    def test_path_missing(self):
+        self.checks = 1
+        checker = check.DirectioChecker(self.loop, "/no/such/path",
+                                        self.complete)
+        checker.start()
+        self.loop.run_forever()
+        pprint.pprint(self.results)
+        result = self.results[0]
+        self.assertRaises(exception.MiscFileReadException, result.delay)
+
+    def test_path_ok(self):
+        self.checks = 1
+        with temporaryPath(data="blah") as path:
+            checker = check.DirectioChecker(self.loop, path, self.complete)
+            checker.start()
+            self.loop.run_forever()
+            pprint.pprint(self.results)
+            result = self.results[0]
+            delay = result.delay()
+            print("delay:", delay)
+            self.assertEqual(type(delay), float)
+
+    @MonkeyPatch(constants, "EXT_DD", "/no/such/executable")
+    def test_executable_missing(self):
+        self.checks = 1
+        with temporaryPath(data="blah") as path:
+            checker = check.DirectioChecker(self.loop, path, self.complete)
+            checker.start()
+            self.loop.run_forever()
+            pprint.pprint(self.results)
+            result = self.results[0]
+            self.assertRaises(exception.MiscFileReadException, result.delay)
+
+    @MonkeyPatch(constants, "EXT_TASKSET", "/no/such/executable")
+    def test_taskset_missing(self):
+        self.checks = 1
+        with temporaryPath(data="blah") as path:
+            checker = check.DirectioChecker(self.loop, path, self.complete)
+            checker.start()
+            self.loop.run_forever()
+            pprint.pprint(self.results)
+            result = self.results[0]
+            self.assertRaises(exception.MiscFileReadException, result.delay)
+
+    @slowtest
+    @permutations([
+        # interval, delay, expected
+        (0.2, 0.1, 0.2),
+        (0.1, 0.1, 0.2),
+    ])
+    def test_interval(self, interval, delay, expected):
+        self.checks = 5
+        clock_res = 0.01
+        with fake_dd(delay):
+            checker = check.DirectioChecker(self.loop, "/path", self.complete,
+                                            interval=interval)
+            checker.start()
+            self.loop.run_forever()
+            pprint.pprint(self.results)
+            for i in range(self.checks - 1):
+                r1 = self.results[i]
+                r2 = self.results[i + 1]
+                actual = r2.time - r1.time
+                self.assertGreater(actual, expected - clock_res)
+                self.assertLess(actual, expected + clock_res)
+
+    def test_idle_stop_ignored(self):
+        self.checks = 1
+        checker = check.DirectioChecker(self.loop, "/path", self.complete)
+        checker.stop()  # Will be ignored
+        checker.start()
+        self.loop.run_forever()
+        self.assertTrue(checker.is_running())
+
+    def test_idle_repr(self):
+        checker = check.DirectioChecker(self.loop, "/path", self.complete)
+        print(checker)
+        self.assertIn("/path", str(checker))
+        self.assertIn(check.IDLE, str(checker))
+        self.assertNotIn("next_check=", str(checker))
+
+    def test_running_start_raises(self):
+        checker = check.DirectioChecker(self.loop, "/path", self.complete)
+        checker.start()
+        self.assertRaises(RuntimeError, checker.start)
+
+    def test_running_repr(self):
+        checker = check.DirectioChecker(self.loop, "/path", self.complete)
+        checker.start()
+        print(checker)
+        self.assertIn("/path", str(checker))
+        self.assertIn(check.RUNNING, str(checker))
+        self.assertIn("next_check=", str(checker))
+
+    def test_stopping_stop_ignored(self):
+        checker = check.DirectioChecker(self.loop, "/path", self.complete)
+        checker.start()
+        checker.stop()
+        checker.stop()  # Will be ignored
+        self.assertTrue(checker.is_running())
+
+    def test_stopping_start_raises(self):
+        checker = check.DirectioChecker(self.loop, "/path", self.complete)
+        checker.start()
+        checker.stop()
+        self.assertRaises(RuntimeError, checker.start)
+
+    def test_stopping_repr(self):
+        checker = check.DirectioChecker(self.loop, "/path", self.complete)
+        checker.start()
+        checker.stop()
+        print(checker)
+        self.assertIn("/path", str(checker))
+        self.assertIn(check.STOPPING, str(checker))
+        self.assertNotIn("next_check=", str(checker))
+
+
+@expandPermutations
+class TestDirectioCheckerWaiting(VdsmTestCase):
+
+    def setUp(self):
+        self.loop = asyncevent.EventLoop()
+        self.thread = concurrent.thread(self.loop.run_forever)
+        self.thread.start()
+        self.completed = threading.Event()
+
+    def tearDown(self):
+        self.loop.call_soon_threadsafe(self.loop.stop)
+        self.thread.join()
+        self.loop.close()
+
+    def complete(self, result):
+        self.completed.set()
+
+    def test_running_stop_during_wait(self):
+        checker = check.DirectioChecker(self.loop, "/path", self.complete)
+        self.loop.call_soon_threadsafe(checker.start)
+        self.assertTrue(self.completed.wait(1.0))
+        self.loop.call_soon_threadsafe(checker.stop)
+        self.assertTrue(checker.wait(1.0))
+        self.assertFalse(checker.is_running())
+
+    @slowtest
+    def test_running_stop_during_check(self):
+        with fake_dd(0.2):
+            checker = check.DirectioChecker(self.loop, "/path", self.complete)
+            self.loop.call_soon_threadsafe(checker.start)
+            self.loop.call_soon_threadsafe(checker.stop)
+            self.assertTrue(checker.wait(1.0))
+            self.assertFalse(self.completed.is_set())
+            self.assertFalse(checker.is_running())
+
+    @slowtest
+    def test_stopping_timeout(self):
+        with fake_dd(0.2):
+            checker = check.DirectioChecker(self.loop, "/path", self.complete)
+            self.loop.call_soon_threadsafe(checker.start)
+            self.loop.call_soon_threadsafe(checker.stop)
+            self.assertFalse(checker.wait(0.1))
+            self.assertTrue(checker.is_running())
+
+
+@expandPermutations
+class TestDirectioCheckerTimings(VdsmTestCase):
+
+    def setUp(self):
+        self.loop = asyncevent.EventLoop()
+        self.results = []
+
+    def tearDown(self):
+        self.loop.close()
+
+    def complete(self, result):
+        self.results.append(result)
+        if len(self.results) == self.checkers:
+            self.loop.stop()
+
+    @slowtest
+    @permutations([[1], [50], [100], [200]])
+    def test_path_ok(self, checkers):
+        self.checkers = checkers
+        with temporaryPath(data="blah") as path:
+            start = time.time()
+            for i in range(checkers):
+                checker = check.DirectioChecker(self.loop, path, self.complete)
+                checker.start()
+            self.loop.run_forever()
+            elapsed = time.time() - start
+            self.assertEqual(len(self.results), self.checkers)
+            print("%d checkers: %f seconds" % (checkers, elapsed))
+            # Make sure all succeeded
+            for res in self.results:
+                res.delay()
+
+    @slowtest
+    @permutations([[1], [50], [100], [200]])
+    def test_path_missing(self, checkers):
+        self.checkers = checkers
+        start = time.time()
+        for i in range(checkers):
+            checker = check.DirectioChecker(self.loop, "/no/such/path",
+                                            self.complete)
+            checker.start()
+        self.loop.run_forever()
+        elapsed = time.time() - start
+        self.assertEqual(len(self.results), self.checkers)
+        print("%d checkers: %f seconds" % (checkers, elapsed))
+        # Make sure all failed
+        for res in self.results:
+            self.assertRaises(exception.MiscFileReadException, res.delay)
+
+
+@expandPermutations
+class TestCheckResult(VdsmTestCase):
+
+    @permutations([
+        # err, seconds
+        ("1\n2\n1 byte (1 B) copied, 1 s, 1 B/s\n",
+            1.0),
+        ("1\n2\n1024 bytes (1 kB) copied, 1 s, 1 kB/s\n",
+            1.0),
+        ("1\n2\n1572864 bytes (1.5 MB) copied, 1.5 s, 1 MB/s\n",
+            1.5),
+        ("1\n2\n1610612736 bytes (1.5 GB) copied, 1000.5 s, 1.53 MB/s\n",
+            1000.5),
+        ("1\n2\n479 bytes (479 B) copied, 5.6832e-05 s, 8.4 MB/s\n",
+            5.6832e-05),
+        ("1\n2\n512 bytes (512e-3 MB) copied, 1 s, 512e-3 MB/s\n",
+            1.0),
+        ("1\n2\n524288 bytes (512e3 B) copied, 1 s, 512e3 B/s\n",
+            1.0),
+        ("1\n2\n517 bytes (517 B) copied, 0 s, Infinity B/s\n",
+            0.0)
+    ])
+    def test_success(self, err, seconds):
+        result = check.CheckResult("/path", 0, err, 0, 0)
+        self.assertEqual(result.delay(), seconds)
+
+    def test_non_zero_exit_code(self):
+        path = "/path"
+        reason = "REASON"
+        result = check.CheckResult(path, 1, reason, 0, 0)
+        with self.assertRaises(exception.MiscFileReadException) as ctx:
+            result.delay()
+        self.assertIn(path, str(ctx.exception))
+        self.assertIn(reason, str(ctx.exception))
+
+    @permutations([
+        ("",),
+        ("1\n2\n\n",),
+        ("1\n2\nBAD, 1 s, 1 kB/s\n",),
+        ("1\n2\n1024 bytes (1 kB) copied, 1 s, 1 BAD\n",),
+        ("1\n2\n1024 bytes (1 kB) copied, BAD, 1 kB/s\n",),
+        ("1\n2\n1024 bytes (1 kB) copied, BAD s, 1 kB/s\n",),
+        ("1\n2\n1024 bytes (1 kB) copied, -1- s, 1 kB/s\n",),
+        ("1\n2\n1024 bytes (1 kB) copied, e3- s, 1 kB/s\n",),
+    ])
+    def test_unexpected_output(self, err):
+        result = check.CheckResult("/path", 0, err, 0, 0)
+        self.assertRaises(exception.MiscFileReadException, result.delay)
+
+
+class TestCheckService(VdsmTestCase):
+
+    def setUp(self):
+        self.service = check.CheckService()
+        self.service.start()
+        self.result = None
+        self.completed = threading.Event()
+
+    def tearDown(self):
+        self.service.stop()
+
+    def complete(self, result):
+        self.result = result
+        self.completed.set()
+
+    def test_start_checking(self):
+        with fake_dd(0.0):
+            self.service.start_checking("/path", self.complete)
+            self.assertTrue(self.service.is_checking("/path"))
+            self.assertTrue(self.completed.wait(1.0))
+            self.assertEqual(self.result.rc, 0)
+
+    def test_start_checking_already_watched(self):
+        with fake_dd(0.0):
+            self.service.start_checking("/path", self.complete)
+            with self.assertRaises(RuntimeError):
+                self.service.start_checking("/path", self.complete)
+
+    def test_stop_checking(self):
+        with fake_dd(0.0):
+            self.service.start_checking("/path", self.complete)
+            self.service.stop_checking("/path")
+            self.assertFalse(self.service.is_checking("/path"))
+
+    def test_stop_checking_not_watched(self):
+        with self.assertRaises(KeyError):
+            self.service.stop_checking("/path")
+
+    def test_stop_checking_and_wait(self):
+        with fake_dd(0.0):
+            self.service.start_checking("/path", self.complete)
+            self.assertTrue(self.service.stop_checking("/path", timeout=1.0))
+            self.assertFalse(self.service.is_checking("/path"))
+
+    @slowtest
+    def test_stop_checking_timeout(self):
+        with fake_dd(0.2):
+            self.service.start_checking("/path", self.complete)
+            self.assertFalse(self.service.stop_checking("/path", timeout=0.1))
+            self.assertFalse(self.service.is_checking("/path"))
+
+
+@contextmanager
+def fake_dd(delay):
+    script = "#!/bin/sh\nsleep %.1f\n" % delay
+    with temporaryPath(data=script) as fake_dd:
+        os.chmod(fake_dd, 0o700)
+        with MonkeyPatchScope([(constants, "EXT_DD", fake_dd)]):
+            yield
