00038e8b1e458e475b9e25a69a2ab1fc1de67942
Francesco Romani <fromani@redhat.com>
2016-03-16 10:45:02 +0100

    virt: common handling of exceptions
    Vm methods should not return responses directly.
    Rather, they should raise proper exception,
    and a common layer should translate them in responses.
    
    In case of succesfull completion, this code automatically
    produces a generic success response. Should the method
    require extra fields, it can return a dictionary
    with the keys to be added to the success response.
    
    Using this way the method could also override the default
    keys (e.g. the message). This is not clean but it could
    be needed for backward compatibility.
    
    Change-Id: Ic186dc8fa062d8b3789c6057bba68fbbc23f311b
    Bug-Url: https://bugzilla.redhat.com/1316128
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/54664
    Continuous-Integration: Jenkins CI
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>

---
+++ lib/vdsm/virt/api.py
@@ -0,0 +1,67 @@
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+from __future__ import absolute_import
+
+from functools import wraps
+import logging
+
+from vdsm import exception
+from vdsm import response
+
+
+_log = logging.getLogger("virt.api")
+
+
+def method(func):
+    """
+    Decorate an instance method, and return a response according to the
+    outcome of the call.
+
+    If the method returns None, return a plain success response.
+    If the method wants to augment the success response, it could return
+    a dict. The dict items will be added to the success response.
+    The method could override the success response message this way.
+
+    If the method raises a VdsmException or one subclass, the decorator
+    will produce the corresponding error response.
+    If the method raises any other exception, the decorator will produce a
+    general exception response with the details of the original error.
+    """
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+
+        _log.debug("START %s args=%s kwargs=%s", func.__name__, args, kwargs)
+        try:
+            ret = func(self, *args, **kwargs)
+        except Exception as e:
+            _log.exception("FINISH %s error=%s", func.__name__, e)
+            if not isinstance(e, exception.VdsmException):
+                e = exception.GeneralException(str(e))
+            return e.response()
+
+        _log.debug("FINISH %s response=%s", func.__name__, ret)
+
+        if ret is None:
+            return response.success()
+        else:
+            return response.success(**ret)
+
+    return wrapper
---
+++ tests/virt_api_test.py
@@ -0,0 +1,101 @@
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from vdsm import exception
+from vdsm import response
+
+from vdsm.virt import api
+
+from testlib import VdsmTestCase as TestCaseBase
+
+
+class TestResponse(TestCaseBase):
+
+    def setUp(self):
+        self.vm = FakeVM()
+
+    def test_success_without_return(self):
+        res = self.vm.succeed()
+        self.assertEquals(res, response.success())
+
+    def test_success_with_return_dict(self):
+        vmList = ['foobar']
+        res = self.vm.succeed_with_return({'vmList': vmList})
+        self.assertEquals(response.is_error(res), False)
+        self.assertEquals(res['vmList'], vmList)
+
+    def test_success_with_args(self):
+        args = ("foo", "bar")
+        res = self.vm.succeed_with_args(*args)
+        self.assertEquals(response.is_error(res), False)
+        self.assertEqual(res['args'], args)
+
+    def test_success_with_kwargs(self):
+        kwargs = {"foo": "bar"}
+        res = self.vm.succeed_with_kwargs(**kwargs)
+        self.assertEqual(res['kwargs'], kwargs)
+        self.assertEquals(response.is_error(res), False)
+
+    def test_success_with_wrong_return(self):
+        vmList = ['foobar']  # wrong type as per @api.method contract
+        self.assertRaises(TypeError,
+                          self.vm.succeed_with_return,
+                          vmList)
+
+    def test_success_with_return_dict_override_message(self):
+        message = 'this message overrides the default'
+        res = self.vm.succeed_with_return({'message': message})
+        self.assertEquals(response.is_error(res), False)
+        self.assertEquals(res['status']['message'], message)
+
+    def test_fail_with_vdsm_exception(self):
+        exc = exception.NoSuchVM()
+        res = self.vm.fail(exc)
+        expected = exception.NoSuchVM().response()
+        self.assertEquals(res, expected)
+
+    def test_fail_with_general_exception(self):
+        exc = ValueError()
+        res = self.vm.fail(exc)
+        expected = exception.GeneralException(str(exc)).response()
+        self.assertEquals(res, expected)
+
+
+class FakeVM(object):
+
+    @api.method
+    def fail(self, exc):
+        raise exc
+
+    @api.method
+    def succeed(self):
+        pass
+
+    @api.method
+    def succeed_with_return(self, ret):
+        return ret
+
+    @api.method
+    def succeed_with_args(self, *args):
+        return {"args": args}
+
+    @api.method
+    def succeed_with_kwargs(self, **kwargs):
+        return {"kwargs": kwargs}
