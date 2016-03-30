50de041e01b098843b0fd7a4d3927783c11c79b6
Edward Haas <edwardh@redhat.com>
2016-03-30 17:45:38 +0300

    net: Libvirt hook that enables ovs-legacy migration
    This hook allows VM/s to be migrated between OVS based hosts and legacy
    based hosts, in both directions.
    
    The hook is added to the OVS hook package.
    
    Note: The OVS hook and this migration hook is limited to a single
    non-vlan network. An attempt to migrate a VM to a host with multi
    non-vlan networks will fail.
    
    Change-Id: I29cf441cc365d3679382e44410dad0906d9be3ec
    Signed-off-by: Edward Haas <edwardh@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/55497
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ tests/network/hook_ovs_test.py
@@ -0,0 +1,165 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+from __future__ import absolute_import
+
+from io import BytesIO
+import sys
+
+from nose.plugins.attrib import attr
+
+from testlib import XMLTestCase
+from monkeypatch import MonkeyPatchScope
+
+from vdsm import netconfpersistence
+
+sys.path.append('../vdsm_hooks/ovs')     # Devel Location
+sys.path.append('../../vdsm_hooks/ovs')  # Devel Location
+sys.path.append('/usr/libexec/vdsm/')    # Deploy Location
+try:
+    import ovs_utils
+except:
+    # Assuming that the failure is due to ovs-vsctl missing (ovs not installed)
+    from vdsm.utils import CommandPath
+    with MonkeyPatchScope([(CommandPath, 'cmd', None)]):
+        import ovs_utils
+import ovs_migrate
+
+
+@attr(type='unit')
+class TestOvsHookMigration(XMLTestCase):
+
+    class MockRunningConfigNoVlan:
+        def __init__(self):
+            self.networks = {'testnet0': {'vlan': None}}
+
+    class MockRunningConfigVlan:
+        def __init__(self):
+            self.networks = {'testnet0': {'vlan': 101}}
+
+    def test_legacy_to_legacy_non_vlan(self):
+        self._assert_device_migration(from_xml=LIBVIRT_XML_DESCR_LEGACY,
+                                      to_xml=LIBVIRT_XML_DESCR_LEGACY,
+                                      is_destination_ovs=False,
+                                      is_vlan_net=False,
+                                      normalize=False)
+
+    def test_legacy_to_legacy_with_vlan(self):
+        self._assert_device_migration(from_xml=LIBVIRT_XML_DESCR_LEGACY,
+                                      to_xml=LIBVIRT_XML_DESCR_LEGACY,
+                                      is_destination_ovs=False,
+                                      is_vlan_net=True,
+                                      normalize=False)
+
+    def test_ovs_to_ovs_non_vlan(self):
+        self._assert_device_migration(from_xml=LIBVIRT_XML_DESCR_OVS,
+                                      to_xml=LIBVIRT_XML_DESCR_OVS,
+                                      is_destination_ovs=True,
+                                      is_vlan_net=False,
+                                      normalize=False)
+
+    def test_ovs_to_ovs_with_vlan(self):
+        self._assert_device_migration(from_xml=LIBVIRT_XML_DESCR_OVS_VLAN,
+                                      to_xml=LIBVIRT_XML_DESCR_OVS_VLAN,
+                                      is_destination_ovs=True,
+                                      is_vlan_net=True,
+                                      normalize=False)
+
+    def test_legacy_to_ovs_non_vlan(self):
+        self._assert_device_migration(from_xml=LIBVIRT_XML_DESCR_LEGACY,
+                                      to_xml=LIBVIRT_XML_DESCR_OVS,
+                                      is_destination_ovs=True,
+                                      is_vlan_net=False,
+                                      normalize=True)
+
+    def test_legacy_to_ovs_with_vlan(self):
+        self._assert_device_migration(from_xml=LIBVIRT_XML_DESCR_LEGACY,
+                                      to_xml=LIBVIRT_XML_DESCR_OVS_VLAN,
+                                      is_destination_ovs=True,
+                                      is_vlan_net=True,
+                                      normalize=True)
+
+    def test_ovs_to_legacy_non_vlan(self):
+        self._assert_device_migration(from_xml=LIBVIRT_XML_DESCR_OVS,
+                                      to_xml=LIBVIRT_XML_DESCR_LEGACY,
+                                      is_destination_ovs=False,
+                                      is_vlan_net=False,
+                                      normalize=True)
+
+    def test_ovs_to_legacy_with_vlan(self):
+        self._assert_device_migration(from_xml=LIBVIRT_XML_DESCR_OVS_VLAN,
+                                      to_xml=LIBVIRT_XML_DESCR_LEGACY,
+                                      is_destination_ovs=False,
+                                      is_vlan_net=True,
+                                      normalize=True)
+
+    def _assert_device_migration(self, from_xml, to_xml, is_destination_ovs,
+                                 is_vlan_net, normalize):
+        stdin = BytesIO(from_xml)
+        stdout = BytesIO()
+
+        MockRunningConfig = (self.MockRunningConfigVlan if is_vlan_net else
+                             self.MockRunningConfigNoVlan)
+        with MonkeyPatchScope([(netconfpersistence, 'RunningConfig',
+                                MockRunningConfig),
+                               (ovs_utils, 'is_ovs_network',
+                                lambda x: is_destination_ovs)]):
+            ovs_migrate.main('do', 'migrate', 'ph', stdin=stdin, stdout=stdout)
+
+        self.assertXMLEqual(stdout.getvalue(), to_xml)
+
+
+LIBVIRT_XML_DESCR_LEGACY = """<domain type="kvm">
+  <devices>
+    <interface type="bridge">
+      <mac address="00:1a:4a:16:01:54" />
+      <source bridge="testnet0" />
+      <model type="virtio" />
+      <filterref filter="vdsm-no-mac-spoofing" />
+      <link state="up" />
+      <boot order="1" />
+      <address bus="0x00" domain="0x0" function="0x0" slot="0x03" type="pci" />
+    </interface>
+  </devices>
+</domain>"""
+
+# Note:
+# libvirt automatically generates the element 'parameters' under 'virtualport'
+# <virtualport type="openvswitch">
+#   <parameters interfaceid="8b44132f-301f-4af5-bc16-f18f0c4c39c1" />
+# </virtualport>
+# When converting from legacy to ovs, the hook does not add it and leaves it up
+# to libvirt to add its own defaults.
+LIBVIRT_XML_DESCR_OVS = """<domain type="kvm">
+  <devices>
+    <interface type="bridge">
+      <mac address="00:1a:4a:16:01:54" />
+      <source bridge="ovsbr0" />
+      <model type="virtio" />
+      <filterref filter="vdsm-no-mac-spoofing" />
+      <link state="up" />
+      <boot order="1" />
+      <address bus="0x00" domain="0x0" function="0x0" slot="0x03" type="pci" />
+      <virtualport type="openvswitch" />
+    </interface>
+  </devices>
+</domain>"""
+
+# Device bound to an OVS vlan network (bridge name is not OVS default)
+LIBVIRT_XML_DESCR_OVS_VLAN = LIBVIRT_XML_DESCR_OVS.replace('ovsbr0',
+                                                           'testnet0', 1)
--- vdsm/virt/libvirt-hook.sh
+++ vdsm/virt/libvirt-hook.sh
@@ -17,5 +17,5 @@ fi
 
 # Fix VMs migrating to host with libvirt >= 1.2.8
 # See https://bugzilla.redhat.com/show_bug.cgi?id=1138340
-exec sed -e 's|<min_guarantee[^>]*>[0-9 ]*</min_guarantee>||g'
+sed -e 's|<min_guarantee[^>]*>[0-9 ]*</min_guarantee>||g' | /usr/libexec/vdsm/ovs_migrate.py $DOMAIN $EVENT $PHASE
 
