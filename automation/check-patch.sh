3956258da8e616a6e1333bcf524e5882022a45d9
Yaniv Bronhaim <ybronhei@redhat.com>
2016-04-03 11:08:52 +0300

    automation: build and install vdsm only after spec or makefile change
    Change-Id: I45a872e0c9d3879e6c73c846f2cf11444b288a4a
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/55604
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Continuous-Integration: Jenkins CI

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -11,12 +11,12 @@ if grep -q 'Fedora' /etc/redhat-release; then
    make check NOSE_WITH_COVERAGE=1 NOSE_COVER_PACKAGE="$PWD/vdsm,$PWD/lib"
 fi
 
-./automation/build-artifacts.sh
-
 # enable complex globs
 shopt -s extglob
-# if specfile was changed, try to install all created packages
-if git diff-tree --no-commit-id --name-only -r HEAD | grep --quiet 'vdsm.spec.in' ; then
+# In case of vdsm specfile or any Makefile.am file modification in commit,
+# try to build and install all new created packages
+if git diff-tree --no-commit-id --name-only -r HEAD | egrep --quiet 'vdsm.spec.in|Makefile.am' ; then
+    ./automation/build-artifacts.sh
     yum -y install "$EXPORT_DIR/"!(*.src).rpm
 fi
 
