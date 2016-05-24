3dc4c481ec46d9ee8e69254388c8f7b985a00f06
Martin Polednik <mpolednik@redhat.com>
2016-05-24 13:36:36 +0200

    hostdev: use specific device classes in HostDevice
    The device functionality has been split into separate classes. As all
    functionality is now present, we can remove it from HostDevice class.
    This class can be reused as a factory.
    
    Rationale of using __new__:
    vm.py initializes devices through
    
    def _devMapFromDevSpecMap(self, dev_spec_map):
        ...
        dev_map[dev_type].append(dev_class(self.conf, self.log, **dev))
    
    To avoid creating special case in the code, we replace HostDevice by
    appropriate device class.
    
    Change-Id: I2734b7ec789c0ce57b64d7699cbe967c774bb608
    Signed-off-by: Martin Polednik <mpolednik@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/57963
    Reviewed-by: Milan Zamazal <mzamazal@redhat.com>
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Continuous-Integration: Jenkins CI

--- vdsm/virt/vmdevices/hostdevice.py
+++ vdsm/virt/vmdevices/hostdevice.py
@@ -18,183 +18,15 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
-import logging
 import xml.etree.ElementTree as ET
 
 from vdsm import utils
 from vdsm import supervdsm
 from vdsm.hostdev import get_device_params, detach_detachable, \
-    pci_address_to_name, CAPABILITY_TO_XML_ATTR, scsi_address_to_adapter, \
-    reattach_detachable
+    pci_address_to_name, scsi_address_to_adapter, reattach_detachable
 from . import core
 from . import hwclass
 from .. import vmxml
-
-
-class HostDevice(core.Base):
-    __slots__ = ('address', 'hostAddress', 'bootOrder', '_deviceParams',
-                 'name')
-
-    def __init__(self, conf, log, **kwargs):
-        super(HostDevice, self).__init__(conf, log, **kwargs)
-
-        self._deviceParams = get_device_params(self.device)
-        self.hostAddress = self._deviceParams.get('address')
-        self.name = self.device
-
-    def setup(self):
-        logging.debug('Detaching device %s from the host.' % self.device)
-        self._deviceParams = detach_detachable(self.device)
-
-    def teardown(self):
-        if CAPABILITY_TO_XML_ATTR[self._deviceParams['capability']] != 'pci':
-            reattach_detachable(self.device)
-        else:
-            supervdsm.getProxy().rmAppropriateIommuGroup(
-                self._deviceParams['iommu_group'])
-
-    @property
-    def _xpath(self):
-        """
-        Returns xpath to the device in libvirt dom xml.
-        The path is relative to the root element.
-        """
-        address_fields = []
-        for key, value in self.hostAddress.items():
-            padding, base = '', ''
-            if CAPABILITY_TO_XML_ATTR[
-                    self._deviceParams['capability']] == 'pci':
-                if key == 'domain':
-                    base = '0x'
-                    padding = '04'
-                elif key == 'function':
-                    base = '0x'
-                    padding = ''
-                else:
-                    base = '0x'
-                    padding = '02'
-
-            address_fields.append('[@{key}="{base}{value:{padding}}"]'.format(
-                key=key, value=int(value), base=base, padding=padding))
-
-        return './devices/hostdev/source/address{}'.format(
-            ''.join(address_fields))
-
-    def is_attached_to(self, xml_string):
-        dom = ET.fromstring(xml_string)
-        return bool(dom.findall(self._xpath))
-
-    def getXML(self):
-        """
-            Create domxml for a host device.
-
-            <devices>
-                <hostdev mode='subsystem' type='pci' managed='no'>
-                <source>
-                    <address domain='0x0000' bus='0x06' slot='0x02'
-                    function='0x0'/>
-                </source>
-                <boot order='1'/>
-                </hostdev>
-            </devices>
-        """
-
-        if (CAPABILITY_TO_XML_ATTR[
-                self._deviceParams['capability']] == 'pci' and
-                utils.tobool(self.specParams.get('iommuPlaceholder', False))):
-            raise core.SkipDevice
-
-        hostdev = self.createXmlElem(hwclass.HOSTDEV, None)
-        hostdev.setAttrs(
-            managed='no', mode='subsystem',
-            type=CAPABILITY_TO_XML_ATTR[self._deviceParams['capability']])
-        source = hostdev.appendChildWithArgs('source')
-
-        # This must be done *before* creating the address element, as we need
-        # remove the 'host' key from real address.
-        if CAPABILITY_TO_XML_ATTR[self._deviceParams['capability']] == 'scsi':
-            source.appendChildWithArgs(
-                'adapter', **scsi_address_to_adapter(self.hostAddress))
-            hostdev.setAttr('rawio', 'yes')
-
-        source.appendChildWithArgs('address', **self.hostAddress)
-
-        if hasattr(self, 'bootOrder'):
-            hostdev.appendChildWithArgs('boot', order=self.bootOrder)
-
-        if hasattr(self, 'address'):
-            hostdev.appendChildWithArgs('address', **self.address)
-
-        return hostdev
-
-    @classmethod
-    def _update_nonpci_device_info(cls, vm, device_conf, x):
-        alias = x.getElementsByTagName('alias')[0].getAttribute('name')
-        host_address = vmxml.device_address(x)
-
-        # The routine is quite unusual because we cannot directly reconstruct
-        # the unique name. Therefore, we first look up correspondoing device
-        # object address,
-        for dev in device_conf:
-            if host_address == dev.hostAddress:
-                dev.alias = alias
-                device = dev.device
-                # RHBZ#1215968
-                if x.getAttribute('type') != 'usb':
-                    dev.address = vmxml.device_address(x, 1)
-
-        # and use that to identify the device in self.conf.
-        for dev in vm.conf['devices']:
-            if dev['device'] == device:
-                dev['alias'] = alias
-                # RHBZ#1215968
-                if x.getAttribute('type') != 'usb':
-                    dev['address'] = vmxml.device_address(x, 1)
-
-        # This has an unfortunate effect that we will not be able to look up
-        # any previously undefined devices, because there is no easy
-        # way of reconstructing the udev name we use as unique id.
-
-    @classmethod
-    def update_device_info(cls, vm, device_conf):
-        for x in vm.domain.get_device_elements('hostdev'):
-            device_type = x.getAttribute('type')
-            if device_type != 'pci':
-                cls._update_nonpci_device_info(vm, device_conf, x)
-                continue
-            alias = x.getElementsByTagName('alias')[0].getAttribute('name')
-            address = vmxml.device_address(x)
-            source = x.getElementsByTagName('source')[0]
-            device = pci_address_to_name(
-                **vmxml.device_address(source))
-
-            # We can assume the device name to be correct since we're
-            # inspecting source element. For the address, we may look at
-            # both addresses and determine the correct one.
-            if (pci_address_to_name(**address) == device):
-                address = vmxml.device_address(x, 1)
-
-            known_device = False
-            for dev in vm.conf['devices']:
-                if dev['device'] == device:
-                    dev['alias'] = alias
-                    dev['address'] = address
-
-            for dev in device_conf:
-                if dev.device == device:
-                    dev.alias = alias
-                    dev.address = address
-                    known_device = True
-
-            if not known_device:
-                device = pci_address_to_name(
-                    **vmxml.device_address(source))
-
-                hostdevice = {'type': hwclass.HOSTDEV,
-                              'device': device,
-                              'alias': alias,
-                              'address': address}
-                vm.conf['devices'].append(hostdevice)
 
 
 class PciDevice(core.Base):
@@ -488,3 +320,27 @@ class ScsiDevice(core.Base):
         # This has an unfortunate effect that we will not be able to look
         # up any previously undefined devices, because there is no easy
         # way of reconstructing the udev name we use as unique id.
+
+
+class HostDevice(core.Base):
+    __slots__ = ('_device',)
+
+    _DEVICE_MAPPING = {
+        'pci': PciDevice,
+        'usb_device': UsbDevice,
+        'usb': UsbDevice,
+        'scsi': ScsiDevice,
+    }
+
+    def __new__(cls, conf, log, **kwargs):
+        device_params = get_device_params(kwargs['device'])
+        device = cls._DEVICE_MAPPING[
+            device_params['capability']](conf, log, **kwargs)
+        return device
+
+    @classmethod
+    def update_device_info(cls, vm, device_conf):
+        for device_xml in vm.domain.get_device_elements('hostdev'):
+            device_type = device_xml.getAttribute('type')
+            cls._DEVICE_MAPPING[device_type].update_from_xml(
+                vm, device_conf, device_xml)
