055e53965c1eea6e7783072037af13ee44081eeb
Martin Polednik <mpolednik@redhat.com>
2016-05-25 14:53:57 +0200

    hostdev: move scsi params processing to device processor
    Change-Id: Ifb0d69ecfa26c63d61296def2d25af63a7d02352
    Signed-off-by: Martin Polednik <mpolednik@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/58054
    Continuous-Integration: Jenkins CI
    Reviewed-by: Francesco Romani <fromani@redhat.com>

--- lib/vdsm/hostdev.py
+++ lib/vdsm/hostdev.py
@@ -245,6 +245,7 @@ def _process_storage(caps, params):
         params['product'] = model
 
 
+@_data_processor('scsi')
 def _process_scsi_device_params(device_xml):
     """
     The information we need about SCSI device is contained within multiple
@@ -340,8 +341,6 @@ def _process_device_params(device_xml):
         # Device does not support sriov, we can safely go on
         pass
 
-    if params['capability'] == 'scsi':
-        params.update(_process_scsi_device_params(devXML))
     return params
 
 
