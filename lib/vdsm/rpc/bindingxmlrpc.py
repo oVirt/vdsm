559a04d7f1f6db18ee2263cf33e5758a2ab31344
Milan Zamazal <mzamazal@redhat.com>
2016-06-06 21:31:57 +0200

    rpc: Log calls of API methods with possibly large results
    We can prevent some API methods from logging their excessively large
    results.  But this doesn't mean we shouldn't log their calls at all, we
    just shouldn't log the large values.
    
    This patch re-enables logging calls of the methods with suppressed
    logging, it just disables logging their outputs.
    
    Change-Id: I85d7f0ceee7e120081e2d9ad4ca3c8148ee70554
    Bug-Url: https://bugzilla.redhat.com/1351247
    Backport-To: 4.0
    Backport-To: 3.6
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/58464
    Continuous-Integration: Jenkins CI
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Reviewed-by: Michal Skrivanek <michal.skrivanek@redhat.com>
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/58709
    Continuous-Integration: Francesco Romani <fromani@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/59993

--- lib/vdsm/rpc/bindingxmlrpc.py
+++ lib/vdsm/rpc/bindingxmlrpc.py
@@ -1192,8 +1192,7 @@ def wrapApiMethod(f):
     def wrapper(*args, **kwargs):
         try:
             logLevel = logging.DEBUG
-            if f.__name__ in ('getAllVmStats',):
-                logLevel = logging.TRACE
+            suppress_logging = f.__name__ in ('getAllVmStats',)
             suppress_args = f.__name__ in ('fenceNode',)
 
             # TODO: This password protection code is fragile and ugly. Password
@@ -1240,8 +1239,9 @@ def wrapApiMethod(f):
                 res = f(*args, **kwargs)
             else:
                 res = errCode['recovery']
+            log_res = "(suppressed)" if suppress_logging else res
             f.__self__.cif.log.log(logLevel, 'return %s with %s',
-                                   f.__name__, res)
+                                   f.__name__, log_res)
             return res
         except libvirt.libvirtError as e:
             f.__self__.cif.log.error("libvirt error", exc_info=True)
