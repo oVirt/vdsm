558fe3a6fe05484f1bda484c630ad94d998b2531
Yaniv Bronhaim <ybronhei@redhat.com>
2016-06-06 12:47:31 +0300

    Introduce reports package
    reports logic allows to send report to external providers by start close
    and send functions. The report itself is simply key value attribute.
    
    This patch includes statsd reporter which implements the send method to
    statsd listener by python-statsd.
    To enable reports add the following to vdsm.conf:
    
    [reports]
    enabled = false|true (disabled by default)
    collector_type = statsd (following patch adds more providers)
    collector_address = localhost|any hostname (currently one destination)
    
    We still miss python-stastd for python3 so we don't require the package
    in spec. If metric collector is configured to be enable and the type
    implementation is not available ImportError should be raised.
    
    Change-Id: I23c1141f097f740441d085f99e0bf76eb7f718c9
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/56880
    Reviewed-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-by: Martin Polednik <mpolednik@redhat.com>
    Continuous-Integration: Jenkins CI

---
+++ lib/vdsm/reports/__init__.py
@@ -0,0 +1,47 @@
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+
+import importlib
+from ..config import config
+
+_reporter = None
+
+
+def start():
+    global _reporter
+    if config.getboolean('reports', 'enabled'):
+        _reporter = importlib.import_module(
+            'vdsm.reports.' + config.get('reports', 'collector_type')
+        )
+        _reporter.start(config.get('reports', 'collector_address'))
+
+
+def stop():
+    global _reporter
+    if _reporter:
+        _reporter.stop()
+        _reporter = None
+
+
+def send(report):
+    if _reporter:
+        _reporter.send(report)
