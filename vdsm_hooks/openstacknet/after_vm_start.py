a655be4db3dfbbda0ed2b3ec29fe287e81d5162d
mirecki <mmirecki@redhat.com>
2016-06-09 16:05:59 +0200

    hooks: openstack hook should wait a while for nic activation
    The vm will be stated in a paused state if a nic managed by
    an external openstack network provider is present. The openstack
    hook after_vm_start  must then wait a given time to allow the
    nic to be activated before unpausing the vm.
    
    Change-Id: Ib4f455789909f090a039863fdcfeb61b9db1042f
    Bug-Url: https://bugzilla.redhat.com/1314371
    Signed-off-by: mirecki <mmirecki@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/58917
    Continuous-Integration: Jenkins CI
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-by: Francesco Romani <fromani@redhat.com>

---
+++ vdsm_hooks/openstacknet/after_vm_start.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+'''
+OpenStack Network Hook (Post vm start)
+=============================================
+The hook unpauses a vm if it was started in the paused state.
+
+Syntax:
+    { 'vmId': 'VM_ID', 'vnic_id': 'port_id' }
+Where:
+    VM_ID should be replaced with the vm id.'''
+
+import libvirt
+import os
+import time
+import traceback
+import hooking
+from openstacknet_utils import MARK_FOR_UNPAUSE_PATH
+from openstacknet_utils import VM_ID_KEY
+from vdsm import vdscli
+
+
+OPENSTACK_NIC_WAIT_TIME = 15
+
+
+def resume_paused_vm(vm_id):
+    unpause_file = MARK_FOR_UNPAUSE_PATH % vm_id
+    if os.path.isfile(unpause_file):
+        vdscli.connect().cont(vm_id)
+        os.remove(unpause_file)
+
+
+def main():
+
+    # TODO (HACK):
+    # This code waits for the nic to be attached to neutron for a
+    # certain amount of time. This is one way of going around the
+    # race between the code and the vm nic becoming active. It is
+    # a very fragile hack, as there is no guarantee the nic will
+    # actually be ready after this.
+    vm_id = os.environ[VM_ID_KEY]
+    launch_flags = hooking.load_vm_launch_flags_from_file(vm_id)
+    if launch_flags == libvirt.VIR_DOMAIN_START_PAUSED:
+        time.sleep(OPENSTACK_NIC_WAIT_TIME)
+        resume_paused_vm(vm_id)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        hooking.exit_hook('openstacknet hook: [unexpected error]: %s\n' %
+                          traceback.format_exc())
