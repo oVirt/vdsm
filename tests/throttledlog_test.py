d2f6f2793d57bfcb870c5349d66555b05127c334
Milan Zamazal <mzamazal@redhat.com>
2016-06-20 11:30:25 +0200

    logging: Introduce throttledlog
    In some situations, we want to log interesting information.  A natural
    place to retrieve and log the information is where it is created.
    However the corresponding code may be called quite often and we don't
    need logging the information that often and fill logs with it
    excessively.  It's sufficient to log it just from time to time.
    
    This patch provides means for occasional logging.  The provided
    functions actually log only every given number of invocations or every
    given time span.  We're going to use the functionality in followup
    patches.
    
    Change-Id: Ief96f74eb84880827ccf24c3e7ae854403090b8a
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1351247
    Reviewed-on: https://gerrit.ovirt.org/59461
    Continuous-Integration: Jenkins CI
    Reviewed-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/59995
    Reviewed-on: https://gerrit.ovirt.org/60100
    Continuous-Integration: Francesco Romani <fromani@redhat.com>

---
+++ tests/throttledlog_test.py
@@ -0,0 +1,85 @@
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+import logging
+
+from vdsm import throttledlog
+
+from monkeypatch import MonkeyPatch
+from testlib import VdsmTestCase
+
+
+class FakeLogger(object):
+
+    def __init__(self, level):
+        self.level = level
+        self.messages = []
+
+    def isEnabledFor(self, level):
+        return level >= self.level
+
+    def log(self, level, message, *args):
+        if not self.isEnabledFor(level):
+            return
+        self.messages.append(message % args)
+
+
+class FakeTime(object):
+
+    def __init__(self):
+        self.time = 0.0
+
+    def __call__(self):
+        return self.time
+
+
+class TestThrottledLogging(VdsmTestCase):
+
+    @MonkeyPatch(throttledlog, "_logger", FakeLogger(logging.DEBUG))
+    def test_throttled_logging(self):
+        throttledlog.throttle('test', 3)
+        for i in range(5):
+            throttledlog.debug('test', "Cycle: %s", i)
+        self.assertEqual(throttledlog._logger.messages,
+                         ['Cycle: 0', 'Cycle: 3'])
+
+    @MonkeyPatch(throttledlog, "_logger", FakeLogger(logging.INFO))
+    def test_no_logging(self):
+        throttledlog.throttle('test', 3)
+        for i in range(5):
+            throttledlog.debug('test', "Cycle: %s", i)
+        self.assertEqual(throttledlog._logger.messages, [])
+
+    @MonkeyPatch(throttledlog, "_logger", FakeLogger(logging.DEBUG))
+    def test_default(self):
+        throttledlog.throttle('test', 3)
+        for i in range(5):
+            throttledlog.debug('other', "Cycle: %s", i)
+        self.assertEqual(throttledlog._logger.messages,
+                         ['Cycle: %s' % (i,) for i in range(5)])
+
+    @MonkeyPatch(throttledlog, "_logger", FakeLogger(logging.DEBUG))
+    @MonkeyPatch(throttledlog, "monotonic_time", FakeTime())
+    def test_timeout(self):
+        throttledlog.throttle('test', 10, timeout=7)
+        for i in range(12):
+            throttledlog.debug('test', "Cycle: %s", i)
+            throttledlog.monotonic_time.time += 1.0
+        self.assertEqual(throttledlog._logger.messages,
+                         ['Cycle: %s' % (i,) for i in (0, 7, 10,)])
