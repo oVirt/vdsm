62618856c1a2e0ef833a98f4ea525e4446833d1d
Adam Litke <alitke@redhat.com>
2016-06-30 15:45:30 -0400

    tests: Add tests for verify_untrusted_volume
    Add some tests to check that verify_untrusted_volume works as expected.
    Tests are placed in a new storage_hsm_test module which can accomodate
    tests for other HSM verbs.
    
    Change-Id: I44d3bbe33430a34e3a3b985e1485c6d46ebdc3aa
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1344289
    Signed-off-by: Adam Litke <alitke@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/60060
    Reviewed-by: Nir Soffer <nsoffer@redhat.com>
    Continuous-Integration: Jenkins CI
    Reviewed-on: https://gerrit.ovirt.org/60983
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Continuous-Integration: Francesco Romani <fromani@redhat.com>

---
+++ tests/storage_hsm_test.py
@@ -0,0 +1,83 @@
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import uuid
+from contextlib import contextmanager
+
+from testlib import VdsmTestCase
+from testlib import permutations, expandPermutations
+from storagetestlib import fake_file_env
+from storagetestlib import make_file_volume
+
+from vdsm import qemuimg
+from vdsm.storage import constants as sc
+from vdsm.storage import exception as se
+
+from storage import hsm
+
+
+class FakeHSM(hsm.HSM):
+    def __init__(self):
+        pass
+
+
+@expandPermutations
+class VerifyUntrustedVolumeTest(VdsmTestCase):
+    SIZE = 1024 * 1024
+
+    @permutations(((sc.RAW_FORMAT,), (sc.COW_FORMAT,)))
+    def test_ok(self, vol_fmt):
+        with self.fake_volume(vol_fmt) as vol:
+            qemu_fmt = sc.FMT2STR[vol_fmt]
+            qemuimg.create(vol.volumePath, size=self.SIZE, format=qemu_fmt)
+            h = FakeHSM()
+            self.assertNotRaises(h.verify_untrusted_volume,
+                                 'sp', vol.sdUUID, vol.imgUUID, vol.volUUID)
+
+    @permutations((
+        (sc.RAW_FORMAT, qemuimg.FORMAT.QCOW2),
+        (sc.COW_FORMAT, qemuimg.FORMAT.RAW),
+    ))
+    def test_wrong_format_raises(self, vol_fmt, qemu_fmt):
+        with self.fake_volume(vol_fmt) as vol:
+            qemuimg.create(vol.volumePath, size=self.SIZE, format=qemu_fmt)
+            h = FakeHSM()
+            self.assertRaises(se.ImageVerificationError,
+                              h.verify_untrusted_volume,
+                              'sp', vol.sdUUID, vol.imgUUID, vol.volUUID)
+
+    def test_backingfile_raises(self):
+        with self.fake_volume(sc.COW_FORMAT) as vol:
+            qemu_fmt = qemuimg.FORMAT.QCOW2
+            qemuimg.create(vol.volumePath, size=self.SIZE, format=qemu_fmt,
+                           backing='foo')
+            h = FakeHSM()
+            self.assertRaises(se.ImageVerificationError,
+                              h.verify_untrusted_volume,
+                              'sp', vol.sdUUID, vol.imgUUID, vol.volUUID)
+
+    @contextmanager
+    def fake_volume(self, vol_fmt):
+        with fake_file_env() as env:
+            img_id = str(uuid.uuid4())
+            vol_id = str(uuid.uuid4())
+            make_file_volume(env.sd_manifest, self.SIZE, img_id, vol_id,
+                             vol_format=vol_fmt)
+            yield env.sd_manifest.produceVolume(img_id, vol_id)
