5f934c4a5c161f4860c4cc3131fe9544ca3b8544
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2016-07-06 14:37:33 +0200

    control messages need specific order
    We need to make sure that connect frame is sent before subscribe. We
    change the order how we add to a queue.
    
    Bug-Url: https://bugzilla.redhat.com/1391410
    Change-Id: Ia115832925ba9b7eca53b271f13c7e1a4753d411
    Signed-off-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>

--- client/src/main/java/org/ovirt/vdsm/jsonrpc/client/reactors/stomp/StompClient.java
+++ client/src/main/java/org/ovirt/vdsm/jsonrpc/client/reactors/stomp/StompClient.java
@@ -30,6 +30,13 @@ public class StompClient extends PlainClient {
                 connected = new CountDownLatch(1);
                 subscribed = new CountDownLatch(1);
 
+                subscribe(getResponseQueue());
+
+                String eventQueue = getEventQueue();
+                if (!isEmpty(eventQueue)) {
+                    subscribe(eventQueue);
+                }
+
                 Message message = new Message().connect().withHeader(HEADER_ACCEPT, "1.2").withHeader(HEADER_HOST, policy.getIdentifier());
                 int outgoing = 0;
                 int incoming = 0;
@@ -43,13 +50,6 @@ public class StompClient extends PlainClient {
                     message.withHeader(HEADER_HEART_BEAT, outgoing + "," + reduceGracePeriod(incoming));
                 }
                 sendNow(message.build());
-
-                subscribe(getResponseQueue());
-
-                String eventQueue = getEventQueue();
-                if (!isEmpty(eventQueue)) {
-                    subscribe(eventQueue);
-                }
 
                 connected.await(policy.getRetryTimeOut(), policy.getTimeUnit());
             } catch (InterruptedException e) {
