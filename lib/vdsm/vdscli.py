108986fe73b80b85f5c92f779352d2116db79d8f
Irit Goihman <igoihman@redhat.com>
2016-07-07 13:07:58 +0300

    vdscli: added DeprecationWarning to module
    vdscli is deprecated and will be replaced soon by jsonrpccli.
    since we want to eliminate the calls to vdscli module a
    DeprecationWarning will be issued once this module will be
    imported.
    
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1359648
    Change-Id: I728db1ed55e1449000f82c78d26f2774a2650c7e
    Signed-off-by: Irit Goihman <igoihman@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/60318
    Continuous-Integration: Jenkins CI
    Reviewed-by: Yaniv Bronhaim <ybronhei@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/61187

--- lib/vdsm/vdscli.py
+++ lib/vdsm/vdscli.py
@@ -25,6 +25,7 @@ import xmlrpclib
 import os
 import re
 import sys
+import warnings
 from xml.parsers.expat import ExpatError
 from .sslcompat import sslutils
 
@@ -132,6 +133,11 @@ def connect(hostPort=None, useSSL=None, tsPath=None,
             SingleRequestTransport(timeout=timeout))
         server = xmlrpclib.Server('http://%s' % hostPort, transport)
     return server
+
+warnings.simplefilter("always", category=DeprecationWarning)
+warnings.warn(
+    "vdscli uses xmlrpc. since ovirt 3.6 xmlrpc is deprecated, please use "
+    "vdsm.jsonrpcvdscli", DeprecationWarning, stacklevel=2)
 
 if __name__ == '__main__':
     print('connecting to %s:%s ssl %s ts %s' % (
