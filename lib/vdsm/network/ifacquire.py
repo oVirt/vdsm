118847249c900c84f165cbd42c438cf64da9d906
Petr Horáček <phoracek@redhat.com>
2016-07-18 16:20:31 +0200

    net: introduce acquire module
    This module will be used to acquire ifaces, both persisted by ifcfg and
    not persisted. It is needed for OVS switch (and eventually also for
    iproute2 and pyroute2).
    
    Change-Id: I180cfd7d69c0ae0a24188bc3d909b9d3d7c12145
    Bug-Url: https://bugzilla.redhat.com/1364081
    Signed-off-by: Petr Horáček <phoracek@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/60974
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Continuous-Integration: Jenkins CI

---
+++ lib/vdsm/network/ifacquire.py
@@ -0,0 +1,143 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+from __future__ import absolute_import
+
+import itertools
+import os
+import re
+
+import six
+
+from vdsm import utils
+from .configurators import ifcfg
+from .ip import address
+from .ip import dhclient
+
+
+ACQUIRED_IFCFG_TAG = u'# This device is now owned by VDSM.\n'
+ACQUIRED_IFCFG_PREFIX = [
+    ACQUIRED_IFCFG_TAG,
+    '# Please do not do any changes here while the device is used by VDSM.\n',
+    '# Once it is detached from VDSM, remove this prefix before applying\n',
+    '# any changes.\n']
+
+
+class Transaction(object):
+    """Acquire external interfaces which are not owned by us yet.
+
+    In case of an unexpected failure, rollback acquired ifcfg-persisted
+    interfaces.
+    """
+    def __init__(self, netinfo_nets):
+        self._owned_ports = frozenset(itertools.chain.from_iterable(
+            [attrs['ports'] for attrs in six.itervalues(netinfo_nets)]))
+        self._ifaces = {}  # {name: ifcfg_lines OR None}
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if type is None:
+            try:
+                self._disable_onboot()
+            except Exception:
+                self._rollback()
+                raise
+        else:
+            self._rollback()
+
+    def acquire(self, ifaces):
+        self._backup(ifaces)
+        self._release_ifaces()
+
+    def _rollback(self):
+        for iface, ifcfg_lines in six.iteritems(self._ifaces):
+            if ifcfg_lines:
+                _rollback_ifcfg_iface(iface, ifcfg_lines)
+
+    def _backup(self, ifaces):
+        for iface in ifaces:
+            if iface not in self._owned_ports:
+                self._ifaces[iface] = (
+                    _get_ifcfg_config(iface) if _is_ifcfg_controlled(iface)
+                    else None)
+
+    def _release_ifaces(self):
+        for iface, ifcfg_lines in six.iteritems(self._ifaces):
+            if ifcfg_lines:
+                _release_ifcfg_iface(iface)
+            else:
+                _release_non_ifcfg_iface(iface)
+
+    def _disable_onboot(self):
+        for iface, ifcfg_lines in six.iteritems(self._ifaces):
+            if ifcfg_lines:
+                _disable_onboot_ifcfg_iface(iface)
+
+
+def _is_ifcfg_controlled(iface):
+    return os.path.isfile(ifcfg.NET_CONF_PREF + iface)
+
+
+def _get_ifcfg_config(iface):
+    with open(ifcfg.NET_CONF_PREF + iface) as f:
+        return f.readlines()
+
+
+def _rollback_ifcfg_iface(iface, ifcfg_lines):
+    with utils.atomic_file_write(ifcfg.NET_CONF_PREF + iface, 'w') as f:
+        f.writelines(ifcfg_lines)
+    ifcfg.ifup(iface)
+
+
+def _release_ifcfg_iface(iface):
+    _set_ifcfg_param(iface, 'NM_CONTROLLED', 'no')
+    ifcfg.ifdown(iface)
+
+
+def _disable_onboot_ifcfg_iface(iface):
+    _set_ifcfg_param(iface, 'ONBOOT', 'no')
+
+
+def _set_ifcfg_param(iface, key, value):
+    with utils.atomic_file_write(ifcfg.NET_CONF_PREF + iface, 'r+') as f:
+        lines = f.readlines()
+
+        if lines[0] != ACQUIRED_IFCFG_TAG:
+            lines = ACQUIRED_IFCFG_PREFIX + lines
+
+        configured = False
+        for i, line in enumerate(lines):
+            parsed_line = re.split('=| |\n', line)
+            if parsed_line[0] == key and parsed_line[1] != value:
+                lines[i] = '{}={}  # Changed by VDSM, original: {}'.format(
+                    key, value, lines[i])
+                configured = True
+        if not configured:
+            lines.append('{}={}  # Set by VDSM\n'.format(key, value))
+
+        f.seek(0)
+        f.writelines(lines)
+
+
+def _release_non_ifcfg_iface(iface):
+    # TODO: Tell NetworkManager to unmanage this iface.
+    dhclient.kill(iface, family=4)
+    dhclient.kill(iface, family=6)
+    address.flush(iface)
---
+++ tests/network/ifacquire_test.py
@@ -0,0 +1,152 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301  USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+from __future__ import absolute_import
+
+from nose.plugins.attrib import attr
+
+from vdsm.network import ifacquire
+
+from testlib import VdsmTestCase, mock
+
+
+NIC_NAME = 'eth3'
+
+NIC_IFCFG = [
+    'DEVICE=' + NIC_NAME + '\n',
+    'BOOTPROTO=dhcp\n',
+    'ONBOOT=yes\n',
+    'MTU=1500\n']
+
+PARTIALLY_ACQUIRED_NIC_IFCFG = ifacquire.ACQUIRED_IFCFG_PREFIX + [
+    'DEVICE=' + NIC_NAME + '\n',
+    'BOOTPROTO=dhcp\n',
+    'ONBOOT=yes\n',
+    'MTU=1500\n',
+    'NM_CONTROLLED=no  # Set by VDSM\n']
+
+FULLY_ACQUIRED_NIC_IFCFG = ifacquire.ACQUIRED_IFCFG_PREFIX + [
+    'DEVICE=' + NIC_NAME + '\n',
+    'BOOTPROTO=dhcp\n',
+    'ONBOOT=no  # Changed by VDSM, original: ONBOOT=yes\n',
+    'MTU=1500\n',
+    'NM_CONTROLLED=no  # Set by VDSM\n']
+
+NETINFO_NETS = {
+    'net1': {
+        'ports': [NIC_NAME, 'vnet0']
+    }
+}
+
+
+class TestException(Exception):
+    pass
+
+
+@attr(type='unit')
+class AcquireNicTest(VdsmTestCase):
+
+    @mock.patch.object(ifacquire.address, 'flush')
+    @mock.patch.object(ifacquire.dhclient, 'kill')
+    @mock.patch.object(ifacquire.ifcfg, 'ifdown')
+    @mock.patch.object(ifacquire, 'open', create=True)
+    def test_do_not_acquire_owned_nic(self, mock_open, mock_ifdown, mock_kill,
+                                      mock_flush):
+        with ifacquire.Transaction(netinfo_nets=NETINFO_NETS) as a:
+            a.acquire(ifaces=[NIC_NAME])
+
+        mock_open.assert_not_called()
+        mock_ifdown.assert_not_called()
+        mock_kill.assert_not_called()
+        mock_flush.assert_not_called()
+
+    @mock.patch.object(ifacquire.os.path, 'isfile', lambda x: False)
+    @mock.patch.object(ifacquire.address, 'flush', return_value=None)
+    @mock.patch.object(ifacquire.dhclient, 'kill', return_value=None)
+    def test_acquire_non_ifcfg_nic(self, mock_kill, mock_flush):
+
+        with ifacquire.Transaction(netinfo_nets={}) as a:
+            a.acquire(ifaces=[NIC_NAME])
+            mock_kill.assert_any_call(NIC_NAME, family=4)
+            mock_kill.assert_any_call(NIC_NAME, family=6)
+            mock_flush.assert_called_with(NIC_NAME)
+
+    def test_acquire_ifcfg_nic(self):
+        self._test_acquire_ifcfg_nic(
+            original_ifcfg=NIC_IFCFG,
+            ifcfg_after_turn_down=PARTIALLY_ACQUIRED_NIC_IFCFG,
+            ifcfg_after_disable_onboot=FULLY_ACQUIRED_NIC_IFCFG)
+
+    def test_acquire_once_owned_ifcfg_nic(self):
+        self._test_acquire_ifcfg_nic(
+            original_ifcfg=FULLY_ACQUIRED_NIC_IFCFG,
+            ifcfg_after_turn_down=FULLY_ACQUIRED_NIC_IFCFG,
+            ifcfg_after_disable_onboot=FULLY_ACQUIRED_NIC_IFCFG)
+
+    @mock.patch.object(ifacquire.os.path, 'isfile', lambda x: True)
+    @mock.patch.object(ifacquire.ifcfg, 'ifdown', return_value=None)
+    @mock.patch.object(ifacquire, 'open', new_callable=mock.mock_open,
+                       create=True)
+    # atomic_file_write has similiar API to open
+    @mock.patch.object(ifacquire.utils, 'atomic_file_write',
+                       new_callable=mock.mock_open)
+    def _test_acquire_ifcfg_nic(self, mock_atomic_write, mock_open,
+                                mock_ifdown,
+                                original_ifcfg,
+                                ifcfg_after_turn_down,
+                                ifcfg_after_disable_onboot):
+        open_file = mock_open()
+        atomic_write_file = mock_atomic_write()
+        open_file.readlines.return_value = original_ifcfg
+        atomic_write_file.readlines.return_value = original_ifcfg
+
+        with ifacquire.Transaction(netinfo_nets={}) as a:
+            a.acquire(ifaces=[NIC_NAME])
+
+            atomic_write_file.writelines.assert_called_with(
+                ifcfg_after_turn_down)
+            atomic_write_file.readlines.return_value = ifcfg_after_turn_down
+            mock_ifdown.assert_called_with(NIC_NAME)
+
+        atomic_write_file.writelines.assert_called_with(
+            ifcfg_after_disable_onboot)
+
+    @mock.patch.object(ifacquire.ifcfg, 'ifdown', lambda x: None)
+    @mock.patch.object(ifacquire.os.path, 'isfile', lambda x: True)
+    @mock.patch.object(ifacquire.ifcfg, 'ifup', return_value=None)
+    @mock.patch.object(ifacquire, 'open', new_callable=mock.mock_open,
+                       create=True)
+    # atomic_file_write has similiar API to open
+    @mock.patch.object(ifacquire.utils, 'atomic_file_write',
+                       new_callable=mock.mock_open)
+    def test_rollback_acquired_ifcfg_nic(self, mock_atomic_write, mock_open,
+                                         mock_ifup):
+        open_file = mock_open()
+        atomic_write_file = mock_atomic_write()
+        open_file.readlines.return_value = NIC_IFCFG
+        atomic_write_file.readlines.return_value = NIC_IFCFG
+
+        with self.assertRaises(TestException):
+            with ifacquire.Transaction(netinfo_nets={}) as a:
+                a.acquire(ifaces=[NIC_NAME])
+
+                raise TestException()
+
+        atomic_write_file.writelines.assert_called_with(NIC_IFCFG)
+        mock_ifup.assert_called_with(NIC_NAME)
