57d318134ae800366a1480250cfbe9f626e0830c
Nir Soffer <nsoffer@redhat.com>
2016-07-20 19:19:10 +0300

    osutils: Start the osutils module
    We use to have code calling os.close() using utils.NoIntrCall. If
    os.close(fd) was interrupted by signal, we would try to close again.
    This may lead to closing other thread file descriptor.
    
    close(2) warns not to retry close() after EINTR:
    
        Note that the return value should be used only for diagnostics. In
        particular close() should not be retried after an EINTR since this
        may cause a reused descriptor from another thread to be closed.
    
        See also this discussion about close and EINTR on linux:
        http://lwn.net/Articles/576478/
    
    The same issue was fixed in cpopen few month ago.
    
    This patch adds osutils.close_fd() - this should be used instead of
    os.close() in vdsm.
    
    This should also be the place for os related utilities like NoIntrCall
    and friends, to be added later.
    
    Change-Id: Ia995c083f31c3489ced16265d459f30355f854b0
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/61142
    Reviewed-by: Adam Litke <alitke@redhat.com>
    Reviewed-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>

---
+++ lib/vdsm/common/osutils.py
@@ -0,0 +1,43 @@
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+import errno
+import os
+
+
+def close_fd(fd):
+    """
+    Close fd once, ignoring EINTR.
+
+    close(2) warns not to retry close() after EINTR:
+
+        Note that the return value should be used only for diagnostics. In
+        particular close() should not be retried after an EINTR since this
+        may cause a reused descriptor from another thread to be closed.
+
+    See also this discussion about close and EINTR on linux:
+    http://lwn.net/Articles/576478/
+    """
+    try:
+        os.close(fd)
+    except EnvironmentError as e:
+        if e.errno != errno.EINTR:
+            raise
---
+++ tests/osutils_test.py
@@ -0,0 +1,48 @@
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+import errno
+import os
+import time
+
+from testlib import VdsmTestCase
+from testlib import forked
+from vdsm.common import osutils
+
+
+class TestCloseFd(VdsmTestCase):
+
+    # Run in a child process to ensure single thread. Otherwise another thread
+    # opening a file descriptor may cause false failure.
+    @forked
+    def test_close(self):
+        fds = os.pipe()
+        for fd in fds:
+            osutils.close_fd(fd)
+        time.sleep(0.1)
+        for fd in fds:
+            path = "/proc/self/fd/%d" % fd
+            self.assertFalse(os.path.exists(path))
+
+    def test_propagate_other_errors(self):
+        with self.assertRaises(OSError) as e:
+            osutils.close_fd(-1)
+        self.assertEqual(e.exception.errno, errno.EBADF)
