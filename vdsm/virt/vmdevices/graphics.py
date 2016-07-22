ebf5943983264524b79c0cf287c9a7fab435fe1f
Edward Haas <edwardh@redhat.com>
2016-07-22 23:32:30 +0300

    Revert "virt net: Change graphics type from network to address"
    This reverts commit 13603192ab2daa0eb39172f0682a864cd64edf88.
    
    The original commit should have been merged together with all
    ovs_backport topic patches.
    
    Change-Id: Iea42b9c0de25b2d1d08e06ef0deaae59b9e53717
    Bug-Url: https://bugzilla.redhat.com/1195208
    Signed-off-by: Edward Haas <edwardh@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/61259
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Tested-by: Dan Kenigsberg <danken@redhat.com>
    Continuous-Integration: Jenkins CI

--- vdsm/virt/vmdevices/graphics.py
+++ vdsm/virt/vmdevices/graphics.py
@@ -18,9 +18,10 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
+import logging
+
 import libvirt
 
-from vdsm import supervdsm
 from vdsm.network import api as net_api
 from vdsm import utils
 from vdsm.config import config
@@ -140,17 +141,7 @@ class Graphics(Base):
                 graphics.appendChildWithArgs('channel', name=chan,
                                              mode='secure')
 
-        # For the listen type IP to be used, the display network must be OVS.
-        # We assume that the cluster in which the host operates is OVS enabled
-        # and all other hosts in the cluster have the migration hook installed.
-        # The migration hook is responsible to convert ip to net and vice versa
-        display_network = self.specParams.get('displayNetwork')
-        display_ip = self.specParams.get('displayIp', '0')
-        if (display_network and display_ip != '0' and
-                supervdsm.getProxy().ovs_bridge(display_network)):
-            graphics.appendChildWithArgs(
-                'listen', type='address', address=display_ip)
-        elif display_network:
+        if self.specParams.get('displayNetwork'):
             graphics.appendChildWithArgs(
                 'listen', type='network',
                 network=net_api.netname_o2l(
@@ -252,16 +243,11 @@ def getFirstGraphics(conf):
 def _getNetworkIp(network):
     try:
         nets = net_api.libvirt_networks()
-        # On a legacy based network, the device is the iface specified in the
-        # network report (supporting real bridgeless networks).
-        # In case the report or the iface key is missing,
-        # the device is defaulted to the network name (i.e. northbound port).
-        device = (nets[network].get('iface', network)
-                  if network in nets else network)
+        device = nets[network].get('iface', network)
         ip, _, _, _ = net_api.ip_addrs_info(device)
     except (libvirt.libvirtError, KeyError, IndexError):
         ip = config.get('addresses', 'guests_gateway_ip')
-    finally:
         if ip == '':
             ip = '0'
+        logging.info('network %s: using %s', network, ip)
     return ip
