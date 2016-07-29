76b8c4ad4a569456db45be14b8644a6438306021
Ondřej Svoboda <osvoboda@redhat.com>
2016-07-29 18:07:15 +0200

    network: rename reported 'dnss' to 'nameservers' for clarity
    Change-Id: Ie66c0e8cdf86da38a8e1f4b35d284c174973389e
    Signed-off-by: Ondřej Svoboda <osvoboda@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/61712
    Continuous-Integration: Jenkins CI
    Reviewed-by: Edward Haas <edwardh@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- lib/vdsm/network/netinfo/dns.py
+++ lib/vdsm/network/netinfo/dns.py
@@ -25,15 +25,15 @@ def get_host_nameservers():
     """Returns a list of nameservers listed in /etc/resolv.conf"""
     with open(DNS_CONF_FILE, 'r') as file_object:
         file_text = file_object.read()
-    return _parse_dnss(file_text)
+    return _parse_nameservers(file_text)
 
 
-def _parse_dnss(file_text):
-    dnss = []
+def _parse_nameservers(file_text):
+    nameservers = []
     for line in file_text.splitlines():
         words = line.strip().split()
         if len(words) < 2:
             continue
         if words[0] == 'nameserver':
-            dnss.append(words[1])
-    return dnss
+            nameservers.append(words[1])
+    return nameservers
