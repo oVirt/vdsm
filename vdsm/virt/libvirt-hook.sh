8d8e3fe0c4881a6326e07d07d1d206e11645032e
Edward Haas <edwardh@redhat.com>
2016-08-09 14:26:17 +0300

    revert net: Libvirt hook that enables ovs-legacy migration
    Both the integrated and older OVS hook have a libvirt hook that
    enables VM migration.
    The two are colliding and only one should be left around.
    Therefore, the ovs hook one is removed.
    
    Future patches should remove the ovs hook completely.
    
    This reverts commit 50de041e01b098843b0fd7a4d3927783c11c79b6.
    
    Change-Id: I16ebce73becbfc0ddc202f49e70e46a36f3aaddc
    Signed-off-by: Edward Haas <edwardh@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/62121
    Continuous-Integration: Jenkins CI
    Reviewed-by: Petr Horáček <phoracek@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- vdsm/virt/libvirt-hook.sh
+++ vdsm/virt/libvirt-hook.sh
@@ -17,9 +17,5 @@ fi
 
 # Fix VMs migrating to host with libvirt >= 1.2.8
 # See https://bugzilla.redhat.com/show_bug.cgi?id=1138340
-if [ -x /usr/libexec/vdsm/ovs_migrate.py ]; then
-	sed -e 's|<min_guarantee[^>]*>[0-9 ]*</min_guarantee>||g' | /usr/libexec/vdsm/ovs_migrate.py $DOMAIN $EVENT $PHASE
-else
-	sed -e 's|<min_guarantee[^>]*>[0-9 ]*</min_guarantee>||g' | /usr/libexec/vdsm/vm_migrate_hook.py $DOMAIN $EVENT $PHASE
-fi
+sed -e 's|<min_guarantee[^>]*>[0-9 ]*</min_guarantee>||g' | /usr/libexec/vdsm/vm_migrate_hook.py $DOMAIN $EVENT $PHASE
 
