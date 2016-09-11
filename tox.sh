b4a9cf52b421824b2a869f9d46902db31d3c44ee
Edward Haas <edwardh@redhat.com>
2016-09-11 11:35:36 +0300

    build: Allow tox to run all checkers in one shot.
    tox has been called for each checker (pep8, pyflakes), with this patch
    it is called once and runs all checkers.
    
    Change-Id: I3efc6e2d6318841892f3cf5bfa27a621922ed025
    Signed-off-by: Edward Haas <edwardh@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/63633
    Continuous-Integration: Jenkins CI
    Reviewed-by: Petr Horáček <phoracek@redhat.com>
    Reviewed-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>
    Reviewed-by: Irit Goihman <igoihman@redhat.com>
    Reviewed-by: Yaniv Bronhaim <ybronhei@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- tox.sh
+++ tox.sh
@@ -24,20 +24,20 @@ PEP8_BLACKLIST=(config.py \
                 vdsm.py \
                )
 
-if [ 'pyflakes' = "$1" ]; then
-    (find . -path './.tox' -prune -type f -o \
-        -path './.git' -prune -type f -o \
-        -path './.ropeproject' -prune -type f -o \
-        -name '*.py' && echo "${WHITELIST[@]}") | \
-        xargs pyflakes | grep -w -v "${SKIP_PYFLAKES_ERR}" | \
-        while read LINE; do echo "$LINE"; false; done
-fi
-
-if [ 'pep8' = "$1" ]; then
-    for x in ${PEP8_BLACKLIST[@]}; do \
-    exclude="${exclude},${x}" ; \
-        done ; \
-        pep8 --exclude="${exclude},.tox,.ropeproject" \
-        --filename '*.py' . \
-        "${WHITELIST[@]}"
-fi
+for checker in "$@"; do
+    if [ 'pyflakes' = "$checker" ]; then
+        (find . -path './.tox' -prune -type f -o \
+            -path './.git' -prune -type f -o \
+            -path './.ropeproject' -prune -type f -o \
+            -name '*.py' && echo "${WHITELIST[@]}") | \
+            xargs pyflakes | grep -w -v "${SKIP_PYFLAKES_ERR}" | \
+            while read LINE; do echo "$LINE"; false; done
+    elif [ 'pep8' = "$checker" ]; then
+        for x in ${PEP8_BLACKLIST[@]}; do \
+        exclude="${exclude},${x}" ; \
+            done ; \
+            pep8 --exclude="${exclude},.tox,.ropeproject" \
+            --filename '*.py' . \
+            "${WHITELIST[@]}"
+    fi
+done
