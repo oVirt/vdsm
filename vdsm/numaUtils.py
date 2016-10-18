599022ade7031e70c735534aa9576f3328a893e4
Milan Zamazal <mzamazal@redhat.com>
2016-10-18 13:18:26 +0200

    numa: Don't crash on hotplugged CPUs when retrieving NUMA info
    getVmNumaNodeRuntimeInfo maps virtual (from Engine) nodes to physical
    nodes by matching nodes and CPUs from the libvirt runtime and from the
    Engine NUMA mapping.  CPUs that are not pinned to any node in the Engine
    NUMA mapping (such as hotplugged CPUs) are irrelevant.  However the
    matching cycle still looks for them in the Engine NUMA mapping
    dictionary, resulting in KeyError.
    
    This patch fixes the KeyError by simply skipping the CPUs not present in
    the Engine NUMA mapping.
    
    Bug-Url: https://bugzilla.redhat.com/1413968
    Backport-To: 3.6
    Change-Id: If7d00b897e430d9c4e8a23cb1451209b6f00e688
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/65562
    Reviewed-by: Martin Polednik <mpolednik@redhat.com>
    Reviewed-by: Francesco Romani <fromani@redhat.com>
    Continuous-Integration: Jenkins CI

--- vdsm/numaUtils.py
+++ vdsm/numaUtils.py
@@ -19,6 +19,7 @@
 #
 
 from collections import defaultdict
+import logging
 
 import os.path
 import xml.etree.cElementTree as ET
@@ -113,7 +114,20 @@ def getVmNumaNodeRuntimeInfo(vm):
         vcpu_to_vnode = _get_mapping_vcpu_to_vnode(vm)
 
         for vcpu_id, pcpu_id in vcpu_to_pcpu.iteritems():
-            vnode_index = str(vcpu_to_vnode[vcpu_id])
+            try:
+                vnode_index = str(vcpu_to_vnode[vcpu_id])
+            except KeyError:
+                # Not all CPUs are mapped to NUMA nodes, e.g.:
+                # - We don't assign hotplugged CPUs to NUMA nodes.
+                # - When Engine assigns equal number of CPUs to each of the
+                #   NUMA nodes, the contingent remaining CPUs are left
+                #   unassigned.
+                # We simply skip the unassigned CPUs here.
+                log = logging.getLogger('NUMA')
+                log.debug("Virtual CPU #%s not assigned to any virtual "
+                          "NUMA node",
+                          vcpu_id)
+                continue
             vm_numa_placement[vnode_index].add(pcpu_to_pnode[pcpu_id])
             vm_numa_placement[vnode_index].update(
                 vcpu_to_pnode.get(vcpu_id, ()))
