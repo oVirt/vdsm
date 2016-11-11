9babd7ee8dfdd1009c55b6415c05ef552bde7964
Nir Soffer <nsoffer@redhat.com>
2016-11-12 01:37:07 +0200

    tests: Add working import check
    Add a working import tests using pytest. The script find all the modules
    in the vdsm package and generate pytest parametried test for each
    module. Running the tests using pytest show detailed traceback for
    module using forbidden imports.
    
    This test run separately from the other tests with clean python search
    path including only the lib directory.
    
    To run the only the import check:
    
        make imports
    
    Modules with bad imports (vdsm.rpc.Bridge and vdsm.rpc.bindingxmlrpc)
    are mark as expected failures.
    
    Modules using soft dependency that are not supported on the current
    platform are expected to raise compat.Unsupported exception on import.
    These modules are skipped.
    
    Here is an example test summary for currnet code:
    
    ==================================================================================
    short test summary info
    ==================================================================================
    XFAIL check_imports.py::test_import[vdsm.rpc.Bridge]
    XFAIL check_imports.py::test_import[vdsm.rpc.bindingxmlrpc]
    SKIP [1] /home/nsoffer/src/vdsm/tests/check_imports.py:59: No module named statsd
    SKIP [1] /home/nsoffer/src/vdsm/tests/check_imports.py:59: No module named hawkular
    
    Change-Id: I4e4433a7c35e39374999d2a45260b55a8ca1f217
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/66479
    Continuous-Integration: Jenkins CI
    Reviewed-by: Leon Goldberg <lgoldber@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

---
+++ tests/check_imports.py
@@ -0,0 +1,59 @@
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+"""
+Ensure that lib/vdsm modules do not depend on private modules in
+/usr/share/vdsm.
+"""
+
+import importlib
+import pkgutil
+import pytest
+
+from vdsm import compat
+
+
+def find_modules():
+    """
+    Yields fully qualified modules names in the vdsm package.
+    """
+    expected_to_fail = {
+        "vdsm.rpc.Bridge",
+        "vdsm.rpc.bindingxmlrpc",
+    }
+
+    def error(name):
+        raise
+
+    vdsm_pkg = importlib.import_module("vdsm")
+    for _, name, _ in pkgutil.walk_packages(vdsm_pkg.__path__,
+                                            prefix="vdsm.",
+                                            onerror=error):
+        if name in expected_to_fail:
+            name = pytest.mark.xfail(name)
+        yield name
+
+
+@pytest.mark.parametrize("name", find_modules())
+def test_import(name):
+    try:
+        importlib.import_module(name)
+    except compat.Unsupported as e:
+        pytest.skip(str(e))
