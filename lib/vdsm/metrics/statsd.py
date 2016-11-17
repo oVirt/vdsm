a149cb7c691c672f9cbe09973d63ef3d26ae9854
Yaniv Bronhaim <ybronhei@redhat.com>
2016-11-17 15:50:29 +0200

    Adding simple client for sending gauge metrics to statsd port using udp
    Introducing simple udp client. The client sends gauge metric statistics in
    the metric 2.0 spec format - key:value|g.
    
    Prior to this implementation we used python-statsd package which  is not built
    out side epel. The code we used to use from python-statsd is only the part
    that sends gauge by udp, we don't have any use for the rest currently.
    
    Change-Id: I7bbc8a4c0e3afc25c205bfec58c5e77096803404
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- lib/vdsm/metrics/statsd.py
+++ lib/vdsm/metrics/statsd.py
@@ -19,29 +19,62 @@
 #
 
 from __future__ import absolute_import
-
 import six
-from vdsm import compat
-
-try:
-    import statsd
-except ImportError as e:
-    raise compat.Unsupported(str(e))
+import socket
 
 _client = None
 
 
-def start(address):
+def start(address, port=8125):
     global _client
     if _client is None:
-        _client = statsd.StatsClient(host=address)
+        _client = _StatsClient(address, port=port)
 
 
 def stop():
-    # client doesn't support any close mechanism
-    pass
+    global _client
+    if _client is not None:
+        _client.close()
 
 
 def send(report):
     for name, value in six.iteritems(report):
         _client.gauge(name, value)
+
+
+class _StatsClient(object):
+    """
+    Simple client that sends udp messages to stastd port in metrics format
+    standard (based on http://metrics20.org/spec).
+
+    Currently supports only gauge reports which is used in VDSM.
+    """
+    def __init__(self, host, port=8125, maxudpsize=512, ipv6=False):
+        fam = socket.AF_INET6 if ipv6 else socket.AF_INET
+        family, _, _, _, addr = socket.getaddrinfo(
+            host, port, fam, socket.SOCK_DGRAM)[0]
+        self._addr = addr
+        self._sock = socket.socket(family, socket.SOCK_DGRAM)
+        self._maxudpsize = maxudpsize
+
+    def _send(self, data):
+        try:
+            self._sock.sendto(data, self._addr)
+        except socket.error:
+            # Keeping python-statsd behavior - we intintially avoid to log
+            # any socket error to allow running this code without reachable
+            # server.
+            pass
+
+    def close(self):
+        self._sock.close()
+
+    def gauge(self, stat, value):
+        """
+        Sending gauge report for specific metric in the format stat:value|g
+
+        Args:
+            stat (string): metric name decoded to utf-8
+            value (int): numeric value for stat
+        """
+        self._send('%s:%s|g' % (stat, value))
---
+++ tests/statsd_tests.py
@@ -0,0 +1,74 @@
+# coding=utf-8
+#
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+from vdsm.metrics import statsd
+from testlib import mock
+from testlib import VdsmTestCase as TestCaseBase
+
+
+class StatsdModuleTest(TestCaseBase):
+    @classmethod
+    def setup_class(cls):
+        cls.old_socket = statsd.socket.socket
+        cls.mock_socket = statsd.socket.socket = mock.Mock()
+        try:
+            statsd.start('localhost')
+            cls._address = ('127.0.0.1', 8125)
+        except:
+            statsd.socket.socket = cls.old_socket
+
+    @classmethod
+    def teardown_class(cls):
+        statsd.socket.socket = cls.old_socket
+
+    def setUp(self):
+        self.mock_socket.reset_mock()
+
+    def test_send_single(self):
+        data = {'hello': 3}
+        statsd.send(data)
+        self.mock_socket.return_value.sendto.assert_called_once_with(
+            b'hello:3|g', self._address)
+
+    def test_send_unicode(self):
+        data = {'\xd7\xa9\xd7\x9c\xd7\x95\xd7\x9d': 3}
+        statsd.send(data)
+        self.mock_socket.return_value.sendto.assert_called_once_with(
+            b'\xd7\xa9\xd7\x9c\xd7\x95\xd7\x9d:3|g', self._address)
+
+    def test_send_mixed_chars(self):
+        data = {'hello.\xd7\xa9\xd7\x9c\xd7\x95\xd7\x9d.ma': 3}
+        statsd.send(data)
+        self.mock_socket.return_value.sendto.assert_called_once_with(
+            b'hello.\xd7\xa9\xd7\x9c\xd7\x95\xd7\x9d.ma:3|g', self._address)
+
+    def test_send_long_metric_name(self):
+        long_metric_name = ".".join(["1234567890"] * 12)
+        data = {long_metric_name: 3}
+        statsd.send(data)
+        self.mock_socket.return_value.sendto.assert_called_once_with(
+            b'%s:3|g' % long_metric_name, self._address)
+
+    def test_send_multiple(self):
+        data = {'hello': 7, 'goodbye': 11}
+        statsd.send(data)
+        calls = [mock.call(b'hello:7|g', self._address),
+                 mock.call(b'goodbye:11|g', self._address)]
+        self.mock_socket.return_value.sendto.assert_has_calls(calls)
