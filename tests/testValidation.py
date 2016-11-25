62ff0ef251f6cad8b277bbd7725a8afdb7e6e6fb
Nir Soffer <nsoffer@redhat.com>
2016-11-25 23:36:17 +0200

    testValidation: Add @skipif decorator
    Allows skipping a test if it cannot run with the current environment.
    The tests is not run in the skip condition is True, but it must pass if
    the skip condition is false. This can be used to skip tests that are not
    compatible on python 3 yet.
    
    Example usage:
    
        from testValidation import skipif
    
        @skipif(six.PY3, "Needs porting to python 3")
        def test_rusty_python2_code(self):
            ...
    
    This is a subset of pytest.mark.skipif.
    
    Change-Id: I71a2ae262482edc8acf303f18eac6f9035e710bc
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- tests/testValidation.py
+++ tests/testValidation.py
@@ -252,6 +252,29 @@ def xfail(reason):
     return wrap
 
 
+def skipif(cond, reason):
+    """
+    Skip a test if cond is True.
+
+    Usage::
+
+        @skipif(six.PY3, "Needs porting to python 3")
+        def test_rusty_pyton_2_code(self):
+            ...
+    """
+    _check_decorator_misuse(reason)
+
+    def wrap(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            if cond:
+                raise SkipTest(reason)
+            return f(*args, **kwargs)
+        return wrapper
+
+    return wrap
+
+
 def brokentest(reason):
     """
     Mark a test as broken.
