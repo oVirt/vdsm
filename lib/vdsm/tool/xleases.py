3acfdbdffbe9cdbf6d72ab3ba586e3141b56d334
Nir Soffer <nsoffer@redhat.com>
2016-12-24 21:01:53 +0200

    vdsm-tool: Add format-xleases command
    This command can be use to format the xleases special volume if the
    volume is corrupted or there is a need to add the volume on a
    development setup.
    
    The caller must know the path to the volume and make sure the volume is
    accessible (activate the lv if needed).
    
    Example usage:
    
        $ vdsm-tool format-xleases -h
        usage: vdsm-tool [-h] sd_id path
    
        positional arguments:
          sd_id       storage domain UUID
          path        path to xleases volume
    
        optional arguments:
          -h, --help  show this help message and exit
    
    Creating xleases volume on file storage:
    
        PATH=/rhev/data-center/mnt/server:_export/sd-id/dom_md/xleases
        truncate -s 1G $PATH
        vdsm-tool format-xleases sd-id $PATH
    
    Creating the xleases volume on block storage:
    
        lvcreate --name xleases --size 1G sd-id
        vdsm-tool format-xleases sd-id /dev/sd-id/xleases
        lvchange -an sd-id/xleases
    
    Change-Id: I5d1a1a34eea80ddca01967c0a2da2e9e50e50656
    Bug-Url: https://bugzilla.redhat.com/1656815
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ lib/vdsm/tool/xleases.py
@@ -0,0 +1,76 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+import argparse
+
+from vdsm import utils
+from vdsm.storage import xlease
+from . import expose
+
+
+@expose('format-xleases')
+def format_xleases(*args):
+    """
+    format-xleases sd_id path
+
+    WARNING:
+
+        This is a destructive operation - you must put the storage
+        domain into maintenance before running this tool.
+
+    Format the xleases volume index, dropping all leases from the index.
+    This does not delete sanlock resources on the volume. If you want to
+    restore existing sanlock resources, use rebuild-index. If you want
+    to delete also sanlock resources on this volume you can wipe the
+    entire volume using dd before formatting it.
+
+    Notes:
+
+    - With iSCSI based storage you may need to connect to the traget
+      using iscsiadm.
+
+    - With file based storage, you may need to mount the storage domain.
+
+    - With block based stoage, you need to activate the xleases logical
+      volume before the operation, and deactivate after the operation.
+
+    If formatting fails, the volume will not be usable (it will be
+    marked as "updating"), but the operation can be tried again.
+
+    Creating xleases volume on file storage:
+
+        PATH=/rhev/data-center/mnt/server:_path/sd-id/dom_md/xleases
+        truncate -s 1G $PATH
+        vdsm-tool format-xleases sd-id $PATH
+
+    Creating the xleases volume on block storage:
+
+        lvcreate --name xleases --size 1g sd-id
+        vdsm-tool format-xleases sd-id /dev/sd-id/xleases
+        lvchange -an sd-id/xleases
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sd_id', help="storage domain UUID")
+    parser.add_argument('path', help="path to xleases volume")
+    args = parser.parse_args(args[1:])
+
+    backend = xlease.DirectFile(args.path)
+    with utils.closing(backend):
+        xlease.format_index(args.sd_id, backend)
