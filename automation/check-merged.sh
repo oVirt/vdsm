c4f2226184973bc3dc03b724d47a7fd2d82cd7ea
Leon Goldberg <lgoldber@redhat.com>
2016-12-26 12:31:02 +0200

    check-merged bugfix: wrong shell variable
    PID=$! was modified to PID=$? accidently, reverting
    
    Change-Id: I92ea79df549605ed5ac694052096e7c43a7b9779
    Signed-off-by: Leon Goldberg <lgoldber@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -98,16 +98,16 @@ for distro in el7; do
     lago ovirt deploy
 
     lago ovirt serve &
-    PID=$?
+    PID=$!
 
     mount_tmpfs
 
     run_functional_tests | tee "$EXPORTS/functional_tests_stdout.$distro.log"
     failed="${PIPESTATUS[0]}"
 
-#    run_network_tests | tee -a "$EXPORTS/functional_tests_stdout.$distro.log"
-#    res="${PIPESTATUS[0]}"
-#    [ "$res" -ne 0 ] && failed="$res"
+    run_network_tests | tee -a "$EXPORTS/functional_tests_stdout.$distro.log"
+    res="${PIPESTATUS[0]}"
+    [ "$res" -ne 0 ] && failed="$res"
 
     kill $PID
 
