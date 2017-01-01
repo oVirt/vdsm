oVirt/vdsm

commit 166bda11d8c6a4af310702601ecc3d7b3db0be0d

xleases: Create and activate the external leases volume
Create the external leases volume when creating a new storage domain, or
upgrading storage domain from version 3 to version 4. Activate the new
volume in the flows activating the other special volumes.

This change introduce a new special volume, that must be created and
activated only on file and block storage domain v4. To support this we
use now helper functions to return the special volumes for the current
domain version and type.

StorageDomainManifest gained these APIs:

- supports_external_leases(version) - tell if the storage domain type
  can use external leases at that version.

- special_volumes(version) - the special volumes used by the storage
  domain.

StorageDomain gained these APIs:

- supports_external_leases(version) - redirect to the manifest class.

- external_leases_path() - return the path to the external lease volume

- create_external_leases() - create the special external leases volume
  or reuse existing volume. Called during upgrade to version 4.  This
  should be used also when creating a new storage domain, but this
  requires a risky refactoring.

- format_external_leases(lockspace, path) - format the external leases
  volume index, destroying existing information. Called when creating a
  new storage domain or when upgrading to storage domain version 4.

The new "xleases" lv was added to the vdsm lvm.rules udev rule, ensuring
correct ownership and permissions of the device.

In the upgrade flow we attempt to create and format the new volume. If
creating or formatting fail, we do not attempt to clean up anything
since removing a partly created volume will likely fail if storage
cannot be accessed. A partly created volume will be reused and
reformatted on the next upgrade attempt.

Currently we use xlease.DirectFile for both file and block storage.
This should be replaced later with storage type specific backend to
protect from blocking entire vdsm process if storage become

vdsm/storage/fileSD.py
@@ -50,12 +50,13 @@ FILE_SD_MD_FIELDS[REMOTE_PATH] = (str, str)
 
 METADATA_PERMISSIONS = 0o660
 
-# On file domains IDS and LEASES volumes don't need a fixed size (they are
+# On file domains sanlock volumes don't need a fixed size (they are
 # allocated as we use them)
 FILE_SPECIAL_VOLUME_SIZES_MIB = sd.SPECIAL_VOLUME_SIZES_MIB.copy()
 FILE_SPECIAL_VOLUME_SIZES_MIB.update({
     sd.IDS: 0,
     sd.LEASES: 0,
+    sd.XLEASES: 0,
 })
 
 # Specific stat(2) block size as defined in the man page
@@ -170,6 +171,13 @@ class FileStorageDomainManifest(sd.StorageDomainManifest):
 
         if not self.oop.fileUtils.pathExists(self.metafile):
             raise se.StorageDomainMetadataNotFound(self.sdUUID, self.metafile)
+
+    @classmethod
+    def special_volumes(cls, version):
+        if cls.supports_external_leases(version):
+            return sd.SPECIAL_VOLUMES_V4
+        else:
+            return sd.SPECIAL_VOLUMES_V0
 
     def getMonitoringPath(self):
         return self.metafile
@@ -406,14 +414,20 @@ class FileStorageDomain(sd.StorageDomain):
         procPool = oop.getProcessPool(sdUUID)
         procPool.fileUtils.createdir(metadataDir, 0o775)
 
-        for metaFile, metaSize in FILE_SPECIAL_VOLUME_SIZES_MIB.iteritems():
-            try:
-                procPool.truncateFile(
-                    os.path.join(metadataDir, metaFile),
-                    metaSize * constants.MEGAB, METADATA_PERMISSIONS)
-            except Exception as e:
-                raise se.StorageDomainMetadataCreationError(
-                    "create meta file '%s' failed: %s" % (metaFile, str(e)))
+        special_volumes = cls.manifestClass.special_volumes(version)
+        for name, size_mb in FILE_SPECIAL_VOLUME_SIZES_MIB.iteritems():
+            if name in special_volumes:
+                try:
+                    procPool.truncateFile(
+                        os.path.join(metadataDir, name),
+                        size_mb * constants.MEGAB, METADATA_PERMISSIONS)
+                except Exception as e:
+                    raise se.StorageDomainMetadataCreationError(
+                        "create meta file '%s' failed: %s" % (name, str(e)))
+
+        if cls.supports_external_leases(version):
+            xleases_path = os.path.join(metadataDir, sd.XLEASES)
+            cls.format_external_leases(sdUUID, xleases_path)
 
         metaFile = os.path.join(metadataDir, sd.METADATA)
 
@@ -718,6 +732,27 @@ class FileStorageDomain(sd.StorageDomain):
         Return a type specific volume generator object
         """
         return fileVolume.FileVolume
+
+    # External leases support
+
+    def external_leases_path(self):
+        return os.path.join(self.getMDPath(), sd.XLEASES)
+
+    def create_external_leases(self):
+        """
+        Create the external leases special volume.
+
+        Called during upgrade from version 3 to version 4.
+        """
+        proc = oop.getProcessPool(self.sdUUID)
+        path = self.external_leases_path()
+        size = FILE_SPECIAL_VOLUME_SIZES_MIB[sd.XLEASES] * constants.MEGAB
+        self.log.info("Creating external leases volume %s", path)
+        try:
+            proc.truncateFile(path, size, METADATA_PERMISSIONS, creatExcl=True)
+        except OSError as e:
+            if e.rrrno == errno.EEXIST:
+                self.log.info("Reusing external leases volume %s", path)
 
 
 def _getMountsList(pattern="*"):

tests/storage_sdm_indirection_tests.py
@@ -241,6 +241,11 @@ class FakeBlockDomainManifest(FakeDomainManifest):
     def getVolumeLease(self, imgUUID, volUUID):
         pass
 
+    @classmethod
+    @recorded
+    def supports_external_leases(cls, version):
+        pass
+
 
 class FakeFileDomainManifest(FakeDomainManifest):
     def __init__(self):
@@ -269,6 +274,11 @@ class FakeFileDomainManifest(FakeDomainManifest):
 
     @recorded
     def getVolumeLease(self, imgUUID, volUUID):
+        pass
+
+    @classmethod
+    @recorded
+    def supports_external_leases(cls, version):
         pass
 
 
@@ -652,6 +662,12 @@ class RedirectionChecker(object):
 
 @expandPermutations
 class DomainTestMixin(object):
+
+    @permutations([
+        ['supports_external_leases', 1],
+    ])
+    def test_class_methods(self, fn, nargs):
+        self.checker.check_classmethod_call(fn, nargs)
 
     @permutations([
         ['sdUUID', 'a6ecac0a-5c6b-46d7-9ba5-df8b34df2d01'],

tests/storage_testlib_test.py
@@ -127,7 +127,8 @@ class FakeBlockEnvTests(VdsmTestCase):
             md_path = env.lvm.lvPath(vg_name, sd.METADATA)
             self.assertTrue(os.path.exists(md_path))
 
-            for lv in blockSD.SPECIAL_LVS:
+            version = env.sd_manifest.getVersion()
+            for lv in env.sd_manifest.special_volumes(version):
                 self.assertEqual(lv, env.lvm.getLV(vg_name, lv).name)
 
             images_dir = os.path.join(env.sd_manifest.domaindir,

tests/storagetestlib.py
@@ -155,8 +155,11 @@ def make_blocksd_manifest(tmpdir, fake_lvm, sduuid=None, devices=None,
     fake_lvm.createLV(sduuid, sd.METADATA, blockSD.SD_METADATA_SIZE)
 
     # Create the rest of the special LVs
-    for metafile, sizemb in sd.SPECIAL_VOLUME_SIZES_MIB.iteritems():
-        fake_lvm.createLV(sduuid, metafile, sizemb)
+    special = blockSD.BlockStorageDomainManifest.special_volumes(sd_version)
+    for name, size_mb in sd.SPECIAL_VOLUME_SIZES_MIB.iteritems():
+        if name in special:
+            fake_lvm.createLV(sduuid, name, size_mb)
+
     fake_lvm.createLV(sduuid, blockSD.MASTERLV, blockSD.MASTERLV_SIZE)
 
     # We'll store the domain metadata in the VG's tags

vdsm/storage/blockSD.py
@@ -66,7 +66,12 @@ STORAGE_DOMAIN_TAG = "RHAT_storage_domain"
 STORAGE_UNREADY_DOMAIN_TAG = STORAGE_DOMAIN_TAG + "_UNREADY"
 
 MASTERLV = "master"
-SPECIAL_LVS = (sd.METADATA, sd.LEASES, sd.IDS, sd.INBOX, sd.OUTBOX, MASTERLV)
+
+# Special lvs available since storage domain version 0
+SPECIAL_LVS_V0 = sd.SPECIAL_VOLUMES_V0 + (MASTERLV,)
+
+# Special lvs avilable since storage domain version 4.
+SPECIAL_LVS_V4 = sd.SPECIAL_VOLUMES_V4 + (MASTERLV,)
 
 MASTERLV_SIZE = "1024"  # In MiB = 2 ** 20 = 1024 ** 2 => 1GiB
 BlockSDVol = namedtuple("BlockSDVol", "name, image, parent")
@@ -170,7 +175,7 @@ def _getVolsTree(sdUUID):
                 vols[lv.name] = BlockSDVol(lv.name, image, parent)
                 break
         else:
-            if lv.name not in SPECIAL_LVS:
+            if lv.name not in SPECIAL_LVS_V4:
                 log.warning("Ignoring Volume %s that lacks minimal tag set"
                             "tags %s" % (lv.name, lv.tags))
     return vols
@@ -406,6 +411,13 @@ class BlockStorageDomainManifest(sd.StorageDomainManifest):
             # Such domains supported only 512 sizes
             self.logBlkSize = 512
             self.phyBlkSize = 512
+
+    @classmethod
+    def special_volumes(cls, version):
+        if cls.supports_external_leases(version):
+            return SPECIAL_LVS_V4
+        else:
+            return SPECIAL_LVS_V0
 
     def readMetadataMapping(self):
         meta = self.getMetadata()
@@ -783,8 +795,9 @@ class BlockStorageDomainManifest(sd.StorageDomainManifest):
         domMD = os.path.join(self.domaindir, sd.DOMAIN_META_DATA)
         fileUtils.createdir(domMD)
 
-        lvm.activateLVs(self.sdUUID, SPECIAL_LVS)
-        for lvName in SPECIAL_LVS:
+        special_lvs = self.special_volumes(self.getVersion())
+        lvm.activateLVs(self.sdUUID, special_lvs)
+        for lvName in special_lvs:
             dst = os.path.join(domMD, lvName)
             if not os.path.lexists(dst):
                 src = lvm.lvPath(self.sdUUID, lvName)
@@ -849,8 +862,9 @@ class BlockStorageDomainManifest(sd.StorageDomainManifest):
     def _getOccupiedMetadataSlots(self):
         stripPrefix = lambda s, pfx: s[len(pfx):]
         occupiedSlots = []
+        special_lvs = self.special_volumes(self.getVersion())
         for lv in lvm.getLV(self.sdUUID):
-            if lv.name in SPECIAL_LVS:
+            if lv.name in special_lvs:
                 # Special LVs have no mapping
                 continue
 
@@ -923,7 +937,11 @@ class BlockStorageDomain(sd.StorageDomain):
     def __init__(self, sdUUID):
         manifest = self.manifestClass(sdUUID)
         sd.StorageDomain.__init__(self, manifest)
-        lvm.activateLVs(self.sdUUID, SPECIAL_LVS)
+
+        # TODO: Move this to manifest.activate_special_lvs
+        special_lvs = manifest.special_volumes(manifest.getVersion())
+        lvm.activateLVs(self.sdUUID, special_lvs)
+
         self.metavol = lvm.lvPath(self.sdUUID, sd.METADATA)
 
         # Check that all devices in the VG have the same logical and physical
@@ -1014,10 +1032,16 @@ class BlockStorageDomain(sd.StorageDomain):
         mapping = cls.getMetaDataMapping(vgName)
 
         # Create the rest of the BlockSD internal volumes
-        for metaFile, metaSizeMb in sd.SPECIAL_VOLUME_SIZES_MIB.iteritems():
-            lvm.createLV(vgName, metaFile, metaSizeMb)
+        special_lvs = cls.manifestClass.special_volumes(version)
+        for name, size_mb in sd.SPECIAL_VOLUME_SIZES_MIB.iteritems():
+            if name in special_lvs:
+                lvm.createLV(vgName, name, size_mb)
 
         lvm.createLV(vgName, MASTERLV, MASTERLV_SIZE)
+
+        if cls.supports_external_leases(version):
+            xleases_path = _external_leases_path(vgName)
+            cls.format_external_leases(vgName, xleases_path)
 
         # Create VMS file system
         _createVMSfs(os.path.join("/dev", vgName, MASTERLV))
@@ -1515,6 +1539,35 @@ class BlockStorageDomain(sd.StorageDomain):
         Return a type specific volume generator object
         """
         return blockVolume.BlockVolume
+
+    # External leases support
+
+    def external_leases_path(self):
+        """
+        Return the path to te external leases volume.
+        """
+        return _external_leases_path(self.sdUUID)
+
+    def create_external_leases(self):
+        """
+        Create the external leases special volume.
+
+        Called during upgrade from version 3 to version 4.
+        """
+        path = self.external_leases_path()
+        try:
+            lvm.getLV(self.sdUUID, sd.XLEASES)
+        except se.LogicalVolumeDoesNotExistError:
+            self.log.info("Creating external leases volume %s", path)
+            size = sd.SPECIAL_VOLUME_SIZES_MIB[sd.XLEASES]
+            lvm.createLV(self.sdUUID, sd.XLEASES, size)
+        else:
+            self.log.info("Reusing external leases volume %s", path)
+            lvm.activateLVs(self.sdUUID, [sd.XLEASES])
+
+
+def _external_leases_path(sdUUID):
+    return lvm.lvPath(sdUUID, sd.XLEASES)
 
 
 def _createVMSfs(dev):

vdsm/storage/hsm.py
@@ -389,7 +389,7 @@ class HSM(object):
 
         def storageRefresh():
             sdCache.refreshStorage()
-            lvm.bootstrap(refreshlvs=blockSD.SPECIAL_LVS)
+            lvm.bootstrap(refreshlvs=blockSD.SPECIAL_LVS_V4)
             self._ready = True
             self.log.debug("HSM is ready")
 

vdsm/storage/imageRepository/formatConverter.py
@@ -343,7 +343,25 @@ def v3DomainConverter(repoPath, hostId, domain, isMsd):
 
 def v4DomainConverter(repoPath, hostId, domain, isMsd):
     targetVersion = 4
+
+    if domain.supports_external_leases(targetVersion):
+        # Try to create and format the new external leases volume. If this
+        # fail, the conversion will fail and the domain will remain in version
+        # 3.  If the volume exists (leftover from previous upgrade), we reuse
+        # it.
+        domain.create_external_leases()
+
+        # We have either a new or existing volume. Always format it to make
+        # sure it is properly formatted - formatting is cheap.
+        xleases_path = domain.external_leases_path()
+        domain.format_external_leases(domain.sdUUID, xleases_path)
+
+    # We may have now a good external leases volume, try to change the domain
+    # version to 4. If this fail, conversion will fail, and the domain will
+    # remain in version 3 with unused external leases volume.  Converting the
+    # domain again will resuse the external leases volume.
     domain.setMetaParam(sd.DMDK_VERSION, targetVersion)
+
     log.debug("Conversion of domain %s to version = %s has been completed.",
               domain.sdUUID, targetVersion)
 

vdsm/storage/localFsSD.py
@@ -39,6 +39,12 @@ class LocalFsStorageDomainManifest(fileSD.FileStorageDomainManifest):
         4: clusterlock.LocalLock,
     }
 
+    # External leases support
+
+    @classmethod
+    def supports_external_leases(cls, version):
+        return False
+
 
 class LocalFsStorageDomain(fileSD.FileStorageDomain):
     manifestClass = LocalFsStorageDomainManifest

commit fca2959114fb3161acc957abfc68511f2af8cc74

pylint: storage/fileSD: fix typo
commit 166bda1 introduced a typo that is hereby fixed.
It's time we introduce static checkers to the project, to avoid similar
cases.

Change-Id: Ia441c59ca994a9d15836b165a1a56e67a5e12ca6
Bug-Url: https://bugzilla.redhat.com/1317429
Signed-off-by: Dan Kenigsberg <danken@redhat.com>

vdsm/storage/fileSD.py
@@ -754,7 +754,7 @@ class FileStorageDomain(sd.StorageDomain):
         try:
             proc.truncateFile(path, size, METADATA_PERMISSIONS, creatExcl=True)
         except OSError as e:
-            if e.rrrno == errno.EEXIST:
+            if e.errno == errno.EEXIST:
                 self.log.info("Reusing external leases volume %s", path)
 
 