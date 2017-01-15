513d677b6d295eaab1c55e5db47a12a1974102ab
Leon Goldberg <lgoldber@redhat.com>
2017-01-15 17:02:56 +0200

    automation: prepare yum.conf before copying to lago guest
    The host's yum.conf sets reposdir to /dev/null/, causing
    localsync to be ignored which en route causes vdsm to be installed
    via the nightly repository.
    
    This patch both excludes vdsm the rest of the repositories configured
    in yum.conf and removes reposdir from yum.conf (resulting in the default
    destination which holds localsync) by modifying and copying
    over a temporary copy of yum.conf.
    
    Bug-Id: https://bugzilla.redhat.com/show_bug.cgi?id=1374328
    Change-Id: Ia301a1750f8755f49285fb202c7ee8bc9555d1ca
    Signed-off-by: Leon Goldberg <lgoldber@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -83,13 +83,28 @@ function run_network_tests {
     return $res
 }
 
+function prepare_and_copy_yum_conf {
+    local vm_name="$1"
+    local tempfile=$(mktemp XXXXXX)
+
+    cat /etc/yum/yum.conf 2>/dev/null | \
+    grep -v "reposdir" | \
+    "$AUTOMATION"/exclude_from_conf 'vdsm*' > "$tempfile"
+
+    lago copy-to-vm "$vm_name" "$tempfile" /etc/yum/yum.conf
+    rm "$tempfile"
+}
+
+mkdir "$EXPORTS"/lago-logs
 VMS_PREFIX="vdsm_functional_tests_host-"
 failed=0
 for distro in el7; do
     vm_name="${VMS_PREFIX}${distro}"
     # starting vms one by one to avoid exhausting memory in the host, it will
     lago start "$vm_name"
-    lago copy-to-vm "$vm_name" /etc/yum/yum.conf /etc/yum/yum.conf
+
+    prepare_and_copy_yum_conf "$vm_name"
+
     # the ovirt deploy is needed because it will not start the local repo
     # otherwise
     lago ovirt deploy
@@ -112,16 +127,12 @@ for distro in el7; do
         "$vm_name" \
         "/tmp/nosetests-${distro}.xml" \
         "$EXPORTS/nosetests-${distro}.xml" || :
+    lago collect --output "$EXPORTS"/lago-logs
     lago stop "$vm_name"
 done
 
 lago cleanup
 
-cat "$EXPORTS/functional_tests_stdout.$distro.log"
-
-[[ -e "logs" ]] \
-&& {
-    tar cvzf "$EXPORTS/lago-logs.tar.gz" logs
-}
+cp "$PREFIX"/current/logs/*.log "$EXPORTS"/lago-logs
 
 exit $failed
