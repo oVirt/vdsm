dfa9e4a09367e54f95c727972316296b52fc89f8
Denis Chaplygin <dchaplyg@redhat.com>
2017-01-19 16:12:43 +0100

    storage: Add helper for posix_fallocate() call
    python wrapper for posix_fallocate(), used in
    resizing file backed prealocated volumes.
    
    We are not able to use fallocate utility
    cause it uses fallocate(2) call, which is not
    supported everywhere (e.g. on NFS) and doesn't
    have fallback capability, like posix_fallocate()
    call.
    
    Change-Id: I5b2cc0b52bfe565b5c1362fe14cc96f87c6ace61
    Bug-Url: https://bugzilla.redhat.com/1391859
    Signed-off-by: Denis Chaplygin <dchaplyg@redhat.com>

---
+++ lib/vdsm/storage/fallocate.py
@@ -0,0 +1,53 @@
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+"""
+fallocate wrapping module
+"""
+
+from __future__ import absolute_import
+
+from vdsm.storage import operation
+
+_FALLOCATE = "/usr/libexec/vdsm/fallocate"
+
+
+def allocate(image, size, offset=0):
+    """
+    Creates a new 'allocate' operation object,
+    that will create new file and preallocate disk space
+    for it when run.
+    Operation can be aborted during it's execution.
+
+    :param str image: filename with path
+    :param int size: expected file size in bytes
+    :param int offset: start allocating from that offset,
+           specified in bytes.
+    :return operation object, encapsulating fallocate helper call
+    """
+    cmd = [_FALLOCATE]
+
+    if offset > 0:
+        cmd.extend(("--offset", str(offset)))
+
+    cmd.append(str(size))
+    cmd.append(image)
+
+    return operation.Command(cmd)
---
+++ tests/fallocate_test.py
@@ -0,0 +1,77 @@
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import io
+import os
+
+from monkeypatch import MonkeyPatch
+from testlib import VdsmTestCase
+from testlib import temporaryPath
+from testlib import namedTemporaryDir
+from vdsm import cmdutils
+from vdsm.storage import fallocate
+
+
+class TestFallocate(VdsmTestCase):
+
+    @MonkeyPatch(fallocate, '_FALLOCATE', '../helpers/fallocate')
+    def test_allocate(self):
+        # Test that allocate call made correctly
+        size = 4096
+        with namedTemporaryDir() as tmpdir:
+            image = os.path.join(tmpdir, "image")
+
+            fallocate.allocate(image, size).run()
+
+            allocated = os.stat(image).st_blocks * 512
+            self.assertEqual(allocated, size)
+
+    @MonkeyPatch(fallocate, '_FALLOCATE', '../helpers/fallocate')
+    def test_negative_size(self):
+        # Test that fallocate call throws exception on error
+        with namedTemporaryDir() as tmpdir:
+            image = os.path.join(tmpdir, "image")
+            with self.assertRaises(cmdutils.Error):
+                fallocate.allocate(image, -1).run()
+
+    @MonkeyPatch(fallocate, '_FALLOCATE', '../helpers/fallocate')
+    def test_zero_size(self):
+        # Test that fallocate call throws exception on error
+        with namedTemporaryDir() as tmpdir:
+            image = os.path.join(tmpdir, "image")
+            with self.assertRaises(cmdutils.Error):
+                fallocate.allocate(image, 0).run()
+
+    @MonkeyPatch(fallocate, '_FALLOCATE', '../helpers/fallocate')
+    def test_resize(self):
+        # Test that resize call actually works
+        size = 4096
+        with temporaryPath(data=b'x' * size) as image:
+            fallocate.allocate(image, size, offset=size).run()
+
+            with io.open(image, 'rb') as f:
+                actual = f.read()
+
+            expected = b'x' * size + b'\0' * size
+
+            self.assertEqual(expected, actual)
+
+            allocated = os.stat(image).st_blocks * 512
+            self.assertEqual(allocated, size * 2)
