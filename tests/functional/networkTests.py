1148055e1a84783417759a1bab9198ac9a452a7e
Leon Goldberg <lgoldber@redhat.com>
2017-02-23 15:10:55 +0200

    net tests: Don't compare running to kernel config with ifcfg persistence
    setupModules compares the running config to the kernel config in order 
    to verify the integrity of the host, however if persistence is set to 
    ifcfg the configs aren't available and shouldn't be compared.
    
    Change-Id: I5c957f89a1a90aac092b207666443e79257ad1e3
    Signed-off-by: Leon Goldberg <lgoldber@redhat.com>

--- tests/functional/networkTests.py
+++ tests/functional/networkTests.py
@@ -103,16 +103,20 @@ NOCHK = {'connectivityCheck': False}
 @RequireDummyMod
 def setupModule():
     vds = getProxy()
-    running_config, kernel_config = _get_running_and_kernel_config(
-        vds.config)
-    if ((running_config['networks'] != kernel_config['networks']) or
-            (running_config['bonds'] != kernel_config['bonds'])):
-        raise SkipTest(
-            "Tested host is not clean (running vs kernel): "
-            "networks: %r != %r; "
-            "bonds: %r != %r" % (
-                running_config['networks'], kernel_config['networks'],
-                running_config['bonds'], kernel_config['bonds']))
+
+    unified = (
+        vdsm.config.config.get('vars', 'net_persistence') == 'unified')
+    if unified:
+        running_config, kernel_config = _get_running_and_kernel_config(
+            vds.config)
+        if ((running_config['networks'] != kernel_config['networks']) or
+                (running_config['bonds'] != kernel_config['bonds'])):
+            raise SkipTest(
+                "Tested host is not clean (running vs kernel): "
+                "networks: %r != %r; "
+                "bonds: %r != %r" % (
+                    running_config['networks'], kernel_config['networks'],
+                    running_config['bonds'], kernel_config['bonds']))
 
     vds.save_config()
     for _ in range(DUMMY_POOL_SIZE):
