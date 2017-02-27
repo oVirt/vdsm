oVirt/vdsm

commit 4677d5b2dbcfccb4679cf74a981fc22a71225d0c

sp: deactivateSd - remove domain from pending for upgrade list
When domain is deactivated its not cleared from the _domainsToUpgrade list,
which causes it to hold wrong and unneeded information (the domain might be
detached later from the storage pool). This patch makes sure the domain
is cleared when deactivated.

Change-Id: I4451b348b8837dd83d95aea2be4a4536b33cdd99
Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1260429
Signed-off-by: Liron Aravot <laravot@redhat.com>
Reviewed-on: https://gerrit.ovirt.org/45773
Continuous-Integration: Jenkins CI
Reviewed-by: Adam Litke <alitke@redhat.com>
Reviewed-by: Nir Soffer <nsoffer@redhat.com>

vdsm/storage/sp.py
@@ -183,14 +183,7 @@ class StoragePool(object):
                             return
 
                 self._domainsToUpgrade.remove(sdUUID)
-                if len(self._domainsToUpgrade) == 0:
-                    self.log.debug("All domains are upgraded, unregistering "
-                                   "from state change event")
-                    try:
-                        self.domainMonitor.onDomainStateChange.\
-                            unregister(self._upgradeCallback)
-                    except KeyError:
-                        pass
+                self._finalizePoolUpgradeIfNeeded()
 
     def _updateDomainsRole(self):
         for sdUUID in self.getDomains(activeOnly=True):
@@ -1114,6 +1107,22 @@ class StoragePool(object):
         domList[sdUUID] = sd.DOM_ATTACHED_STATUS
         self._backend.setDomainsMap(domList)
         self.updateMonitoringThreads()
+        try:
+            self._domainsToUpgrade.remove(sdUUID)
+        except ValueError:
+            return
+
+        self._finalizePoolUpgradeIfNeeded()
+
+    def _finalizePoolUpgradeIfNeeded(self):
+        if len(self._domainsToUpgrade) == 0:
+            self.log.debug("No domains left for upgrade, unregistering "
+                           "from state change event")
+            try:
+                self.domainMonitor.onDomainStateChange.unregister(
+                    self._upgradeCallback)
+            except KeyError:
+                pass
 
     @unsecured
     def _linkStorageDomain(self, linkTarget, linkName):

commit 04e49dad95c429147789aef6b43eeb2afb999903

sp: Mark _finalizePoolUpgradeIfNeeded as @unsecure
StoragePool._finalizePoolUpgradeIfNeeded while a host is not the SPM. In
this case it will fail with this error:

    Traceback (most recent call last):
      File "/usr/lib/python2.7/site-packages/vdsm/utils.py", line 372, in wrapper
	return f(*a, **kw)
      File "/usr/lib/python2.7/site-packages/vdsm/concurrent.py", line 177, in run
	return func(*args, **kwargs)
      File "/usr/lib/python2.7/site-packages/vdsm/storage/securable.py", line 78, in wrapper
	return method(self, *args, **kwargs)
      File "/usr/share/vdsm/storage/sp.py", line 207, in _upgradePoolDomain
	self._finalizePoolUpgradeIfNeeded()
      File "/usr/lib/python2.7/site-packages/vdsm/storage/securable.py", line 76, in wrapper
	raise SecureError("Secured object is not in safe state")
    SecureError: Secured object is not in safe state

When this fails, we do not unregister StoragePool._upgradeCallback from domain
state changes events. Each time a domain changes state to valid, we will try to
to perform a useless domain upgrade, creating more noise in the log.

I think this issue was introduced by commit 4677d5b2dbcf (sp:
deactivateSd - remove domain from pending for upgrade list). Not
sure why the issue surfaced only recently.

Change-Id: I39c65223dd1c03928e4d74efa11c2177f8f9b854
Bug-Url: https://bugzilla.redhat.com/1400534
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

vdsm/storage/sp.py
@@ -1182,6 +1182,7 @@ class StoragePool(object):
 
         self._finalizePoolUpgradeIfNeeded()
 
+    @unsecured
     def _finalizePoolUpgradeIfNeeded(self):
         if len(self._domainsToUpgrade) == 0:
             self.log.debug("No domains left for upgrade, unregistering "