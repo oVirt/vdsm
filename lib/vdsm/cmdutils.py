2f52c233defec9b259c22177d691e92dff59c64f
Nir Soffer <nsoffer@redhat.com>
2017-03-28 03:38:45 +0300

    cmdutils: Introduce the receive() helper
    cmdutils.receive() replaces both commands.AsyncProc and
    utils.CommandStream. This function allows reading from sub process
    stdout and stderr in the same time, without using callbacks as in
    utils.CommandStream, and avoiding the performance issues and the
    complexity of utils.AsyncProc. This function is also python 3
    compatible.
    
    cmdutils.receive() does not support writing to and reading from a sub
    process in one thread at the same time. This is supported by
    commands.AsyncProc, but I don't know of any real world use case for
    this.
    
    Example usage:
    
        # Reading from both stdout and stderr:
    
        p = CPopen(["foo", "--progress"], cmd, stdout=PIPE, stderr=PIPE)
    
        for src, data in cmdutils.receive(p):
            if src == cmdutils.OUT:
                print data
            if src == cmdutils.ERR:
                print data
    
        # Waiting for process 10 seconds without risk of deadlock:
    
        for _ in cmdutils.receive(p, timeout=10):
            pass
    
    Comparing with AsyncProc and plain Popen.stdout, new tests copy 1g of
    data to/from subprocess. commands.AsycProc() is limited to 0.06g/s,
    while using cmdutils.receive() only somewhat slower than reading
    directly from subproces stdout.
    
    test_asyncproc_read   1.00g in 17.02 seconds (0.06g/s) OK
    test_asyncproc_write  1.00g in 13.71 seconds (0.07g/s) OK
    test_plain_read       1.00g in 0.45 seconds (2.22g/s) OK
    test_read             1.00g in 0.53 seconds (1.89g/s) OK
    test_write            1.00g in 0.49 seconds (2.04g/s) OK
    
    Currently we are using utils.CommandStream only in qemuinfo.convert()
    and qemuimg.commit(). This usage will be replace with cmdutils.received
    in these series.
    
    Usage of commands.AsyncProc will be update later.
    
    Change-Id: I7d193caa5da0ed564b4fab12aa85e3751f1a1df7
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- lib/vdsm/cmdutils.py
+++ lib/vdsm/cmdutils.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2014 Red Hat, Inc.
+# Copyright 2014-2017 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,14 +19,29 @@
 #
 
 from __future__ import absolute_import
+
+import errno
+import io
+import logging
 import os
 import re
+import select
+import time
 
 from . import constants
-from .common import errors
-from .config import config
+from . common import errors
+from . common import osutils
+from . config import config
+
+from . common.time import monotonic_time
 
 SUDO_NON_INTERACTIVE_FLAG = "-n"
+
+# receive() source names
+OUT = "out"
+ERR = "err"
+
+log = logging.getLogger("procutils")
 
 
 class Error(errors.Base):
@@ -38,6 +53,13 @@ class Error(errors.Base):
         self.rc = rc
         self.out = out
         self.err = err
+
+
+class TimeoutExpired(errors.Base):
+    msg = "Timeout waiting for process pid={self.pid}"
+
+    def __init__(self, pid):
+        self.pid = pid
 
 
 def nice(cmd, nice):
@@ -154,3 +176,133 @@ def command_log_line(args, cwd=None):
 def retcode_log_line(code, err=None):
     result = "SUCCESS" if code == 0 else "FAILED"
     return "{0}: <err> = {1!r}; <rc> = {2!r}".format(result, err, code)
+
+
+def receive(p, timeout=None, bufsize=io.DEFAULT_BUFFER_SIZE):
+    """
+    Receive data from a process, yielding data read from stdout and stderr
+    until proccess terminates or timeout expires.
+
+    Unlike Popen.communicate(), this supports a timeout, and allows
+    reading both stdout and stderr with a single thread.
+
+    Example usage::
+
+        # Reading data from both stdout and stderr until process
+        # terminates:
+
+        for src, data in cmdutils.receive(p):
+            if src == cmdutils.OUT:
+                # handle output
+            elif src == cmdutils.ERR:
+                # handler errors
+
+        # Receiving data with a timeout:
+
+        try:
+            received = list(cmdutils.receive(p, timeout=10))
+        except cmdutils.TimeoutExpired:
+            # handle timeout
+
+    Arguments:
+        p (`subprocess.Popen`): A subprocess created with
+            subprocess.Popen or cpopen.CPopen.
+        timeout (float): Number of seconds to wait for process. Timeout
+            resolution is limited by the resolution of
+            `common.time.monotonic_time`, typically 10 milliseconds.
+        bufsize (int): Number of bytes to read from the process in each
+            iteration.
+
+    Returns:
+        Generator of tuples (SRC, bytes). SRC may be either
+        `cmdutils.OUT` or `cmdutils.ERR`, and bytes is a bytes object
+        read from process stdout or stderr.
+
+    Raises:
+        `cmdutils.TimeoutExpired` if process did not terminate within
+            the specified timeout.
+    """
+    if timeout is not None:
+        deadline = monotonic_time() + timeout
+        remaining = timeout
+    else:
+        deadline = None
+        remaining = None
+
+    fds = {}
+    if p.stdout:
+        fds[p.stdout.fileno()] = OUT
+    if p.stderr:
+        fds[p.stderr.fileno()] = ERR
+
+    if fds:
+        poller = select.poll()
+        for fd in fds:
+            poller.register(fd, select.POLLIN)
+
+        def discard(fd):
+            if fd in fds:
+                del fds[fd]
+                poller.unregister(fd)
+
+    while fds:
+        log.debug("Waiting for process (pid=%d, remaining=%s)",
+                  p.pid, remaining)
+        # Unlike all other time apis, poll is using milliseconds
+        remaining_msec = remaining * 1000 if deadline else None
+        try:
+            ready = poller.poll(remaining_msec)
+        except select.error as e:
+            if e[0] != errno.EINTR:
+                raise
+            log.debug("Polling process (pid=%d) interrupted", p.pid)
+        else:
+            for fd, mode in ready:
+                if mode & select.POLLIN:
+                    data = osutils.uninterruptible(os.read, fd, bufsize)
+                    if not data:
+                        log.debug("Fd %d closed, unregistering", fd)
+                        discard(fd)
+                        continue
+                    yield fds[fd], data
+                else:
+                    log.debug("Fd %d hangup/error, unregistering", fd)
+                    discard(fd)
+        if deadline:
+            remaining = deadline - monotonic_time()
+            if remaining <= 0:
+                raise TimeoutExpired(p.pid)
+
+    _wait(p, deadline)
+
+
+def _wait(p, deadline=None):
+    """
+    Wait until process terminates, or if deadline is specified,
+    `common.time.monotonic_time` exceeds deadline.
+
+    Raises:
+        `cmdutils.TimeoutExpired` if process did not terminate within
+            deadline.
+    """
+    log.debug("Waiting for process (pid=%d)", p.pid)
+    if deadline is None:
+        p.wait()
+    else:
+        # We need to wait until deadline, Popen.wait() does not support
+        # timeout. Python 3 is using busy wait in this case with a timeout of
+        # 0.0005 seocnds. In vdsm we cannot allow such busy loops, and we don't
+        # have a need to support very exact wait time. This loop uses
+        # exponential backoff to detect termination quickly if the process
+        # terminates quickly, and avoid busy loop if the process is stuck for
+        # long time. Timeout will double from 0.0078125 to 1.0, and then
+        # continue at 1.0 seconds, until deadline is reached.
+        timeout = 1.0 / 256
+        while p.poll() is None:
+            remaining = deadline - monotonic_time()
+            if remaining <= 0:
+                raise TimeoutExpired(p.pid)
+            time.sleep(min(timeout, remaining))
+            if timeout < 1.0:
+                timeout *= 2
+    log.debug("Process (pid=%d) terminated", p.pid)
--- tests/cmdutils_test.py
+++ tests/cmdutils_test.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 Red Hat, Inc.
+# Copyright 2015-2017 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,9 +18,21 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
+from __future__ import print_function
+
+import io
+import os
+import signal
+import subprocess
+
 from vdsm import cmdutils
+from vdsm import commands
 from vdsm import constants
 
+from vdsm.common.time import monotonic_time
+from vdsm.compat import CPopen
+
+from testValidation import slowtest
 from testlib import VdsmTestCase
 
 
@@ -117,3 +129,239 @@ class TestError(VdsmTestCase):
     def test_format(self):
         # Should not raise
         str(cmdutils.Error(["cmd"], 1, "out\n", "err\n"))
+
+
+class TestReceive(VdsmTestCase):
+
+    def test_no_output_success(self):
+        p = CPopen(["true"],
+                   stdin=None,
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        received = list(cmdutils.receive(p))
+        self.assertEqual(received, [])
+        self.assertEqual(p.returncode, 0)
+
+    def test_no_output_error(self):
+        p = CPopen(["false"],
+                   stdin=None,
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        received = list(cmdutils.receive(p))
+        self.assertEqual(received, [])
+        self.assertEqual(p.returncode, 1)
+
+    def test_stdout(self):
+        p = CPopen(["echo", "output"],
+                   stdin=None,
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        received = list(cmdutils.receive(p))
+        self.assertEqual(received, [(cmdutils.OUT, b"output\n")])
+        self.assertEqual(p.returncode, 0)
+
+    def test_stderr(self):
+        p = CPopen(["sh", "-c", "echo error >/dev/stderr"],
+                   stdin=None,
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        received = list(cmdutils.receive(p))
+        self.assertEqual(received, [(cmdutils.ERR, b"error\n")])
+        self.assertEqual(p.returncode, 0)
+
+    def test_both_stdout_stderr(self):
+        p = CPopen(["sh", "-c", "echo output; echo error >/dev/stderr;"],
+                   stdin=None,
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        received = list(cmdutils.receive(p))
+        self.assertEqual(sorted(received), sorted([
+            (cmdutils.OUT, b"output\n"), (cmdutils.ERR, b"error\n")
+        ]))
+        self.assertEqual(p.returncode, 0)
+
+    def test_timeout(self):
+        p = CPopen(["sleep", "1"],
+                   stdin=None,
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        try:
+            with self.assertRaises(cmdutils.TimeoutExpired):
+                for _ in cmdutils.receive(p, 0.5):
+                    pass
+        finally:
+            p.kill()
+            p.wait()
+
+    def test_timeout_with_data(self):
+        p = CPopen(["yes"],
+                   stdin=None,
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        try:
+            with self.assertRaises(cmdutils.TimeoutExpired):
+                for _ in cmdutils.receive(p, 0.5):
+                    pass
+        finally:
+            p.kill()
+            p.wait()
+
+    def test_no_fds(self):
+        p = CPopen(["sleep", "1"],
+                   stdin=None,
+                   stdout=None,
+                   stderr=None)
+        try:
+            with self.assertRaises(cmdutils.TimeoutExpired):
+                for _ in cmdutils.receive(p, 0.5):
+                    pass
+        finally:
+            p.kill()
+            p.wait()
+
+    def test_fds_closed(self):
+        cmd = ["python", "-c",
+               "import os, time; os.close(1); os.close(2); time.sleep(1)"]
+        p = CPopen(cmd, stdin=None, stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        try:
+            with self.assertRaises(cmdutils.TimeoutExpired):
+                for _ in cmdutils.receive(p, 0.5):
+                    pass
+        finally:
+            p.kill()
+            p.wait()
+
+    def test_terminate(self):
+        p = CPopen(["sleep", "1"],
+                   stdin=None,
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        p.terminate()
+        list(cmdutils.receive(p))
+        self.assertEqual(p.returncode, -signal.SIGTERM)
+
+    def test_kill(self):
+        p = CPopen(["sleep", "1"],
+                   stdin=None,
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        p.kill()
+        list(cmdutils.receive(p))
+        self.assertEqual(p.returncode, -signal.SIGKILL)
+
+
+class TestRecieveBench(VdsmTestCase):
+
+    COUNT = 1024
+    BUFSIZE = 1024**2
+
+    def test_plain_read(self):
+        p = CPopen(["dd", "if=/dev/zero", "bs=%d" % self.BUFSIZE,
+                    "count=%d" % self.COUNT],
+                   stdin=None,
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        start = monotonic_time()
+        received = 0
+        while True:
+            data = os.read(p.stdout.fileno(), self.BUFSIZE)
+            if not data:
+                break
+            received += len(data)
+        p.wait()
+        elapsed = monotonic_time() - start
+        received_gb = received / float(1024**3)
+        print("%.2fg in %.2f seconds (%.2fg/s)"
+              % (received_gb, elapsed, received_gb / elapsed), end=" ")
+        self.assertEqual(received, self.COUNT * self.BUFSIZE)
+        self.assertEqual(p.returncode, 0)
+
+    def test_read(self):
+        p = CPopen(["dd", "if=/dev/zero", "bs=%d" % self.BUFSIZE,
+                    "count=%d" % self.COUNT],
+                   stdin=None,
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE)
+        start = monotonic_time()
+        received = 0
+        for src, data in cmdutils.receive(p, bufsize=self.BUFSIZE):
+            if src == cmdutils.OUT:
+                received += len(data)
+        elapsed = monotonic_time() - start
+        received_gb = received / float(1024**3)
+        print("%.2fg in %.2f seconds (%.2fg/s)"
+              % (received_gb, elapsed, received_gb / elapsed), end=" ")
+        self.assertEqual(received, self.COUNT * self.BUFSIZE)
+        self.assertEqual(p.returncode, 0)
+
+    def test_write(self):
+        p = CPopen(["dd", "of=/dev/null", "bs=%d" % self.BUFSIZE],
+                   stdin=subprocess.PIPE,
+                   stdout=None,
+                   stderr=subprocess.PIPE)
+        start = monotonic_time()
+        total = self.COUNT * self.BUFSIZE
+        sent = 0
+        with io.open("/dev/zero", "rb") as f:
+            while sent < total:
+                n = min(total - sent, self.BUFSIZE)
+                data = f.read(n)
+                if not data:
+                    raise RuntimeError("/dev/zero closed?!")
+                p.stdin.write(data)
+                sent += len(data)
+        p.stdin.flush()
+        p.stdin.close()
+        for _, data in cmdutils.receive(p, 10):
+            pass
+        elapsed = monotonic_time() - start
+        sent_gb = sent / float(1024**3)
+        print("%.2fg in %.2f seconds (%.2fg/s)"
+              % (sent_gb, elapsed, sent_gb / elapsed), end=" ")
+        self.assertEqual(p.returncode, 0)
+
+    @slowtest
+    def test_asyncproc_read(self):
+        p = commands.execCmd(["dd", "if=/dev/zero", "bs=%d" % self.BUFSIZE,
+                              "count=%d" % self.COUNT],
+                             sync=False, raw=True)
+        start = monotonic_time()
+        p.blocking = True
+        received = 0
+        while True:
+            data = p.stdout.read(self.BUFSIZE)
+            if not data:
+                break
+            received += len(data)
+        p.wait()
+        elapsed = monotonic_time() - start
+        received_gb = received / float(1024**3)
+        print("%.2fg in %.2f seconds (%.2fg/s)"
+              % (received_gb, elapsed, received_gb / elapsed), end=" ")
+        self.assertEqual(received, self.COUNT * self.BUFSIZE)
+        self.assertEqual(p.returncode, 0)
+
+    @slowtest
+    def test_asyncproc_write(self):
+        p = commands.execCmd(["dd", "of=/dev/null", "bs=%d" % self.COUNT],
+                             sync=False, raw=True)
+        start = monotonic_time()
+        total = self.COUNT * self.BUFSIZE
+        sent = 0
+        with io.open("/dev/zero", "rb") as f:
+            while sent < total:
+                n = min(total - sent, self.BUFSIZE)
+                data = f.read(n)
+                if not data:
+                    raise RuntimeError("/dev/zero closed?!")
+                p.stdin.write(data)
+                sent += len(data)
+        p.stdin.flush()
+        p.stdin.close()
+        p.wait()
+        elapsed = monotonic_time() - start
+        sent_gb = sent / float(1024**3)
+        print("%.2fg in %.2f seconds (%.2fg/s)"
+              % (sent_gb, elapsed, sent_gb / elapsed), end=" ")
+        self.assertEqual(p.returncode, 0)
