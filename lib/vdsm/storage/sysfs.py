eec5f1905c78b4eef1d6db499bfcbdab20801315
Nir Soffer <nsoffer@redhat.com>
2017-04-22 22:25:07 +0300

    pylint: Introduce the sysfs module
    This module provides helpers for accessing sysfs attributes. This will
    be used in the next patch for reading iscsi session attributes, and will
    be used later in other modules.
    
    Change-Id: I63680cee2ea43d346da0fafebdbb98601c53993f
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ lib/vdsm/storage/sysfs.py
@@ -0,0 +1,76 @@
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+"""
+sysfs - helper for accessing sysfs attributes.
+"""
+
+from __future__ import absolute_import
+
+import errno
+import io
+from vdsm import password
+
+
+def read_int(path, default=None):
+    """
+    Return integer value of a sysfs attribute, or defualt if default is
+    specified and the attribute does not exist.
+
+    Raises:
+        EnviromentError if reading the attribute fails, or default was not
+            specified and the attribute does not exist.
+        ValueError if the attribute is cannot be parsed as int
+    """
+    value = read(path, default)
+    return int(value)
+
+
+def read_password(path, default=None):
+    """
+    Return ProtectedPassword wrapping value of a sysfs attribute, or defualt if
+    default is specified and the attribute does not exists.
+
+    Raises:
+        EnviromentError if reading the attribute fails, or default was not
+            specified and the attribute does not exist.
+    """
+    value = read(path, default)
+    return password.ProtectedPassword(value)
+
+
+def read(path, default=None):
+    """
+    Return the contents of a sysfs attribute, or defualt if default is
+    specified and the attribute does not exist.
+
+    Raises:
+        EnviromentError if reading the attribute fails, or default was not
+            specified and the attribute does not exist.
+    """
+    try:
+        with io.open(path, "r") as f:
+            value = f.read()
+    except EnvironmentError as e:
+        if e.errno != errno.ENOENT or default is None:
+            raise
+        return default
+    else:
+        return value.strip()
---
+++ tests/storage_sysfs_test.py
@@ -0,0 +1,96 @@
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import os
+
+from testlib import VdsmTestCase
+from testlib import temporaryPath
+
+from vdsm import password
+from vdsm.storage import sysfs
+
+
+class TestSysfs(VdsmTestCase):
+
+    # read
+
+    def test_read_str(self):
+        with temporaryPath(data=b"value") as path:
+            self.assertEqual(sysfs.read(path), "value")
+
+    def test_read_str_strip(self):
+        with temporaryPath(data=b" \t\n1\n2\n\t ") as path:
+            self.assertEqual(sysfs.read(path), "1\n2")
+
+    def test_read_str_missing_default(self):
+        self.assertEqual(sysfs.read("/no/such/path", "default"), "default")
+
+    def test_read_str_missing_no_default(self):
+        with self.assertRaises(EnvironmentError):
+            sysfs.read("/no/such/path")
+
+    def test_read_str_error(self):
+        with self.assertRaises(EnvironmentError):
+            sysfs.read(os.getcwd())
+
+    # read_int
+
+    def test_read_int(self):
+        with temporaryPath(data=b"42") as path:
+            self.assertEqual(sysfs.read_int(path), 42)
+
+    def test_read_int_strip(self):
+        with temporaryPath(data=b" 42\n ") as path:
+            self.assertEqual(sysfs.read_int(path), 42)
+
+    def test_read_int_missing_default(self):
+        self.assertEqual(sysfs.read_int("/no/such/path", 7), 7)
+
+    def test_read_int_missing_no_default(self):
+        with self.assertRaises(EnvironmentError):
+            sysfs.read_int("/no/such/path")
+
+    def test_read_int_error(self):
+        with self.assertRaises(EnvironmentError):
+            sysfs.read_int(os.getcwd())
+
+    # read_password
+
+    def test_read_password(self):
+        with temporaryPath(data=b"password") as path:
+            self.assertEqual(sysfs.read_password(path),
+                             password.ProtectedPassword("password"))
+
+    def test_read_password_strip(self):
+        with temporaryPath(data=b" password\n ") as path:
+            self.assertEqual(sysfs.read_password(path),
+                             password.ProtectedPassword("password"))
+
+    def test_read_password_missing_default(self):
+        self.assertEqual(sysfs.read_password("/no/such/path", ""),
+                         password.ProtectedPassword(""))
+
+    def test_read_password_missing_no_default(self):
+        with self.assertRaises(EnvironmentError):
+            sysfs.read_password("/no/such/path")
+
+    def test_read_password_error(self):
+        with self.assertRaises(EnvironmentError):
+            sysfs.read_password(os.getcwd())
