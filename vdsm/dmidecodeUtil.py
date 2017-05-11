b62da31e2100bca1f248956e6807ce42848ee5a2
Irit Goihman <igoihman@redhat.com>
2017-05-11 11:40:33 +0300

    dmidecode: collect only system info
    We report only system related info in getHardwareInfo so
    collecting other verbs is unnecessary.
    
    Change-Id: I97488bf273d69eb987360da152d712dde6d2167a
    Signed-off-by: Irit Goihman <igoihman@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1467545

--- vdsm/dmidecodeUtil.py
+++ vdsm/dmidecodeUtil.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2012 Red Hat, Inc.
+# Copyright 2012-2017 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -36,27 +36,20 @@ def __leafDict(d):
 
 
 @utils.memoized
-def getAllDmidecodeInfo():
+def getSystemInfo():
     import dmidecode
 
-    myLeafDict = {}
-    for k in ('system', 'bios', 'cache', 'processor', 'chassis', 'memory'):
-        myLeafDict[k] = __leafDict(getattr(dmidecode, k)())
-    return myLeafDict
+    return __leafDict(dmidecode.system())
 
 
 @utils.memoized
 def getHardwareInfoStructure():
-    dmiInfo = getAllDmidecodeInfo()
+    dmiInfo = getSystemInfo()
     sysStruct = {}
-    for k1, k2 in (('system', 'Manufacturer'),
-                   ('system', 'Product Name'),
-                   ('system', 'Version'),
-                   ('system', 'Serial Number'),
-                   ('system', 'UUID'),
-                   ('system', 'Family')):
-        val = dmiInfo.get(k1, {}).get(k2, None)
+    for k in ('Manufacturer', 'Product Name', 'Version', 'Serial Number',
+              'UUID', 'Family'):
+        val = dmiInfo.get(k, None)
         if val not in [None, 'Not Specified']:
-            sysStruct[(k1 + k2).replace(' ', '')] = val
+            sysStruct[('system' + k).replace(' ', '')] = val
 
     return sysStruct
