b804476962dfb6b1c1d111028a9819ef88a9b186
Petr Horáček <phoracek@redhat.com>
2017-05-11 15:10:15 +0200

    network: add driverloader module
    DriverLoader class provides tools to gather and provide drivers
    (interface implementations).
    
    Bug-Url: https://bugzilla.redhat.com/1479677
    Change-Id: I8609f81f3b38f5f88f46c0d020de2b16da26adbd
    Signed-off-by: Dominik Holler <dholler@redhat.com>
    Signed-off-by: Petr Horáček <phoracek@redhat.com>
    (cherry picked from commit 4b40a62ab25fdaca9b23d8121ab00d35e9f76ca4)

--- lib/vdsm/network/driverloader.py
+++ lib/vdsm/network/driverloader.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Red Hat, Inc.
+# Copyright 2017 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,30 +15,31 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
 # Refer to the README and COPYING files for full details of the license
-#
 
-SUBDIRS = configurators ip link netinfo netlink ovs tc nm
+from __future__ import absolute_import
 
-include $(top_srcdir)/build-aux/Makefile.subs
+from importlib import import_module
+from pkgutil import iter_modules
 
-vdsmnetworkdir = $(vdsmpylibdir)/network
-dist_vdsmnetwork_PYTHON = \
-	__init__.py \
-	api.py \
-	errors.py \
-	canonicalize.py \
-	connectivity.py \
-	driverloader.py \
-	ifacquire.py \
-	ipwrapper.py \
-	kernelconfig.py \
-	legacy_switch.py \
-	libvirt.py \
-	models.py \
-	netconfpersistence.py \
-	netrestore.py \
-	netswitch.py \
-	sourceroute.py \
-	sourceroutethread.py \
-	utils.py \
-	$(NULL)
+from vdsm.utils import memoized
+
+
+class NoDriverError(Exception):
+    pass
+
+
+@memoized
+def load_drivers(driver_class, package_name, package_path):
+    drivers = {}
+    for _, module_name, _ in iter_modules([package_path]):
+        module = import_module('{}.{}'.format(package_name, module_name))
+        if hasattr(module, driver_class):
+            drivers[module_name] = getattr(module, driver_class)
+    return drivers
+
+
+def get_driver(driver, drivers):
+    try:
+        return drivers[driver]
+    except KeyError:
+        raise NoDriverError(driver)
