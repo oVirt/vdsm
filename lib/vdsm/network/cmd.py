050ec4d20535bef1ec228a30be5a899aa3f9c0a2
Edward Haas <edwardh@redhat.com>
2017-05-21 17:11:12 +0300

    net: Introduce a simplified cmd exec implementation
    The network package is frequently utilizing the execCmd function to
    execute shell based commands.
    This patch introduces a simplified version of execCmd, focused on the
    limited needs of the network package.
    
    The new cmd module exposes a blocking exec function (exec_sync) and a
    systemd exec function.
    
    In the following patches, the users of execCmd will get migrated to the
    new cmd module implementations, reducing the dependency on the generic
    VDSM libs.
    
    Change-Id: I37380d51b648bd64792b6c26419dd33c96392ddd
    Signed-off-by: Edward Haas <edwardh@redhat.com>

---
+++ lib/vdsm/network/cmd.py
@@ -0,0 +1,47 @@
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+from __future__ import absolute_import
+
+import logging
+import subprocess
+import uuid
+
+from vdsm import cmdutils
+from vdsm.compat import CPopen as Popen
+
+
+def exec_sync(cmds):
+    logging.debug(cmdutils.command_log_line(cmds))
+
+    p = Popen(
+        cmds, close_fds=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    out, err = p.communicate()
+
+    return p.returncode, out, err
+
+
+def exec_systemd_new_unit(cmds, slice_name):
+    # TODO: We set unique uuid for every run to not use the same unit twice
+    # and prevent systemd_run race (BZ#1259468). This uuid could be dropped
+    # when BZ#1272368 will be solved or when we use systemd >= v220.
+    unit = uuid.uuid4()
+    cmds = cmdutils.systemd_run(cmds, scope=True, unit=unit, slice=slice_name)
+
+    return exec_sync(cmds)
---
+++ tests/network/cmd_test.py
@@ -0,0 +1,55 @@
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+from __future__ import absolute_import
+
+from nose.plugins.attrib import attr
+
+from testlib import VdsmTestCase
+from .nettestlib import requires_systemctl
+
+from vdsm.network import cmd
+
+
+@attr(type='integration')
+class CmdTest(VdsmTestCase):
+
+    def test_exec_sync_with_no_output(self):
+        rc, out, err = cmd.exec_sync(('true',))
+        self.assertEqual(out.decode(), '')
+        self.assertEqual(err.decode(), '')
+
+    def test_exec_sync_with_success_output(self):
+        rc, out, err = cmd.exec_sync(('echo', 'hello world'))
+        self.assertEqual(rc, 0, err)
+        self.assertEqual(out.decode(), 'hello world\n')
+        self.assertEqual(err.decode(), '')
+
+    def test_exec_sync_with_error_output(self):
+        rc, out, err = cmd.exec_sync(('ls', 'no such prog'))
+        self.assertNotEqual(rc, 0)
+        self.assertIn('No such file or directory', err.decode())
+        self.assertEqual(out.decode(), '')
+
+    @requires_systemctl
+    def test_exec_systemd_new_unit(self):
+        rc, out, err = cmd.exec_systemd_new_unit(('echo', 'hello world'),
+                                                 slice_name='test-group')
+        self.assertEqual(rc, 0, err)
+        self.assertEqual(out.decode(), 'hello world\n')
