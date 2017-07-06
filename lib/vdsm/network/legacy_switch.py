9a970382a73958747cbf866ee4e133d125f9b4c4
Edward Haas <edwardh@redhat.com>
2017-07-06 18:15:50 +0300

    net: On MTU change to the default value update the ifcfg file
    When changing only the MTU on a bridged network to the default value
    (1500), the new value has not been updated in the ifcfg file.
    
    The problem has been detected on RHEL 7.4, where the MTU of the bridge
    is automatically updated to the default when it has no ports connected.
    
    Change-Id: Ib55712725c19e7d20fa8491c8ee78f6f9298ff9a
    Bug-Url: https://bugzilla.redhat.com/1460687
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- lib/vdsm/network/legacy_switch.py
+++ lib/vdsm/network/legacy_switch.py
@@ -31,6 +31,7 @@ from vdsm.common.conv import tobool
 from vdsm.network import ipwrapper
 from vdsm.network import kernelconfig
 from vdsm.network.link import dpdk
+from vdsm.network.link import iface as link_iface
 from vdsm.network.configurators.ifcfg import Ifcfg
 from vdsm.network.netinfo import NET_PATH
 from vdsm.network.netinfo import bridges
@@ -218,9 +219,15 @@ def _add_network(network, configurator, _netinfo, nameservers,
 
 
 def _update_mtu_for_an_existing_bridge(dev_name, configurator, mtu):
+    # When the MTU changes to the default MTU, reading the existing MTU on the
+    # bridge is misleading, as with the latest OS, when no ports are connected
+    # to it, it will fall down to the default.
+    # In such a scenario, the ifcfg file still needs to be updated.
     if mtu != mtus.getMtu(dev_name):
         configurator.configApplier.setIfaceMtu(dev_name, mtu)
         _update_bridge_ports_mtu(dev_name, mtu)
+    elif mtu == link_iface.DEFAULT_MTU:
+        configurator.configApplier.setIfaceMtu(dev_name, mtu)
 
 
 def _update_bridge_ports_mtu(bridge, mtu):
