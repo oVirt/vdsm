f758b0c28533fe35ea13492b327e474b679e7dbf
Edward Haas <edwardh@redhat.com>
2017-07-12 17:21:09 +0300

    net: NM acquire should ignore duplicate ifcfg files
    When quering NM DBUS to translate a file path to a connection ID, an
    error with "is not an ifcfg base file" is raised when the file is a copy
    of an existing ifcfg file (a backup).
    
    This patch ignores these king of error the same way it did for non ifcfg
    files that are named with the ifcfg prefix.
    
    Change-Id: I337db762d7c6f6247894d7f75096328f5726c273
    Bug-Url: https://bugzilla.redhat.com/1441530
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- lib/vdsm/network/nm/nmdbus/__init__.py
+++ lib/vdsm/network/nm/nmdbus/__init__.py
@@ -50,7 +50,8 @@ class NMDbusIfcfgRH1(object):
     IF_NAME = 'com.redhat.ifcfgrh1'
     OBJ_PATH = '/com/redhat/ifcfgrh1'
 
-    ERROR_INV_CON = "ifcfg file '{}' unknown"
+    ERROR_INV_CON = ["ifcfg file '{}' unknown",
+                     "ifcfg path '{}' is not an ifcfg base file"]
 
     def __init__(self):
         ifcfg_proxy = NMDbus.bus.get_object(NMDbusIfcfgRH1.IF_NAME,
@@ -68,7 +69,9 @@ class NMDbusIfcfgRH1(object):
         try:
             con_info = self.ifcfg.GetIfcfgDetails(ifcfg_path)
         except DBusException as ex:
-            if NMDbusIfcfgRH1.ERROR_INV_CON.format(ifcfg_path) != ex.args[0]:
-                raise
+            for err_base_message in NMDbusIfcfgRH1.ERROR_INV_CON:
+                if err_base_message.format(ifcfg_path) == ex.args[0]:
+                    return con_info
+            raise
 
         return con_info
