2773d60fd89e643ef0eb3e60d28b715c97cfd701
Petr Horáček <phoracek@redhat.com>
2017-07-17 15:04:12 +0200

    tool: list/clear networks configured by VDSM
    Provide tools to list networks configured by VDSM and to clear
    them.
    
    This command could be called before vdsm removal to leave host
    in a clean state. Typically a user would find default route network
    name with `vdsm-tool list-nets` and then remove all networks but
    the default route one with `vdsm-tool clear-nets --exclude-net netname`
    
    Change-Id: I63702da4dd1c5a0328797f524734813ef251bdb6
    Signed-off-by: Petr Horacek <phoracek@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1430799

--- lib/vdsm/tool/network.py
+++ lib/vdsm/tool/network.py
@@ -17,10 +17,16 @@
 # Refer to the README and COPYING files for full details of the license
 #
 from __future__ import absolute_import
+from __future__ import print_function
+
+import argparse
 import logging
 import logging.config
 import threading
 
+import six
+
+from vdsm.network import api as net_api
 from vdsm.network import netrestore
 from vdsm.network import netupgrade
 from vdsm.network.link.bond import sysfs_options_mapper
@@ -86,3 +92,64 @@ def dump_bonding_options(*args):
         raise ExtraArgsError()
 
     sysfs_options_mapper.dump_bonding_options()
+
+
+@expose('list-nets')
+def list_networks(*args):
+    """
+    list-nets
+
+    List configured VDSM networks and mark the one with default route.
+    """
+    caps = net_api.network_caps()
+    output = ''
+    for net, attrs in six.viewitems(caps['networks']):
+        output += net
+        if attrs['ipv4defaultroute']:
+            output += ' (default route)\n'
+        else:
+            output += '\n'
+    print(output, end='')
+
+
+@expose('clear-nets')
+def clear_networks(*args):
+    """
+    clear-nets [--exclude-net [network to keep [...]]] [--all]
+
+    Remove networks configured by VDSM. Networks that should be kept could
+    be listed with --exclude-net argument. In case no network is given,
+    explicit --all is required to prevent accidental loss of connectivity.
+
+    This command can be executed before VDSM removal to keep the host clean.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-e',
+        '--exclude-net',
+        metavar='EXCLUDED_NETWORK',
+        nargs='*',
+        default=[],
+        help='VDSM networks that should be kept'
+    )
+    parser.add_argument(
+        '-a',
+        '--all',
+        action='store_true',
+        help='set this flag in case no network should be kept'
+    )
+    arguments = parser.parse_args(args[1:])
+
+    if not arguments.exclude_net and not arguments.all:
+        parser.error('Either --exclude-net with a network to be kept or '
+                     '--all is required as an argument. Use vdsm-tool '
+                     'list-nets to list configured networks.')
+
+    caps = net_api.network_caps()
+    networks_request = {
+        net: {'remove': True}
+        for net in caps['networks']
+        if net not in arguments.exclude_net
+    }
+    net_api.setupNetworks(networks_request, {}, {'connectivityCheck': False})
+    net_api.setSafeNetworkConfig()
