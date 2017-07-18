a41b4c5c3a060a8fee637f163b2c89fc014be8f9
Idan Shaby <ishaby@redhat.com>
2017-07-18 16:16:50 +0300

    storage: move blockdev_test to use pytest
    Change-Id: Ic02b265f97f45c26384da26f4bdf795cc8d14fb6
    Signed-off-by: Idan Shaby <ishaby@redhat.com>

--- tests/storage/blockdev_test.py
+++ tests/storage/blockdev_test.py
@@ -25,8 +25,6 @@ from contextlib import contextmanager
 
 import pytest
 
-from testlib import VdsmTestCase
-from testlib import expandPermutations, permutations
 from testlib import namedTemporaryDir
 import loopback
 
@@ -35,115 +33,100 @@ from vdsm.storage import blockdev
 from vdsm.storage import constants as sc
 from vdsm.storage import exception as se
 
-SIZE = blockdev.OPTIMAL_BLOCK_SIZE
+OPTIMAL_BLOCK_SIZE = blockdev.OPTIMAL_BLOCK_SIZE
+FILE_SIZE = 2 * OPTIMAL_BLOCK_SIZE
 
 
-@expandPermutations
-class TestZero(VdsmTestCase):
+@pytest.fixture
+def loop_device(tmpdir):
+    backing_file = str(tmpdir.join("backing_file"))
+    with io.open(backing_file, "wb") as f:
+        f.write(b"x" * FILE_SIZE)
+    with loopback.Device(backing_file) as loop_device:
+        yield loop_device
 
-    @pytest.mark.skipif(os.geteuid() != 0, reason="requires root")
-    def test_entire_device(self):
-        with namedTemporaryDir() as tmpdir:
-            # Prepare device poisoned with "x"
-            path = os.path.join(tmpdir, "file")
-            with io.open(path, "wb") as f:
-                f.write(b"x" * SIZE)
-            # Zero the entire device
-            with loopback.Device(path) as loop_device:
-                blockdev.zero(loop_device.path)
-            # Check that it contains zeros
-            with io.open(path, "rb") as f:
-                data = f.read()
-                self.assertEqual(data, b"\0" * SIZE, "data was not zeroed")
+
+class TestZero:
 
     @pytest.mark.skipif(os.geteuid() != 0, reason="requires root")
-    def test_size(self):
-        with namedTemporaryDir() as tmpdir:
-            # Prepare device poisoned with "x"
-            path = os.path.join(tmpdir, "file")
-            with io.open(path, "wb") as f:
-                f.write(b"x" * SIZE * 2)
-            # Zero the first 1Mib
-            with loopback.Device(path) as loop_device:
-                blockdev.zero(loop_device.path, size=SIZE)
-            with io.open(path, "rb") as f:
-                # Verify that the first 1MiB is zeroed
-                data = f.read(SIZE)
-                self.assertEqual(data, b"\0" * SIZE, "data was not zeroed")
-                # And the rest was not modified
-                data = f.read(SIZE)
-                self.assertEqual(data, b"x" * SIZE, "data was modified")
+    def test_entire_device(self, loop_device):
+        # Zero the entire device
+        blockdev.zero(loop_device.path)
+        # Check that it contains zeros
+        with io.open(loop_device.backing_file, "rb") as f:
+            data = f.read()
+            assert data == b"\0" * FILE_SIZE
 
     @pytest.mark.skipif(os.geteuid() != 0, reason="requires root")
-    @permutations([
-        (sc.BLOCK_SIZE,),
-        (blockdev.OPTIMAL_BLOCK_SIZE - sc.BLOCK_SIZE,),
-        (blockdev.OPTIMAL_BLOCK_SIZE + sc.BLOCK_SIZE,),
+    def test_size(self, loop_device):
+        # Zero the first OPTIMAL_BLOCK_SIZE
+        blockdev.zero(loop_device.path, size=OPTIMAL_BLOCK_SIZE)
+        with io.open(loop_device.backing_file, "rb") as f:
+            # Verify that the first OPTIMAL_BLOCK_SIZE is zeroed
+            data = f.read(OPTIMAL_BLOCK_SIZE)
+            assert data == b"\0" * OPTIMAL_BLOCK_SIZE
+            # And the rest was not modified
+            data = f.read(OPTIMAL_BLOCK_SIZE)
+            assert data == b"x" * OPTIMAL_BLOCK_SIZE
+
+    @pytest.mark.skipif(os.geteuid() != 0, reason="requires root")
+    @pytest.mark.parametrize("size", [
+        (sc.BLOCK_SIZE),
+        (blockdev.OPTIMAL_BLOCK_SIZE - sc.BLOCK_SIZE),
+        (blockdev.OPTIMAL_BLOCK_SIZE + sc.BLOCK_SIZE),
     ])
-    def test_special_volumes(self, size):
-        with namedTemporaryDir() as tmpdir:
-            # Prepare device posioned with "x"
-            path = os.path.join(tmpdir, "file")
-            with io.open(path, "wb") as f:
-                f.write(b"x" * size)
-            # Zero size bytes
-            with loopback.Device(path) as loop_device:
-                blockdev.zero(loop_device.path, size=size)
-            with io.open(path, "rb") as f:
-                # Verify that size bytes were zeroed
-                data = f.read(size)
-                self.assertEqual(data, b"\0" * size, "data was not zeroed")
+    def test_special_volumes(self, size, loop_device):
+        # Zero size bytes
+        blockdev.zero(loop_device.path, size=size)
+        with io.open(loop_device.backing_file, "rb") as f:
+            # Verify that size bytes were zeroed
+            data = f.read(size)
+            assert data == b"\0" * size
 
-    def test_abort(self):
-        with namedTemporaryDir() as tmpdir:
-            # Prepare device poisoned with "x"
-            path = os.path.join(tmpdir, "file")
-            with io.open(path, "wb") as f:
-                f.write(b"x" * SIZE)
-            # Create a task that will be aborted immediately
-            task = AbortingTask()
-            # The operation should be stopped
-            with self.assertRaises(exception.ActionStopped):
-                blockdev.zero(path, size=SIZE, task=task)
-            # And the file should not be zeroed
-            with io.open(path, "rb") as f:
-                data = f.read(SIZE)
-                self.assertEqual(data, b"x" * SIZE, "data was modified")
+    def test_abort(self, tmpdir):
+        # Prepare device poisoned with "x"
+        path = str(tmpdir.join("file"))
+        with io.open(path, "wb") as f:
+            f.write(b"x" * FILE_SIZE)
+        # Create a task that will be aborted immediately
+        task = AbortingTask()
+        # The operation should be stopped
+        with pytest.raises(exception.ActionStopped):
+            blockdev.zero(path, size=FILE_SIZE, task=task)
+        # And the file should not be zeroed
+        with io.open(path, "rb") as f:
+            data = f.read(FILE_SIZE)
+            assert data == b"x" * FILE_SIZE
 
-    @permutations([(SIZE - 1,), (SIZE + 1,)])
+    @pytest.mark.parametrize("size", [
+        (FILE_SIZE - 1),
+        (FILE_SIZE + 1),
+    ])
     def test_unaligned_size(self, size):
-        with self.assertRaises(se.InvalidParameterException):
+        with pytest.raises(se.InvalidParameterException):
             blockdev.zero("/no/such/path", size=size)
 
 
-class TestDiscard(VdsmTestCase):
+class TestDiscard:
 
     def test_not_supported(self):
         with namedTemporaryDir() as tmpdir:
             # Prepare file poisoned with "x"
             path = os.path.join(tmpdir, "backing_file")
             with io.open(path, "wb") as f:
-                f.write(b"x" * SIZE)
+                f.write(b"x" * FILE_SIZE)
             # And discard it - this will fail silently.
             blockdev.discard(path)
             # File should not be modified
             with io.open(path, "rb") as f:
-                data = f.read(SIZE)
-                self.assertEqual(data, b"x" * SIZE, "data was modified")
+                data = f.read(FILE_SIZE)
+                assert data == b"x" * FILE_SIZE
 
     @pytest.mark.skipif(os.geteuid() != 0, reason="requires root")
-    def test_supported(self):
-        with namedTemporaryDir() as tmpdir:
-            # Prepare backing file poisoned with "x"
-            backing_file = os.path.join(tmpdir, "backing_file")
-            with io.open(backing_file, "wb") as f:
-                f.write(b"x" * SIZE)
-            # Create a loop device
-            with loopback.Device(backing_file) as loop_device:
-                # And discard it
-                blockdev.discard(loop_device.path)
-                stat = os.stat(backing_file)
-                self.assertEqual(stat.st_blocks, 0)
+    def test_supported(self, loop_device):
+        blockdev.discard(loop_device.path)
+        stat = os.stat(loop_device.backing_file)
+        assert stat.st_blocks == 0
 
 
 class AbortingTask(object):
