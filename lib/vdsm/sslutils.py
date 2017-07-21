94c787948894488b88bcaca84300bde48e8c4aca
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2017-07-21 12:49:20 +0200

    ssl: handle handshake errors
    There are two possible errors (sslerror and socket.error) which are
    raised during ssl handshake that need to be handled. We do not want
    to see unhandled error backtraces in the logs which could easily
    generate enourmos amount of logs when malicious client exploit it.
    We log now only the occurence of the error and the offending IP address
    that caused the error.
    
    We fix the issue by handling both errors and cleanly closing file
    descriptor.
    
    Bug-url: https://bugzilla.redhat.com/1473295
    Change-Id: I99cfa35e608f429640455c35495be1783854e3da
    Signed-off-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>
    Signed-off-by: Irit Goihman <igoihman@redhat.com>

--- lib/vdsm/sslutils.py
+++ lib/vdsm/sslutils.py
@@ -254,8 +254,13 @@ class SSLHandshakeDispatcher(object):
             elif err.args[0] == ssl.SSL_ERROR_WANT_WRITE:
                 self.want_write = True
             else:
+                self.log.error("ssl handshake: SSLError, address: {}".format(
+                    dispatcher.socket.getpeername()[0]))
                 dispatcher.close()
-                raise
+        except socket.error:
+            self.log.error("ssl handshake: socket error, address: {}".format(
+                dispatcher.socket.getpeername()[0]))
+            dispatcher.close()
         else:
             self.want_read = self.want_write = True
             self._is_handshaking = False
