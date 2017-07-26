4801e26e739002575723a3c9783902e2b87275bb
Dominik Holler <dholler@redhat.com>
2017-07-26 10:43:59 +0200

    net: Enable LLDP on NICs
    lldpad has to be configured to provide LLDP on network interfaces.
    
    This change activates LLDP on all NICs.
    
    Bug-Url: https://bugzilla.redhat.com/1479677
    Change-Id: I6cc0cc21a189bcdcff3f4ac3ad97620ed4ff8f50
    Signed-off-by: Dominik Holler <dholler@redhat.com>
    (cherry picked from commit 53d332fa38bb1e4d75dbe9a7760d6e9e961cdde4)

--- lib/vdsm/network/initializer.py
+++ lib/vdsm/network/initializer.py
@@ -18,8 +18,28 @@
 #
 from __future__ import absolute_import
 
+import logging
+
+from vdsm.network import lldp
+from vdsm.network import netswitch
 from vdsm.network.nm import networkmanager
+
+Lldp = lldp.driver()
 
 
 def init_privileged_network_components():
     networkmanager.init()
+    _lldp_init()
+
+
+def _lldp_init():
+    """"
+    Enables receiving of LLDP frames for all nics. If sending or receiving
+    LLDP frames is already enabled on a nic, it is not modified.
+    """
+    for device in netswitch.netinfo()['nics']:
+        if not Lldp.is_lldp_enabled_on_iface(device):
+            try:
+                Lldp.enable_lldp_on_iface(device)
+            except lldp.EnableLldpError:
+                logging.exception('Failed to enable LLDP on %s', device)
