eead23e686e9caf980d3761b766850904bb98418
Edward Haas <edwardh@redhat.com>
2017-08-03 17:12:36 +0300

    net: Introduce iface interface driver
    The iface interface is defined and implemented using a hybrid set of
    tools, including sysfs, netlink, iproute2 and ethtool.
    
    Change-Id: Ia5d50f47194f023826aafa5e3e77ec495ea4299c
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- lib/vdsm/network/link/iface.py
+++ lib/vdsm/network/link/iface.py
@@ -18,14 +18,17 @@
 #
 from __future__ import absolute_import
 
+import abc
 import os
 import random
 import string
 
+import six
+
 from vdsm.network import ipwrapper
 from vdsm.network.link import dpdk
 from vdsm.network.netlink import libnl
-from vdsm.network.netlink.link import get_link, is_link_up
+from vdsm.network.netlink import link
 from vdsm.network.netlink.waitfor import waitfor_linkup
 
 
@@ -37,6 +40,162 @@ NET_PATH = '/sys/class/net'
 DEFAULT_MTU = 1500
 
 
+@six.add_metaclass(abc.ABCMeta)
+class IfaceAPI(object):
+    """
+    Link iface driver interface.
+    """
+    @abc.abstractmethod
+    def up(self, admin_blocking=True, oper_blocking=False):
+        """
+        Set link state to UP, optionally blocking on the action.
+        :param dev: iface name.
+        :param admin_blocking: Block until the administrative state is UP.
+        :param oper_blocking: Block until the link is operational.
+        admin state is at kernel level, while link state is at driver level.
+        """
+
+    @abc.abstractmethod
+    def down(self):
+        pass
+
+    @abc.abstractmethod
+    def properties(self):
+        pass
+
+    @abc.abstractmethod
+    def is_up(self):
+        pass
+
+    @abc.abstractmethod
+    def is_admin_up(self):
+        pass
+
+    @abc.abstractmethod
+    def is_oper_up(self):
+        pass
+
+    @abc.abstractmethod
+    def is_promisc(self):
+        pass
+
+    @abc.abstractmethod
+    def exists(self):
+        pass
+
+    @abc.abstractmethod
+    def address(self):
+        pass
+
+    @abc.abstractmethod
+    def set_address(self, address):
+        pass
+
+    @abc.abstractmethod
+    def mtu(self, properties=None):
+        pass
+
+
+class IfaceHybrid(IfaceAPI):
+    """
+    Link iface driver implemented by a mix of iproute2, netlink and sysfs.
+    """
+    def __init__(self):
+        self._dev = None
+        self._vfid = None
+        self._is_dpdk_type = None
+
+    @property
+    def device(self):
+        return self._dev
+
+    @device.setter
+    def device(self, dev):
+        if self._dev:
+            raise AttributeError('Constant attribute, unable to modify')
+        self._dev = dev
+
+    @property
+    def vfid(self):
+        return self._vfid
+
+    @vfid.setter
+    def vfid(self, vf):
+        if self._vfid:
+            raise AttributeError('Constant attribute, unable to modify')
+        self._vfid = vf
+
+    def properties(self):
+        if self._is_dpdk_type:
+            info = dpdk.link_info(self._dev)
+        else:
+            info = link.get_link(self._dev)
+        return info
+
+    def up(self, admin_blocking=True, oper_blocking=False):
+        if self._is_dpdk_type:
+            dpdk.up(self._dev)
+            return
+        if admin_blocking:
+            self._up_blocking(oper_blocking)
+        else:
+            ipwrapper.linkSet(self._dev, [STATE_UP])
+
+    def down(self):
+        if self._is_dpdk_type:
+            dpdk.down(self._dev)
+            return
+        ipwrapper.linkSet(self._dev, [STATE_DOWN])
+
+    def is_up(self):
+        return self.is_admin_up()
+
+    def is_admin_up(self):
+        properties = self.properties()
+        return link.is_link_up(properties['flags'], check_oper_status=False)
+
+    def is_oper_up(self):
+        if self._is_dpdk_type:
+            return dpdk.is_oper_up(self._dev)
+        properties = self.properties()
+        return link.is_link_up(properties['flags'], check_oper_status=True)
+
+    def is_promisc(self):
+        properties = self.properties()
+        return bool(properties['flags'] & libnl.IfaceStatus.IFF_PROMISC)
+
+    def exists(self):
+        if dpdk.is_dpdk(self._dev):
+            return self._dev in dpdk.get_dpdk_devices()
+        return os.path.exists(os.path.join(NET_PATH, self._dev))
+
+    def address(self):
+        return self.properties()['address']
+
+    def set_address(self, address):
+        if self._vfid is None:
+            link_set_args = ['address', address]
+        else:
+            link_set_args = ['vf', str(self._vfid), 'mac', address]
+        ipwrapper.linkSet(self._dev, link_set_args)
+
+    def mtu(self):
+        return self.properties()['mtu']
+
+    def _up_blocking(self, link_blocking):
+        with waitfor_linkup(self._dev, link_blocking):
+            ipwrapper.linkSet(self._dev, [STATE_UP])
+
+
+def iface(device, vfid=None):
+    """ Iface factory """
+    interface = IfaceHybrid()
+    interface.device = device
+    interface._is_dpdk_type = dpdk.is_dpdk(device)
+    interface.vfid = vfid
+    return interface
+
+
 def up(dev, admin_blocking=True, oper_blocking=False):
     """
     Set link state to UP, optionally blocking on the action.
@@ -45,64 +204,43 @@ def up(dev, admin_blocking=True, oper_blocking=False):
     :param oper_blocking: Block until the link is operational.
     admin state is at kernel level, while link state is at driver level.
     """
-    if dpdk.is_dpdk(dev):
-        dpdk.up(dev)
-        return
-    if admin_blocking:
-        _up_blocking(dev, oper_blocking)
-    else:
-        ipwrapper.linkSet(dev, [STATE_UP])
+    iface(dev).up(admin_blocking, oper_blocking)
 
 
 def down(dev):
-    if dpdk.is_dpdk(dev):
-        dpdk.down(dev)
-        return
-    ipwrapper.linkSet(dev, ['down'])
+    iface(dev).down()
 
 
 def is_up(dev):
-    return is_admin_up(dev)
+    return iface(dev).is_up()
 
 
 def is_admin_up(dev):
-    return is_link_up(get_link(dev)['flags'], check_oper_status=False)
+    return iface(dev).is_admin_up()
 
 
 def is_oper_up(dev):
-    if dpdk.is_dpdk(dev):
-        return dpdk.is_oper_up(dev)
-    return is_link_up(get_link(dev)['flags'], check_oper_status=True)
+    return iface(dev).is_oper_up()
 
 
 def is_promisc(dev):
-    return bool(get_link(dev)['flags'] & libnl.IfaceStatus.IFF_PROMISC)
+    return iface(dev).is_promisc()
 
 
 def exists(dev):
-    return os.path.exists(os.path.join(NET_PATH, dev))
+    return iface(dev).exists()
 
 
 def set_mac_address(dev, mac_address, vf_num=None):
-    if vf_num is None:
-        ipwrapper.linkSet(dev, ['address', mac_address])
-    else:
-        ipwrapper.linkSet(dev, ['vf', str(vf_num), 'mac', mac_address])
+    iface(dev, vf_num).set_address(mac_address)
 
 
 def mac_address(dev):
-    if dpdk.is_dpdk(dev):
-        return dpdk.link_info(dev)['address']
-    return get_link(dev)['address']
+    return iface(dev).address()
 
 
 def get_mtu(dev):
-    return get_link(dev)['mtu']
-
-
-def _up_blocking(dev, link_blocking):
-    with waitfor_linkup(dev, link_blocking):
-        ipwrapper.linkSet(dev, [STATE_UP])
+    return iface(dev).mtu()
 
 
 def random_iface_name(prefix='', max_length=15, digit_only=False):
