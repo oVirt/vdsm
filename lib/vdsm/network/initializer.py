d405725fb0df08db69bed6e613a48c20ffc295c9
Dominik Holler <dholler@redhat.com>
2017-08-07 12:40:56 +0200

    net: Introduce network initializer module
    In following commit, the network initialization will require
    multiple calls to privileged initialization functions. To avoid
    the details of initialization of privileged network components
    in supervdsmServer, this patch moves the initialization of
    privileged network components to network.initializer.
    
    Bug-Url: https://bugzilla.redhat.com/1479677
    Change-Id: Id9a78d24b6c1cb247397433e02c0bae3e1e7852a
    Signed-off-by: Dominik Holler <dholler@redhat.com>

--- lib/vdsm/network/initializer.py
+++ lib/vdsm/network/initializer.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Red Hat, Inc.
+# Copyright 2017 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,30 +16,10 @@
 #
 # Refer to the README and COPYING files for full details of the license
 #
+from __future__ import absolute_import
 
-SUBDIRS = configurators ip link lldp lldpad netinfo netlink ovs tc nm
+from vdsm.network.nm import networkmanager
 
-include $(top_srcdir)/build-aux/Makefile.subs
 
-vdsmnetworkdir = $(vdsmpylibdir)/network
-dist_vdsmnetwork_PYTHON = \
-	__init__.py \
-	api.py \
-	errors.py \
-	canonicalize.py \
-	connectivity.py \
-	driverloader.py \
-	ifacquire.py \
-	initializer.py \
-	ipwrapper.py \
-	kernelconfig.py \
-	legacy_switch.py \
-	libvirt.py \
-	models.py \
-	netconfpersistence.py \
-	netrestore.py \
-	netswitch.py \
-	sourceroute.py \
-	sourceroutethread.py \
-	utils.py \
-	$(NULL)
+def init_privileged_network_components():
+    networkmanager.init()
