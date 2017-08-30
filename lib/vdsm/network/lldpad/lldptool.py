5fdb75960d313f4a30e17363edd0f0f38b9c8690
Dominik Holler <dholler@redhat.com>
2017-08-30 13:53:54 +0200

    net: Fail early on initialization of LLDP
    If LLDP is detected as not active, the initialization of LLDP
    is not continued.
    This has the advantage that there is no further time spent on
    the initialization of LLDP, if LLDP is not working properly.
    
    Change-Id: I6d0ca7613b7f3cf022ed46d07eea615367af36b6
    Bug-Url: https://bugzilla.redhat.com/1488358
    Signed-off-by: Dominik Holler <dholler@redhat.com>
    (cherry picked from commit f2e9f7c2dcebd105c5db5457a39b31fec34bfdb0)

--- lib/vdsm/network/lldpad/lldptool.py
+++ lib/vdsm/network/lldpad/lldptool.py
@@ -38,6 +38,11 @@ def is_lldpad_service_running():
     return rc == 0
 
 
+def is_lldptool_functional():
+    rc, _, _ = commands.execCmd([LLDPTOOL, '-ping'])
+    return rc == 0
+
+
 def enable_lldp_on_iface(iface, rx_only=True):
     rc, out, err = commands.execCmd(
         [LLDPTOOL, 'set-lldp', '-i', iface,
