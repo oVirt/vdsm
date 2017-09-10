58ebca97c3a199c386e8b32bb1ad12831dabef07
Dan Kenigsberg <danken@redhat.com>
2017-09-10 14:59:18 +0300

    net: nic: speed: drop infiniband speed hack
    EL-7.4 kernels do not need it, as they report the correct speed for
    InfiniBand devices.
    
    Change-Id: I54ee955f21eec72dbd2c91dd0b7c17835a7dff5d
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- lib/vdsm/network/link/nic.py
+++ lib/vdsm/network/link/nic.py
@@ -18,11 +18,9 @@
 #
 from __future__ import absolute_import
 
-import errno
 import io
 import logging
 
-from vdsm.network.ethtool import driver_name
 from vdsm.network.link import dpdk
 from vdsm.network.link.iface import iface
 
@@ -39,11 +37,6 @@ def speed(nic_name):
             return dpdk.speed(nic_name)
         try:
             return read_speed_using_sysfs(nic_name)
-        except IOError as ose:
-            if ose.errno == errno.EINVAL:
-                return _ib_hacked_speed(nic_name)
-            else:
-                logging.exception('cannot read %s speed', nic_name)
         except Exception:
             logging.exception('cannot read %s speed', nic_name)
     return 0
@@ -58,15 +51,3 @@ def read_speed_using_sysfs(nic_name):
     if s in (2 ** 16 - 1, 2 ** 32 - 1) or s <= 0:
         raise ReadSpeedValueError(s)
     return s
-
-
-def _ib_hacked_speed(nic_name):
-    """If the nic is an InfiniBand device, return a speed of 10000 Mbps.
-
-    This is only needed until the kernel reports ib*/speed, see
-    https://bugzilla.redhat.com/show_bug.cgi?id=1101314
-    """
-    try:
-        return 10000 if driver_name(nic_name) == 'ib_ipoib' else 0
-    except IOError:
-        return 0
