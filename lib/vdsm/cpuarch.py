cd7e68c15ca4bf0dc85cb9c82ca38ec33abcf999
Viktor Mihajlovski <mihajlov@linux.vnet.ibm.com>
2017-09-19 13:32:35 +0200

    arch: add s390 as a supported architecture
    Note that s390 is used as a common denomination for the IBM mainframe
    architecture, whereas s390x specifically identifies the 64-bit hardware.
    
    Change-Id: I5146c0d4afc0343b11cdc6573bb1672fb3eba0a7
    Signed-off-by: Viktor Mihajlovski <mihajlov@linux.vnet.ibm.com>

--- lib/vdsm/cpuarch.py
+++ lib/vdsm/cpuarch.py
@@ -30,8 +30,9 @@ X86_64 = 'x86_64'
 PPC64 = 'ppc64'
 PPC64LE = 'ppc64le'
 AARCH64 = 'aarch64'
+S390X = 's390x'
 
-SUPPORTED_ARCHITECTURES = (X86_64, PPC64, PPC64LE, AARCH64)
+SUPPORTED_ARCHITECTURES = (X86_64, PPC64, PPC64LE, AARCH64, S390X)
 
 PAGE_SIZE_BYTES = os.sysconf('SC_PAGESIZE')
 
@@ -92,6 +93,10 @@ def is_x86(arch):
 
 def is_arm(arch):
     return arch == AARCH64
+
+
+def is_s390(arch):
+    return arch == S390X
 
 
 def _supported(target_arch):
