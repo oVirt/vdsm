5636db0b11c765949272ce5010eee4d02e9f4411
Germano Veit Michel <germano@redhat.com>
2017-11-20 14:29:08 +1000

    hooks: add boot_hostdev
    This hook allows to boot VMs from Passthrough devices such as scsi, usb
    and pci cards such as SR-IOV NICs and SAS/SATA controllers.
    
    To use:
    1. Pass a device to a VM using the Host Devices tab
    2. Copy the device name (i.e.: scsi_2_0_0_0)
    3. Add vm custom property boot_hostdev and paste the device name from above.
    
    Change-Id: Id1b87e800e663cb3800f91005a466e41cb860315
    Bug-Url: https://bugzilla.redhat.com/1511234
    Signed-off-by: Germano Veit Michel <germano@redhat.com>

---
+++ tests/virttests/boot_hostdev_test.py
@@ -0,0 +1,212 @@
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301  USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import io
+import os
+import sys
+
+
+from testlib import XMLTestCase
+from testlib import expandPermutations
+from testlib import permutations
+from testlib import temporaryPath
+
+from vdsm.common import commands
+
+
+_HOOK_PATH = '../vdsm_hooks/boot_hostdev/before_vm_start.py'
+_DEV_NAMES = [
+    ['pci_0000_0b_00_0'],
+    ['scsi_2_0_0_0'],
+    ['usb_usb7'],
+]
+_UNRELATED_DEV_NAMES = [
+    ['pci_0000_0c_00_5'],
+    ['scsi_3_0_0_1'],
+    ['usb2_usb5'],
+]
+
+_HOSTDEV_XML = """<?xml version="1.0" encoding="utf-8"?>
+  <domain type="kvm" xmlns:ovirt="http://ovirt.org/vm/tune/1.0">
+    <name>test</name>
+    <devices>
+      <disk type='file' device='disk' snapshot='no'>
+        <boot order='1'/>
+      </disk>
+      <disk type='file' device='cdrom'>
+        <boot order='2'/>
+      </disk>
+      <interface type='bridge'>
+        <boot order='3'/>
+      </interface>
+      <hostdev mode='subsystem' type='pci' managed='no'>
+        <driver name='vfio'/>
+        <source>
+          <address domain='0x0000' bus='0x0b' slot='0x00' function='0x0'/>
+        </source>
+      </hostdev>
+      <hostdev mode='subsystem' type='scsi' managed='no' rawio='yes'>
+        <source>
+          <adapter name='scsi_host2'/>
+          <address bus='0' target='0' unit='0'/>
+        </source>
+      </hostdev>
+      <hostdev managed="no" mode="subsystem" type="usb">
+          <source>
+              <address bus="7" device="1"/>
+          </source>
+      </hostdev>
+    </devices>
+  </domain>"""
+
+_EXPECTED_HOSTDEV_XML = {
+    'pci_0000_0b_00_0': """<?xml version="1.0" encoding="utf-8"?>
+      <domain type="kvm" xmlns:ovirt="http://ovirt.org/vm/tune/1.0">
+        <name>test</name>
+        <devices>
+          <disk type='file' device='disk' snapshot='no'>
+            <boot order='2'/>
+          </disk>
+          <disk type='file' device='cdrom'>
+            <boot order='3'/>
+          </disk>
+          <interface type='bridge'>
+            <boot order='4'/>
+          </interface>
+          <hostdev mode='subsystem' type='pci' managed='no'>
+            <driver name='vfio'/>
+            <source>
+              <address domain='0x0000' bus='0x0b' slot='0x00' function='0x0'/>
+            </source>
+            <boot order='1'/>
+          </hostdev>
+          <hostdev mode='subsystem' type='scsi' managed='no' rawio='yes'>
+            <source>
+              <adapter name='scsi_host2'/>
+              <address bus='0' target='0' unit='0'/>
+            </source>
+          </hostdev>
+          <hostdev managed="no" mode="subsystem" type="usb">
+            <source>
+              <address bus="7" device="1"/>
+            </source>
+          </hostdev>
+        </devices>
+      </domain>""",
+    'scsi_2_0_0_0': """<?xml version="1.0" encoding="utf-8"?>
+      <domain type="kvm" xmlns:ovirt="http://ovirt.org/vm/tune/1.0">
+        <name>test</name>
+        <devices>
+          <disk type='file' device='disk' snapshot='no'>
+            <boot order='2'/>
+          </disk>
+          <disk type='file' device='cdrom'>
+           <boot order='3'/>
+          </disk>
+          <interface type='bridge'>
+            <boot order='4'/>
+          </interface>
+          <hostdev mode='subsystem' type='pci' managed='no'>
+            <driver name='vfio'/>
+            <source>
+              <address domain='0x0000' bus='0x0b' slot='0x00' function='0x0'/>
+            </source>
+          </hostdev>
+          <hostdev mode='subsystem' type='scsi' managed='no' rawio='yes'>
+            <source>
+              <adapter name='scsi_host2'/>
+              <address bus='0' target='0' unit='0'/>
+            </source>
+            <boot order='1'/>
+          </hostdev>
+          <hostdev managed="no" mode="subsystem" type="usb">
+            <source>
+              <address bus="7" device="1"/>
+            </source>
+          </hostdev>
+        </devices>
+      </domain>""",
+    'usb_usb7': """<?xml version="1.0" encoding="utf-8"?>
+      <domain type="kvm" xmlns:ovirt="http://ovirt.org/vm/tune/1.0">
+        <name>test</name>
+        <devices>
+          <disk type='file' device='disk' snapshot='no'>
+            <boot order='2'/>
+          </disk>
+          <disk type='file' device='cdrom'>
+            <boot order='3'/>
+          </disk>
+          <interface type='bridge'>
+            <boot order='4'/>
+          </interface>
+          <hostdev mode='subsystem' type='pci' managed='no'>
+            <driver name='vfio'/>
+            <source>
+              <address domain='0x0000' bus='0x0b' slot='0x00' function='0x0'/>
+            </source>
+          </hostdev>
+          <hostdev mode='subsystem' type='scsi' managed='no' rawio='yes'>
+            <source>
+              <adapter name='scsi_host2'/>
+              <address bus='0' target='0' unit='0'/>
+            </source>
+          </hostdev>
+          <hostdev managed="no" mode="subsystem" type="usb">
+            <source>
+              <address bus="7" device="1"/>
+            </source>
+            <boot order='1'/>
+          </hostdev>
+        </devices>
+      </domain>"""
+}
+
+
+@expandPermutations
+class BootHostdevHookTests(XMLTestCase):
+
+    @permutations(_UNRELATED_DEV_NAMES)
+    def test_ignore_unrelated_hostdev(self, dev_name):
+        xml, rc, out, err = self._run_hook(_HOSTDEV_XML, boot_hostdev=dev_name)
+        self.assertXMLEqual(xml, _HOSTDEV_XML)
+        self.assertEqual(rc, 0)
+
+    @permutations(_DEV_NAMES)
+    def test_boot_order_hostdev(self, dev_name):
+        xml, rc, out, err = self._run_hook(_HOSTDEV_XML, boot_hostdev=dev_name)
+        self.assertXMLEqual(xml, _EXPECTED_HOSTDEV_XML[dev_name])
+        self.assertEqual(rc, 0)
+
+    def _setup_env(self, temp_path, boot_hostdev):
+        env = dict(os.environ)
+        env["PYTHONPATH"] = ":".join([os.environ["PYTHONPATH"], _HOOK_PATH])
+        env["_hook_domxml"] = temp_path
+        env["boot_hostdev"] = boot_hostdev
+        return env
+
+    def _run_hook(self, domxml, boot_hostdev):
+        with temporaryPath(data=domxml.encode('utf-8')) as temp_path:
+            env = self._setup_env(temp_path, boot_hostdev)
+            rc, out, err = commands.execCmd(
+                [sys.executable, _HOOK_PATH], env=env)
+            with io.open(temp_path, 'r') as outxml:
+                outxml = outxml.read()
+            return outxml, rc, out, err
---
+++ vdsm_hooks/boot_hostdev/before_vm_start.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python2
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import os
+import traceback
+
+from vdsm.hook import hooking
+
+
+def main():
+    dev = os.environ.get('boot_hostdev')
+    if dev:
+        domxml = hooking.read_domxml()
+        # Free boot order 1, move all existing ones one up
+        for boot in domxml.getElementsByTagName('boot'):
+            order = int(boot.getAttribute('order'))
+            boot.setAttribute('order', str(order + 1))
+
+        # Find specified hostdev, set order = 1
+        usr_dev = get_user_device_attrs(dev)
+        for hostdev in domxml.getElementsByTagName('hostdev'):
+            xml_dev = get_xml_device_attrs(hostdev)
+            if xml_dev == usr_dev:
+                boot = domxml.createElement('boot')
+                boot.setAttribute('order', '1')
+                hostdev.appendChild(boot)
+                hooking.write_domxml(domxml)
+                break
+
+
+def get_user_device_attrs(dev):
+    """
+    VFIO: pci_0000_0b_00_0
+        <hostdev mode='subsystem' type='pci' managed='no'>
+          <driver name='vfio'/>
+          <source>
+            <address domain='0x0000' bus='0x0b' slot='0x00' function='0x0'/>
+          </source>
+        </hostdev>
+    SCSI: scsi_2_0_0_0
+        <hostdev mode='subsystem' type='scsi' managed='no' rawio='yes'>
+          <source>
+            <adapter name='scsi_host2'/>
+            <address bus='0' target='0' unit='0'/>
+          </source>
+        </hostdev>
+    USB: usb_usb7
+        <hostdev managed="no" mode="subsystem" type="usb">
+            <source>
+                <address bus="7" device="1"/>
+            </source>
+        </hostdev>
+
+    """
+    attrs = set()
+    split_dev = dev.split('_')
+    if dev.startswith('scsi'):
+        attrs.add(('type', 'scsi'))
+        attrs.add(('name', 'scsi_host' + split_dev[1]))
+        attrs.add(('bus', split_dev[2]))
+        attrs.add(('target', split_dev[3]))
+        attrs.add(('unit', split_dev[4]))
+    elif dev.startswith('pci'):
+        attrs.add(('type', 'pci'))
+        attrs.add(('domain', '0x' + split_dev[1]))
+        attrs.add(('bus', '0x' + split_dev[2]))
+        attrs.add(('slot', '0x' + split_dev[3]))
+        attrs.add(('function', '0x' + split_dev[4]))
+    elif dev.startswith('usb'):
+        attrs.add(('type', 'usb'))
+        device = split_dev[0][3:]
+        if not device:
+            device = '1'
+        attrs.add(('device', device))
+        attrs.add(('bus', split_dev[1][3:]))
+    return attrs
+
+
+def get_xml_device_attrs(hostdev):
+    source = hostdev.getElementsByTagName('source')[0]
+    address = source.getElementsByTagName('address')[0]
+    attrs = {i for i in address.attributes.items()}
+    devtype = hostdev.getAttribute('type')
+    attrs.add(('type', devtype))
+    if devtype == 'scsi':
+        adapter = hostdev.getElementsByTagName('adapter')[0]
+        attrs.add(('name', adapter.getAttribute('name')))
+    return attrs
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        hooking.exit_hook(
+            'boot_hostdev: %s' % (
+                traceback.format_exc()
+            ),
+            return_code=1
+        )
