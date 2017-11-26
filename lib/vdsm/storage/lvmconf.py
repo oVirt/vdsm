5781aab642f025704e5aeb48eeea088b995bf75c
Nir Soffer <nsoffer@redhat.com>
2017-11-27 00:23:14 +0200

    lvmconf: Introduce the storage lvmconf module
    This module helps to access LVM configuration, required for analyzing
    and modifying current configuration when configuring a host. We also
    plan to use it later for verifying that a host has a safe LVM
    configuration during Vdsm startup.
    
    We use the amazing augeas library, but provide an easier to use
    ConfigParser like interface.
    
    Here is example usage of this module:
    
        with lvmconf.LVMConfig() as conf:
            old_filter = conf.getlist("devices", "filter")
            ...
            conf.setlist("devices", "filter", new_filter)
            conf.save()
    
    Change-Id: I31f088df483d75aa3342b5d03660a997ffbbfd93
    Bug-Url: https://bugzilla.redhat.com/1523152
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>
    Signed-off-by: Germano Veit Michel <germano@redhat.com>

---
+++ lib/vdsm/storage/lvmconf.py
@@ -0,0 +1,188 @@
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+"""
+lvmconf - Access LVM configuration file
+
+This module provides the LVMConfig class, for reading and writing LVM
+configuration. This class is a simple wrapper around augeas.Augeas, providing
+easier to use, ConfigParser like interface for accessing options.
+
+
+Reading configuration
+---------------------
+
+Use the get* methods::
+
+    with lvmconf.LVMConfig(path) as conf:
+        conf.getlist("devices", "filter")                   # ["a|.*|"]
+        conf.getint("global", "use_lvmetad")                # 1
+        conf.getstr("activation", "missing_stripe_filler")  # "error"
+
+Unlike ConfigParser, reading integer options as strings does not work, so this
+class does not provide untyped get() or set() method. Using the wrong type
+returns None.
+
+
+Modifying configuration
+-----------------------
+
+Use the set* methods::
+
+    with lvmconf.LVMConfig(path) as conf:
+        conf.setlist("devices", "section", ["a|^/dev/sda2$|", "r|.*|"])
+        conf.setint("global", "use_lvmetad", 0)
+        conf.setstr("activation", "missing_stripe_filler", "ignore")
+        conf.save()
+
+We use augeas backup option to save the previous file at file.augsave.
+
+Anyone can read LVM configuration, but to modify it you must run as root.
+
+
+TODO
+----
+
+- Add "# Vdsm: description..." comment when modifying a value possible with
+  tricky XPATH voodoo.
+
+- Add blank line before new options seems that is not supported by augeas.
+
+- Insert options after at the default location, under the default comment.
+  Should be possible using tricky XPATH and lot of work.
+
+- Indent new options properly - seems that it is not supported in augeas.
+
+"""
+
+from __future__ import absolute_import
+
+import logging
+
+from augeas import Augeas
+
+log = logging.getLogger("storage.lvmconf")
+
+
+class LVMConfig(object):
+
+    def __init__(self, path="/etc/lvm/lvm.conf"):
+        self.path = path
+
+        # Augeas loads by default tons of unneeded lenses and configuration
+        # files. On my test host, it fails to load, trying to read my 500 MiB
+        # /etc/lvm/archive/.
+        #
+        # These are the standard LVM lens includes:
+        # /augeas/load/LVM/incl[1] /etc/lvm/lvm.conf
+        # /augeas/load/LVM/incl[2] /etc/lvm/backup/*
+        # /augeas/load/LVM/incl[3] /etc/lvm/archive/*.vg
+        #
+        # We need only the first entry to work with lvm.conf. Using customized
+        # load setup, as explained in
+        # https://github.com/hercules-team/augeas/wiki/Loading-specific-files
+        #
+        # Removing the archive and backup entries, we can load augeas in 0.7
+        # seconds on my test vm. Removing all other lenses shorten the time to
+        # 0.04 seconds.
+
+        log.debug("Loading LVM configuration from %r", path)
+        self.aug = Augeas(flags=Augeas.NO_MODL_AUTOLOAD | Augeas.SAVE_BACKUP)
+        self.aug.add_transform("lvm.lns", [path])
+        self.aug.load()
+
+    # Context manager interface
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, t, v, tb):
+        try:
+            self.close()
+        except Exception as e:
+            # Caller succeeded, raise the close error.
+            if t is None:
+                raise
+            # Caller has failed, do not hide the original error.
+            log.exception("Error closing %s: %s" % (self, e))
+
+    # Accessing list of strings
+
+    def getlist(self, section, option):
+        pat = "/files%s/%s/dict/%s/list/*/str" % (self.path, section, option)
+        matches = self.aug.match(pat)
+        if not matches:
+            return None  # Cannot store/read empty list
+        return [self.aug.get(m) for m in matches]
+
+    def setlist(self, section, option, value):
+        log.debug("Setting %s/%s to %s", section, option, value)
+        opt_path = "/files%s/%s/dict/%s" % (self.path, section, option)
+        self.aug.remove(opt_path)
+        item_path = opt_path + "/list/%d/str"
+        for i, item in enumerate(value, 1):
+            self.aug.set(item_path % i, item)
+
+    # Accessing flat values (int, string)
+
+    def getint(self, section, option):
+        val = self._get_flat(section, option, "int")
+        return int(val) if val is not None else None
+
+    def setint(self, section, option, value):
+        self._set_flat(section, option, "int", str(value))
+
+    def getstr(self, section, option):
+        return self._get_flat(section, option, "str")
+
+    def setstr(self, section, option, value):
+        self._set_flat(section, option, "str", value)
+
+    def _get_flat(self, section, option, opt_type):
+        path = self._flat_path(section, option, opt_type)
+        return self.aug.get(path)
+
+    def _set_flat(self, section, option, opt_type, value):
+        log.debug("Setting %s/%s to %r", section, option, value)
+        path = self._flat_path(section, option, opt_type)
+        return self.aug.set(path, value)
+
+    def _flat_path(self, section, option, opt_type):
+        return "/files%s/%s/dict/%s/%s" % (
+            self.path, section, option, opt_type)
+
+    # Removing options
+
+    def remove(self, section, option):
+        log.debug("Removing %s/%s", section, option)
+        path = "/files%s/%s/dict/%s" % (self.path, section, option)
+        self.aug.remove(path)
+
+    # File operations
+
+    def save(self):
+        log.info("Saving new LVM configuration to %r, previous configuration "
+                 "saved to %r",
+                 self.path, self.path + ".augsave")
+        self.aug.save()
+
+    def close(self):
+        log.debug("Closing LVM configuration %s", self.path)
+        self.aug.close()
---
+++ tests/storagetests/lvmconf_test.py
@@ -0,0 +1,225 @@
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301  USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+
+import os
+import pytest
+
+from vdsm.storage import lvmconf
+
+
+CONF = """
+global {
+    # For testing integer options
+    use_lvmetad = 1
+}
+
+devices {
+    # For testing list of strings
+    filter = [ "a|^/dev/sda2$|", "r|.*|" ]
+
+    # For testing commented value
+    # gloabl_filter = [ "a|^/dev/sda2$|", "r|.*|" ]
+}
+
+activation {
+    # For testing string options
+    missing_stripe_filler = "error"
+}
+"""
+
+
+def test_read_empty(tmpdir):
+    path = create_lvm_conf(tmpdir, "")
+    with lvmconf.LVMConfig(path) as conf:
+        # No option - and no type information
+        assert conf.getlist("devices", "filter") is None
+        assert conf.getint("devices", "filter") is None
+        assert conf.getstr("devices", "filter") is None
+        # Commented is same as no option
+        assert conf.getlist("devices", "global_filter") is None
+
+
+def test_read_options(tmpdir):
+    path = create_lvm_conf(tmpdir, CONF)
+    with lvmconf.LVMConfig(path) as conf:
+        # Correct type
+        assert conf.getlist("devices", "filter") == ["a|^/dev/sda2$|", "r|.*|"]
+        assert conf.getint("global", "use_lvmetad") == 1
+        assert conf.getstr("activation", "missing_stripe_filler") == "error"
+
+        # Wrong type
+        assert conf.getstr("devices", "filter") is None
+        assert conf.getint("devices", "filter") is None
+        assert conf.getlist("global", "use_lvmetad") is None
+        assert conf.getstr("global", "use_lvmetad") is None
+        assert conf.getint("activation", "missing_stripe_filler") is None
+        assert conf.getlist("activation", "missing_stripe_filler") is None
+
+
+@pytest.mark.parametrize("data", [
+    pytest.param(CONF, id="example"),
+    pytest.param("", id="empty"),
+])
+def test_modify(tmpdir, data):
+    path = create_lvm_conf(tmpdir, data)
+    new_list = ["a|^/dev/sda2$|", "a|^/dev/sdb3$|", "r|.*|"]
+    new_int = 0
+    new_str = "ignore"
+
+    with lvmconf.LVMConfig(path) as conf:
+        old_list = conf.getlist("devices", "filter")
+        old_int = conf.getint("global", "use_lvmetad")
+        old_str = conf.getstr("activation", "missing_stripe_filler")
+
+        conf.setlist("devices", "filter", new_list)
+        conf.setint("global", "use_lvmetad", new_int)
+        conf.setstr("activation", "missing_stripe_filler", new_str)
+
+        assert conf.getlist("devices", "filter") == new_list
+        assert conf.getint("global", "use_lvmetad") == new_int
+        assert conf.getstr("activation", "missing_stripe_filler") == new_str
+
+    with lvmconf.LVMConfig(path) as conf:
+        assert conf.getlist("devices", "filter") == old_list
+        assert conf.getint("global", "use_lvmetad") == old_int
+        assert conf.getstr("activation", "missing_stripe_filler") == old_str
+
+
+@pytest.mark.parametrize("data", [
+    pytest.param(CONF, id="typcial"),
+    pytest.param("", id="empty"),
+])
+def test_save(tmpdir, data):
+    path = create_lvm_conf(tmpdir, data)
+    new_list = ["a|^/dev/sda2$|", "a|^/dev/sdb3$|", "r|.*|"]
+    new_int = 0
+    new_str = "ignore"
+
+    with lvmconf.LVMConfig(path) as conf:
+        conf.setlist("devices", "filter", new_list)
+        conf.setint("global", "use_lvmetad", new_int)
+        conf.setstr("activation", "missing_stripe_filler", new_str)
+        conf.save()
+
+    with lvmconf.LVMConfig(path) as conf:
+        assert conf.getlist("devices", "filter") == new_list
+        assert conf.getint("global", "use_lvmetad") == new_int
+        assert conf.getstr("activation", "missing_stripe_filler") == new_str
+
+
+def test_save_backup(tmpdir):
+    path = create_lvm_conf(tmpdir, CONF)
+
+    with lvmconf.LVMConfig(path) as conf:
+        old_filter = conf.getlist("devices", "filter")
+        conf.setlist("devices", "filter", ["a|.*|"])
+        conf.save()
+
+    with lvmconf.LVMConfig(path + ".augsave") as backup:
+        assert backup.getlist("devices", "filter") == old_filter
+
+
+def test_save_error(tmpdir):
+    path = create_lvm_conf(tmpdir, CONF)
+
+    with lvmconf.LVMConfig(path) as conf:
+        conf.setlist("devices", "filter", ["a|.*|"])
+        # Make save fail even when runing as root.
+        os.unlink(path)
+        os.mkdir(path)
+        with pytest.raises(EnvironmentError):
+            conf.save()
+
+
+def test_set_empty_list(tmpdir):
+    path = create_lvm_conf(tmpdir, CONF)
+
+    with lvmconf.LVMConfig(path) as conf:
+        conf.setlist("devices", "filter", [])
+        conf.save()
+
+    with lvmconf.LVMConfig(path) as conf:
+        assert conf.getlist("devices", "filter") is None
+
+
+def test_remove(tmpdir):
+    path = create_lvm_conf(tmpdir, CONF)
+
+    with lvmconf.LVMConfig(path) as conf:
+        conf.remove("devices", "filter")
+        conf.save()
+
+    with lvmconf.LVMConfig(path) as conf:
+        assert conf.getlist("devices", "filter") is None
+
+
+def test_save_keep_other_options(tmpdir):
+    path = create_lvm_conf(tmpdir, CONF)
+
+    with lvmconf.LVMConfig(path) as conf:
+        conf.setlist("devices", "filter", ["a|.*|"])
+        conf.save()
+
+    with lvmconf.LVMConfig(path) as conf:
+        assert conf.getint("global", "use_lvmetad") == 1
+
+
+def test_context_exit_error(tmpdir):
+    # Raise if __exit__ fails.
+    path = create_lvm_conf(tmpdir, "")
+
+    def fail():
+        raise RuntimeError
+
+    with pytest.raises(RuntimeError):
+        with lvmconf.LVMConfig(path) as conf:
+            conf.close = fail
+
+
+def test_context_propagate_user_error(tmpdir):
+    # Raise user errors if __exit__ fails.
+    path = create_lvm_conf(tmpdir, "")
+
+    def fail():
+        raise RuntimeError
+
+    class UserError(Exception):
+        pass
+
+    with pytest.raises(UserError):
+        with lvmconf.LVMConfig(path) as conf:
+            conf.close = fail
+            raise UserError
+
+
+@pytest.mark.skipif(not os.path.exists("/etc/lvm/lvm.conf"),
+                    reason="lvm.conf not found")
+def test_real_lvm_conf():
+    with lvmconf.LVMConfig() as conf:
+        assert conf.getint("global", "use_lvmetad") in (1, 0, None)
+
+
+def create_lvm_conf(tmpdir, data):
+    path = tmpdir.join("lvm.conf")
+    path.write(data)
+    return str(path)
