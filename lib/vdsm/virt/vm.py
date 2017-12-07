407e158118beba6661dd832655e15236f03638a1
Milan Zamazal <mzamazal@redhat.com>
2017-12-07 18:12:26 +0100

    virt: Create also device conf when making devices from XML
    When a VM initialized from XML Engine or XML recovery is migrated to a host
    running Vdsm < 4.2, conf['devices'] device configuration may be required on the
    destination.  Currently some important pieces of that configuration are
    missing, which may lead to migration failure.  This has already been observed
    when migrating a recovered (after Vdsm restart) VM containing a rng device from
    4.2 host to 4.1 host.
    
    This patch adds all devices retrieved from XML to Vm.conf['devices'] in order
    to allow migration to hosts supporting only legacy VM configuration.
    
    Change-Id: If248ad69cdc71bb1cb72ff1c0e42e84011987f72
    Bug-Url: https://bugzilla.redhat.com/1522901
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>

--- lib/vdsm/virt/vm.py
+++ lib/vdsm/virt/vm.py
@@ -2586,6 +2586,17 @@ class Vm(object):
         else:
             self._override_disk_device_config(disk_params)
 
+        # REQUIRED_FOR: vdsm < 4.2
+        # We need to create self.conf['devices'] to be able to migrate to hosts
+        # running Vdsm < 4.2.
+        if 'xml' in self.conf:
+            for devices in dev_objs_from_xml.values():
+                for dev in devices:
+                    if dev.type != hwclass.DISK:
+                        # Disk devices have already been added in
+                        # _override_disk_device_config above.
+                        self.conf['devices'].append(dev.conf_parameters())
+
         self.log.debug('Built %d devices', len(dev_objs_from_xml))
         return dev_objs_from_xml
 
