00b24709c5368697b7bb5d85708f2c818a76a8d5
Francesco Romani <fromani@redhat.com>
2018-02-09 19:02:20 +0100

    vmdevices: fix config() in backward-compatible way
    In 4.1 compatibility mode, we cannot expose the devices' configuration
    as Vdsm stored it, because in the initialization flow
    some normalization/trasformation has been done.
    
    Furthermore, between 3.6 and 4.1, the API got saner, and we now send
    more meaningful values, like true booleans instead of True,
    or less convoluted way to express the configuration data (e.g cdrom
    paths).
    
    This patch starts the compatibility layer, implementing the
    reverse of the normalization Vdsm does for backward compatibility sake.
    We isolate those changes in one vmdevices.compat module, to make
    it easier to kill it as soon as we can.
    
    At the moment, we don't aim for full backward compatibility, which
    is not needed anyway. Most of the vm.conf is meant to be set by Engine
    and consumed by Vdsm; when checking the Vdsm responses, Engine reads
    only few selected fields, most notably 'deviceId', 'alias', 'address',
    for VFIO NICs, 'hostdev', for disks, 'volumeChain'.
    All of those are already returned by Vdsm even in the backward
    compatible flow.
    
    We used to dump all the vm.conf in the responses to Engine because
    it was simpler, but it always was a bad idea.
    
    Change-Id: I179c40d67db2e2f4c098c11e535a865b0cb2f7a4
    Backport-To: 4.2
    Bug-Url: https://bugzilla.redhat.com/1542117
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ lib/vdsm/virt/vmdevices/compat.py
@@ -0,0 +1,62 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+from __future__ import absolute_import
+
+from vdsm import utils
+
+
+def device_config(conf):
+    for key in ('vm_custom', 'vmid'):
+        # added internally by Vdsm. Engine doesn't know or care
+        # Easy to detect, safe to remove. We do that to reduce
+        # the noise.
+        conf.pop(key, None)
+    return conf
+
+
+def interface_config(conf):
+    # Reverse action of the conversion in __init__.
+    if conf.get('nicModel', '') == 'virtio':
+        conf['nicModel'] = 'pv'
+    return conf
+
+
+def drive_config(conf, drive):
+    ret_conf = dict_values_to_str(conf)
+    # metadata intentionally don't restore it.
+    vol_info = getattr(drive, 'volumeInfo', {})
+    ret_conf['volumeInfo'] = dict_values_to_str(vol_info)
+    # needed by live merge tracking.
+    vol_chain = getattr(drive, 'volumeChain', [])
+    ret_conf['volumeChain'] = [
+        utils.picklecopy(chain_item)
+        for chain_item in vol_chain
+    ]
+    return ret_conf
+
+
+def dict_values_to_str(d):
+    ret = {}
+    for key, value in d.items():
+        if isinstance(value, int):
+            ret[key] = str(value)
+        else:
+            ret[key] = value
+    return ret
