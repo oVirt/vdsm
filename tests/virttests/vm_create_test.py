9cca6a67e67b52104c954579aa5d2bc099974687
Francesco Romani <fromani@redhat.com>
2018-02-27 10:13:21 +0100

    vm: leases: handle drive leases in fixLeases
    The placeholder support code in fixLeases assumed
    the lease was a VM lease.
    For HE, we must support also drive leases.
    
    This patch make fixLeases support both lease types.
    
    Change-Id: Iceee1df88134130e3662024a03ecf486c4b0d619
    Bug-Url: https://bugzilla.redhat.com/1547479
    Backport-To: 4.2
    Branch-Name: fix-drive-leases
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- tests/virttests/vm_create_test.py
+++ tests/virttests/vm_create_test.py
@@ -25,6 +25,7 @@ import logging
 from vdsm.virt import domain_descriptor
 from vdsm.virt import metadata
 from vdsm.virt import vmdevices
+from vdsm.virt import vmxml
 
 from testlib import VdsmTestCase
 from testlib import read_data
@@ -165,6 +166,135 @@ class RestoreStateTests(VdsmTestCase):
         self.assertEqual(sda['poolID'], 'poolID_updated')
         self.assertEqual(sda['domainID'], 'domainID_updated')
         self.assertEqual(sda['volumeID'], 'volumeID_updated')
+
+
+class TestFixLease(VdsmTestCase):
+
+    def setUp(self):
+        self.vmParams = {
+            'vmId': 'ac698e04-d30b-426a-9f3c-0e95641b2b79',  # from XML
+            'xml': read_data('hostedengine_lease.xml'),
+        }
+        self.driveVolInfo = {
+            'leasePath': 'drive-lease-path',
+            'leaseOffset': 'drive-lease-offset',
+        }
+        self.vmVolInfo = {
+            'leasePath': 'vm-lease-path',
+            'leaseOffset': 'vm-lease-offset',
+        }
+
+    def test_drive_lease(self):
+        """
+        test that we fill the drive lease. Happy path.
+        """
+        with fake.VM(self.vmParams) as testvm:
+            testvm._devices = testvm._make_devices()
+
+            self._inject_volume_chain(testvm, self.driveVolInfo)
+
+            xml_str = vmdevices.lease.fixLeases(
+                testvm.cif.irs,
+                testvm.conf['xml'],
+                testvm._devices.get(vmdevices.hwclass.DISK, []))
+
+        self._check_leases(xml_str, [self.driveVolInfo])
+
+    def test_drive_lease_without_volume_chain(self):
+        """
+        Should we lack volumeChain attribute (like cdroms), this
+        should not raise.
+        We treat leases like VM lease, because we cannot distinguish
+        this case.
+        """
+        with fake.VM(self.vmParams) as testvm:
+            testvm._devices = testvm._make_devices()
+
+            def _fake_lease_info(*args, **kwargs):
+                return {
+                    'result': {
+                        'path': self.vmVolInfo['leasePath'],
+                        'offset': self.vmVolInfo['leaseOffset'],
+                    }
+                }
+
+            testvm.cif.irs.lease_info = _fake_lease_info
+
+            xml_str = vmdevices.lease.fixLeases(
+                testvm.cif.irs,
+                testvm.conf['xml'],
+                testvm._devices.get(vmdevices.hwclass.DISK, []))
+
+        self._check_leases(xml_str, [self.vmVolInfo])
+
+    def test_drive_lease_chain_not_matches(self):
+        """
+        We have no choice but consider this a VM lease.
+        """
+        with fake.VM(self.vmParams) as testvm:
+            testvm._devices = testvm._make_devices()
+
+            self._inject_volume_chain(
+                testvm, self.driveVolInfo,
+                domainID='unknwonDomainID',
+                volumeID='unknownVolumeID')
+
+            def _fake_lease_info(*args, **kwargs):
+                return {
+                    'result': {
+                        'path': self.vmVolInfo['leasePath'],
+                        'offset': self.vmVolInfo['leaseOffset'],
+                    }
+                }
+
+            testvm.cif.irs.lease_info = _fake_lease_info
+
+            xml_str = vmdevices.lease.fixLeases(
+                testvm.cif.irs,
+                testvm.conf['xml'],
+                testvm._devices.get(vmdevices.hwclass.DISK, []))
+
+        self._check_leases(xml_str, [self.vmVolInfo])
+
+    def _check_leases(self, xml_str, vol_infos):
+        xml_dom = vmxml.parse_xml(xml_str)
+        lease_elems = xml_dom.findall('./devices/lease')
+        self.assertEqual(len(lease_elems), len(vol_infos))
+
+        for lease_elem, vol_info in zip(lease_elems, vol_infos):
+            target = vmxml.find_first(lease_elem, 'target')
+            self.assertEqual(
+                target.attrib['path'], vol_info['leasePath'])
+            self.assertEqual(
+                target.attrib['offset'], vol_info['leaseOffset'])
+
+    def _inject_volume_chain(self, testvm, volInfo,
+                             domainID=None, volumeID=None):
+        drives = []
+        for drive in testvm._devices.get(vmdevices.hwclass.DISK, []):
+            if drive.device != 'disk':
+                continue
+
+            # the leases code uses only the leaf node. So let's use
+            # obviously bogus nodes which we should ignore
+            bogus = {
+                'domainID': 'bogus because should be ignored',
+                'volumeID': 'bogus because should be ignored',
+                'leasePath': 'bogus because should be ignored',
+                'leaseOffset': 'bogus because should be ignored',
+            }
+            # the leaf node is the only one used, so we fill it with
+            # meaningful data
+            leaf = {
+                'domainID': domainID or drive.domainID,
+                'volumeID': volumeID or drive.volumeID,
+            }
+            leaf.update(volInfo)
+            drive.volumeChain = [bogus, bogus, leaf]
+            drives.append(drive)
+            break  # assume only one disk
+
+        testvm._devices[vmdevices.hwclass.DISK] = drives
 
 
 # TODO: almost dupe of Vm._findDriveConfigByName
