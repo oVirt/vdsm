6b905c2c134bcf344961d28eefbd05f2838d2ec8
Dan Kenigsberg <danken@redhat.com>
2018-04-11 18:21:13 +0300

    automation: separate py2 and py3 coverage reports
    Per public request, this patch creates to different coverage reports,
    for Python 2 and Python 3, when Python 3 is available (i.e. on Fedora).
    
    Now it is easier to see that our coverage is 58% on py2, and only 49%
    on py3.
    
    Change-Id: I1a1c8c97f459cb90ae600a757d2d6ffe22c71e8c
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -24,8 +24,16 @@ TIMEOUT=600 make --jobs=2 check NOSE_WITH_COVERAGE=1 NOSE_COVER_PACKAGE="$PWD/vd
 
 # Generate coverage report in HTML format
 pushd tests
-coverage combine -a .coverage-storage-py27 .coverage-network-py27
+pwd
+ls .cov*
+coverage combine .coverage-nose-py2 .coverage-storage-py27 .coverage-network-py27 .coverage-virt-py27
 coverage html -d "$EXPORT_DIR/htmlcov"
+
+if grep -q 'Fedora' /etc/redhat-release; then
+    rm .coverage
+    coverage combine .coverage-nose-py3 .coverage-storage-py36 .coverage-network-py36 .coverage-virt-py36
+    coverage html -d "$EXPORT_DIR/htmlcov-py36"
+fi
 popd
 
 # Export subsystem coverage reports for viewing in jenkins.
