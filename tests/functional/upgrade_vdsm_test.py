41a87c7448af62a58ce19e967282afd2a9f20880
Marcin Sobczyk <msobczyk@redhat.com>
2018-10-23 11:09:56 +0200

    tests: Replace 'run_command' usage
    Local 'run_command' function is not needed - all it's functionality
    can be covered with 'common.commands.run'.
    
    Change-Id: Id0dbb65c7bf75f5d5a31458c663422d32c7ab5b4
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- tests/functional/upgrade_vdsm_test.py
+++ tests/functional/upgrade_vdsm_test.py
@@ -20,7 +20,8 @@
 
 from __future__ import absolute_import
 from __future__ import division
-import subprocess
+
+from vdsm.common import commands
 from vdsm.tool.service import service_start, service_status, service_stop
 
 from testlib import VdsmTestCase
@@ -28,51 +29,42 @@ from testlib import VdsmTestCase
 el7_ovirt36_repo = 'http://resources.ovirt.org/pub/ovirt-3.6/rpm/el7/'
 
 
-def run_command(command):
-    p = subprocess.Popen(command, stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    out, err = p.communicate()
-    if p.returncode != 0:
-        raise Exception(command, p.returncode, out, err)
-    return out
-
-
 def downgrade_vdsm(url):
-    run_command(['yum-config-manager', '--add-repo', url])
-    run_command(['yum', 'swap', '--', 'erase', '-y', 'vdsm\*',
+    commands.run(['yum-config-manager', '--add-repo', url])
+    commands.run(['yum', 'swap', '--', 'erase', '-y', 'vdsm\*',
                  '--', 'install', '-y', 'vdsm-4.17.10.1-0.el7.centos.noarch'])
 
 
 def upgrade_vdsm():
-    run_command(['yum-config-manager', '--enable', 'localsync'])
-    run_command(['yum', 'update', '-y', 'vdsm'])
+    commands.run(['yum-config-manager', '--enable', 'localsync'])
+    commands.run(['yum', 'update', '-y', 'vdsm'])
 
 
 class UpgradeTest(VdsmTestCase):
     def setUp(self):
-        run_command(['yum-config-manager', '--disable', 'localsync'])
+        commands.run(['yum-config-manager', '--disable', 'localsync'])
 
     def tearDown(self):
-        run_command(['yum-config-manager', '--disable', '*ovirt-3.6*'])
-        run_command(['yum-config-manager', '--enable', 'localsync'])
+        commands.run(['yum-config-manager', '--disable', '*ovirt-3.6*'])
+        commands.run(['yum-config-manager', '--enable', 'localsync'])
         # make sure vdsm is installed and running
-        run_command(['yum', 'install', '-y', 'vdsm'])
+        commands.run(['yum', 'install', '-y', 'vdsm'])
         service_start('vdsmd')
 
     def service_up_test(self):
         service_start('vdsmd')
-        vdsm_version = run_command(['rpm', '-q', 'vdsm'])
+        vdsm_version = commands.run(['rpm', '-q', 'vdsm'])
         downgrade_vdsm(el7_ovirt36_repo)
         upgrade_vdsm()
 
-        self.assertEqual(run_command(['rpm', '-q', 'vdsm']), vdsm_version)
+        self.assertEqual(commands.run(['rpm', '-q', 'vdsm']), vdsm_version)
         self.assertEqual(service_status('vdsmd'), 0)
 
     def service_down_test(self):
         service_stop('vdsmd')
-        vdsm_version = run_command(['rpm', '-q', 'vdsm'])
+        vdsm_version = commands.run(['rpm', '-q', 'vdsm'])
         downgrade_vdsm(el7_ovirt36_repo)
         upgrade_vdsm()
 
-        self.assertEqual(run_command(['rpm', '-q', 'vdsm']), vdsm_version)
+        self.assertEqual(commands.run(['rpm', '-q', 'vdsm']), vdsm_version)
         self.assertEqual(service_status('vdsmd'), 1)
