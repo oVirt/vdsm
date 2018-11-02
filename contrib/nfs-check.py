212bdf46aeb58b68df592eef25e95cb0e17589a3
Dan Kenigsberg <danken@redhat.com>
2018-11-02 21:00:57 +0200

    py3: drop nfs-check from blacklist
    Change-Id: I968263278f9ad9ac5975b95d191c1dcea7874780
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>

--- contrib/nfs-check.py
+++ contrib/nfs-check.py
@@ -15,6 +15,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 from __future__ import print_function
+from __future__ import absolute_import
+
 import os
 import sys
 import subprocess
