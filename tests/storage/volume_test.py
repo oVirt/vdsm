a7f036e5f7a95d26541a6501c8348c3d68634f33
Denis Chaplygin <dchaplyg@redhat.com>
2018-11-21 13:15:16 +0100

    tests: volume_test converted to pytest.
    Change-Id: I5a351ac7468a940349bf3db677b78e02fab3c1c2
    Signed-off-by: Denis Chaplygin <dchaplyg@redhat.com>

--- tests/storage/volume_test.py
+++ tests/storage/volume_test.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2016 Red Hat, Inc.
+# Copyright 2016-2018 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,6 +23,8 @@ from __future__ import division
 
 from contextlib import contextmanager
 
+import pytest
+
 from monkeypatch import MonkeyPatchScope
 from storage.storagefakelib import FakeStorageDomainCache
 
@@ -31,10 +33,8 @@ from storage.storagetestlib import (
     fake_env,
 )
 
-from testlib import expandPermutations, permutations
 from testlib import make_uuid
 from testlib import recorded
-from testlib import VdsmTestCase
 
 from vdsm.storage import constants as sc
 from vdsm.storage import exception as se
@@ -56,39 +56,37 @@ class FakeSDManifest(object):
         pass
 
 
-@expandPermutations
-class TestVolumeLease(VdsmTestCase):
+class TestVolumeLease:
 
     def test_properties(self):
         a = volume.VolumeLease(HOST_ID, 'dom', 'img', 'vol')
-        self.assertEqual(rm.getNamespace(sc.VOLUME_LEASE_NAMESPACE, 'dom'),
-                         a.ns)
-        self.assertEqual('vol', a.name)
-        self.assertEqual(rm.EXCLUSIVE, a.mode)
+        assert rm.getNamespace(sc.VOLUME_LEASE_NAMESPACE, 'dom') == a.ns
+        assert 'vol' == a.name
+        assert rm.EXCLUSIVE == a.mode
 
-    @permutations((
+    @pytest.mark.parametrize("a, b", [
         (('domA', 'img', 'vol'), ('domB', 'img', 'vol')),
-        (('dom', 'img', 'volA'), ('dom', 'img', 'volB')),
-    ))
+        (('dom', 'img', 'volA'), ('dom', 'img', 'volB'))
+    ])
     def test_less_than(self, a, b):
         b = volume.VolumeLease(HOST_ID, *b)
         a = volume.VolumeLease(HOST_ID, *a)
-        self.assertLess(a, b)
+        assert a < b
 
     def test_equality(self):
         a = volume.VolumeLease(HOST_ID, 'dom', 'img', 'vol')
         b = volume.VolumeLease(HOST_ID, 'dom', 'img', 'vol')
-        self.assertEqual(a, b)
+        assert a == b
 
     def test_equality_different_image(self):
         a = volume.VolumeLease(HOST_ID, 'dom', 'img1', 'vol')
         b = volume.VolumeLease(HOST_ID, 'dom', 'img2', 'vol')
-        self.assertEqual(a, b)
+        assert a == b
 
     def test_equality_different_host_id(self):
         a = volume.VolumeLease(0, 'dom', 'img', 'vol')
         b = volume.VolumeLease(1, 'dom', 'img', 'vol')
-        self.assertEqual(a, b)
+        assert a == b
 
     def test_acquire_release(self):
         sdcache = FakeStorageDomainCache()
@@ -99,22 +97,21 @@ class TestVolumeLease(VdsmTestCase):
         with MonkeyPatchScope([(volume, 'sdCache', sdcache)]):
             lock = volume.VolumeLease(HOST_ID, 'dom', 'img', 'vol')
             lock.acquire()
-            self.assertEqual(expected[:1], manifest.__calls__)
+            assert expected[:1] == manifest.__calls__
             lock.release()
-            self.assertEqual(expected, manifest.__calls__)
+            assert expected == manifest.__calls__
 
     def test_repr(self):
         lock = volume.VolumeLease(HOST_ID, 'dom', 'img', 'vol')
         lock_string = str(lock)
-        self.assertIn("VolumeLease", lock_string)
-        self.assertIn("ns=04_lease_dom", lock_string)
-        self.assertIn("name=vol", lock_string)
-        self.assertIn("mode=exclusive", lock_string)
-        self.assertIn("%x" % id(lock), lock_string)
+        assert "VolumeLease" in lock_string
+        assert "ns=04_lease_dom" in lock_string
+        assert "name=vol" in lock_string
+        assert "mode=exclusive" in lock_string
+        assert "%x" % id(lock) in lock_string
 
 
-@expandPermutations
-class TestVolumeManifest(VdsmTestCase):
+class TestVolumeManifest:
 
     @contextmanager
     def volume(self):
@@ -128,28 +125,28 @@ class TestVolumeManifest(VdsmTestCase):
     def test_operation(self):
         with self.volume() as vol:
             vol.setMetadata = CountedInstanceMethod(vol.setMetadata)
-            self.assertEqual(sc.LEGAL_VOL, vol.getLegality())
+            assert sc.LEGAL_VOL == vol.getLegality()
             with vol.operation():
-                self.assertEqual(sc.ILLEGAL_VOL, vol.getLegality())
-                self.assertEqual(1, vol.setMetadata.nr_calls)
-            self.assertEqual(sc.LEGAL_VOL, vol.getLegality())
-            self.assertEqual(2, vol.setMetadata.nr_calls)
+                assert sc.ILLEGAL_VOL == vol.getLegality()
+                assert 1 == vol.setMetadata.nr_calls
+            assert sc.LEGAL_VOL == vol.getLegality()
+            assert 2 == vol.setMetadata.nr_calls
 
     def test_operation_fail_inside_context(self):
         with self.volume() as vol:
-            self.assertEqual(sc.LEGAL_VOL, vol.getLegality())
-            with self.assertRaises(ValueError):
+            assert sc.LEGAL_VOL == vol.getLegality()
+            with pytest.raises(ValueError):
                 with vol.operation():
                     raise ValueError()
-            self.assertEqual(sc.ILLEGAL_VOL, vol.getLegality())
+            assert sc.ILLEGAL_VOL == vol.getLegality()
 
-    @permutations(((None, 0), (100, 100)))
+    @pytest.mark.parametrize("orig_gen, info_gen", [(None, 0), (100, 100)])
     def test_get_info_generation_id(self, orig_gen, info_gen):
         with self.volume() as vol:
             vol.getLeaseStatus = lambda: {}
             if orig_gen is not None:
                 vol.setMetaParam(sc.GENERATION, orig_gen)
-            self.assertEqual(info_gen, vol.getInfo()['generation'])
+            assert info_gen == vol.getInfo()['generation']
 
     def test_operation_valid_generation(self):
         generation = 100
@@ -157,29 +154,30 @@ class TestVolumeManifest(VdsmTestCase):
             vol.setMetaParam(sc.GENERATION, generation)
             with vol.operation(generation):
                 pass
-            self.assertEqual(generation + 1, vol.getMetaParam(sc.GENERATION))
+            assert generation + 1 == vol.getMetaParam(sc.GENERATION)
 
-    @permutations(((100, 99), (100, 101)))
+    @pytest.mark.parametrize("actual_generation, requested_generation", [
+        (100, 99), (100, 101)
+    ])
     def test_operation_invalid_generation_raises(self, actual_generation,
                                                  requested_generation):
         with self.volume() as vol:
             vol.setMetaParam(sc.GENERATION, actual_generation)
-            with self.assertRaises(se.GenerationMismatch):
+            with pytest.raises(se.GenerationMismatch):
                 with vol.operation(requested_generation):
                     pass
-            self.assertEqual(actual_generation,
-                             vol.getMetaParam(sc.GENERATION))
+            assert actual_generation == vol.getMetaParam(sc.GENERATION)
 
-    @permutations((
+    @pytest.mark.parametrize("first_gen, next_gen", [
         (sc.MAX_GENERATION, 0),
-        (sc.MAX_GENERATION - 1, sc.MAX_GENERATION),
-    ))
+        (sc.MAX_GENERATION - 1, sc.MAX_GENERATION)
+    ])
     def test_generation_wrapping(self, first_gen, next_gen):
         with self.volume() as vol:
             vol.setMetaParam(sc.GENERATION, first_gen)
             with vol.operation(first_gen):
                 pass
-            self.assertEqual(next_gen, vol.getMetaParam(sc.GENERATION))
+            assert next_gen == vol.getMetaParam(sc.GENERATION)
 
     def test_operation_on_illegal_volume(self):
         with self.volume() as vol:
@@ -188,11 +186,11 @@ class TestVolumeManifest(VdsmTestCase):
             vol.setMetaParam(sc.GENERATION, 0)
             with vol.operation(requested_gen=0, set_illegal=False):
                 # It should remain illegal during the operation
-                self.assertEqual(sc.ILLEGAL_VOL, vol.getMetaParam(sc.LEGALITY))
+                assert sc.ILLEGAL_VOL == vol.getMetaParam(sc.LEGALITY)
                 pass
-            self.assertEqual(1, vol.getMetaParam(sc.GENERATION))
+            assert 1 == vol.getMetaParam(sc.GENERATION)
             # It should remain illegal after the operation
-            self.assertEqual(sc.ILLEGAL_VOL, vol.getMetaParam(sc.LEGALITY))
+            assert sc.ILLEGAL_VOL == vol.getMetaParam(sc.LEGALITY)
 
     def test_operation_modifying_metadata(self):
         with self.volume() as vol:
@@ -200,7 +198,7 @@ class TestVolumeManifest(VdsmTestCase):
                 vol.setMetaParam(sc.DESCRIPTION, "description")
             # Metadata changes inside the context should not be overriden by
             # wirting the new generation.
-            self.assertEqual("description", vol.getMetaParam(sc.DESCRIPTION))
+            assert "description" == vol.getMetaParam(sc.DESCRIPTION)
 
 
 class CountedInstanceMethod(object):
