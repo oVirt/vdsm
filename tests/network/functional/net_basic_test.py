50ac1128a3034e8ce9c6f3147eeed143fd9fd69f
Bell Levin <blevin@redhat.com>
2018-11-28 16:20:24 +0200

    net tests : Allow net_basic_test to run with lib
    Introduce a new ability to run the tests with a target flag via
    the command line.
    
    Use one of the following pytest flags to choose the target:
    --target-lib
    --target-service
    
    Change-Id: If34e003c549ff9bb72ec502e1f92a09db3e07858
    Signed-off-by: Bell Levin <blevin@redhat.com>

--- tests/network/functional/net_basic_test.py
+++ tests/network/functional/net_basic_test.py
@@ -45,9 +45,9 @@ adapter = None
 
 
 @pytest.fixture(scope='module', autouse=True)
-def create_adapter():
+def create_adapter(target):
     global adapter
-    adapter = NetFuncTestAdapter()
+    adapter = NetFuncTestAdapter(target)
 
 
 @nftestlib.parametrize_switch
