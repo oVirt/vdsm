daf5b3c3aaa3796b8f9be22fe2059f6f6152a3ce
Nir Soffer <nsoffer@redhat.com>
2018-12-09 16:53:28 +0200

    supervdsm: Add back systemd support
    We need systemd-run to start qemu-nbd as a transient service, and
    systemctl to stop it.
    
    This patch:
    - reverts commit f85f0527f142 (supervdsm: systemd: remove support)
    - adds the capability to start a service as another user/group
    - replaces commands.execCmd() with commands.run()
    
    Change-Id: I8d7d23ac6090036bbcfdc5d6d8f7c99fcff7eba7
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ lib/vdsm/supervdsm_api/systemd.py
@@ -0,0 +1,47 @@
+# Copyright 2016-2018 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+from __future__ import division
+
+from vdsm.common import cmdutils
+from vdsm.common import commands
+
+from . import expose
+
+SYSTEMCTL = "/usr/bin/systemctl"
+
+
+@expose
+def systemd_run(cmd, scope=False, unit=None, slice=None, uid=None, gid=None,
+                accounting=None):
+    cmd = cmdutils.systemd_run(
+        cmd,
+        scope=scope,
+        unit=unit,
+        slice=slice,
+        uid=uid,
+        gid=gid,
+        accounting=accounting)
+    return commands.run(cmd)
+
+
+@expose
+def systemctl_stop(name):
+    return commands.run([SYSTEMCTL, 'stop', name])
