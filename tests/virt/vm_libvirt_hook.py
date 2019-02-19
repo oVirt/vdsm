9f9a0b7ade5bebb0949f45edc9d09447b8f6a4a8
Milan Zamazal <mzamazal@redhat.com>
2019-02-19 16:29:17 +0100

    virt: Add missing seclabel's on migrations
    Vdsm 4.3 enabled dynamic ownership.  It also ensures that appropriate
    seclabel's are added to disks so that libvirt doesn't change their
    permissions.
    
    When a VM is migrated from a host running older Vdsm < 4.3, where
    dynamic ownership is not enabled, the VM domain XML doesn't contain
    the seclabel's.  When the VM is powered off in any way, libvirt
    assumes defaults in such a case and changes the owners of the VM disk
    images to root.  Once VM is attempted to start again, the disk images
    are inaccessible, resulting in an error.
    
    This patch installs a libvirt hook for both host and file migrations
    that adds the missing seclabel's.
    
    Change-Id: Ib77888fc6824a5a49fe4b66c0230a8f989090437
    Bug-Url: https://bugzilla.redhat.com/1666795
    Backport-To: 4.3
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>

---
+++ tests/virt/vm_libvirt_hook.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+
+import six
+
+from vdsm.virt import vm_libvirt_hook
+
+from testlib import XMLTestCase
+
+
+_DISK_XML = '''<domain xmlns:ns0="http://ovirt.org/vm/tune/1.0"
+                       xmlns:ovirt-vm="http://ovirt.org/vm/1.0" type="kvm">
+    <name>test</name>
+    <devices>
+        <channel type="spicevmc">
+            <target name="com.redhat.spice.0" type="virtio" />
+        </channel>
+        <disk device="cdrom" snapshot="no" type="file">
+            <driver error_policy="report" name="qemu" type="raw" />
+            <source file="" startupPolicy="optional"/>
+            <target bus="ide" dev="hdc" />
+            <readonly />
+        </disk>
+        <disk device="disk" snapshot="no" type="file">
+            <target bus="scsi" dev="sda" />
+            <source file="/path/to/file"/>
+        </disk>
+        <disk device="disk" snapshot="no" type="file">
+            <target bus="scsi" dev="sdb" />
+            <source file="/path/to/file">
+                <something>inside</something>
+            </source>
+        </disk>
+        <disk device="disk" snapshot="no" type="file">
+            <target bus="scsi" dev="sdc" />
+            <source file="/path/to/file">
+                <seclabel model="dac" relabel="yes" />
+            </source>
+        </disk>
+        <disk device="disk" snapshot="no" type="network">
+            <source name="poolname/volumename" protocol="rbd">
+                <host name="1.2.3.4" port="6789" transport="tcp"/>
+            </source>
+        </disk>
+        <disk device="disk" type="unknown">
+            <source name="poolname/volumename" protocol="rbd"/>
+        </disk>
+        <disk device="disk" snapshot="no" type="dev">
+            <source file="/path/to/dev">
+                <seclabel model="dac" relabel="no" type="none" />
+            </source>
+        </disk>
+        <interface type="bridge">
+            <model type="virtio" />
+            <link state="up" />
+            <source bridge="ovirtmgmt" />
+        </interface>
+    </devices>
+    <metadata>
+        <ns0:qos />
+        <ovirt-vm:vm>
+            <ovirt-vm:device devtype="disk" name="sda">
+                <ovirt-vm:poolID>120a3ee2-5e8e-11e8-af64-525400dfa5a4</ovirt-vm:poolID>
+                <ovirt-vm:volumeID>439cffc7-551c-4975-a404-72d7e216a115</ovirt-vm:volumeID>
+                <ovirt-vm:imageID>2710a43b-7925-42b5-9f29-afc54e4d6f9c</ovirt-vm:imageID>
+                <ovirt-vm:domainID>b32ebbee-ee25-42c8-90ea-77bc76ecbcb4</ovirt-vm:domainID>
+            </ovirt-vm:device>
+        </ovirt-vm:vm>
+    </metadata>
+</domain>
+'''
+
+_MODIFIED_DISK_XML = '''<domain xmlns:ns0="http://ovirt.org/vm/tune/1.0"
+                           xmlns:ovirt-vm="http://ovirt.org/vm/1.0" type="kvm">
+    <name>test</name>
+    <devices>
+        <channel type="spicevmc">
+            <target name="com.redhat.spice.0" type="virtio" />
+        </channel>
+        <disk device="cdrom" snapshot="no" type="file">
+            <driver error_policy="report" name="qemu" type="raw" />
+            <source file="" startupPolicy="optional">
+                <seclabel model="dac" relabel="no" type="none" />
+            </source>
+            <target bus="ide" dev="hdc" />
+            <readonly />
+        </disk>
+        <disk device="disk" snapshot="no" type="file">
+            <target bus="scsi" dev="sda" />
+            <source file="/path/to/file">
+                <seclabel model="dac" relabel="no" type="none" />
+            </source>
+        </disk>
+        <disk device="disk" snapshot="no" type="file">
+            <target bus="scsi" dev="sdb" />
+            <source file="/path/to/file">
+                <something>inside</something>
+                <seclabel model="dac" relabel="no" type="none" />
+            </source>
+        </disk>
+        <disk device="disk" snapshot="no" type="file">
+            <target bus="scsi" dev="sdc" />
+            <source file="/path/to/file">
+                <seclabel model="dac" relabel="yes" />
+            </source>
+        </disk>
+        <disk device="disk" snapshot="no" type="network">
+            <source name="poolname/volumename" protocol="rbd">
+                <host name="1.2.3.4" port="6789" transport="tcp"/>
+            </source>
+        </disk>
+        <disk device="disk" type="unknown">
+            <source name="poolname/volumename" protocol="rbd"/>
+        </disk>
+        <disk device="disk" snapshot="no" type="dev">
+            <source file="/path/to/dev">
+                <seclabel model="dac" relabel="no" type="none" />
+            </source>
+        </disk>
+        <interface type="bridge">
+            <model type="virtio" />
+            <link state="up" />
+            <source bridge="ovirtmgmt" />
+        </interface>
+    </devices>
+    <metadata>
+        <ns0:qos />
+        <ovirt-vm:vm>
+            <ovirt-vm:device devtype="disk" name="sda">
+                <ovirt-vm:poolID>120a3ee2-5e8e-11e8-af64-525400dfa5a4</ovirt-vm:poolID>
+                <ovirt-vm:volumeID>439cffc7-551c-4975-a404-72d7e216a115</ovirt-vm:volumeID>
+                <ovirt-vm:imageID>2710a43b-7925-42b5-9f29-afc54e4d6f9c</ovirt-vm:imageID>
+                <ovirt-vm:domainID>b32ebbee-ee25-42c8-90ea-77bc76ecbcb4</ovirt-vm:domainID>
+            </ovirt-vm:device>
+        </ovirt-vm:vm>
+    </metadata>
+</domain>
+'''
+
+
+class MigrateHookTestCase(XMLTestCase):
+
+    def _test_hook(self, xml, modified_xml,
+                   domain='foo', event='migrate', phase='begin'):
+        stdin = six.StringIO(xml)
+        stdout = six.StringIO()
+        vm_libvirt_hook.main(domain, event, phase, stdin=stdin, stdout=stdout)
+        self.assertXMLEqual(stdout.getvalue(), modified_xml)
+
+    def test_empty(self):
+        xml = '<domain/>'
+        self._test_hook(xml, xml)
+
+    def test_disks(self):
+        self._test_hook(_DISK_XML, _MODIFIED_DISK_XML)
