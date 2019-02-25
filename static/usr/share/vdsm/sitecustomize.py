f6fa74de7e8a95bece9f8ffce4810dcfcb7dc8e7
Nir Soffer <nsoffer@redhat.com>
2019-02-25 16:11:07 +0200

    pylint: Silence error about sys.setdefaultencoding
    sys.setdefaultencoding is a special function that can be used only from
    sitecustomize.py, and later removed from the module namespace. pylint is
    not aware of this and complains about:
    
        E:  8, 4: Module 'sys' has no 'setdefaultencoding' member; maybe 'getdefaultencoding'? (no-member)
    
    I could not reproduce this locally with the same version of pylint we
    require (1.9.3), but this avoid this failure in the CI.
    
    See https://docs.python.org/2/library/sys.html#sys.setdefaultencoding
    
    Change-Id: Ie23b1ed86573183c03372bf79ec38b7323d5003a
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- static/usr/share/vdsm/sitecustomize.py
+++ static/usr/share/vdsm/sitecustomize.py
@@ -5,7 +5,7 @@ from vdsm.config import config
 
 if sys.version_info[0] == 2:
     # Allow mixing of unicode objects and strings encoded in utf8.
-    sys.setdefaultencoding('utf8')
+    sys.setdefaultencoding('utf8')  # pylint: disable=no-member
 
 if config.getboolean('devel', 'coverage_enable'):
     import coverage  # pylint: disable=import-error
