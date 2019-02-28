45f586949854cfcce4d21943620ba891f0616051
Eyal Shenitzky <eshenitz@redhat.com>
2019-02-28 14:47:57 +0200

    localfssd_test: extract create domain logic tmprepo.py
    extract create domain logic to tmprepo.py to prevent code duplication
    and in order to re-use in different tests.
    
    Change-Id: I87d545ba9b931a1449fefdf1495e7f4bd77f18f1
    Signed-off-by: Eyal Shenitzky <eshenitz@redhat.com>

--- tests/storage/tmprepo.py
+++ tests/storage/tmprepo.py
@@ -21,9 +21,13 @@ from __future__ import absolute_import
 from __future__ import division
 
 import os
+import uuid
 
 from vdsm.storage import constants as sc
 from vdsm.storage import fileUtils
+from vdsm.storage import localFsSD
+from vdsm.storage import sd
+from vdsm.storage.sdc import sdCache
 
 
 class TemporaryRepo(object):
@@ -31,11 +35,14 @@ class TemporaryRepo(object):
     Temporary storage repository replacing /rhev/data-center during tests.
     """
 
-    def __init__(self, path, pool_id):
-        self.path = path
-        self.pool_id = pool_id
-        self.pool_dir = os.path.join(path, pool_id)
-        self.mnt_dir = os.path.join(path, sc.DOMAIN_MNT_POINT)
+    def __init__(self, tmpdir):
+        self.tmpdir = tmpdir
+        # Create rhev/data-center directory in the tmpdir, so we don't mix
+        # temporary files created by the same test in the data-center.
+        self.path = str(tmpdir.mkdir("rhev").mkdir("data-center"))
+        self.pool_id = str(uuid.uuid4())
+        self.pool_dir = os.path.join(self.path, self.pool_id)
+        self.mnt_dir = os.path.join(self.path, sc.DOMAIN_MNT_POINT)
 
         # TODO: Should we create pool_dir now?
         os.makedirs(self.mnt_dir)
@@ -55,3 +62,31 @@ class TemporaryRepo(object):
         local_path = fileUtils.transformPath(remote_path)
         dom_link = os.path.join(self.mnt_dir, local_path)
         os.remove(dom_link)
+
+    def create_localfs_domain(self, name, version):
+        """
+        Create local FS file storage domain in the repository
+        """
+        remote_path = str(self.tmpdir.mkdir(name))
+        self.connect_localfs(remote_path)
+
+        sd_uuid = str(uuid.uuid4())
+
+        dom = localFsSD.LocalFsStorageDomain.create(
+            sdUUID=sd_uuid,
+            domainName=name,
+            domClass=sd.DATA_DOMAIN,
+            remotePath=remote_path,
+            version=version,
+            storageType=sd.LOCALFS_DOMAIN,
+            block_size=sc.BLOCK_SIZE_512,
+            alignment=sc.ALIGNMENT_1M)
+
+        sdCache.knownSDs[sd_uuid] = localFsSD.findDomain
+        sdCache.manuallyAddDomain(dom)
+
+        # sd.StorageDomainManifest.getRepoPath() assumes at least one pool is
+        # attached
+        dom.attach(self.pool_id)
+
+        return dom
