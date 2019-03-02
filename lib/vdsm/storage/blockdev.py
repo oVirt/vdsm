a8a242bd01bd77689250ec790e470031a9084853
Nir Soffer <nsoffer@redhat.com>
2019-03-03 01:18:07 +0200

    blockdev: Always use blkdiscard
    Zeroing with dd is horribly inefficient, and we are pretty confident now
    with blkdicard, used since 4.2. Remove the dd zero method and
    configuration, and unneeded tests related to dd.
    
    Change-Id: I94d46de92bb346a5013a080f2843a8226d6b0949
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- lib/vdsm/storage/blockdev.py
+++ lib/vdsm/storage/blockdev.py
@@ -28,22 +28,15 @@ import logging
 
 from contextlib import contextmanager
 
-from vdsm import constants
 from vdsm import utils
 from vdsm.common import cmdutils
-from vdsm.common import exception
-from vdsm.config import config
 
 from vdsm.storage import blkdiscard
 from vdsm.storage import fsutils
-from vdsm.storage import operation
 from vdsm.storage import constants as sc
 from vdsm.storage import exception as se
 
 log = logging.getLogger("storage.blockdev")
-
-MIN_BLOCK_SIZE_MB = 1
-MAX_BLOCK_SIZE_MB = 64
 
 
 class _NullTask(object):
@@ -84,74 +77,13 @@ def zero(device_path, size=None, task=_NullTask()):
     log.info("Zeroing device %s (size=%d)", device_path, size)
     with utils.stopwatch("Zero device %s" % device_path,
                          level=logging.INFO, log=log):
-        zero_method = config.get('irs', 'zero_method')
         try:
-            if zero_method == "blkdiscard":
-                _zero_blkdiscard(device_path, size, task)
-            elif zero_method == "dd":
-                _zero_dd(device_path, size, task)
-            else:
-                raise exception.InvalidConfiguration(
-                    reason="Unsupported value for irs:zero_method",
-                    zero_method=zero_method)
+            op = blkdiscard.zeroout_operation(device_path, size)
+            with task.abort_callback(op.abort):
+                op.run()
         except se.StorageException as e:
             raise se.VolumesZeroingError("Zeroing device %s failed: %s"
                                          % (device_path, e))
-
-
-def _zero_blkdiscard(device_path, size, task):
-    op = blkdiscard.zeroout_operation(device_path, size)
-    with task.abort_callback(op.abort):
-        op.run()
-
-
-def _zero_dd(device_path, size, task):
-    # Write optimal size blocks. Images are always aligned to
-    # optimal size blocks, so we typically have only one call.
-    block_size = zero_block_size()
-    blocks = size // block_size
-    if blocks > 0:
-        _run_dd(device_path, 0, block_size, blocks, task)
-
-    # When zeroing special volumes size may not be aligned to
-    # optimal block size, so we need to write the last block.
-    rest = size % block_size
-    if rest > 0:
-        offset = blocks * block_size
-        _run_dd(device_path, offset, rest, 1, task)
-
-
-def _run_dd(path, offset, block_size, count, task):
-    op = operation.Command([
-        constants.EXT_DD,
-        "if=/dev/zero",
-        "of=%s" % path,
-        "bs=%d" % block_size,
-        "count=%d" % count,
-        "seek=%d" % offset,
-        "oflag=direct,seek_bytes",
-        "conv=notrunc,fsync",
-    ])
-    with task.abort_callback(op.abort):
-        op.run()
-
-
-def zero_block_size():
-    value = config.get('irs', 'zero_block_size_mb')
-    try:
-        value = int(value)
-    except ValueError:
-        raise exception.InvalidConfiguration(
-            reason="Unsupported value for irs:zero_block_size_mb",
-            zero_block_size_mb=value)
-    if value < MIN_BLOCK_SIZE_MB or value > MAX_BLOCK_SIZE_MB:
-        raise exception.InvalidConfiguration(
-            reason="Out of range value for irs:zero_block_size_mb",
-            zero_block_size_mb=value,
-            min=MIN_BLOCK_SIZE_MB,
-            max=MAX_BLOCK_SIZE_MB)
-
-    return value * constants.MEGAB
 
 
 def discard(device_path):
--- tests/storage/blockdev_test.py
+++ tests/storage/blockdev_test.py
@@ -28,129 +28,137 @@ from contextlib import contextmanager
 
 import pytest
 
-from testlib import make_config
-from testlib import namedTemporaryDir
-import loopback
-
-from vdsm import constants
 from vdsm.common import exception
 from vdsm.storage import blockdev
 from vdsm.storage import constants as sc
+from vdsm.storage import directio
 from vdsm.storage import exception as se
 
-DEFAULT_BLOCK_SIZE = blockdev.zero_block_size()
-FILE_SIZE = 2 * DEFAULT_BLOCK_SIZE
+import loopback
+
+# Zeroing and discarding block device is instant, so we can use real lv size.
+FILE_SIZE = 128 * 1024**2
+
+# Read and write 128k chunks to keep the tests fast.
+DATA = b"x" * 128 * 1024
+ZERO = b"\0" * 128 * 1024
 
 
 @pytest.fixture
 def loop_device(tmpdir):
     backing_file = str(tmpdir.join("backing_file"))
     with io.open(backing_file, "wb") as f:
-        f.write(b"x" * FILE_SIZE)
+        f.truncate(FILE_SIZE)
     with loopback.Device(backing_file) as loop_device:
         yield loop_device
-
-
-@pytest.fixture(params=["dd", "blkdiscard"])
-def zero_method(request, monkeypatch):
-    cfg = make_config([('irs', 'zero_method', request.param)])
-    monkeypatch.setattr(blockdev, "config", cfg)
-    return request.param
 
 
 class TestZero:
 
     @pytest.mark.skipif(os.geteuid() != 0, reason="requires root")
-    def test_entire_device(self, loop_device, zero_method):
+    def test_entire_device(self, loop_device):
+        # Write some data to the device.
+        with directio.DirectFile(loop_device.path, "r+") as f:
+            f.write(DATA)
+            f.seek(FILE_SIZE - len(DATA))
+            f.write(DATA)
+
         # Zero the entire device
         blockdev.zero(loop_device.path)
-        # Check that it contains zeros
-        with io.open(loop_device.backing_file, "rb") as f:
-            data = f.read()
-            assert data == b"\0" * FILE_SIZE
+
+        # Verify that parts with data were zeroed.
+        with directio.DirectFile(loop_device.path, "r") as f:
+            data = f.read(len(ZERO))
+            assert data == ZERO
+            f.seek(FILE_SIZE - len(ZERO))
+            data = f.read(len(ZERO))
+            assert data == ZERO
 
     @pytest.mark.skipif(os.geteuid() != 0, reason="requires root")
-    def test_size(self, loop_device, zero_method):
-        # Zero the first DEFAULT_BLOCK_SIZE
-        blockdev.zero(loop_device.path, size=DEFAULT_BLOCK_SIZE)
-        with io.open(loop_device.backing_file, "rb") as f:
-            # Verify that the first DEFAULT_BLOCK_SIZE is zeroed
-            data = f.read(DEFAULT_BLOCK_SIZE)
-            assert data == b"\0" * DEFAULT_BLOCK_SIZE
-            # And the rest was not modified
-            data = f.read(DEFAULT_BLOCK_SIZE)
-            assert data == b"x" * DEFAULT_BLOCK_SIZE
+    def test_size(self, loop_device):
+        # Write some data to the device.
+        with directio.DirectFile(loop_device.path, "r+") as f:
+            f.write(DATA * 2)
+
+        # Zero bytes at the start of the device.
+        blockdev.zero(loop_device.path, size=len(ZERO))
+
+        # Verify that the area was zeroed.
+        with directio.DirectFile(loop_device.path, "r") as f:
+            data = f.read(len(ZERO))
+            assert data == ZERO
+            data = f.read(len(DATA))
+            assert data == DATA
 
     @pytest.mark.skipif(os.geteuid() != 0, reason="requires root")
-    @pytest.mark.parametrize("size", [
-        (sc.BLOCK_SIZE),
-        (DEFAULT_BLOCK_SIZE - sc.BLOCK_SIZE),
-        (DEFAULT_BLOCK_SIZE + sc.BLOCK_SIZE),
-    ])
-    def test_special_volumes(self, size, loop_device, zero_method):
+    @pytest.mark.parametrize("size", [sc.BLOCK_SIZE, 250 * 4096])
+    def test_special_volumes(self, size, loop_device):
+        # Write some data to the device.
+        with directio.DirectFile(loop_device.path, "r+") as f:
+            f.write(b"x" * size * 2)
+
         # Zero size bytes
         blockdev.zero(loop_device.path, size=size)
-        with io.open(loop_device.backing_file, "rb") as f:
-            # Verify that size bytes were zeroed
+
+        # Verify that size bytes were zeroed and the rest not modified.
+        with directio.DirectFile(loop_device.path, "r") as f:
             data = f.read(size)
             assert data == b"\0" * size
+            data = f.read(size)
+            assert data == b"x" * size
 
     def test_abort(self, tmpdir):
         # Prepare device poisoned with "x"
         path = str(tmpdir.join("file"))
         with io.open(path, "wb") as f:
             f.write(b"x" * FILE_SIZE)
+
         # Create a task that will be aborted immediately
         task = AbortingTask()
+
         # The operation should be stopped
         with pytest.raises(exception.ActionStopped):
             blockdev.zero(path, size=FILE_SIZE, task=task)
+
         # And the file should not be zeroed
         with io.open(path, "rb") as f:
             data = f.read(FILE_SIZE)
             assert data == b"x" * FILE_SIZE
 
-    @pytest.mark.parametrize("size", [
-        (FILE_SIZE - 1),
-        (FILE_SIZE + 1),
-    ])
+    @pytest.mark.parametrize("size", [1024**2 - 1, 1024**2 + 1])
     def test_unaligned_size(self, size):
         with pytest.raises(se.InvalidParameterException):
             blockdev.zero("/no/such/path", size=size)
 
-    @pytest.mark.parametrize("block_size_mb", ["1", "30", "64"])
-    def test_zero_block_size_valid(self, block_size_mb, monkeypatch):
-        cfg = make_config([('irs', 'zero_block_size_mb', block_size_mb)])
-        monkeypatch.setattr(blockdev, "config", cfg)
-        block_size = int(block_size_mb) * constants.MEGAB
-        assert blockdev.zero_block_size() == block_size
-
-    @pytest.mark.parametrize("block_size_mb", ["0", "5.5", "65", "str", ""])
-    def test_zero_block_size_invalid(self, block_size_mb, monkeypatch):
-        cfg = make_config([('irs', 'zero_block_size_mb', block_size_mb)])
-        monkeypatch.setattr(blockdev, "config", cfg)
-        with pytest.raises(exception.InvalidConfiguration):
-            blockdev.zero_block_size()
-
 
 class TestDiscard:
 
-    def test_not_supported(self):
-        with namedTemporaryDir() as tmpdir:
-            # Prepare file poisoned with "x"
-            path = os.path.join(tmpdir, "backing_file")
-            with io.open(path, "wb") as f:
-                f.write(b"x" * FILE_SIZE)
-            # And discard it - this will fail silently.
-            blockdev.discard(path)
-            # File should not be modified
-            with io.open(path, "rb") as f:
-                data = f.read(FILE_SIZE)
-                assert data == b"x" * FILE_SIZE
+    def test_not_supported(self, tmpdir):
+        # Prepare file poisoned with "x"
+        path = str(tmpdir.join("backing_file"))
+        with io.open(path, "wb") as f:
+            f.write(DATA)
+
+        # And discard it - this will fail silently.
+        blockdev.discard(path)
+
+        # File should not be modified
+        with io.open(path, "rb") as f:
+            data = f.read(len(DATA))
+            assert data == DATA
 
     @pytest.mark.skipif(os.geteuid() != 0, reason="requires root")
     def test_supported(self, loop_device):
+        # Write some data to the device.
+        with directio.DirectFile(loop_device.path, "r+") as f:
+            f.write(DATA)
+            f.seek(FILE_SIZE - len(DATA))
+            f.write(DATA)
+
+        # Discard entire device.
         blockdev.discard(loop_device.path)
+
+        # Check that all data was discarded.
         stat = os.stat(loop_device.backing_file)
         assert stat.st_blocks == 0
 
