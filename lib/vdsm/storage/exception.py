oVirt/vdsm

commit 5ec4428c4d2e122ba20539925f18ed121d354da0

upgrade: Skip volumes with cleared metadata
vdsm version before 4.20.34-1 (ovirt 4.2.5) had a very old bug when
volume metadata was cleared by replacing the metadata with:

NONE=#####################################################...

after we failed to remove a LV. The result is a volume with invalid
metadata. Trying to access the volume metadata will fail with this
error:

    MetaDataKeyNotFoundError: Meta Data key not found error: ("Missing
    metadata key: 'SIZE': found: {'NONE': '###################...

This bug was fixed in block storage in commit 7bbb4b867249 (blockVolume:
Remove metadata only if the LV successfully removed). But storage
domains used with older versions may have such partly-deleted volumes.

When converting storage domain to V5, reading the metadata fail, and the
entire conversion is aborted. The next SPM trying to upgrade this domain
will fail in the same way, bringing the entire data center down.

To fix this issue, we detect now cleared metadata when parsing volume
metadata, and raise a new MetadataCleared exception. The code converting
volume metadata was updated to skip these volumes since we don't have
any metadata to convert.

With this change converting to V5 will succeed, leaving the
partly-deleted volumes on storage. We may need to consider deleting
these volumes in a future version.

A new warning is logged when detecting volumes with this issue:

    WARN Skipping volume 25869545-0dca-464e-91f1-6b7b6e332352 with
    cleared metadata, volume is partly deleted

Finding such volume is very unlikely, so the simplest solution of one
log per volume was chosen.

Change-Id: I789f358460cfa3fe32097e958a8261e75a5f9621
Relates-To: https://bugzilla.redhat.com/1574631
Bug-Url: https://bugzilla.redhat.com/1713724
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

lib/vdsm/storage/exception.py
@@ -1776,6 +1776,11 @@ class MetadataOverflowError(MetaDataGeneralError):
         self.value = "data=%r" % data
 
 
+class MetadataCleared(MetaDataGeneralError):
+    code = 757
+    message = "Metadata was cleared, volume is partly deleted"
+
+
 #################################################
 #  Import/Export Exceptions
 #################################################

lib/vdsm/storage/blockSD.py
@@ -1731,7 +1731,15 @@ class BlockStorageDomain(sd.StorageDomain):
                                    slot, v4_off)
                     v4_data = src[v4_off:v4_off + sc.METADATA_SIZE]
                     v4_data = v4_data.rstrip(b"\0")
-                    md = VolumeMetadata.from_lines(v4_data.splitlines())
+
+                    try:
+                        md = VolumeMetadata.from_lines(v4_data.splitlines())
+                    except se.MetadataCleared:
+                        self.log.warning(
+                            "Skipping metadata slot %s offset=%s with "
+                            "cleared metadata, volume is partly deleted",
+                            slot, v4_off)
+                        continue
 
                     v5_off = self._manifest.metadata_offset(slot, version=5)
 

lib/vdsm/storage/fileSD.py
@@ -812,7 +812,14 @@ class FileStorageDomain(sd.StorageDomain):
 
         for vol in self.iter_volumes():
             self.log.debug("Converting volume %s metadata", vol.volUUID)
-            vol_md = vol.getMetadata()
+            try:
+                vol_md = vol.getMetadata()
+            except se.MetadataCleared:
+                self.log.warning(
+                    "Skipping volume %s with cleared metadata, volume is "
+                    "partly deleted",
+                    vol.volUUID)
+                continue
             vol.setMetadata(vol_md, CAP=vol_md.capacity)
 
     def finalize_volumes_metadata(self, target_version):
@@ -829,7 +836,11 @@ class FileStorageDomain(sd.StorageDomain):
 
         for vol in self.iter_volumes():
             self.log.debug("Finalizing volume %s metadata", vol.volUUID)
-            vol_md = vol.getMetadata()
+            try:
+                vol_md = vol.getMetadata()
+            except se.MetadataCleared:
+                # We already logged about this in convert_volume_metadata().
+                continue
             vol.setMetadata(vol_md)
 
 

lib/vdsm/storage/volumemetadata.py
@@ -100,6 +100,13 @@ class VolumeMetadata(object):
                        generation=int(md.get(sc.GENERATION,
                                              sc.DEFAULT_GENERATION)))
         except KeyError as e:
+            if "NONE" in md or not md:
+                # Volume metadata was cleared; {} for V5, {"NONE": "####..."}
+                # for older versions. Before 4.20.34-1 (ovirt 4.2.5) volume
+                # metadata could be cleared after failing to remove the LV.
+                # See https://bugzilla.redhat.com/1574631.
+                raise exception.MetadataCleared
+
             raise exception.MetaDataKeyNotFoundError(
                 "Missing metadata key: %s: found: %s" % (e, md))
 

commit fabb92fd99cd0e0035e4a51b50209b3efbe6d8a3

storage.exception: Fix cleared metadata regressions
In commit 5ec4428c4d2e (upgrade: Skip volumes with cleared metadata) we
added a new specific exception for the special case of cleared metadata.
This broke error handlers expecting MetaDataKeyNotFoundError:

- volume.Volume.getLeagality(), volume.Volume.isLeagal(), and
  volume.Volume.isFake() pretend that volume with invalid metadata is
  legal. This looks very wrong but we don't know if the new behaviour is
  safe.

- volume.Volume.validateDelate() allows deleting of a volume with
  missing key, assuming that the reason for the missing key is
  partly-deleted volume. Now it disallow deleting partly deleted volume.

- formatconveter.v3DomainConverter() was skipping volume size fix if
  metadata key is missing. Now it will abort conversion if a volume with
  cleared metadata is found.

This change restores the original behaviour, handling both missing key
and cleared metadata in these cases.

Change-Id: I8a79475b5f975ac7bcad08236420a09f2a59496a
Bug-Url: https://bugzilla.redhat.com/1713724
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

lib/vdsm/storage/exception.py
@@ -1776,7 +1776,7 @@ class MetadataOverflowError(MetaDataGeneralError):
         self.value = "data=%r" % data
 
 
-class MetadataCleared(MetaDataGeneralError):
+class MetadataCleared(MetaDataKeyNotFoundError):
     code = 757
     message = "Metadata was cleared, volume is partly deleted"
 