3b251ecd50ab844a569f425034cb53f5a1b2df7d
Milan Zamazal <mzamazal@redhat.com>
2019-06-26 13:53:18 +0200

    hooks: Remove `floppy' hook
    Floppy won't be supported in 4.4 generally.
    
    Change-Id: If9f070cbb91a75109b535826b09c8da0ae6868ab
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>

--- vdsm_hooks/floppy/before_vm_migrate_source.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python2
-
-import os
-import sys
-
-if 'floppy' in os.environ:
-    sys.stderr.write('floppy before_vm_migrate_source: cannot migrate VM with '
-                     'floppy hook\n')
-    sys.exit(2)
