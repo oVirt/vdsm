eca753919b9bbabc27100928f83132bd8495df89
Marcin Sobczyk <msobczyk@redhat.com>
2019-06-26 08:29:03 +0200

    rpc: Added 'HttpDetector' tests
    Adding tests for 'HttpDetector.detect' method.
    
    Change-Id: If67cbb5674c822c0639630a923076dcc3a4c668c
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

---
+++ tests/lib/rpc/http_detector_test.py
@@ -0,0 +1,51 @@
+#
+# Copyright 2019 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+
+import pytest
+import six
+
+from vdsm.rpc.http import HttpDetector
+from yajsonrpc import stomp
+
+
+@pytest.mark.parametrize("data", [
+    b"PUT /",
+    b"GET /",
+])
+@pytest.mark.skipif(six.PY3, reason="needs porting to py3")
+def test_http_detector_should_detect_http_protocol(data):
+    assert HttpDetector(server=None).detect(data)
+
+
+@pytest.mark.parametrize("data", [
+    b"smth",
+    b"\x23\x54"
+])
+@pytest.mark.skipif(six.PY3, reason="needs porting to py3")
+def test_http_detector_should_reject_non_http_protocol(data):
+    assert not HttpDetector(server=None).detect(data)
+
+
+@pytest.mark.parametrize("data", [c.encode("utf-8") for c in stomp.COMMANDS])
+@pytest.mark.skipif(six.PY3, reason="needs porting to py3")
+def test_http_detector_should_reject_stomp_commands(data):
+    assert not HttpDetector(server=None).detect(data)
