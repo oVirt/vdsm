0f28eafa3bffc4d22a7f86ea3e5cc52b655b2bb9
Marcin Sobczyk <msobczyk@redhat.com>
2019-06-26 09:54:41 +0200

    stomp: Add 'StompDetector' tests
    This patch adds tests for 'StompDetector.detect' method.
    
    Change-Id: Ifbf6e744fe80c58a64083bd0c889a1ffd7a46af0
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

---
+++ tests/lib/yajsonrpc/stomp_detector_test.py
@@ -0,0 +1,56 @@
+#
+# Copyright 2019 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+
+import pytest
+import six
+
+from yajsonrpc import stomp
+from yajsonrpc.stompserver import StompDetector
+
+
+class Dummy(object):
+    def __init__(self):
+        self.reactor = None
+
+
+@pytest.mark.parametrize("data", [c.encode("utf-8") for c in stomp.COMMANDS])
+@pytest.mark.skipif(six.PY3, reason="needs porting to py3")
+def test_stomp_detector_should_detect_stomp_protocol(data):
+    assert StompDetector(json_binding=Dummy()).detect(data)
+
+
+@pytest.mark.parametrize("data", [
+    b"smth",
+    b"\x23\x54",
+])
+@pytest.mark.skipif(six.PY3, reason="needs porting to py3")
+def test_stomp_detector_should_reject_garbage(data):
+    assert not StompDetector(json_binding=Dummy()).detect(data)
+
+
+@pytest.mark.parametrize("data", [
+    b"GET /",
+    b"PUT /",
+])
+@pytest.mark.skipif(six.PY3, reason="needs porting to py3")
+def test_stomp_detector_should_reject_http_verbs(data):
+    assert not StompDetector(json_binding=Dummy()).detect(data)
