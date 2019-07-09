51c65653fc73b64fd6c09c33252386af160068a4
Vojtech Juranek <vjuranek@redhat.com>
2019-07-09 13:22:45 +0200

    storage: accept bytes in Volume.extend()
    Method Volume.extend() now accepts new size in bytes.
    
    Change-Id: I115db322db01a7f21798b5ac84a39f517717391c
    Bug-Url: https://bugzilla.redhat.com/1592916
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- lib/vdsm/storage/image.py
+++ lib/vdsm/storage/image.py
@@ -395,9 +395,7 @@ class Image:
                                                    volUUID=srcVol.volUUID)
 
                     # Extend volume (for LV only) size to the actual size
-                    size_aligned = utils.round(
-                        volParams['apparentsize'], sc.BLOCK_SIZE_512)
-                    dstVol.extend(size_aligned // sc.BLOCK_SIZE_512)
+                    dstVol.extend(volParams['apparentsize'])
 
                     # Change destination volume metadata to preallocated in
                     # case we've used a sparse volume to accelerate the
@@ -595,14 +593,14 @@ class Image:
                 # tmpVolume needs to be as large as the virtual disk size for
                 # the worst case.
                 # TODO: Some extra space may be needed for QCOW2 headers
-                tmpVolume.extend(tmpVolume.getCapacity() // sc.BLOCK_SIZE_512)
+                tmpVolume.extend(tmpVolume.getCapacity())
                 # For the dstVolume we may think of an optimization where the
                 # extension is as large as the source (and at the end we
                 # shrinkToOptimalSize).
                 # TODO: Extend the dstVolume only as much as the actual size of
                 # srcVolume
                 # TODO: Some extra space may be needed for QCOW2 headers
-                dstVolume.extend(tmpVolume.getCapacity() // sc.BLOCK_SIZE_512)
+                dstVolume.extend(tmpVolume.getCapacity())
 
                 srcFormat = sc.fmt2str(srcVolume.getFormat())
                 dstFormat = sc.fmt2str(dstVolume.getFormat())
@@ -1019,7 +1017,7 @@ class Image:
         srcVol = sdDom.produceVolume(imgUUID=srcVolParams['imgUUID'],
                                      volUUID=srcVolParams['volUUID'])
         # Extend successor volume to new accumulated subchain size
-        srcVol.extend(new_size_blk)
+        srcVol.extend(new_size_blk * sc.BLOCK_SIZE_512)
 
         srcVol.prepare(rw=True, chainrw=True, setrw=True)
         try:
@@ -1055,7 +1053,7 @@ class Image:
         srcVol = sdDom.produceVolume(imgUUID=srcVolParams['imgUUID'],
                                      volUUID=srcVolParams['volUUID'])
         # Extend successor volume to new accumulated subchain size
-        srcVol.extend(new_size_blk)
+        srcVol.extend(new_size_blk * sc.BLOCK_SIZE_512)
         # Step 1: Create temporary volume with destination volume's parent
         #         parameters
         newUUID = str(uuid.uuid4())
@@ -1430,8 +1428,7 @@ class Image:
         vol = self._activateVolumeForImportExport(domain, imgUUID, volUUID)
         try:
             # Extend the volume (if relevant) to the image size
-            img_size_blk = imageSharing.getSize(methodArgs) / sc.BLOCK_SIZE
-            vol.extend(img_size_blk)
+            vol.extend(imageSharing.getSize(methodArgs))
             imageSharing.download(vol.getVolumePath(), methodArgs)
         finally:
             domain.deactivateImage(imgUUID)
@@ -1451,9 +1448,7 @@ class Image:
         vol = self._activateVolumeForImportExport(domain, imgUUID, volUUID)
         try:
             # Extend the volume (if relevant) to the image size
-            img_size_blk = (imageSharing.getLengthFromArgs(methodArgs) /
-                            sc.BLOCK_SIZE)
-            vol.extend(img_size_blk)
+            vol.extend(imageSharing.getLengthFromArgs(methodArgs))
             imageSharing.copyToImage(vol.getVolumePath(), methodArgs)
         finally:
             domain.deactivateImage(imgUUID)
