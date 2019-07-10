0f71d2839ed1ccd6092d09039ab8528f17c99bd4
Nir Soffer <nsoffer@redhat.com>
2019-07-10 21:28:29 +0300

    tests: Mark test_event as broken on travis
    The tests was marked as broken on oVirt CI for ages, and recently
    started to fail also in Travis. Mark the tests as broken on Travis until
    we have time to investigate.
    
    Change-Id: I6ed3687b962729d24f74addd75a6db95ca7d0c11
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- tests/stomp_test.py
+++ tests/stomp_test.py
@@ -100,8 +100,9 @@ class StompTests(TestCaseBase):
                                                    str(uuid4())),
                                  data)
 
-    @broken_on_ci(
-        "Fails randomly in oVirt CI, see https://gerrit.ovirt.org/c/95899/")
+    # See https://gerrit.ovirt.org/c/95899 for experimental fix.
+    @broken_on_ci("Fails randomly in oVirt CI", name="OVIRT_CI")
+    @broken_on_ci("Fails consistently in Travis CI", name="TRAVIS_CI")
     @permutations(_USE_SSL)
     def test_event(self, use_ssl):
         with constructAcceptor(self.log, use_ssl, _SampleBridge(),
