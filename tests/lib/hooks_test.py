0a7cad28e6e3334d3f05af3a804497bbb6d7c037
Marcin Sobczyk <msobczyk@redhat.com>
2019-07-16 16:09:33 +0200

    test: Move 'hooks._runHooksDir' tests to 'pytest'
    In an effort to move away from homegrown 'testlib' and deprecated
    'nose' testing framework, we're rewriting our tests to 'pytest'
    and running them with 'tox'.
    
    This patch replaces the test from 'TestHooks' suite that validates
    the 'hooks._runHooksDir' function with more comprehensive test set.
    
    Some of the new tests are marked with 'xfail'. There are
    two reasons behind that - py3-incompatibilities and undesired
    behaviors of the function exposed by the tests:
    
    - passing unicode strings to 'os.write'
    - py3-incompatible usage of 'pkgutil'
    - error messages from failing scripts are lost
    - improper handling of invalid utf-8 sequences
    - usage of deprecated 'execCmd' function
    
    Fixes for these issues will be introduced in next patches.
    
    Change-Id: Ia843b3b6fa869caf1de303c4c4abd9b9e43b5dc6
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- tests/lib/hooks_test.py
+++ tests/lib/hooks_test.py
@@ -1,5 +1,3 @@
-# encoding: utf-8
-#
 # Copyright 2012-2019 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -23,20 +21,24 @@
 from __future__ import absolute_import
 from __future__ import division
 
-import contextlib
+import itertools
 import libvirt
+import logging
 import tempfile
+import textwrap
 import os
 import os.path
+import pickle
 import pytest
 import six
+import sys
 
 from collections import namedtuple
-from contextlib import contextmanager
 from monkeypatch import MonkeyPatchScope
 from testlib import VdsmTestCase as TestCaseBase
 from testlib import namedTemporaryDir
 
+from vdsm.common import exception
 from vdsm.common import hooks
 
 
@@ -72,7 +74,8 @@ def fake_hooks_root(monkeypatch, tmpdir):
 @pytest.fixture
 def hooks_dir(fake_hooks_root, request):
     hooks_dir = fake_hooks_root.mkdir("hooks_dir")
-    for entry in request.param:
+    entries = getattr(request, 'param', [])
+    for entry in entries:
         entry.apply(hooks_dir)
     yield hooks_dir
 
@@ -161,42 +164,322 @@ def test_scripts_per_dir_should_accept_root_without_trailing_slash(monkeypatch,
         assert len(scripts) == 1
 
 
+def test_rhd_should_return_unmodified_data_when_no_hooks(hooks_dir):
+    assert hooks._runHooksDir(u"algo", hooks_dir.basename) == u"algo"
+
+
+@pytest.fixture
+def dummy_hook(hooks_dir):
+    FileEntry("hook.sh", 0o755, "#!/bin/bash").apply(hooks_dir)
+    yield
+
+
+@pytest.mark.parametrize("data, expected", [
+    pytest.param(
+        None,
+        u"",
+        id="no XML data"
+    ),
+    pytest.param(
+        u"",
+        u"",
+        id="empty XML data"
+    ),
+    pytest.param(
+        u"<abc>def</abc>",
+        u"<abc>def</abc>",
+        id="simple XML data"
+    ),
+])
+@pytest.mark.xfail(six.PY3, reason="'str' passed to 'os.write'")
+def test_rhd_should_handle_xml_data(dummy_hook, hooks_dir, data, expected):
+    result = hooks._runHooksDir(data, hooks_dir.basename,
+                                hookType=hooks._DOMXML_HOOK)
+    assert result == expected
+
+
+@pytest.mark.parametrize("data, expected", [
+    pytest.param(
+        None,
+        None,
+        id="no JSON data"
+    ),
+    pytest.param(
+        {"abc": "def"},
+        {"abc": "def"},
+        id="simple JSON data"
+    ),
+    pytest.param(
+        {"key": b"\xc4\x85b\xc4\x87".decode("utf-8")},
+        {"key": b"\xc4\x85b\xc4\x87".decode("utf-8")},
+        id="JSON data with localized chars"
+    ),
+])
+@pytest.mark.xfail(six.PY3, reason="need to encode JSON for 'os.write'")
+def test_rhd_should_handle_json_data(dummy_hook, hooks_dir, data, expected):
+    result = hooks._runHooksDir(data, hooks_dir.basename,
+                                hookType=hooks._JSON_HOOK)
+    assert result == expected
+
+
+def appender_script(script_name, exit_code=0):
+    code = textwrap.dedent(
+        """\
+        #!/bin/bash
+        myname="$(basename "$0")"
+        echo "$myname" >> "$_hook_domxml"
+        >&2 echo "$myname"
+        exit {exit_code}
+        """.format(exit_code=exit_code))
+    return FileEntry(script_name, 0o777, code)
+
+
+@pytest.mark.parametrize("hooks_dir", indirect=True, argvalues=[
+    pytest.param(
+        [
+            appender_script("myhook.sh"),
+        ],
+        id="single hook"
+    ),
+])
+@pytest.mark.xfail(six.PY3, reason="'str' passed to 'os.write'")
+def test_rhd_should_run_a_hook(hooks_dir):
+    assert hooks._runHooksDir(u"123", hooks_dir.basename) == u"123myhook.sh\n"
+
+
+@pytest.mark.parametrize("hooks_dir", indirect=True, argvalues=[
+    pytest.param(
+        perm,
+        id="-".join(script.name for script in perm)
+    ) for perm in itertools.permutations([
+        appender_script("1.sh"),
+        appender_script("2.sh"),
+        appender_script("3.sh")
+    ])
+])
+@pytest.mark.xfail(six.PY3, reason="'str' passed to 'os.write'")
+def test_rhd_should_run_hooks_in_order(hooks_dir):
+    assert hooks._runHooksDir(u"", hooks_dir.basename) == u"1.sh\n2.sh\n3.sh\n"
+
+
+@pytest.mark.parametrize("hooks_dir,error", indirect=["hooks_dir"], argvalues=[
+    pytest.param(
+        [
+            appender_script("1.sh"),
+            appender_script("2.sh", exit_code=1),
+            appender_script("3.sh", exit_code=1)
+        ],
+        ["2.sh", "3.sh"],
+        id="non-fatal hook errors",
+        marks=pytest.mark.xfail(reason="doesn't include all 'err's")
+    ),
+    pytest.param(
+        [
+            appender_script("1.sh"),
+            appender_script("2.sh", exit_code=2),
+            appender_script("3.sh")
+        ],
+        ["2.sh"],
+        id="fatal hook error, '3.sh' skipped"
+    ),
+])
+@pytest.mark.xfail(six.PY3, reason="'str' passed to 'os.write'")
+def test_rhd_should_raise_hook_errors(hooks_dir, error):
+    with pytest.raises(exception.HookError) as e:
+        hooks._runHooksDir(u"", hooks_dir.basename)
+
+    for err in error:
+        assert err in str(e.value)
+
+
+@pytest.mark.parametrize("hooks_dir,expected", indirect=["hooks_dir"],
+                         argvalues=[
+    pytest.param(
+        [
+            appender_script("1.sh"),
+            appender_script("2.sh", exit_code=1),
+            appender_script("3.sh")
+        ],
+        u"1.sh\n2.sh\n3.sh\n",
+        id="non-fatal hook error"
+    ),
+    pytest.param(
+        [
+            appender_script("1.sh"),
+            appender_script("2.sh", exit_code=2),
+            appender_script("3.sh")
+        ],
+        u"1.sh\n2.sh\n",
+        id="fatal hook error, '3.sh' skipped"
+    ),
+])
+@pytest.mark.xfail(six.PY3, reason="'str' passed to 'os.write'")
+def test_rhd_should_handle_hook_errors(hooks_dir, expected):
+    assert hooks._runHooksDir(u"", hooks_dir.basename, raiseError=False) == \
+        expected
+
+
+@pytest.mark.parametrize("hooks_dir", indirect=True, argvalues=[
+    pytest.param(
+        [
+            appender_script("1.sh", exit_code=111),
+        ],
+        id="invalid exit code"
+    ),
+])
+@pytest.mark.xfail(six.PY3, reason="'str' passed to 'os.write'")
+def test_rhd_should_report_invalid_hook_error_codes(caplog, hooks_dir):
+    hooks._runHooksDir(u"", hooks_dir.basename, raiseError=False)
+
+    assert "111" in "".join(msg
+                            for (_, lvl, msg) in caplog.record_tuples
+                            if lvl == logging.WARNING)
+
+
+@pytest.mark.parametrize("hooks_dir", indirect=True, argvalues=[
+    pytest.param(
+        [
+            appender_script("1.sh"),
+        ],
+        id="script writing to stderr"
+    ),
+])
+@pytest.mark.xfail(six.PY3, reason="'str' passed to 'os.write'")
+def test_rhd_should_report_hook_stderr(caplog, hooks_dir):
+    caplog.set_level(logging.INFO)
+    hooks._runHooksDir(u"", hooks_dir.basename)
+
+    assert "1.sh" in "".join(msg
+                             for _, lvl, msg in caplog.record_tuples
+                             if lvl == logging.INFO)
+
+
+@pytest.fixture
+def env_dump(hooks_dir):
+    dump_path = str(hooks_dir.join("env_dump.pickle"))
+    code = textwrap.dedent(
+        """\
+        #!{}
+        import os
+        import pickle
+        import six
+
+        with open("{}", "wb") as dump_file:
+            env = dict()
+            for k, v in os.environ.items():
+                if isinstance(v, six.binary_type):
+                    v = v.decode("utf-8")
+                env[k] = v
+            pickle.dump(env, dump_file)
+        """).format(sys.executable, dump_path)
+    FileEntry("env_dump.py", 0o755, code).apply(hooks_dir)
+    yield dump_path
+
+
+@pytest.mark.parametrize("vmconf, params, expected", [
+    pytest.param(
+        {},
+        {"abc": "def"},
+        {"abc": "def"},
+        id="simple variable"
+    ),
+    pytest.param(
+        {},
+        {"abc": b"\xc4\x85b\xc4\x87".decode("utf-8")},
+        {"abc": b"\xc4\x85b\xc4\x87".decode("utf-8")},
+        id="variable with local chars"
+    ),
+    pytest.param(
+        {},
+        {"abc": u"\udcfc"},
+        {},
+        id="variable with invalid utf-8 should be ignored",
+        marks=pytest.mark.xfail(six.PY3, reason="wrong exception caught")
+    ),
+    pytest.param(
+        {"vmId": "myvm"},
+        {},
+        {"vmId": "myvm"},
+        id="VM id"
+    ),
+    pytest.param(
+        {"custom": {"abc": "def"}},
+        {},
+        {"abc": "def"},
+        id="vmconf param"
+    ),
+    pytest.param(
+        {"custom": {"abc": "geh"}},
+        {"abc": "def"},
+        {"abc": "geh"},
+        id="vmconf param override"
+    ),
+])
+@pytest.mark.xfail(six.PY3, reason="'str' passed to 'os.write'")
+def test_rhd_should_assemble_environment_for_hooks(hooks_dir, env_dump, vmconf,
+                                                   params, expected):
+    hooks._runHooksDir(u"", hooks_dir.basename, vmconf, params=params)
+    env = pickle.load(open(env_dump, "rb"))
+
+    for k, v in expected.items():
+        assert env[k] == v
+
+
+@pytest.fixture
+def mkstemp_path(monkeypatch, hooks_dir):
+    with monkeypatch.context() as m:
+        tmp_path = str(hooks_dir.join("tmp_file"))
+
+        def impl():
+            return os.open(tmp_path, os.O_RDWR | os.O_CREAT, 0o600), tmp_path
+
+        m.setattr(hooks.tempfile, 'mkstemp', impl)
+        yield tmp_path
+
+
+@pytest.mark.parametrize("var_name, hook_type", [
+    pytest.param(
+        "_hook_domxml",
+        hooks._DOMXML_HOOK,
+        id="xml hook"
+    ),
+    pytest.param(
+        "_hook_json",
+        hooks._JSON_HOOK,
+        id="JSON hook"
+    )
+])
+@pytest.mark.xfail(six.PY3, reason="'str' passed to 'os.write'")
+def test_rhd_should_pass_data_file_to_hooks(hooks_dir, env_dump, mkstemp_path,
+                                            var_name, hook_type):
+    hooks._runHooksDir(None, hooks_dir.basename, hookType=hook_type)
+    env = pickle.load(open(env_dump, "rb"))
+
+    assert env[var_name] == mkstemp_path
+
+
+@pytest.fixture
+def hooking_client(hooks_dir):
+    code = textwrap.dedent(
+        """\
+        #!{}
+        import sys
+
+        try:
+            import hooking
+        except ImportError:
+            sys.exit(2)
+        """).format(sys.executable)
+    FileEntry("hook_client.py", 0o755, code).apply(hooks_dir)
+    yield
+
+
+@pytest.mark.xfail(six.PY3, reason="'str' passed to 'os.write'")
+def test_rhd_should_make_import_hooking_possible(hooks_dir, hooking_client):
+    hooks._runHooksDir(u"", hooks_dir.basename)
+
+
 class TestHooks(TestCaseBase):
-    def test_emptyDir(self):
-        with namedTemporaryDir() as dirName:
-            DOMXML = "algo"
-            self.assertEqual(DOMXML, hooks._runHooksDir(DOMXML, dirName))
-
-    @contextlib.contextmanager
-    def tempScripts(self):
-        with namedTemporaryDir() as dirName:
-            Q = 3
-            code = """#! /bin/bash
-echo -n %s >> "$_hook_domxml"
-            """
-            scripts = [tempfile.NamedTemporaryFile(dir=dirName, delete=False)
-                       for n in range(Q)]
-            scripts.sort(key=lambda f: f.name)
-            for n, script in enumerate(scripts):
-                script.write(code % n)
-                os.chmod(os.path.join(dirName, script.name), 0o775)
-                script.close()
-            yield dirName, scripts
-
-    @pytest.mark.xfail(six.PY3, reason="needs porting to py3")
-    def test_runHooksDir(self):
-        # Add an unicode value to the environment variables
-        # to test whether the utf-8 recoding works properly
-        os.environ["FAKE_GERRIT_USERNAME"] = "Pěkný žluťoučký kůň"
-
-        with self.tempScripts() as (dirName, scripts):
-            Q = 3
-            DOMXML = "algo"
-            expectedResult = DOMXML
-            for n in range(Q):
-                expectedResult = expectedResult + str(n)
-            res = hooks._runHooksDir(DOMXML, dirName)
-            self.assertEqual(expectedResult, res)
 
     def test_getNEScriptInfo(self):
         path = '/tmp/nonExistent'
@@ -229,44 +512,6 @@ echo "81212590184644762"
                 info = hooks._getHookInfo(dir)
                 expectedRes = dict([(os.path.basename(sName), {'md5': md5})])
                 self.assertEqual(expectedRes, info)
-
-    @contextmanager
-    def _deviceCustomPropertiesTestFile(self):
-        with namedTemporaryDir() as dirName:
-            # two nested with blocks to be python 2.6 friendly
-            with tempfile.NamedTemporaryFile(dir=dirName, delete=False) as f:
-                code = """#!/usr/bin/python2
-
-import os
-import hooking
-
-domXMLFile = open(os.environ['_hook_domxml'], 'a')
-customProperty = os.environ['customProperty']
-domXMLFile.write(customProperty)
-domXMLFile.close()
-            """
-                f.write(code)
-                os.chmod(f.name, 0o775)
-            yield dirName
-
-    @pytest.mark.xfail(six.PY3, reason="needs porting to py3")
-    def test_deviceCustomProperties(self):
-        with self._deviceCustomPropertiesTestFile() as dirName:
-            result = hooks._runHooksDir("oVirt", dirName,
-                                        params={'customProperty': ' rocks!'})
-            self.assertEqual(result, "oVirt rocks!")
-
-    @pytest.mark.xfail(six.PY3, reason="needs porting to py3")
-    def test_deviceVmConfProperties(self):
-        with self._deviceCustomPropertiesTestFile() as dirName:
-            vmconf = {
-                'custom': {
-                    'customProperty': ' rocks more!'}}
-
-            result = hooks._runHooksDir("oVirt", dirName,
-                                        params={'customProperty': ' rocks!'},
-                                        vmconf=vmconf)
-            self.assertEqual(result, "oVirt rocks more!")
 
     def test_pause_flags(self):
         vm_id = '042f6258-3446-4437-8034-0c93e3bcda1b'
