6cba805249b2d7d7a046feca5fb448f21c2d9e44
Vojtech Juranek <vjuranek@redhat.com>
2019-07-30 15:20:39 +0200

    tests: use XFS as default file system in userstorage
    So far we use default file system ext2 which caused issues in tests
    which preallocate volumes as fallocate on ext2 allocated files with
    bigger actual size than requested size (see bellow). Switch to XFS
    as a default file system to avoid such issues.
    
    fallocate on ext2:
    
        $ /usr/libexec/vdsm/fallocate 10485760 /var/tmp/vdsm-storage/mount.mount-512/test
        $ qemu-img info --output json /var/tmp/vdsm-storage/mount.mount-512/test
        {
    	"virtual-size": 10485760,
        	"filename": "/var/tmp/vdsm-storage/mount.mount-512/test",
        	"format": "raw",
        	"actual-size": 10502144,
        	"dirty-flag": false
        }
    
    Change-Id: I9a515f04569f92b446b38a450511236b58b38ee7
    Bug-Url: https://bugzilla.redhat.com/1592916
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- tests/storage/userstorage.py
+++ tests/storage/userstorage.py
@@ -167,8 +167,8 @@ class Mount(Path):
         return False
 
     def _create_filesystem(self):
-        # TODO: Use -t xfs (requires xfsprogs package).
-        subprocess.check_call(["sudo", "mkfs", "-q", self._loop.path])
+        subprocess.check_call(
+            ["sudo", "mkfs", "-t", "xfs", "-q", self._loop.path])
 
     def _mount_loop(self):
         subprocess.check_call(["sudo", "mount", self._loop.path, self.path])
