a999cff60439993eaa48d9be044aebfef18f7a3c
Marcin Sobczyk <msobczyk@redhat.com>
2019-08-30 12:40:34 +0200

    py3: tests: Remove usage of 'TestCaseBase' in 'stompasyncdispatcher_test'
    In an effort to move away from homegrown 'testlib' and deprecated
    'nose' testing framework, we're rewriting our tests to 'pytest'
    and running them with 'tox'.
    
    This patch removes the usage of 'testlib.TestCaseBase' class
    in 'stompasyncdispatcher_test' module.
    
    Change-Id: Ibfead18a3e153e7ef3160a091d77ae08f4cc9fa8
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- tests/lib/yajsonrpc/stompasyncdispatcher_test.py
+++ tests/lib/yajsonrpc/stompasyncdispatcher_test.py
@@ -23,7 +23,6 @@ from __future__ import division
 
 import json
 
-from testlib import VdsmTestCase as TestCaseBase
 from stomp_test_utils import (
     FakeAsyncDispatcher,
     FakeConnection,
@@ -39,116 +38,123 @@ from yajsonrpc.stomp import (
 )
 
 
-class AsyncDispatcherTest(TestCaseBase):
+def test_handle_connect():
+    frame_handler = FakeFrameHandler()
+    dispatcher = AsyncDispatcher(FakeConnection(), frame_handler)
 
-    def test_handle_connect(self):
-        frame_handler = FakeFrameHandler()
-        dispatcher = AsyncDispatcher(FakeConnection(), frame_handler)
+    dispatcher.handle_connect(None)
 
-        dispatcher.handle_connect(None)
+    assert frame_handler.handle_connect_called
 
-        self.assertTrue(frame_handler.handle_connect_called)
 
-    def test_handle_read(self):
-        frame_handler = FakeFrameHandler()
-        headers = {Headers.CONTENT_LENGTH: '78',
-                   Headers.DESTINATION: 'jms.topic.vdsm_responses',
-                   Headers.CONTENT_TYPE: 'application/json',
-                   Headers.SUBSCRIPTION: 'ad052acb-a934-4e10-8ec3-00c7417ef8d'}
-        body = json.dumps({
-            "jsonrpc": "2.0",
-            "id": "e8a936a6-d886-4cfa-97b9-2d54209053ff",
-            "result": [],
-        }).encode("utf-8")
-        frame = Frame(command=Command.MESSAGE, headers=headers, body=body)
-        dispatcher = AsyncDispatcher(FakeConnection(), frame_handler)
-        dispatcher.handle_read(FakeAsyncDispatcher(None, data=frame.encode()))
+def test_handle_read():
+    frame_handler = FakeFrameHandler()
+    headers = {Headers.CONTENT_LENGTH: '78',
+               Headers.DESTINATION: 'jms.topic.vdsm_responses',
+               Headers.CONTENT_TYPE: 'application/json',
+               Headers.SUBSCRIPTION: 'ad052acb-a934-4e10-8ec3-00c7417ef8d'}
+    body = json.dumps({
+        "jsonrpc": "2.0",
+        "id": "e8a936a6-d886-4cfa-97b9-2d54209053ff",
+        "result": [],
+    }).encode("utf-8")
+    frame = Frame(command=Command.MESSAGE, headers=headers, body=body)
+    dispatcher = AsyncDispatcher(FakeConnection(), frame_handler)
+    dispatcher.handle_read(FakeAsyncDispatcher(None, data=frame.encode()))
 
-        self.assertTrue(frame_handler.has_outgoing_messages)
-        recv_frame = frame_handler.pop_message()
-        self.assertEqual(Command.MESSAGE, recv_frame.command)
-        self.assertEqual(body, recv_frame.body)
+    assert frame_handler.has_outgoing_messages
+    recv_frame = frame_handler.pop_message()
+    assert Command.MESSAGE == recv_frame.command
+    assert body == recv_frame.body
 
-    def test_handle_error(self):
-        frame_handler = FakeFrameHandler()
-        connection = FakeConnection()
-        dispatcher = AsyncDispatcher(connection, frame_handler)
-        dispatcher.handle_error(dispatcher)
-        self.assertTrue(connection.closed)
 
-    def test_heartbeat_calc(self):
-        dispatcher = AsyncDispatcher(
-            FakeConnection(), FakeFrameHandler(),
-            clock=FakeTimeGen([4000000.0, 4000002.0]).get_fake_time
-        )
-        dispatcher.setHeartBeat(8000, 0)
+def test_handle_error():
+    frame_handler = FakeFrameHandler()
+    connection = FakeConnection()
+    dispatcher = AsyncDispatcher(connection, frame_handler)
+    dispatcher.handle_error(dispatcher)
+    assert connection.closed
 
-        self.assertEqual(6, dispatcher.next_check_interval())
 
-    def test_heartbeat_exceeded(self):
-        frame_handler = FakeFrameHandler()
-        dispatcher = AsyncDispatcher(
-            FakeConnection(), frame_handler,
-            clock=FakeTimeGen([4000000.0, 4000012.0]).get_fake_time
-        )
-        dispatcher.setHeartBeat(8000, 0)
+def test_heartbeat_calc():
+    dispatcher = AsyncDispatcher(
+        FakeConnection(), FakeFrameHandler(),
+        clock=FakeTimeGen([4000000.0, 4000002.0]).get_fake_time
+    )
+    dispatcher.setHeartBeat(8000, 0)
 
-        self.assertTrue(dispatcher.writable(None))
-        self.assertTrue(frame_handler.has_outgoing_messages)
+    assert 6 == dispatcher.next_check_interval()
 
-    def test_incoming_heartbeat_exceeded(self):
-        frame_handler = FakeFrameHandler()
-        connection = FakeConnection()
-        dispatcher = AsyncDispatcher(
-            connection, frame_handler,
-            clock=FakeTimeGen(
-                [4000000.0, 4000003.0, 4000006.0,
-                 4000009.0, 4000012.0]).get_fake_time)
 
-        dispatcher.setHeartBeat(12000, 4000)
-        self.assertFalse(dispatcher.writable(None))
-        self.assertFalse(frame_handler.has_outgoing_messages)
+def test_heartbeat_exceeded():
+    frame_handler = FakeFrameHandler()
+    dispatcher = AsyncDispatcher(
+        FakeConnection(), frame_handler,
+        clock=FakeTimeGen([4000000.0, 4000012.0]).get_fake_time
+    )
+    dispatcher.setHeartBeat(8000, 0)
 
-    def test_no_heartbeat(self):
-        dispatcher = AsyncDispatcher(FakeConnection(), FakeFrameHandler())
-        dispatcher.setHeartBeat(0, 0)
+    assert dispatcher.writable(None)
+    assert frame_handler.has_outgoing_messages
 
-        self.assertEqual(dispatcher.next_check_interval(), DEFAULT_INTERVAL)
 
-    def test_no_outgoing_heartbeat(self):
-        dispatcher = AsyncDispatcher(FakeConnection(), FakeFrameHandler())
-        dispatcher = AsyncDispatcher(
-            FakeConnection(), FakeFrameHandler(),
-            clock=FakeTimeGen([4000000.0, 4000002.0, 4000004.0,
-                               4000006.0]).get_fake_time
-        )
-        dispatcher.setHeartBeat(0, 8000)
-        self.assertEqual(dispatcher.next_check_interval(), DEFAULT_INTERVAL)
+def test_incoming_heartbeat_exceeded():
+    frame_handler = FakeFrameHandler()
+    connection = FakeConnection()
+    dispatcher = AsyncDispatcher(
+        connection, frame_handler,
+        clock=FakeTimeGen(
+            [4000000.0, 4000003.0, 4000006.0,
+             4000009.0, 4000012.0]).get_fake_time)
 
-    def test_handle_write(self):
-        headers = {Headers.CONTENT_LENGTH: '78',
-                   Headers.DESTINATION: 'jms.topic.vdsm_responses',
-                   Headers.CONTENT_TYPE: 'application/json',
-                   Headers.SUBSCRIPTION: 'ad052acb-a934-4e10-8ec3-00c7417ef8d'}
-        body = json.dumps({
-            "jsonrpc": "2.0",
-            "id": "e8a936a6-d886-4cfa-97b9-2d54209053ff",
-            "result": [],
-        }).encode("utf-8")
-        frame = Frame(command=Command.MESSAGE, headers=headers, body=body)
-        frame_handler = FakeFrameHandler()
-        frame_handler.handle_frame(None, frame)
+    dispatcher.setHeartBeat(12000, 4000)
+    assert not dispatcher.writable(None)
+    assert not frame_handler.has_outgoing_messages
 
-        dispatcher = AsyncDispatcher(FakeConnection(), frame_handler)
-        self.assertTrue(dispatcher.writable(None))
 
-        dispatcher.handle_write(FakeAsyncDispatcher(''))
-        self.assertFalse(frame_handler.has_outgoing_messages)
+def test_no_heartbeat():
+    dispatcher = AsyncDispatcher(FakeConnection(), FakeFrameHandler())
+    dispatcher.setHeartBeat(0, 0)
 
-    def test_handle_close(self):
-        connection = FakeConnection()
-        dispatcher = AsyncDispatcher(connection, FakeFrameHandler())
+    assert dispatcher.next_check_interval() == DEFAULT_INTERVAL
 
-        dispatcher.handle_close(None)
 
-        self.assertTrue(connection.closed)
+def test_no_outgoing_heartbeat():
+    dispatcher = AsyncDispatcher(FakeConnection(), FakeFrameHandler())
+    dispatcher = AsyncDispatcher(
+        FakeConnection(), FakeFrameHandler(),
+        clock=FakeTimeGen([4000000.0, 4000002.0, 4000004.0,
+                           4000006.0]).get_fake_time
+    )
+    dispatcher.setHeartBeat(0, 8000)
+    assert dispatcher.next_check_interval() == DEFAULT_INTERVAL
+
+
+def test_handle_write():
+    headers = {Headers.CONTENT_LENGTH: '78',
+               Headers.DESTINATION: 'jms.topic.vdsm_responses',
+               Headers.CONTENT_TYPE: 'application/json',
+               Headers.SUBSCRIPTION: 'ad052acb-a934-4e10-8ec3-00c7417ef8d'}
+    body = json.dumps({
+        "jsonrpc": "2.0",
+        "id": "e8a936a6-d886-4cfa-97b9-2d54209053ff",
+        "result": [],
+    }).encode("utf-8")
+    frame = Frame(command=Command.MESSAGE, headers=headers, body=body)
+    frame_handler = FakeFrameHandler()
+    frame_handler.handle_frame(None, frame)
+
+    dispatcher = AsyncDispatcher(FakeConnection(), frame_handler)
+    assert dispatcher.writable(None)
+
+    dispatcher.handle_write(FakeAsyncDispatcher(''))
+    assert not frame_handler.has_outgoing_messages
+
+
+def test_handle_close():
+    connection = FakeConnection()
+    dispatcher = AsyncDispatcher(connection, FakeFrameHandler())
+
+    dispatcher.handle_close(None)
+
+    assert connection.closed
