92f3cee2bddfb2c17b2d46cdf95d19556cfc8f0e
Amit Bawer <abawer@redhat.com>
2019-09-09 13:13:46 +0300

    py3, tests: Generate random devices names using hexlify instead of encode('hex')
    This will make get_random_device() python 3 compatible.
    
    Change-Id: I45a36d5a230bb32e647813f9d528f553523b3fd8
    Signed-off-by: Amit Bawer <abawer@redhat.com>

--- tests/storage/storagetestlib.py
+++ tests/storage/storagetestlib.py
@@ -20,6 +20,7 @@
 from __future__ import absolute_import
 from __future__ import division
 
+import binascii
 import os
 import shutil
 import tempfile
@@ -284,7 +285,7 @@ def make_blocksd_manifest(tmpdir, fake_lvm, sduuid=None, devices=None,
 
 
 def get_random_devices(count=NR_PVS):
-    return ['/dev/mapper/{0}'.format(os.urandom(16).encode('hex'))
+    return ['/dev/mapper/{0}'.format(binascii.hexlify(os.urandom(16)))
             for _ in range(count)]
 
 
