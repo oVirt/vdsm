9e1393d51c6d2699235cbafc783c40164b246a3f
Marcin Sobczyk <msobczyk@redhat.com>
2019-10-17 15:05:26 +0200

    py3: tests: Move 'stomp_test' to tox
    In an effort to clean up 'tests' directory, we're moving
    'stomp_test' module to 'tests/lib/yajsonrpc' dir and run it with tox.
    
    Change-Id: I412feac4e65d62cdbfbd964f73ff39e9b87fd80e
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- tests/lib/yajsonrpc/stomp_test.py
+++ tests/lib/yajsonrpc/stomp_test.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2015-2017 Red Hat, Inc.
+# Copyright 2015-2019 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
