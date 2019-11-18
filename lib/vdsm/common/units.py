285ccc4ec879a56ed494a6e50f6dbb21a07fc5fe
Pavel Bar <pbar@redhat.com>
2019-11-18 17:03:34 +0200

    constants: Add a dedicated "units" module for size constants.
    Change-Id: I9e6bfb7d8306c10e43720ec2d10d966d9a0557aa
    Signed-off-by: Pavel Bar <pbar@redhat.com>

---
+++ lib/vdsm/common/units.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2019 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+"""
+Constants for file/disk sizes.
+"""
+
+KiB = 1024
+MiB = 1024**2
+GiB = 1024**3
+TiB = 1024**4
+PiB = 1024**5
