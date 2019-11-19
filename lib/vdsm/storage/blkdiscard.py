428332119f768e04ab0097c3498d2f69f145fe71
Pavel Bar <pbar@redhat.com>
2019-11-19 17:18:46 +0200

    constants, blkdiscar: Use size constants for MiB
    - Use "MiB" constant instead of magic numbers.
    
    Change-Id: I84473bcaa23c8028538a0e46b21a8a205779e023
    Signed-off-by: Pavel Bar <pbar@redhat.com>

--- lib/vdsm/storage/blkdiscard.py
+++ lib/vdsm/storage/blkdiscard.py
@@ -22,13 +22,14 @@ from __future__ import absolute_import
 
 from vdsm.common import commands
 from vdsm.common.cmdutils import CommandPath
+from vdsm.common.units import MiB
 
 from vdsm.storage import operation
 
 # From tests that we've run on netapp, 32M is the most optimal discard step
 # size in terms of total discard time and minimal starvation of other processes
 # that run in parallel.
-OPTIMAL_DISCARD_STEP = 32 * 1024**2
+OPTIMAL_DISCARD_STEP = 32 * MiB
 
 _blkdiscard = CommandPath("blkdiscard", "/sbin/blkdiscard")
 
