60ee57a31e87b24b0bcc41d7db19b82b6581b607
Bell Levin <blevin@redhat.com>
2020-03-30 10:37:21 +0300

    net, CI: Add containerized unit tests
    The possibility to run the unit tests on a container.
    
    This introduces the possibilities to be able to run the tests on
    a fixed environment, from sources.
    
    Change-Id: I2d691b280c0198a9e0e0249b917f0f59bf930d85
    Signed-off-by: Bell Levin <blevin@redhat.com>

---
+++ tests/network/unit/run-tests.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -xe
+
+PROJECT=${PROJECT:-${PWD##*/}}
+PROJECT_PATH="$PWD"
+CONTAINER_WORKSPACE="/workspace/$PROJECT"
+CONTAINER_IMAGE="${CONTAINER_IMAGE:=ovirt/$PROJECT-test-unit-network-centos-8}"
+CONTAINER_CMD=${CONTAINER_CMD:=podman}
+VDSM_WORKDIR="/vdsm-tmp"
+
+test -t 1 && USE_TTY="t"
+
+function container_exec {
+    ${CONTAINER_CMD} exec "-i$USE_TTY" "$CONTAINER_ID" /bin/bash -c "$1"
+}
+
+function container_shell {
+    ${CONTAINER_CMD} exec "-i$USE_TTY" "$CONTAINER_ID" /bin/bash
+}
+
+function remove_container {
+    res=$?
+    [ "$res" -ne 0 ] && echo "*** ERROR: $res"
+    ${CONTAINER_CMD} rm -f "$CONTAINER_ID"
+}
+
+function copy_sources_to_workdir {
+    container_exec "
+    mkdir $VDSM_WORKDIR && \
+    cp -rf $CONTAINER_WORKSPACE $VDSM_WORKDIR/"
+}
+
+function run_unit_tests {
+    container_exec "
+    cd $VDSM_WORKDIR/$PROJECT \
+    && \
+    pytest -vv --log-level=DEBUG tests/network/unit
+    "
+}
+
+CONTAINER_ID="$($CONTAINER_CMD run -d -v $PROJECT_PATH:$CONTAINER_WORKSPACE:Z --env PYTHONPATH=lib $CONTAINER_IMAGE)"
+trap remove_container EXIT
+
+copy_sources_to_workdir
+
+if [ "$1" == "--shell" ];then
+    container_shell
+    exit 0
+fi
+
+run_unit_tests
