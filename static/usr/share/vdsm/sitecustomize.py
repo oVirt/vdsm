9f3f67b721baf829de668ec33ab0bae80e7fe363
Nir Soffer <nsoffer@redhat.com>
2020-04-26 18:05:37 +0300

    sitecustomize: Remove sys.setdefaultencoding() hack
    In python 2 we used a hack to set the default encoding to "utf-8"
    instead of "ascii":
    
        sys.setdefaultencoding('utf8')
    
    This hack is not needed in python 3 since the default encoding is
    already "utf-8", and we don't support python 2 for a while.
    
    Change-Id: I93785ff6a8e76d21c85d615ecb744c7e560b2f21
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- static/usr/share/vdsm/sitecustomize.py
+++ static/usr/share/vdsm/sitecustomize.py
@@ -1,11 +1,4 @@
-from __future__ import absolute_import
-
-import sys
 from vdsm.config import config
-
-if sys.version_info[0] == 2:
-    # Allow mixing of unicode objects and strings encoded in utf8.
-    sys.setdefaultencoding('utf8')  # pylint: disable=no-member
 
 if config.getboolean('devel', 'coverage_enable'):
     import coverage  # pylint: disable=import-error
