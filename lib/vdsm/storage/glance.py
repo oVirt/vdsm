05cf8aa9a90d5c474219375b9e4601e1b348282e
Vojtech Juranek <vjuranek@redhat.com>
2020-05-20 14:48:18 +0200

    storage: add glance module
    Add Glance module which provides function for getting image size using
    both Glance v1 API (deprecated) and v2 API (current) and upload and
    download of images.
    
    Add support for Glance v2 API. This API doesn't support obtaining image
    metadata using HTTP HEAD as in v1 and return HTTP 405 (method not
    allowed) in API v2. HTTP GET has to be used instead.
    
    When downloading an image, in v1 API we just call HTTP GET, while in
    case of v2 API, we need to add "/file" suffix to image URL.
    
    OpenStack v1 API was deprecated by v2 API long time ago and removed from
    OpenStack Rocky release [1], which was released on Aug. 30 2018 [2].
    As Glance v1 is currently still used in engine, keep support for v1 API
    until it's removed from engine.
    
    Add also tests for these function. As we have public oVirt Glance server,
    use this server for tests to have realistic response and don't have to
    mock it. If the server is not available, tests should be skipped.
    
    [1] https://docs.openstack.org/api-ref/image/versions/index.html#what-happened-to-the-v1-api
    [2] https://releases.openstack.org/rocky/index.html
    
    Change-Id: If9a556a3712f92e4cc009547625a78508b7c9485
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

---
+++ lib/vdsm/storage/glance.py
@@ -0,0 +1,161 @@
+# Copyright 2020 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+"""
+Support for upload and download images to/from OpenStack Glance service.
+
+Relevant API calls are described bellow. Currently, engine still uses v1 API
+so we provide support for v1 API here as well, but this API is deprecated and
+v2 API should be used.
+
+v1 API, for more details see
+https://docs.openstack.org/glance/latest/user/glanceapi.html
+- get image metadata: HEAD /v1/images/{image_id}
+- get image content: GET /v1/images/{image_id}
+- upload new image: POST /v1/images
+
+v2 API, for more details see https://docs.openstack.org/api-ref/image/v2/
+- get image metadata: GET /v2/images/{image_id}
+- get image content: GET /v2/images/{image_id}/file
+- upload new image: POST /v2/images
+
+
+Example of v1 API (trimmed) metadata response:
+
+    HTTP/1.1 200 OK
+    Content-Type: text/html; charset=UTF-8
+    Content-Length: 0
+    X-Image-Meta-Id: 34bec3e1-7526-474a-ac80-225209a5ad54
+    X-Image-Meta-Name: CirrOS 0.4.0 for x86_64
+    X-Image-Meta-Size: 12716032
+    [...]
+    Etag: 443b7623e27ecf03dc9e01ee93f67afe
+    X-Openstack-Request-Id: req-6a0c7a56-f1e4-42cd-a344-d1efa361154a
+    Date: Wed, 20 May 2020 13:05:50 GMT
+
+
+Example of v2 API (trimmed) metadata response:
+
+    {
+        "id": "34bec3e1-7526-474a-ac80-225209a5ad54",
+        "name": "CirrOS 0.4.0 for x86_64",
+        "size": 12716032,
+        [...]
+        "schema": "/v2/schemas/image"
+    }
+
+"""
+
+import json
+import logging
+
+from urllib.parse import urlparse
+
+from vdsm import utils
+
+from vdsm.common import errors
+
+from vdsm.storage import curlImgWrap
+
+log = logging.getLogger("storage.glance")
+
+
+class ApiVersionError(errors.Base):
+    msg = "Wrong API version or malformed URL: {self.url}"
+
+    def __init__(self, url):
+        self.url = url
+
+
+class ImageSizeError(errors.Base):
+    msg = ("Unable to get image size: {self.reason}, url={self.url},"
+           "headers={self.headers}, response={self.response}")
+
+    def __init__(self, reason, url, headers, response):
+        self.reason = reason
+        self.url = url
+        self.headers = headers
+        self.response = response
+
+
+def _image_info_v1(url, headers=None):
+    """
+    Return dict with image info. Only the "size" key is reported for v1 API.
+    """
+    resp = curlImgWrap.head(url, headers)
+
+    try:
+        return {"size": int(resp['X-Image-Meta-Size'])}
+    except (KeyError, ValueError) as e:
+        raise ImageSizeError(str(e), url, headers, resp)
+
+
+def _image_info_v2(url, headers=None):
+    """
+    Return dict with image info as returned by glance.
+    See glance docs for more info:
+    https://docs.openstack.org/api-ref/image/v2/#show-image
+    """
+    out = curlImgWrap.get(url, headers)
+    info = json.loads(out)
+
+    if "size" not in info:
+        raise ImageSizeError("Size not in response", url, headers, info)
+
+    return info
+
+
+def api_version(url):
+    path = urlparse(url).path.lower()
+    return path.split("/")[1]
+
+
+def image_info(url, headers=None):
+    """
+    Return image info.
+    """
+    version = api_version(url)
+
+    if version == "v1":
+        return _image_info_v1(url, headers)
+    elif version == "v2":
+        return _image_info_v2(url, headers)
+    else:
+        raise ApiVersionError(url)
+
+
+def download_image(image_path, url, headers=None):
+    # In case of v2 API, we need to append /file suffix to URL to download
+    # image content. In case of v1 API, no URL changes are needed.
+    if api_version(url) == "v2":
+        url = url + "/file"
+
+    with utils.stopwatch(
+            "Downloading {} to {}".format(url, image_path),
+            level=logging.INFO,
+            log=log):
+        curlImgWrap.download(url, image_path, headers)
+
+
+def upload_image(image_path, url, headers=None):
+    with utils.stopwatch(
+            "Uploading {} to {}".format(image_path, url),
+            level=logging.INFO,
+            log=log):
+        curlImgWrap.upload(url, image_path, headers)
---
+++ tests/storage/glance_test.py
@@ -0,0 +1,89 @@
+#
+# Copyright 2020 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import hashlib
+import json
+
+import pytest
+
+from vdsm import constants
+
+from vdsm.storage import curlImgWrap
+from vdsm.storage import glance
+
+OVIRT_GLANCE_URL = "http://glance.ovirt.org:9292"
+
+
+@pytest.fixture(
+    params=[
+        pytest.param(OVIRT_GLANCE_URL + "/v1/images", id="v1"),
+        pytest.param(OVIRT_GLANCE_URL + "/v2/images", id="v2"),
+    ],
+    scope="module",
+)
+def glance_image(request):
+    """
+    Get the metadata of the smallest image in the oVirt Glance server.
+    """
+    try:
+        out = curlImgWrap.get(
+            request.param + "?sort_key=size&sort_dir=asc&limit=1")
+    except curlImgWrap.CurlError:
+        return pytest.skip("Glance server is not reachable")
+
+    images = json.loads(out)
+    smallest_image = images["images"][0]
+    smallest_image["url"] = request.param + "/" + smallest_image["id"]
+    return smallest_image
+
+
+def test_api_version():
+    url = OVIRT_GLANCE_URL + "/{}/images/34bec3e1-7526-474a-ac80-225209a5ad54"
+    assert glance.api_version(url.format("v1")) == "v1"
+    assert glance.api_version(url.format("v2")) == "v2"
+
+
+def test_image_info_error():
+    with pytest.raises(glance.ApiVersionError):
+        glance.image_info(OVIRT_GLANCE_URL + "/fail")
+    with pytest.raises(glance.ApiVersionError):
+        glance.image_info(OVIRT_GLANCE_URL + "//v1")
+    with pytest.raises(glance.ApiVersionError):
+        glance.image_info(OVIRT_GLANCE_URL + "//v2")
+
+
+def test_image_size(glance_image):
+    info = glance.image_info(glance_image["url"])
+    assert info["size"] == glance_image["size"]
+
+
+def test_image_download(monkeypatch, tmpdir, glance_image):
+    monkeypatch.setattr(
+        constants, "EXT_CURL_IMG_WRAP", "../lib/vdsm/storage/curl-img-wrap")
+
+    img_dest = str(tmpdir.join(glance_image["id"]))
+    glance.download_image(img_dest, glance_image["url"])
+
+    md5_hash = hashlib.md5()
+    with open(img_dest, "rb") as f:
+        md5_hash.update(f.read())
+    checksum = md5_hash.hexdigest()
+
+    assert checksum == glance_image["checksum"]
