ae32b5cae0f33f715efaf23c85f5dd0b55ac2bf3
Andrej Cernek <acernek@redhat.com>
2020-06-30 11:56:06 +0200

    net, tests: move cmd tests to pytest
    Change-Id: I78a229ecdb00efd67fdaf05c0a669a0002adb2fa
    Signed-off-by: Andrej Cernek <acernek@redhat.com>

--- tests/network/integration/cmd_test.py
+++ tests/network/integration/cmd_test.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2017-2019 Red Hat, Inc.
+# Copyright 2017-2020 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,38 +18,33 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
-from __future__ import absolute_import
-from __future__ import division
-
-import unittest
+from vdsm.network import cmd
 
 from .netintegtestlib import requires_systemdrun
 
-from vdsm.network import cmd
 
-
-class CmdTest(unittest.TestCase):
+class TestCmd(object):
     def test_exec_sync_with_no_output(self):
-        rc, out, err = cmd.exec_sync(('true',))
-        self.assertEqual(out, '')
-        self.assertEqual(err, '')
+        _, out, err = cmd.exec_sync(('true',))
+        assert out == ''
+        assert err == ''
 
     def test_exec_sync_with_success_output(self):
         rc, out, err = cmd.exec_sync(('echo', 'hello world'))
-        self.assertEqual(rc, 0, err)
-        self.assertEqual(out, 'hello world\n')
-        self.assertEqual(err, '')
+        assert rc == 0, err
+        assert out == 'hello world\n'
+        assert err == ''
 
     def test_exec_sync_with_error_output(self):
         rc, out, err = cmd.exec_sync(('ls', 'no such prog'))
-        self.assertNotEqual(rc, 0)
-        self.assertIn('No such file or directory', err)
-        self.assertEqual(out, '')
+        assert rc != 0
+        assert 'No such file or directory' in err
+        assert out == ''
 
     @requires_systemdrun
     def test_exec_systemd_new_unit(self):
         rc, out, err = cmd.exec_systemd_new_unit(
             ('echo', 'hello world'), slice_name='test-group'
         )
-        self.assertEqual(rc, 0, err)
-        self.assertEqual(out, 'hello world\n')
+        assert rc == 0, err
+        assert out == 'hello world\n'
