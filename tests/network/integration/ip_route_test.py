87f463ee19bf4ec7efdde52ac34cccd0e035a7e0
Andrej Cernek <acernek@redhat.com>
2020-07-01 10:57:58 +0200

    net, tests: move ip route tests to pytest
    Change-Id: I474d238e988bd8879df7c2dea2cfd790eaa57e13
    Signed-off-by: Andrej Cernek <acernek@redhat.com>

--- tests/network/integration/ip_route_test.py
+++ tests/network/integration/ip_route_test.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2018 Red Hat, Inc.
+# Copyright 2017-2020 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,49 +16,51 @@
 #
 # Refer to the README and COPYING files for full details of the license
 
-from __future__ import absolute_import
-from __future__ import division
-
 from contextlib import contextmanager
 
-import unittest
+import pytest
 
 from vdsm.network.ip import route as ip_route
+from vdsm.network.ip.route import IPRouteAddError
 from vdsm.network.ip.route import IPRouteData
-from vdsm.network.ip.route import IPRouteAddError, IPRouteDeleteError
+from vdsm.network.ip.route import IPRouteDeleteError
+
 
 IPV4_ADDRESS = '192.168.99.1'
 
 
-class IPRouteTest(unittest.TestCase):
-    IPRoute = ip_route.driver(ip_route.Drivers.IPROUTE2)
+@pytest.fixture(scope='module')
+def ip_route_driver():
+    return ip_route.driver(ip_route.Drivers.IPROUTE2)
 
-    def test_add_delete_and_read_route(self):
+
+class TestIPRoute(object):
+    def test_add_delete_and_read_route(self, ip_route_driver):
         route = IPRouteData(to=IPV4_ADDRESS, via=None, family=4, device='lo')
-        with self.create_route(route):
+        with self._create_route(ip_route_driver, route):
             routes = [
                 r
-                for r in IPRouteTest.IPRoute.routes(table='main')
+                for r in ip_route_driver.routes(table='main')
                 if r.to == IPV4_ADDRESS
             ]
-            self.assertEqual(1, len(routes))
-            self.assertEqual(routes[0].device, 'lo')
-            self.assertEqual(routes[0].table, 'main')
+            assert len(routes) == 1
+            assert routes[0].device == 'lo'
+            assert routes[0].table == 'main'
 
-    def test_delete_non_existing_route(self):
+    def test_delete_non_existing_route(self, ip_route_driver):
         route = IPRouteData(to=IPV4_ADDRESS, via=None, family=4, device='lo')
-        with self.assertRaises(IPRouteDeleteError):
-            IPRouteTest.IPRoute.delete(route)
+        with pytest.raises(IPRouteDeleteError):
+            ip_route_driver.delete(route)
 
-    def test_add_route_with_non_existing_device(self):
+    def test_add_route_with_non_existing_device(self, ip_route_driver):
         route = IPRouteData(to=IPV4_ADDRESS, via=None, family=4, device='NoNe')
-        with self.assertRaises(IPRouteAddError):
-            IPRouteTest.IPRoute.add(route)
+        with pytest.raises(IPRouteAddError):
+            ip_route_driver.add(route)
 
     @contextmanager
-    def create_route(self, route_data):
-        IPRouteTest.IPRoute.add(route_data)
+    def _create_route(self, route_driver, route_data):
+        route_driver.add(route_data)
         try:
             yield
         finally:
-            IPRouteTest.IPRoute.delete(route_data)
+            route_driver.delete(route_data)
