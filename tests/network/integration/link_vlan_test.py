6797c5f43140a2f88b4a8aeda045d32e53b827df
Andrej Cernek <acernek@redhat.com>
2020-07-14 12:10:06 +0200

    net, tests: move link vlan tests to pytest
    Change-Id: I6811b3a7759884e258bb28aeede51197bbdef93e
    Signed-off-by: Andrej Cernek <acernek@redhat.com>

--- tests/network/integration/link_vlan_test.py
+++ tests/network/integration/link_vlan_test.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Red Hat, Inc.
+# Copyright 2018-2020 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,30 +17,32 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
-from __future__ import absolute_import
-from __future__ import division
-
-import unittest
+import pytest
 
 from network.nettestlib import dummy_device, vlan_device
 
 from vdsm.network.link import vlan
 
 
-class LinkIfaceTests(unittest.TestCase):
-    def test_list_vlans_on_base_device(self):
-        with dummy_device() as nic:
-            with vlan_device(nic, tag=999) as vlan_dev:
-                self.assertEqual(
-                    [vlan_dev.devName],
-                    list(vlan.get_vlans_on_base_device(nic)),
-                )
+@pytest.fixture
+def vlan999(nic0):
+    with vlan_device(nic0, tag=999) as vlan_dev:
+        yield vlan_dev
 
-    def test_identify_vlan_base_device(self):
-        with dummy_device() as nic:
-            with vlan_device(nic, tag=999):
-                self.assertTrue(vlan.is_base_device(nic))
 
-    def test_identify_non_vlan_base_device(self):
-        with dummy_device() as nic:
-            self.assertFalse(vlan.is_base_device(nic))
+@pytest.fixture
+def nic0():
+    with dummy_device() as nic:
+        yield nic
+
+
+class TestLinkIface(object):
+    def test_list_vlans_on_base_device(self, nic0, vlan999):
+        base_device_vlans = list(vlan.get_vlans_on_base_device(nic0))
+        assert base_device_vlans == [vlan999.devName]
+
+    def test_identify_vlan_base_device(self, nic0, vlan999):
+        assert vlan.is_base_device(nic0)
+
+    def test_identify_non_vlan_base_device(self, nic0):
+        assert not vlan.is_base_device(nic0)
