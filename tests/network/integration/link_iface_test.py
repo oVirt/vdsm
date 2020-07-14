028049c3f2edd9bbc1dab71438716cc2833494eb
Andrej Cernek <acernek@redhat.com>
2020-07-14 12:09:19 +0200

    net, tests: move link iface tests to pytest
    Change-Id: Ice5c058fe150e0aff596b3b503e317798d07881f
    Signed-off-by: Andrej Cernek <acernek@redhat.com>

--- tests/network/integration/link_iface_test.py
+++ tests/network/integration/link_iface_test.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018 Red Hat, Inc.
+# Copyright 2016-2020 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,40 +17,36 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
-from __future__ import absolute_import
-from __future__ import division
-
-import unittest
+import pytest
 
 from network.nettestlib import dummy_device
 
 from vdsm.network.link.iface import iface
 
 
-class LinkIfaceTests(unittest.TestCase):
-    def test_iface_up(self):
-        with dummy_device() as nic:
-            _iface = iface(nic)
-            _iface.up()
-            self.assertTrue(_iface.is_up())
+@pytest.fixture
+def link_iface():
+    with dummy_device() as nic:
+        _iface = iface(nic)
+        yield _iface
 
-    def test_iface_down(self):
-        with dummy_device() as nic:
-            _iface = iface(nic)
-            _iface.up()
-            _iface.down()
-            self.assertFalse(_iface.is_up())
 
-    def test_iface_notpromisc(self):
-        with dummy_device() as nic:
-            _iface = iface(nic)
-            _iface.up()
-            self.assertFalse(_iface.is_promisc())
+class TestLinkIface(object):
+    def test_iface_up(self, link_iface):
+        link_iface.up()
+        assert link_iface.is_up()
 
-    def test_iface_hwaddr(self):
+    def test_iface_down(self, link_iface):
+        link_iface.up()
+        link_iface.down()
+        assert not link_iface.is_up()
+
+    def test_iface_notpromisc(self, link_iface):
+        link_iface.up()
+        assert not link_iface.is_promisc()
+
+    def test_iface_hwaddr(self, link_iface):
         MAC_ADDR = '02:00:00:00:00:01'
 
-        with dummy_device() as nic:
-            _iface = iface(nic)
-            _iface.set_address(MAC_ADDR)
-            self.assertEqual(MAC_ADDR, _iface.address())
+        link_iface.set_address(MAC_ADDR)
+        assert link_iface.address() == MAC_ADDR
