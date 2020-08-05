87d6958a187aad9ab7abede75c43fa1592a11da9
Andrej Cernek <acernek@redhat.com>
2020-08-05 11:03:57 +0200

    net, tests: refactor link bond tests to use fixtures
    To take advantage of pytest fixtures, test setups have been separated
    from tests themselves. This allows better reusability as well as more
    readable and less indented code.
    
    Change-Id: I733e61eeaf02e7f21b9509818f6fa542c82dfa4a
    Signed-off-by: Andrej Cernek <acernek@redhat.com>

--- tests/network/integration/link_bond_test.py
+++ tests/network/integration/link_bond_test.py
@@ -49,129 +49,132 @@ def _sorted_arp_ip_target(options):
     return options
 
 
+@pytest.fixture
+def nics():
+    with dummy_devices(2) as nics:
+        yield nics
+
+
+@pytest.fixture
+def bond0():
+    with bond_device() as bond:
+        yield bond
+
+
+@pytest.fixture
+def bond1():
+    with bond_device() as bond:
+        yield bond
+
+
+@pytest.fixture
+def bond2():
+    with bond_device() as bond:
+        yield bond
+
+
+@pytest.fixture
+def bond_with_slaves(bond0, nics):
+    bond0.add_slaves(nics)
+    yield bond0
+
+
 class TestLinkBond(object):
-    def test_bond_without_slaves(self):
-        with bond_device() as bond:
-            assert not iface(bond.master).is_up()
+    def test_bond_without_slaves(self, bond0):
+        assert not iface(bond0.master).is_up()
 
-    def test_bond_with_slaves(self):
-        with dummy_devices(2) as (nic1, nic2):
-            with bond_device() as bond:
-                bond.add_slaves((nic1, nic2))
-                assert not iface(bond.master).is_up()
+    def test_bond_with_slaves(self, bond_with_slaves):
+        assert not iface(bond_with_slaves.master).is_up()
 
-    def test_bond_devices_are_up(self):
-        with dummy_devices(2) as (nic1, nic2):
-            with bond_device() as bond:
-                bond.add_slaves((nic1, nic2))
-                bond.up()
-                assert iface(nic1).is_up()
-                assert iface(nic2).is_up()
-                assert iface(bond.master).is_up()
+    def test_bond_devices_are_up(self, bond_with_slaves):
+        bond_with_slaves.up()
+        for nic in bond_with_slaves.slaves:
+            assert iface(nic).is_up()
+        assert iface(bond_with_slaves.master).is_up()
 
-    def test_bond_exists(self):
+    def test_bond_exists(self, bond_with_slaves):
         OPTIONS = {'mode': '1', 'miimon': '300'}
-        with dummy_devices(2) as (nic1, nic2):
-            with bond_device() as _bond:
-                _bond.set_options(OPTIONS)
-                _bond.add_slaves((nic1, nic2))
-                _bond.up()
+        bond_with_slaves.set_options(OPTIONS)
+        bond_with_slaves.up()
 
-                bond = Bond(_bond.master)
-                assert bond.slaves == set((nic1, nic2))
-                assert bond.options == OPTIONS
+        bond = Bond(bond_with_slaves.master)
+        assert bond.slaves == set(bond_with_slaves.slaves)
+        assert bond.options == OPTIONS
 
-    def test_bond_list(self):
-        with bond_device() as b1, bond_device() as b2, bond_device() as b3:
-            actual_bond_set = set(Bond.bonds())
-            expected_bond_set = set([b1.master, b2.master, b3.master])
-            assert expected_bond_set <= actual_bond_set
+    def test_bond_list(self, bond0, bond1, bond2):
+        actual_bond_set = set(Bond.bonds())
+        expected_bond_set = {b.master for b in (bond0, bond1, bond2)}
+        assert expected_bond_set <= actual_bond_set
 
-    def test_bond_create_failure_on_slave_add(self):
-        with dummy_devices(2) as (nic1, nic2):
-            with bond_device() as base_bond:
-                base_bond.add_slaves((nic1, nic2))
+    def test_bond_create_failure_on_slave_add(self, bond_with_slaves):
+        bond_name = random_iface_name('bond_', max_length=11)
+        with pytest.raises(IOError):
+            with Bond(bond_name) as broken_bond:
+                broken_bond.create()
+                broken_bond.add_slaves(bond_with_slaves.slaves)
+        assert not Bond(bond_name).exists()
 
-                bond_name = random_iface_name('bond_', max_length=11)
-                with pytest.raises(IOError):
-                    with Bond(bond_name) as broken_bond:
-                        broken_bond.create()
-                        broken_bond.add_slaves((nic1, nic2))
-                assert not Bond(bond_name).exists()
+    def test_bond_edit_failure_on_slave_add(self, bond0, bond1, nics):
+        base_bond, edit_bond = bond0, bond1
+        base_bond.add_slaves((nics[0],))
+        edit_bond.add_slaves((nics[1],))
 
-    def test_bond_edit_failure_on_slave_add(self):
-        with dummy_devices(2) as (nic1, nic2):
-            with bond_device() as base_bond, bond_device() as edit_bond:
-                base_bond.add_slaves((nic1,))
-                edit_bond.add_slaves((nic2,))
+        with pytest.raises(IOError):
+            with Bond(edit_bond.master) as broken_bond:
+                assert broken_bond.exists()
+                broken_bond.add_slaves((nics[0],))
+        assert edit_bond.exists()
+        assert edit_bond.slaves == {nics[1]}
 
-                with pytest.raises(IOError):
-                    with Bond(edit_bond.master) as broken_bond:
-                        assert broken_bond.exists()
-                        broken_bond.add_slaves((nic1,))
-                assert edit_bond.exists()
-                assert edit_bond.slaves == set((nic2,))
-
-    def test_bond_set_options(self):
+    def test_bond_set_options(self, bond_with_slaves):
         OPTIONS = {'mode': '1', 'miimon': '300'}
+        bond_with_slaves.set_options(OPTIONS)
+        bond_with_slaves.up()
 
-        with dummy_devices(2) as (nic1, nic2):
-            with bond_device() as bond:
-                bond.set_options(OPTIONS)
-                bond.add_slaves((nic1, nic2))
-                bond.up()
+        bond = Bond(bond_with_slaves.master)
+        assert bond.options == OPTIONS
 
-                _bond = Bond(bond.master)
-                assert _bond.options == OPTIONS
-
-    def test_bond_edit_options(self):
+    def test_bond_edit_options(self, bond_with_slaves):
         OPTIONS_A = {'mode': '1', 'miimon': '300'}
         OPTIONS_B = {'mode': '2'}
         OPTIONS_C = {'mode': 'balance-rr', 'miimon': '150'}
 
-        with dummy_devices(2) as (nic1, nic2):
-            with bond_device() as bond:
-                bond.set_options(OPTIONS_A)
-                bond.add_slaves((nic1, nic2))
-                _bond = Bond(bond.master)
-                assert _bond.options == OPTIONS_A
+        bond_with_slaves.set_options(OPTIONS_A)
+        bond = Bond(bond_with_slaves.master)
+        assert bond.options == OPTIONS_A
 
-                bond.set_options(OPTIONS_B)
-                _bond.refresh()
-                assert _bond.options == OPTIONS_B
+        bond_with_slaves.set_options(OPTIONS_B)
+        bond.refresh()
+        assert bond.options == OPTIONS_B
 
-                bond.set_options(OPTIONS_C)
-                _bond.refresh()
-                OPTIONS_C['mode'] = '0'
-                assert _bond.options == OPTIONS_C
+        bond_with_slaves.set_options(OPTIONS_C)
+        bond.refresh()
+        OPTIONS_C['mode'] = '0'
+        assert bond.options == OPTIONS_C
 
-    def test_bond_set_one_arp_ip_target(self):
+    def test_bond_set_one_arp_ip_target(self, bond0):
         OPTIONS = {
             'mode': '1',
             'arp_interval': '1000',
             'arp_ip_target': '192.168.122.1',
         }
+        bond0.set_options(OPTIONS)
+        bond0.refresh()
+        assert bond0.options == OPTIONS
 
-        with bond_device() as bond:
-            bond.set_options(OPTIONS)
-            bond.refresh()
-            assert bond.options == OPTIONS
-
-    def test_bond_set_two_arp_ip_targets(self):
+    def test_bond_set_two_arp_ip_targets(self, bond0):
         OPTIONS = {
             'mode': '1',
             'arp_interval': '1000',
             'arp_ip_target': '10.1.3.1,10.1.2.1',
         }
+        bond0.set_options(OPTIONS)
+        bond0.refresh()
+        sorted_bond_opts = _sorted_arp_ip_target(bond0.options)
+        sorted_opts = _sorted_arp_ip_target(OPTIONS)
+        assert sorted_bond_opts == sorted_opts
 
-        with bond_device() as bond:
-            bond.set_options(OPTIONS)
-            bond.refresh()
-            sorted_bond_opts = _sorted_arp_ip_target(bond.options)
-            sorted_opts = _sorted_arp_ip_target(OPTIONS)
-            assert sorted_bond_opts == sorted_opts
-
-    def test_bond_clear_arp_ip_target(self):
+    def test_bond_clear_arp_ip_target(self, bond0):
         OPTIONS_A = {
             'mode': '1',
             'arp_interval': '1000',
@@ -179,13 +182,12 @@ class TestLinkBond(object):
         }
         OPTIONS_B = {'mode': '1', 'arp_interval': '1000'}
 
-        with bond_device() as bond:
-            bond.set_options(OPTIONS_A)
-            bond.set_options(OPTIONS_B)
-            bond.refresh()
-            assert bond.options == OPTIONS_B
+        bond0.set_options(OPTIONS_A)
+        bond0.set_options(OPTIONS_B)
+        bond0.refresh()
+        assert bond0.options == OPTIONS_B
 
-    def test_bond_update_existing_arp_ip_targets(self):
+    def test_bond_update_existing_arp_ip_targets(self, bond0):
         preserved_addr = '10.1.4.1'
         removed_addr = '10.1.3.1,10.1.2.1'
         added_addr = '10.1.1.1'
@@ -201,29 +203,26 @@ class TestLinkBond(object):
             'arp_ip_target': ','.join([preserved_addr, added_addr]),
         }
 
-        with bond_device() as bond:
-            bond.set_options(OPTIONS_A)
-            bond.set_options(OPTIONS_B)
-            bond.refresh()
-            assert bond.options == OPTIONS_B
+        bond0.set_options(OPTIONS_A)
+        bond0.set_options(OPTIONS_B)
+        bond0.refresh()
+        assert bond0.options == OPTIONS_B
 
-    def test_bond_properties_includes_non_options_keys(self):
-        with bond_device() as bond:
-            assert 'active_slave' in bond.properties
+    def test_bond_properties_includes_non_options_keys(self, bond0):
+        assert 'active_slave' in bond0.properties
 
-    def test_bond_monitor(self):
+    def test_bond_monitor(self, nics):
         notifier = FakeNotifier()
-        with dummy_devices(2) as (nic1, nic2):
-            with bond_device(slaves=(nic1, nic2)) as bond:
-                bond.set_options({'mode': '1'})
-                bond.up()
-                bond.set_options({'active_slave': nic1})
-                bond_monitor.initialize_monitor(notifier)
-                try:
-                    bond.set_options({'active_slave': nic2})
-                    _wait_until(lambda: notifier.calls)
-                finally:
-                    bond_monitor.stop()
+        with bond_device(slaves=nics) as bond:
+            bond.set_options({'mode': '1'})
+            bond.up()
+            bond.set_options({'active_slave': nics[0]})
+            bond_monitor.initialize_monitor(notifier)
+            try:
+                bond.set_options({'active_slave': nics[1]})
+                _wait_until(lambda: notifier.calls)
+            finally:
+                bond_monitor.stop()
         assert notifier.calls == [('|net|host_conn|no_id', None)]
 
 
@@ -234,33 +233,30 @@ def _wait_until(condition, timeout=1, interval=0.1):
 
 
 class TestLinkBondSysFS(object):
-    def test_do_not_detach_slaves_while_changing_options(self):
+    def test_do_not_detach_slaves_while_changing_options(
+        self, bond_with_slaves
+    ):
         OPTIONS = {'miimon': '110'}
 
-        with dummy_devices(2) as (nic1, nic2):
-            with bond_device() as bond:
-                bond.add_slaves((nic1, nic2))
-                mock_slaves = bond.del_slaves = bond.add_slaves = mock.Mock()
+        bond = bond_with_slaves
+        mock_slaves = bond.del_slaves = bond.add_slaves = mock.Mock()
+        bond.set_options(OPTIONS)
 
-                bond.set_options(OPTIONS)
+        mock_slaves.assert_not_called()
 
-                mock_slaves.assert_not_called()
+    def test_bond_properties_with_filter(self, bond0):
+        properties = sysfs_options.properties(
+            bond0.master, filter_properties=('mode',)
+        )
+        assert 'mode' in properties
+        assert len(properties) == 1
 
-    def test_bond_properties_with_filter(self):
-        with bond_device() as bond:
-            properties = sysfs_options.properties(
-                bond.master, filter_properties=('mode',)
-            )
-            assert 'mode' in properties
-            assert len(properties) == 1
-
-    def test_bond_properties_with_filter_out(self):
-        with bond_device() as bond:
-            properties = sysfs_options.properties(
-                bond.master, filter_out_properties=('mode',)
-            )
-            assert 'mode' not in properties
-            assert len(properties) > 1
+    def test_bond_properties_with_filter_out(self, bond0):
+        properties = sysfs_options.properties(
+            bond0.master, filter_out_properties=('mode',)
+        )
+        assert 'mode' not in properties
+        assert len(properties) > 1
 
 
 class TestBondingSysfsOptionsMapper(object):
