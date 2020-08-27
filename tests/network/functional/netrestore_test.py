77f174b34a92bd47b051c252e1c5b799f6559840
Andrej Cernek <acernek@redhat.com>
2020-08-27 10:01:40 +0200

    net, tests: clearly separate test and setup in netrestore_test
    Change-Id: Ib81d519bc463214055b65fe59b026e6ef50af48a
    Signed-off-by: Andrej Cernek <acernek@redhat.com>

--- tests/network/functional/netrestore_test.py
+++ tests/network/functional/netrestore_test.py
@@ -22,7 +22,7 @@ from .netfunctestlib import NOCHK
 from .netfunctestlib import parametrize_bridged
 from .netfunctestlib import parametrize_switch
 from network.compat import mock
-from network.nettestlib import dummy_devices
+from network.nettestlib import dummy_device
 from network.nettestlib import veth_pair
 
 from vdsm.network import netrestore
@@ -36,62 +36,68 @@ IPv4_PREFIX_LEN = '24'
 NETWORK_NAME = 'test-network'
 
 
+@pytest.fixture
+def nic0():
+    with dummy_device() as nic:
+        yield nic
+
+
+@pytest.fixture
+def nic1():
+    with dummy_device() as nic:
+        yield nic
+
+
 @pytest.mark.ovs_switch
 class TestRestoreOvsBond(object):
     @mock.patch.object(netrestore, 'NETS_RESTORED_MARK', 'does/not/exist')
-    def test_restore_bond(self, adapter):
-        with dummy_devices(2) as (nic1, nic2):
-            BONDCREATE = {BOND_NAME: {'nics': [nic1, nic2], 'switch': 'ovs'}}
+    def test_restore_bond(self, adapter, nic0, nic1):
+        BONDCREATE = {BOND_NAME: {'nics': [nic0, nic1], 'switch': 'ovs'}}
 
-            with adapter.reset_persistent_config():
-                with adapter.setupNetworks({}, BONDCREATE, NOCHK):
-                    adapter.setSafeNetworkConfig()
+        with adapter.reset_persistent_config():
+            with adapter.setupNetworks({}, BONDCREATE, NOCHK):
+                adapter.setSafeNetworkConfig()
 
-                    Bond(BOND_NAME).destroy()
+                Bond(BOND_NAME).destroy()
 
-                    netrestore.init_nets()
+                netrestore.init_nets()
 
-                    adapter.update_netinfo()
-                    adapter.assertBond(BOND_NAME, BONDCREATE[BOND_NAME])
+                adapter.update_netinfo()
+                adapter.assertBond(BOND_NAME, BONDCREATE[BOND_NAME])
 
 
 @pytest.mark.legacy_switch
 @pytest.mark.nmstate
 class TestRestoreLegacyBridge(object):
-    def test_restore_bridge_with_custom_opts(self, adapter):
-        with dummy_devices(1) as (nic,):
-            CUSTOM_OPTS1 = {
-                'bridge_opts': 'multicast_router=0 multicast_snooping=0'
+    def test_restore_bridge_with_custom_opts(self, adapter, nic0):
+        CUSTOM_OPTS1 = {
+            'bridge_opts': 'multicast_router=0 multicast_snooping=0'
+        }
+        CUSTOM_OPTS2 = {
+            'bridge_opts': 'multicast_router=0 multicast_snooping=1'
+        }
+        NETCREATE = {
+            NETWORK_NAME: {
+                'nic': nic0,
+                'switch': 'legacy',
+                'custom': CUSTOM_OPTS1,
             }
-            CUSTOM_OPTS2 = {
-                'bridge_opts': 'multicast_router=0 multicast_snooping=1'
+        }
+        NETEDIT = {
+            NETWORK_NAME: {
+                'nic': nic0,
+                'switch': 'legacy',
+                'custom': CUSTOM_OPTS2,
             }
-            NETCREATE = {
-                NETWORK_NAME: {
-                    'nic': nic,
-                    'switch': 'legacy',
-                    'custom': CUSTOM_OPTS1,
-                }
-            }
-            NETEDIT = {
-                NETWORK_NAME: {
-                    'nic': nic,
-                    'switch': 'legacy',
-                    'custom': CUSTOM_OPTS2,
-                }
-            }
-            with adapter.reset_persistent_config():
-                with adapter.setupNetworks(NETCREATE, {}, NOCHK):
-                    adapter.setSafeNetworkConfig()
-                    adapter.setupNetworks(NETEDIT, {}, NOCHK)
-                    adapter.assertBridgeOpts(
-                        NETWORK_NAME, NETEDIT[NETWORK_NAME]
-                    )
+        }
+        with adapter.reset_persistent_config():
+            with adapter.setupNetworks(NETCREATE, {}, NOCHK):
+                adapter.setSafeNetworkConfig()
+                adapter.setupNetworks(NETEDIT, {}, NOCHK)
+                adapter.assertBridgeOpts(NETWORK_NAME, NETEDIT[NETWORK_NAME])
 
-                    adapter.restore_nets()
-                    adapter.assertBridgeOpts(
-                        NETWORK_NAME, NETCREATE[NETWORK_NAME]
-                    )
+                adapter.restore_nets()
+                adapter.assertBridgeOpts(NETWORK_NAME, NETCREATE[NETWORK_NAME])
 
 
 @parametrize_switch
@@ -99,28 +105,23 @@ class TestRestoreLegacyBridge(object):
 class TestRestore(object):
     @parametrize_bridged
     def test_restore_missing_network_from_config(
-        self, adapter, switch, bridged
+        self, adapter, switch, bridged, nic0
     ):
-        with dummy_devices(1) as (nic,):
-            SETUP_NET = {
-                NETWORK_NAME: {
-                    'nic': nic,
-                    'bridged': bridged,
-                    'switch': switch,
-                }
-            }
-            REMOVE_NET = {NETWORK_NAME: {'remove': True}}
+        SETUP_NET = {
+            NETWORK_NAME: {'nic': nic0, 'bridged': bridged, 'switch': switch}
+        }
+        REMOVE_NET = {NETWORK_NAME: {'remove': True}}
 
-            with adapter.reset_persistent_config():
-                with adapter.setupNetworks(SETUP_NET, {}, NOCHK):
-                    adapter.setSafeNetworkConfig()
-                    adapter.setupNetworks(REMOVE_NET, {}, NOCHK)
+        with adapter.reset_persistent_config():
+            with adapter.setupNetworks(SETUP_NET, {}, NOCHK):
+                adapter.setSafeNetworkConfig()
+                adapter.setupNetworks(REMOVE_NET, {}, NOCHK)
 
-                    adapter.assertNoNetworkExists(NETWORK_NAME)
+                adapter.assertNoNetworkExists(NETWORK_NAME)
 
-                    adapter.restore_nets()
+                adapter.restore_nets()
 
-                    adapter.assertNetworkExists(NETWORK_NAME)
+                adapter.assertNetworkExists(NETWORK_NAME)
 
     @parametrize_bridged
     def test_restore_missing_dynamic_ipv4_network(
@@ -167,65 +168,58 @@ class TestRestore(object):
 
     @parametrize_bridged
     def test_restore_network_static_ip_from_config(
-        self, adapter, switch, bridged
+        self, adapter, switch, bridged, nic0
     ):
-        with dummy_devices(1) as (nic,):
-            NET_WITH_IP_ATTRS = {
-                'nic': nic,
-                'bridged': bridged,
-                'ipaddr': IPv4_ADDRESS,
-                'prefix': IPv4_PREFIX_LEN,
-                'switch': switch,
-            }
-            NET_WITHOUT_IP_ATTRS = {
-                'nic': nic,
-                'bridged': bridged,
-                'switch': switch,
-            }
-            NET_WITH_IP = {NETWORK_NAME: NET_WITH_IP_ATTRS}
-            NET_WITHOUT_IP = {NETWORK_NAME: NET_WITHOUT_IP_ATTRS}
+        NET_WITH_IP_ATTRS = {
+            'nic': nic0,
+            'bridged': bridged,
+            'ipaddr': IPv4_ADDRESS,
+            'prefix': IPv4_PREFIX_LEN,
+            'switch': switch,
+        }
+        NET_WITHOUT_IP_ATTRS = {
+            'nic': nic0,
+            'bridged': bridged,
+            'switch': switch,
+        }
+        NET_WITH_IP = {NETWORK_NAME: NET_WITH_IP_ATTRS}
+        NET_WITHOUT_IP = {NETWORK_NAME: NET_WITHOUT_IP_ATTRS}
 
-            with adapter.reset_persistent_config():
-                with adapter.setupNetworks(NET_WITH_IP, {}, NOCHK):
-                    adapter.setSafeNetworkConfig()
-                    adapter.setupNetworks(NET_WITHOUT_IP, {}, NOCHK)
+        with adapter.reset_persistent_config():
+            with adapter.setupNetworks(NET_WITH_IP, {}, NOCHK):
+                adapter.setSafeNetworkConfig()
+                adapter.setupNetworks(NET_WITHOUT_IP, {}, NOCHK)
 
-                    adapter.assertNetworkIp(NETWORK_NAME, NET_WITHOUT_IP_ATTRS)
+                adapter.assertNetworkIp(NETWORK_NAME, NET_WITHOUT_IP_ATTRS)
 
-                    adapter.restore_nets()
+                adapter.restore_nets()
 
-                    adapter.assertNetworkIp(NETWORK_NAME, NET_WITH_IP_ATTRS)
+                adapter.assertNetworkIp(NETWORK_NAME, NET_WITH_IP_ATTRS)
 
-    def test_restore_missing_bond(self, adapter, switch):
-        with dummy_devices(2) as (nic1, nic2):
-            BONDCREATE = {BOND_NAME: {'nics': [nic1, nic2], 'switch': switch}}
-            BONDREMOVE = {BOND_NAME: {'remove': True}}
+    def test_restore_missing_bond(self, adapter, switch, nic0, nic1):
+        BONDCREATE = {BOND_NAME: {'nics': [nic0, nic1], 'switch': switch}}
+        BONDREMOVE = {BOND_NAME: {'remove': True}}
 
-            with adapter.reset_persistent_config():
-                with adapter.setupNetworks({}, BONDCREATE, NOCHK):
-                    adapter.setSafeNetworkConfig()
-                    adapter.setupNetworks({}, BONDREMOVE, NOCHK)
+        with adapter.reset_persistent_config():
+            with adapter.setupNetworks({}, BONDCREATE, NOCHK):
+                adapter.setSafeNetworkConfig()
+                adapter.setupNetworks({}, BONDREMOVE, NOCHK)
 
-                    adapter.restore_nets()
+                adapter.restore_nets()
 
-                    adapter.assertBond(BOND_NAME, BONDCREATE[BOND_NAME])
+                adapter.assertBond(BOND_NAME, BONDCREATE[BOND_NAME])
 
     @parametrize_bridged
     def test_restore_removes_unpersistent_network(
-        self, adapter, switch, bridged
+        self, adapter, switch, bridged, nic0
     ):
-        with dummy_devices(1) as (nic,):
-            SETUP_NET = {
-                NETWORK_NAME: {
-                    'nic': nic,
-                    'bridged': bridged,
-                    'switch': switch,
-                }
-            }
+        SETUP_NET = {
+            NETWORK_NAME: {'nic': nic0, 'bridged': bridged, 'switch': switch}
+        }
 
-            with adapter.reset_persistent_config():
-                with adapter.setupNetworks(SETUP_NET, {}, NOCHK):
+        with adapter.reset_persistent_config():
+            with adapter.setupNetworks(SETUP_NET, {}, NOCHK):
 
-                    adapter.restore_nets()
+                adapter.restore_nets()
 
-                    adapter.assertNoNetworkExists(NETWORK_NAME)
+                adapter.assertNoNetworkExists(NETWORK_NAME)
