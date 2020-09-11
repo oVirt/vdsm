81fccfcc9d9fc0109ce95275a54941939f555e36
Ales Musil <amusil@redhat.com>
2020-09-11 11:41:41 +0200

    net, tests: Enable switch type change tests
    Switch type change should be possible for basic scenarios.
    Enable functional tests to test all provided scenarios.
    
    Change-Id: Ia15306bf9822e183660193d16275ee5263401363
    Bug-Url: https://bugzilla.redhat.com/1809102
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- tests/network/functional/switch_type_change_test.py
+++ tests/network/functional/switch_type_change_test.py
@@ -97,6 +97,7 @@ def dynamic_ipv4_iface():
             yield client
 
 
+@pytest.mark.nmstate
 @parametrize_switch_change
 class TestBasicSwitchChange(object):
     def test_switch_change_basic_network(self, adapter, sw_src, sw_dst, nic0):
@@ -135,6 +136,7 @@ class TestBasicSwitchChange(object):
             adapter.assertBond(BOND_NAME, BONDSETUP_TARGET[BOND_NAME])
 
 
+@pytest.mark.nmstate
 @parametrize_switch_change
 class TestIpSwitch(object):
     def test_switch_change_bonded_network_with_static_ip(
@@ -183,6 +185,7 @@ class TestIpSwitch(object):
             adapter.assertBond(BOND_NAME, BONDSETUP_TARGET[BOND_NAME])
 
 
+@pytest.mark.nmstate
 @parametrize_switch_change
 class TestSwitchRollback(object):
     def test_rollback_target_configuration_with_invalid_ip(
@@ -251,6 +254,7 @@ class TestSwitchRollback(object):
             adapter.assertNetwork(NET2_NAME, NETSETUP_SOURCE[NET2_NAME])
 
 
+@pytest.mark.nmstate
 @parametrize_switch_change
 class TestSwitchValidation(object):
     def test_switch_change_with_not_all_existing_networks_specified(
