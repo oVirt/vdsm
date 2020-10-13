16c494c74bb115f4382ec4f7d3d4217fd2e0ec39
Vojtech Juranek <vjuranek@redhat.com>
2020-10-13 10:25:55 +0200

    tests: convert vmfakelib_test to pytest
    Change-Id: I6b0fb2a0ae7617cf65684c114d0c3a95ef4e1fc0
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- tests/vmfakelib_test.py
+++ tests/vmfakelib_test.py
@@ -18,15 +18,13 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
-from __future__ import absolute_import
-from __future__ import division
-
 import libvirt
-from testlib import VdsmTestCase
+import pytest
+
 import vmfakelib
 
 
-class SecretsTests(VdsmTestCase):
+class SecretsTests:
 
     def test_define_new(self):
         con = vmfakelib.Connection()
@@ -40,10 +38,10 @@ class SecretsTests(VdsmTestCase):
         """
         con.secretDefineXML(xml)
         sec = con.secrets['uuid']
-        self.assertEqual(sec.uuid, "uuid")
-        self.assertEqual(sec.usage_type, "ceph")
-        self.assertEqual(sec.usage_id, "name")
-        self.assertEqual(sec.description, None)
+        assert sec.uuid == "uuid"
+        assert sec.usage_type == "ceph"
+        assert sec.usage_id == "name"
+        assert sec.description is None
 
     def test_define_new_with_description(self):
         con = vmfakelib.Connection()
@@ -58,7 +56,7 @@ class SecretsTests(VdsmTestCase):
         """
         con.secretDefineXML(xml)
         sec = con.secrets['uuid']
-        self.assertEqual(sec.description, "description")
+        assert sec.description == "description"
 
     def test_define_replace(self):
         con = vmfakelib.Connection()
@@ -83,7 +81,7 @@ class SecretsTests(VdsmTestCase):
         con.secretDefineXML(xml1)
         con.secretDefineXML(xml2)
         sec = con.secrets['uuid']
-        self.assertEqual(sec.description, "new description")
+        assert sec.description == "new description"
 
     def test_define_cannot_change_usage_id(self):
         con = vmfakelib.Connection()
@@ -104,13 +102,9 @@ class SecretsTests(VdsmTestCase):
         </secret>
         """
         con.secretDefineXML(xml1)
-        try:
+        with pytest.raises(libvirt.libvirtError) as e:
             con.secretDefineXML(xml2)
-        except libvirt.libvirtError as e:
-            self.assertEqual(e.get_error_code(),
-                             libvirt.VIR_ERR_INTERNAL_ERROR)
-        else:
-            self.fail("libvirtError was not raised")
+        assert e.value.get_error_code() == libvirt.VIR_ERR_INTERNAL_ERROR
 
     def test_define_usage_not_unique(self):
         con = vmfakelib.Connection()
@@ -131,13 +125,9 @@ class SecretsTests(VdsmTestCase):
         </secret>
         """
         con.secretDefineXML(xml1)
-        try:
+        with pytest.raises(libvirt.libvirtError) as e:
             con.secretDefineXML(xml2)
-        except libvirt.libvirtError as e:
-            self.assertEqual(e.get_error_code(),
-                             libvirt.VIR_ERR_INTERNAL_ERROR)
-        else:
-            self.fail("libvirtError was not raised")
+        assert e.value.get_error_code() == libvirt.VIR_ERR_INTERNAL_ERROR
 
     def test_lookup(self):
         con = vmfakelib.Connection()
@@ -151,13 +141,10 @@ class SecretsTests(VdsmTestCase):
         """
         con.secretDefineXML(xml)
         sec = con.secretLookupByUUIDString('uuid')
-        self.assertEqual(sec.usage_id, "name")
+        assert sec.usage_id == "name"
 
     def test_lookup_error(self):
         con = vmfakelib.Connection()
-        try:
+        with pytest.raises(libvirt.libvirtError) as e:
             con.secretLookupByUUIDString('no-such-uuid')
-        except libvirt.libvirtError as e:
-            self.assertEqual(e.get_error_code(), libvirt.VIR_ERR_NO_SECRET)
-        else:
-            self.fail("libvirtError was not raised")
+        assert e.value.get_error_code() == libvirt.VIR_ERR_NO_SECRET
