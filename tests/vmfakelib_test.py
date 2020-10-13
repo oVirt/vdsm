510f712c642087912aa5ddbe2969478cf323164e
Vojtech Juranek <vjuranek@redhat.com>
2020-10-13 10:39:46 +0200

    tests: make vmfakelib_test tests simple functions
    Remove test class from vmfakelib_test and make tests simple functions.
    
    Change-Id: I8717a54b7dceb4d08d9318893eca4af4debc5b15
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- tests/vmfakelib_test.py
+++ tests/vmfakelib_test.py
@@ -24,127 +24,131 @@ import pytest
 import vmfakelib
 
 
-class SecretsTests:
+def test_secret_define_new():
+    con = vmfakelib.Connection()
+    xml = """
+    <secret>
+        <uuid>uuid</uuid>
+        <usage type="ceph">
+            <name>name</name>
+        </usage>
+    </secret>
+    """
+    con.secretDefineXML(xml)
+    sec = con.secrets['uuid']
+    assert sec.uuid == "uuid"
+    assert sec.usage_type == "ceph"
+    assert sec.usage_id == "name"
+    assert sec.description is None
 
-    def test_define_new(self):
-        con = vmfakelib.Connection()
-        xml = """
-        <secret>
-            <uuid>uuid</uuid>
-            <usage type="ceph">
-                <name>name</name>
-            </usage>
-        </secret>
-        """
-        con.secretDefineXML(xml)
-        sec = con.secrets['uuid']
-        assert sec.uuid == "uuid"
-        assert sec.usage_type == "ceph"
-        assert sec.usage_id == "name"
-        assert sec.description is None
 
-    def test_define_new_with_description(self):
-        con = vmfakelib.Connection()
-        xml = """
-        <secret>
-            <description>description</description>
-            <uuid>uuid</uuid>
-            <usage type="ceph">
-                <name>name</name>
-            </usage>
-        </secret>
-        """
-        con.secretDefineXML(xml)
-        sec = con.secrets['uuid']
-        assert sec.description == "description"
+def test_secret_define_new_with_description():
+    con = vmfakelib.Connection()
+    xml = """
+    <secret>
+        <description>description</description>
+        <uuid>uuid</uuid>
+        <usage type="ceph">
+            <name>name</name>
+        </usage>
+    </secret>
+    """
+    con.secretDefineXML(xml)
+    sec = con.secrets['uuid']
+    assert sec.description == "description"
 
-    def test_define_replace(self):
-        con = vmfakelib.Connection()
-        xml1 = """
-        <secret>
-            <description>old description</description>
-            <uuid>uuid</uuid>
-            <usage type="ceph">
-                <name>name</name>
-            </usage>
-        </secret>
-        """
-        xml2 = """
-        <secret>
-            <description>new description</description>
-            <uuid>uuid</uuid>
-            <usage type="ceph">
-                <name>name</name>
-            </usage>
-        </secret>
-        """
-        con.secretDefineXML(xml1)
+
+def test_secret_define_replace():
+    con = vmfakelib.Connection()
+    xml1 = """
+    <secret>
+        <description>old description</description>
+        <uuid>uuid</uuid>
+        <usage type="ceph">
+            <name>name</name>
+        </usage>
+    </secret>
+    """
+    xml2 = """
+    <secret>
+        <description>new description</description>
+        <uuid>uuid</uuid>
+        <usage type="ceph">
+            <name>name</name>
+        </usage>
+    </secret>
+    """
+    con.secretDefineXML(xml1)
+    con.secretDefineXML(xml2)
+    sec = con.secrets['uuid']
+    assert sec.description == "new description"
+
+
+def test_secret_define_cannot_change_usage_id():
+    con = vmfakelib.Connection()
+    xml1 = """
+    <secret>
+        <uuid>uuid</uuid>
+        <usage type="ceph">
+            <name>name 1</name>
+        </usage>
+    </secret>
+    """
+    xml2 = """
+    <secret>
+        <uuid>uuid</uuid>
+        <usage type="ceph">
+            <name>name 2</name>
+        </usage>
+    </secret>
+    """
+    con.secretDefineXML(xml1)
+    with pytest.raises(libvirt.libvirtError) as e:
         con.secretDefineXML(xml2)
-        sec = con.secrets['uuid']
-        assert sec.description == "new description"
+    assert e.value.get_error_code() == libvirt.VIR_ERR_INTERNAL_ERROR
 
-    def test_define_cannot_change_usage_id(self):
-        con = vmfakelib.Connection()
-        xml1 = """
-        <secret>
-            <uuid>uuid</uuid>
-            <usage type="ceph">
-                <name>name 1</name>
-            </usage>
-        </secret>
-        """
-        xml2 = """
-        <secret>
-            <uuid>uuid</uuid>
-            <usage type="ceph">
-                <name>name 2</name>
-            </usage>
-        </secret>
-        """
-        con.secretDefineXML(xml1)
-        with pytest.raises(libvirt.libvirtError) as e:
-            con.secretDefineXML(xml2)
-        assert e.value.get_error_code() == libvirt.VIR_ERR_INTERNAL_ERROR
 
-    def test_define_usage_not_unique(self):
-        con = vmfakelib.Connection()
-        xml1 = """
-        <secret>
-            <uuid>uuid 1</uuid>
-            <usage type="ceph">
-                <name>name</name>
-            </usage>
-        </secret>
-        """
-        xml2 = """
-        <secret>
-            <uuid>uuid 2</uuid>
-            <usage type="ceph">
-                <name>name</name>
-            </usage>
-        </secret>
-        """
-        con.secretDefineXML(xml1)
-        with pytest.raises(libvirt.libvirtError) as e:
-            con.secretDefineXML(xml2)
-        assert e.value.get_error_code() == libvirt.VIR_ERR_INTERNAL_ERROR
+def test_secret_define_usage_not_unique():
+    con = vmfakelib.Connection()
+    xml1 = """
+    <secret>
+        <uuid>uuid 1</uuid>
+        <usage type="ceph">
+            <name>name</name>
+        </usage>
+    </secret>
+    """
+    xml2 = """
+    <secret>
+        <uuid>uuid 2</uuid>
+        <usage type="ceph">
+            <name>name</name>
+        </usage>
+    </secret>
+    """
+    con.secretDefineXML(xml1)
+    with pytest.raises(libvirt.libvirtError) as e:
+        con.secretDefineXML(xml2)
+    assert e.value.get_error_code() == libvirt.VIR_ERR_INTERNAL_ERROR
 
-    def test_lookup(self):
-        con = vmfakelib.Connection()
-        xml = """
-        <secret>
-            <uuid>uuid</uuid>
-            <usage type="ceph">
-                <name>name</name>
-            </usage>
-        </secret>
-        """
-        con.secretDefineXML(xml)
-        sec = con.secretLookupByUUIDString('uuid')
-        assert sec.usage_id == "name"
 
-    def test_lookup_error(self):
-        con = vmfakelib.Connection()
-        with pytest.raises(libvirt.libvirtError) as e:
-            con.secretLookupByUUIDString('no-such-uuid')
-        assert e.value.get_error_code() == libvirt.VIR_ERR_NO_SECRET
+def test_secret_lookup():
+    con = vmfakelib.Connection()
+    xml = """
+    <secret>
+        <uuid>uuid</uuid>
+        <usage type="ceph">
+            <name>name</name>
+        </usage>
+    </secret>
+    """
+    con.secretDefineXML(xml)
+    sec = con.secretLookupByUUIDString('uuid')
+    assert sec.usage_id == "name"
+
+
+def test_secret_lookup_error():
+    con = vmfakelib.Connection()
+    with pytest.raises(libvirt.libvirtError) as e:
+        con.secretLookupByUUIDString('no-such-uuid')
+    assert e.value.get_error_code() == libvirt.VIR_ERR_NO_SECRET
