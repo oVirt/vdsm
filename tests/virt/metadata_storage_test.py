81b0c4097a47fb4c8e8e6fea6dc85414b88ecce7
Vojtech Juranek <vjuranek@redhat.com>
2020-11-18 17:19:28 +0100

    tests: fix metadata_storage_test
    Running metadata_storage_test with python 3.8, many tests fail with
    
        E   AssertionError: '<?xm[53 chars]vice devtype="disk" name="vda">\n        <devi[689 chars]m>\n' != '<?xm[53 chars]vice name="vda" devtype="disk">\n        <devi[689 chars]m>\n'
        E     <?xml version='1.0' encoding='utf-8'?>
        E     <vm>
        E   -     <device devtype="disk" name="vda">
        E   +     <device name="vda" devtype="disk">
    
    as the identifying attrs is a dict with keys "devtype" and "name" (in
    this order), in this cases specified in
    _get_drive_conf_identifying_attrs(), while all expected XML samples use
    ordering "name" and "devtype". Fix the expected XML samples to use
    correct ordering.
    
    Tests are passing on CI as it used older python version. Starting python
    3.8, XML attributes are kept in the same order as provided by user,
    while in previous versions attributes were orders lexically, see [1] for
    more details.
    
    [1] https://bugs.python.org/issue34160
    
    Change-Id: I69e8741b9e0fbba43502b0f1f02ad685781e6369
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- tests/virt/metadata_storage_test.py
+++ tests/virt/metadata_storage_test.py
@@ -63,7 +63,7 @@ _CDROM_DATA = _TestData(
     },
     metadata_xml="""<?xml version='1.0' encoding='UTF-8'?>
     <vm>
-    <device name="hdc" devtype="disk">
+    <device devtype="disk" name="hdc">
         <device>cdrom</device>
         <deviceId>e59c985c-46c2-4489-b355-a6f374125eb9</deviceId>
         <iface>ide</iface>
@@ -109,7 +109,7 @@ _CDROM_PAYLOAD_DATA = _TestData(
     },
     metadata_xml="""<?xml version='1.0' encoding='UTF-8'?>
     <vm>
-    <device name="hdd" devtype="disk">
+    <device devtype="disk" name="hdd">
         <device>cdrom</device>
         <deviceId>423af2b3-5d02-44c5-9d2e-9e69de6eef44</deviceId>
         <iface>ide</iface>
@@ -166,7 +166,7 @@ _DISK_DATA = _TestData(
     },
     metadata_xml="""<?xml version='1.0' encoding='UTF-8'?>
     <vm>
-    <device name="vda" devtype="disk">
+    <device devtype="disk" name="vda">
         <bootOrder>1</bootOrder>
         <device>disk</device>
         <deviceId>66441539-f7ac-4946-8a25-75e422f939d4</deviceId>
@@ -232,7 +232,7 @@ _DISK_DATA_CUSTOM = _TestData(
     },
     metadata_xml="""<?xml version='1.0' encoding='UTF-8'?>
     <vm>
-    <device name="vdb" devtype="disk">
+    <device  devtype="disk" name="vdb">
         <bootOrder>2</bootOrder>
         <device>disk</device>
         <deviceId>b4f8d9d7-2701-47ae-ab9a-d1e0194eb796</deviceId>
@@ -287,7 +287,7 @@ _DISK_DATA_SGIO = _TestData(
     },
     metadata_xml="""<?xml version='1.0' encoding='UTF-8'?>
     <vm>
-    <device name="sdb" devtype="disk">
+    <device devtype="disk" name="sdb">
         <GUID>36001405e9bebaa680864c98a280e6544</GUID>
         <device>lun</device>
         <deviceId>07749931-667c-4388-8ba5-4f63ad84a0d7</deviceId>
@@ -346,7 +346,7 @@ _DISK_DATA_NETWORK = _TestData(
     },
     metadata_xml="""<?xml version='1.0' encoding='UTF-8'?>
     <vm>
-    <device name="vda" devtype="disk">
+    <device devtype="disk" name="vda">
         <device>disk</device>
         <diskType>network</diskType>
         <format>raw</format>
@@ -403,7 +403,7 @@ _DISK_DATA_REPLICA = _TestData(
     },
     metadata_xml="""<?xml version='1.0' encoding='UTF-8'?>
     <vm>
-    <device name="vda" devtype="disk">
+    <device devtype="disk" name="vda">
         <device>disk</device>
         <format>cow</format>
         <iface>virtio</iface>
