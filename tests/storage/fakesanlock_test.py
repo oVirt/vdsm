8a445c56721f236b886df5e66dc50b792d5d9681
Benny Zlotnik <bzlotnik@redhat.com>
2020-12-06 19:41:55 +0200

    fakesanlock: add lvb support
    To match sanlock behavior[1]: support the lvb flag in fakesanlock#acquire, add set_lv and get_lvb
    
    [1] https://lists.fedorahosted.org/archives/list/sanlock-devel@lists.fedorahosted.org/thread/ZFKKAHPBZCIJ5ZF45X5IV7ILK7AIMXYQ/
    
    Change-Id: Iacf03c886fdf9f56ef410f38ba61bbdf3ed467fe
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- tests/storage/fakesanlock_test.py
+++ tests/storage/fakesanlock_test.py
@@ -218,6 +218,7 @@ def test_write_read_resource():
         "acquired": False,
         "align": sc.ALIGNMENT_1M,
         "sector": sc.BLOCK_SIZE_512,
+        "lvb": False,
     }
     assert info == expected
 
@@ -546,6 +547,7 @@ def test_write_resource():
         "acquired": False,
         "align": sc.ALIGNMENT_1M,
         "sector": sc.BLOCK_SIZE_512,
+        "lvb": False,
     }
     assert info == expected
 
@@ -720,3 +722,41 @@ def test_read_resource_owners_validate_bytes():
     with pytest.raises(TypeError):
         fs.read_resource_owners(
             LOCKSPACE_NAME, u"resource_name", [("path", 0)], sector=0)
+
+
+def test_set_lvb_after_acquire():
+    fs = FakeSanlock()
+    fs.write_lockspace(LOCKSPACE_NAME, "path")
+    fs.write_resource(LOCKSPACE_NAME, RESOURCE_NAME, [("path", MiB)])
+    fs.add_lockspace(LOCKSPACE_NAME, 1, "path")
+    fd = fs.register()
+    fs.acquire(LOCKSPACE_NAME, RESOURCE_NAME, [("path", MiB)], slkfd=fd,
+               lvb=True)
+    data = b"{generation:0}"
+    fs.set_lvb(LOCKSPACE_NAME, RESOURCE_NAME, [("path", MiB)], data)
+    res = fs.read_resource("path", MiB)
+
+    assert res["lvb"]
+
+    lvb_data = fs.get_lvb(LOCKSPACE_NAME, RESOURCE_NAME, [("path", MiB)])
+    assert lvb_data.rstrip(b"\0") == data
+
+
+def test_set_lvb_without_acquire():
+    fs = FakeSanlock()
+    fs.write_lockspace(LOCKSPACE_NAME, "path")
+    fs.write_resource(LOCKSPACE_NAME, RESOURCE_NAME, [("path", MiB)])
+    fs.add_lockspace(LOCKSPACE_NAME, 1, "path")
+
+    data = b"{generation:0}"
+    with pytest.raises(fs.SanlockException) as e:
+        fs.set_lvb(LOCKSPACE_NAME, RESOURCE_NAME, [("path", MiB)], data)
+    assert e.value.errno == errno.ENOENT
+
+
+def test_set_lvb_without_lockspace():
+    fs = FakeSanlock()
+    data = b"{generation:0}"
+    with pytest.raises(fs.SanlockException) as e:
+        fs.set_lvb(LOCKSPACE_NAME, RESOURCE_NAME, [("path", MiB)], data)
+    assert e.value.errno == errno.ENOSPC
