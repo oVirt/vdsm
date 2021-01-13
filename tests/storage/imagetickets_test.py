e1d7f4f43e116cdb74cfcc13ca766d7bd8fd3994
Vojtech Juranek <vjuranek@redhat.com>
2021-01-13 18:16:04 +0100

    tests: convert tests from TestImageTickets into functions
    As we converted imagetickets test to pytest in previous patch, there's
    no need to group the tests in TestImageTickets class as all the tests
    are for image tickets. Remove this class and define the tests as orinary
    functions.
    
    Change-Id: Ic810e6a73043990a4a4e4e2845bad0f782ab7cdc
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- tests/storage/imagetickets_test.py
+++ tests/storage/imagetickets_test.py
@@ -85,137 +85,147 @@ def fake_connection(monkeypatch):
         imagetickets, "UnixHTTPConnection", FakeUnixHTTPConnection())
 
 
-class TestImageTickets:
+@pytest.mark.parametrize("method, args", [
+    ["add_ticket", [{}]],
+    ["extend_ticket", ["uuid", 300]],
+    ["remove_ticket", ["uuid"]],
+])
+def test_not_supported(invalid_socket, method, args):
+    with pytest.raises(se.ImageDaemonUnsupported):
+        func = getattr(imagetickets, method)
+        func(*args)
 
-    @pytest.mark.parametrize("method, args", [
-        ["add_ticket", [{}]],
-        ["extend_ticket", ["uuid", 300]],
-        ["remove_ticket", ["uuid"]],
-    ])
-    def test_not_supported(self, invalid_socket, method, args):
-        with pytest.raises(se.ImageDaemonUnsupported):
-            func = getattr(imagetickets, method)
-            func(*args)
 
-    def test_add_ticket(self, fake_connection):
-        ticket = create_ticket(uuid="uuid")
-        body = json.dumps(ticket).encode("utf8")
-        expected = [
-            ("request", ("PUT", "/tickets/uuid"), {"body": body}),
-        ]
+def test_add_ticket(fake_connection):
+    ticket = create_ticket(uuid="uuid")
+    body = json.dumps(ticket).encode("utf8")
+    expected = [
+        ("request", ("PUT", "/tickets/uuid"), {"body": body}),
+    ]
+    imagetickets.add_ticket(ticket)
+    assert imagetickets.UnixHTTPConnection.__calls__ == expected
+    assert imagetickets.UnixHTTPConnection.closed
+
+
+def test_get_ticket(fake_connection):
+    filename = u"\u05d0.raw"  # hebrew aleph
+    ticket = create_ticket(uuid="uuid", filename=filename)
+    data = json.dumps(ticket).encode("utf8")
+    imagetickets.UnixHTTPConnection.response = FakeResponse(data=data)
+    expected = [
+        ("request", ("GET", "/tickets/uuid"), {"body": None}),
+    ]
+    result = imagetickets.get_ticket(ticket_id="uuid")
+    assert result == ticket
+    assert imagetickets.UnixHTTPConnection.__calls__ == expected
+    assert imagetickets.UnixHTTPConnection.closed
+
+
+def test_extend_ticket(fake_connection):
+    timeout = 300
+    imagetickets.extend_ticket("uuid", timeout)
+    body = '{"timeout": ' + str(timeout) + '}'
+    expected = [
+        ("request", ("PATCH", "/tickets/uuid"),
+         {"body": body.encode("utf8")}),
+    ]
+
+    assert imagetickets.UnixHTTPConnection.__calls__ == expected
+    assert imagetickets.UnixHTTPConnection.closed
+
+
+def test_remove_ticket(fake_connection):
+    # New imageio daemon will not return Content-Length header, as
+    # specified in RFC 7230.
+    imagetickets.UnixHTTPConnection.response = FakeResponse(
+        status=204, reason="No Content", headers={})
+    imagetickets.remove_ticket("uuid")
+    expected = [
+        ("request", ("DELETE", "/tickets/uuid"), {"body": None}),
+    ]
+
+    assert imagetickets.UnixHTTPConnection.__calls__ == expected
+    assert imagetickets.UnixHTTPConnection.closed
+
+
+def test_remove_ticket_with_content_length(fake_connection):
+    # Legacy imageio daemon used to return "Content-Length: 0". This is not
+    # correct according to RFC 7230, but we must support it.
+    imagetickets.UnixHTTPConnection.response = FakeResponse(
+        status=204, reason="No Content")
+    imagetickets.remove_ticket("uuid")
+    expected = [
+        ("request", ("DELETE", "/tickets/uuid"), {"body": None}),
+    ]
+
+    assert imagetickets.UnixHTTPConnection.__calls__ == expected
+    assert imagetickets.UnixHTTPConnection.closed
+
+
+def test_res_header_error(fake_connection):
+    imagetickets.UnixHTTPConnection.response = FakeResponse(
+        status=300, headers={"content-length": "invalid"})
+    with pytest.raises(se.ImageDaemonError):
+        imagetickets.remove_ticket("uuid")
+
+
+def test_res_invalid_json_ret(fake_connection):
+    imagetickets.UnixHTTPConnection.response = FakeResponse(
+        status=300, data=b"not a json string")
+    with pytest.raises(se.ImageDaemonError):
+        imagetickets.remove_ticket("uuid")
+
+
+def test_image_daemon_error_ret(fake_connection):
+    imagetickets.UnixHTTPConnection.response = FakeResponse(
+        status=300, data=b'{"image_daemon_message":"content"}')
+    try:
+        imagetickets.remove_ticket("uuid")
+    except se.ImageDaemonError as e:
+        assert "image_daemon_message" in e.value
+        assert "content" in e.value
+
+
+def test_res_read_error(fake_connection):
+    imagetickets.UnixHTTPConnection.response = FakeResponse(
+        status=300, data=b'{"image_daemon_message":"ignored"}')
+    err_msg = "Environment error message"
+
+    def read(amt=None):
+        raise EnvironmentError(err_msg)
+
+    imagetickets.UnixHTTPConnection.response.read = read
+
+    with pytest.raises(se.ImageDaemonError) as e:
+        imagetickets.remove_ticket("uuid")
+        assert err_msg in e.value
+
+
+@pytest.mark.parametrize("exc_type", [
+    http.client.HTTPException, socket.error, OSError
+])
+def test_image_tickets_error(fake_connection, exc_type):
+    ticket = create_ticket(uuid="uuid")
+
+    def request(method, path, body=None):
+        raise exc_type
+
+    imagetickets.UnixHTTPConnection.request = request
+    with pytest.raises(se.ImageTicketsError):
         imagetickets.add_ticket(ticket)
-        assert imagetickets.UnixHTTPConnection.__calls__ == expected
-        assert imagetickets.UnixHTTPConnection.closed
 
-    def test_get_ticket(self, fake_connection):
-        filename = u"\u05d0.raw"  # hebrew aleph
-        ticket = create_ticket(uuid="uuid", filename=filename)
-        data = json.dumps(ticket).encode("utf8")
-        imagetickets.UnixHTTPConnection.response = FakeResponse(data=data)
-        expected = [
-            ("request", ("GET", "/tickets/uuid"), {"body": None}),
-        ]
-        result = imagetickets.get_ticket(ticket_id="uuid")
-        assert result == ticket
-        assert imagetickets.UnixHTTPConnection.__calls__ == expected
-        assert imagetickets.UnixHTTPConnection.closed
 
-    def test_extend_ticket(self, fake_connection):
-        timeout = 300
-        imagetickets.extend_ticket("uuid", timeout)
-        body = '{"timeout": ' + str(timeout) + '}'
-        expected = [
-            ("request", ("PATCH", "/tickets/uuid"),
-             {"body": body.encode("utf8")}),
-        ]
+def test_request_with_response(fake_connection):
+    ticket = create_ticket(uuid="uuid")
+    data = json.dumps(ticket).encode("utf8")
+    imagetickets.UnixHTTPConnection.response = FakeResponse(data=data)
+    response = imagetickets.request("GET", "uuid")
+    assert response == ticket
 
-        assert imagetickets.UnixHTTPConnection.__calls__ == expected
-        assert imagetickets.UnixHTTPConnection.closed
 
-    def test_remove_ticket(self, fake_connection):
-        # New imageio daemon will not return Content-Length header, as
-        # specified in RFC 7230.
-        imagetickets.UnixHTTPConnection.response = FakeResponse(
-            status=204, reason="No Content", headers={})
-        imagetickets.remove_ticket("uuid")
-        expected = [
-            ("request", ("DELETE", "/tickets/uuid"), {"body": None}),
-        ]
-
-        assert imagetickets.UnixHTTPConnection.__calls__ == expected
-        assert imagetickets.UnixHTTPConnection.closed
-
-    def test_remove_ticket_with_content_length(self, fake_connection):
-        # Legacy imageio daemon used to return "Content-Length: 0". This is not
-        # correct according to RFC 7230, but we must support it.
-        imagetickets.UnixHTTPConnection.response = FakeResponse(
-            status=204, reason="No Content")
-        imagetickets.remove_ticket("uuid")
-        expected = [
-            ("request", ("DELETE", "/tickets/uuid"), {"body": None}),
-        ]
-
-        assert imagetickets.UnixHTTPConnection.__calls__ == expected
-        assert imagetickets.UnixHTTPConnection.closed
-
-    def test_res_header_error(self, fake_connection):
-        imagetickets.UnixHTTPConnection.response = FakeResponse(
-            status=300, headers={"content-length": "invalid"})
-        with pytest.raises(se.ImageDaemonError):
-            imagetickets.remove_ticket("uuid")
-
-    def test_res_invalid_json_ret(self, fake_connection):
-        imagetickets.UnixHTTPConnection.response = FakeResponse(
-            status=300, data=b"not a json string")
-        with pytest.raises(se.ImageDaemonError):
-            imagetickets.remove_ticket("uuid")
-
-    def test_image_daemon_error_ret(self, fake_connection):
-        imagetickets.UnixHTTPConnection.response = FakeResponse(
-            status=300, data=b'{"image_daemon_message":"content"}')
-        try:
-            imagetickets.remove_ticket("uuid")
-        except se.ImageDaemonError as e:
-            assert "image_daemon_message" in e.value
-            assert "content" in e.value
-
-    def test_res_read_error(self, fake_connection):
-        imagetickets.UnixHTTPConnection.response = FakeResponse(
-            status=300, data=b'{"image_daemon_message":"ignored"}')
-        err_msg = "Environment error message"
-
-        def read(amt=None):
-            raise EnvironmentError(err_msg)
-
-        imagetickets.UnixHTTPConnection.response.read = read
-
-        with pytest.raises(se.ImageDaemonError) as e:
-            imagetickets.remove_ticket("uuid")
-            assert err_msg in e.value
-
-    @pytest.mark.parametrize("exc_type", [
-        http.client.HTTPException, socket.error, OSError
-    ])
-    def test_image_tickets_error(self, fake_connection, exc_type):
-        ticket = create_ticket(uuid="uuid")
-
-        def request(method, path, body=None):
-            raise exc_type
-
-        imagetickets.UnixHTTPConnection.request = request
-        with pytest.raises(se.ImageTicketsError):
-            imagetickets.add_ticket(ticket)
-
-    def test_request_with_response(self, fake_connection):
-        ticket = create_ticket(uuid="uuid")
-        data = json.dumps(ticket).encode("utf8")
-        imagetickets.UnixHTTPConnection.response = FakeResponse(data=data)
-        response = imagetickets.request("GET", "uuid")
-        assert response == ticket
-
-    def test_request_with_empty_dict_response(self, fake_connection):
-        response = imagetickets.request("DELETE", "uuid")
-        assert response == {}
+def test_request_with_empty_dict_response(fake_connection):
+    response = imagetickets.request("DELETE", "uuid")
+    assert response == {}
 
 
 def create_ticket(uuid, ops=("read", "write"), timeout=300,
