fea21e03251ad491dc537247804a2e048415cba7
Ales Musil <amusil@redhat.com>
2021-07-08 11:21:32 +0200

    net, tests: Cleanup node from leftover interfaces
    Cleanup any interface that might left on the node from
    previous network tests run.
    
    Change-Id: Ia5f260085ff98170788453df3b33a16519d48f4c
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- tests/network/integration/conftest.py
+++ tests/network/integration/conftest.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2020 Red Hat, Inc.
+# Copyright 2018-2021 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,20 +21,46 @@ from __future__ import division
 
 import logging
 import os
+import re
 
 import pytest
 
 from vdsm.network import cmd
 from vdsm.network import sourceroute
 from vdsm.network.ip import rule as ip_rule
+from vdsm.network.ipwrapper import getLinks
+
+from network.nettestlib import Interface
 
 IPV4_ADDRESS1 = '192.168.99.1'  # Tracking the address used in ip_rule_test
+
+TEST_NIC_REGEX = re.compile(
+    r'''
+    dummy_[a-zA-Z0-9]+|     # match dummy devices
+    veth_[a-zA-Z0-9]+|      # match veth devices
+    bond_[a-zA-Z0-9]+|      # match bond devices
+    test-network[0-9]*|br1|אבג[a-zA-Z0-9]*|     # match bridges
+    vdsm-[a-zA-Z0-9]*       # match any generic vdsm test interface
+    ''',
+    re.VERBOSE,
+)
 
 
 @pytest.fixture(scope='session', autouse=True)
 def requires_root():
     if os.geteuid() != 0:
         pytest.skip('Integration tests require root')
+
+
+@pytest.fixture(scope='session', autouse=True)
+def cleanup_leftover_interfaces():
+    for interface in getLinks():
+        if TEST_NIC_REGEX.match(interface.name):
+            logging.warning('Found leftover interface %s', interface)
+            try:
+                Interface.from_existing_dev_name(interface.name).remove()
+            except Exception as e:
+                logging.warning('Removal of "%s" failed: %s', interface, e)
 
 
 class StaleIPRulesError(Exception):
