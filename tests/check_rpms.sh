16aded05ff0c2cef96a406e98bdde6dfde8bf1c9
Marcin Sobczyk <msobczyk@redhat.com>
2021-09-20 13:17:49 +0200

    el9s: spec: Require 'crontabs'
    We have an hourly cron job distributed with vdsm [1], so we should
    require 'crontabs' package.
    
    This was found with 'rpmlint' in el9s, reported as
    'missing-dependency-to-cron'. The change in the spec doesn't fix the
    'rpmlint' run though, as the check in the shipped version  (1.11 at the
    moment of writing) is broken.  It requires a dependency to 'cron'
    package, but the usual name for the package providing cron functionality
    is 'cronie'. Furthermore, the dependency should actually be on
    'crontabs', which is fixed in newer versions of 'rpmlint' [2].
    
    [1] https://github.com/oVirt/vdsm/blob/b4bbb2df896d5ed15f4bc0c5ca4adff0335535e4/vdsm.spec.in#L870
    [2] https://github.com/rpm-software-management/rpmlint/pull/404
    
    Change-Id: Idf2b537e9ec0d6424319aeb3db78feacc12f9b94
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- tests/check_rpms.sh
+++ tests/check_rpms.sh
@@ -14,4 +14,6 @@ rpmlint "$ARTIFACTS_DIR/"*.src.rpm
     grep ': E: ' | \
     grep -v explicit-lib-dependency | \
     grep -v no-binary | \
-    grep -v non-readable | grep -v non-standard-dir-perm
+    grep -v non-readable | \
+    grep -v non-standard-dir-perm | \
+    grep -v missing-dependency-to-cron # FIXME: missing-dependency-to-cron check is broken in rpmlint 1.11, valid check is available since ver. 2.1
