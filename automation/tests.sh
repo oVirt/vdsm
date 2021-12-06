92026520f979eccda886312b67eb9839cfb27899
Ales Musil <amusil@redhat.com>
2021-12-06 09:33:40 +0100

    net, tests: Load bonding module on GH actions
    Create and then remove bond to load the module.
    Since we are already running inside container
    we cannot simply use modprobe bonding. Fortunately
    creating bond through iproute2 loads the module for us.
    
    Change-Id: I0d894b6914a692fbb9ddda35543388926f9e66f4
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- automation/tests.sh
+++ automation/tests.sh
@@ -2,6 +2,9 @@
 
 # Enable IPv6
 echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+# Load bonding module
+SUFFIX=$RANDOM
+ip link add mod_bond$SUFFIX type bond && ip link del mod_bond$SUFFIX
 
 ./autogen.sh --system
 make
