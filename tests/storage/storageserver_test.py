09f6afc5f2e954de0a04d6bcb93460457bc99cf9
Vojtěch Juránek <vjuranek@redhat.com>
2021-12-21 16:34:47 +0100

    tests: convert storageserver_test to pytest
    Convert storageserver_test to pytest. Also remove __future__ imports,
    which are not needed any more.
    
    Change-Id: Ibf26c7a6af8e0ce047fdf9b51e6a1b25537bce69
    Signed-off-by: Vojtěch Juránek <vjuranek@redhat.com>

--- tests/storage/storageserver_test.py
+++ tests/storage/storageserver_test.py
@@ -18,14 +18,8 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
-from __future__ import absolute_import
-from __future__ import division
-
 import pytest
 
-from monkeypatch import MonkeyPatch
-from testlib import permutations, expandPermutations
-from testlib import VdsmTestCase
 from vdsm.storage import sd
 from vdsm.storage import storageServer
 from vdsm.storage.storageServer import GlusterFSConnection
@@ -48,28 +42,26 @@ class FakeMount(object):
         self.fs_file = fs_file
 
 
-class TestIscsiConnectionMismatch(VdsmTestCase):
+class TestIscsiConnectionMismatch:
 
     def test_no_args(self):
         s = str(IscsiConnection.Mismatch("error 1"))
-        self.assertEqual(s, "error 1")
+        assert s == "error 1"
 
     def test_format(self):
         s = str(IscsiConnection.Mismatch("error %d with %r", 1, "text"))
-        self.assertEqual(s, "error 1 with 'text'")
+        assert s == "error 1 with 'text'"
 
     def test_format_mismatches_list(self):
         errors = [IscsiConnection.Mismatch("error 1"),
                   IscsiConnection.Mismatch("error 2")]
         expected = "%s" % str(["error 1", "error 2"])
-        self.assertEqual(str(errors), expected)
+        assert str(errors) == expected
 
 
-@expandPermutations
-class TestMountConnection(VdsmTestCase):
+class TestMountConnection:
 
-    @permutations([
-        # spec, fs_spec, fs_file
+    @pytest.mark.parametrize("spec, fs_spec, fs_file", [
         ("server:/a/", "server:/a", "/tmp/server:_a"),
         ("server:/a//", "server:/a", "/tmp/server:_a"),
         ("server:/a/b", "server:/a/b", "/tmp/server:_a_b"),
@@ -81,26 +73,25 @@ class TestMountConnection(VdsmTestCase):
     ])
     def test_normalize_local_path(self, spec, fs_spec, fs_file):
         con = MountConnection("id", spec, mountClass=FakeMount)
-        self.assertEqual(con._mount.fs_spec, fs_spec)
-        self.assertEqual(con._mount.fs_file, fs_file)
+        assert con._mount.fs_spec == fs_spec
+        assert con._mount.fs_file == fs_file
 
 
-@expandPermutations
-class TestMountConnectionEquality(VdsmTestCase):
+class TestMountConnectionEquality:
 
     def test_eq_equal(self):
         c1 = MountConnection("id", "server:/path", "vfstype", "options")
         c2 = MountConnection("id", "server:/path", "vfstype", "options")
-        self.assertTrue(c1 == c2, "%s should equal %s" % (c1, c2))
+        assert c1 == c2
 
     def test_eq_subclass(self):
         class Subclass(MountConnection):
             pass
         c1 = MountConnection("id", "server:/path", "vfstype", "options")
         c2 = Subclass("id", "server:/path", "vfstype", "options")
-        self.assertFalse(c1 == c2, "%s should not equal %s" % (c1, c2))
+        assert c1 != c2
 
-    @permutations([
+    @pytest.mark.parametrize("i1,i2,s1,s2,t1,t2,o1,o2", [
         ("id", "id", "server:/path1", "server:/path2", "t", "t", "o", "o"),
         ("id", "id", "server:/path", "server:/path", "t1", "t2", "o", "o"),
         ("id", "id", "server:/path", "server:/path", "t", "t", "o1", "o2"),
@@ -109,30 +100,29 @@ class TestMountConnectionEquality(VdsmTestCase):
     def test_eq_different(self, i1, i2, s1, s2, t1, t2, o1, o2):
         c1 = MountConnection(i1, s1, t1, o1)
         c2 = MountConnection(i2, s2, t2, o2)
-        self.assertFalse(c1 == c2, "%s should not equal %s" % (c1, c2))
+        assert not c1 == c2
 
     def test_ne_equal(self):
         c1 = MountConnection("id", "server:/path", "vfstype", "options")
         c2 = MountConnection("id", "server:/path", "vfstype", "options")
-        self.assertFalse(c1 != c2, "%s should equal %s" % (c1, c2))
+        assert not c1 != c2
 
 
-@expandPermutations
-class TestMountConnectionHash(VdsmTestCase):
+class TestMountConnectionHash:
 
     def test_equal_same_hash(self):
         c1 = MountConnection("id", "server:/path", "vfstype", "options")
         c2 = MountConnection("id", "server:/path", "vfstype", "options")
-        self.assertEqual(hash(c1), hash(c2))
+        assert hash(c1) == hash(c2)
 
     def test_subclass_different_hash(self):
         class Subclass(MountConnection):
             pass
         c1 = MountConnection("id", "server:/path", "vfstype", "options")
         c2 = Subclass("id", "server:/path", "vfstype", "options")
-        self.assertNotEqual(hash(c1), hash(c2))
+        assert hash(c1) != hash(c2)
 
-    @permutations([
+    @pytest.mark.parametrize("i1,i2,s1,s2,t1,t2,o1,o2", [
         ("id", "id", "server:/path1", "server:/path2", "t", "t", "o", "o"),
         ("id", "id", "server:/path", "server:/path", "t1", "t2", "o", "o"),
         ("id1", "id", "server:/path", "server:/path", "t", "t", "o1", "o2"),
@@ -141,27 +131,25 @@ class TestMountConnectionHash(VdsmTestCase):
     def test_not_equal_different_hash(self, i1, i2, s1, s2, t1, t2, o1, o2):
         c1 = MountConnection(i1, s1, t1, o1)
         c2 = MountConnection(i2, s2, t2, o2)
-        self.assertNotEqual(hash(c1), hash(c2))
+        assert hash(c1) != hash(c2)
 
 
-@expandPermutations
-class TestGlusterFSConnection(VdsmTestCase):
+class TestGlusterFSConnection:
 
     def test_mountpoint(self):
         mount_con = GlusterFSConnection("id",
                                         "server:/volume",
                                         mountClass=FakeMount)
-        self.assertEqual(mount_con._mount.fs_spec,
-                         "server:/volume")
-        self.assertEqual(mount_con._mount.fs_file,
-                         "/tmp/glusterSD/server:_volume")
+        assert mount_con._mount.fs_spec == "server:/volume"
+        assert mount_con._mount.fs_file == "/tmp/glusterSD/server:_volume"
 
-    @MonkeyPatch(gluster_cli, 'exists', lambda: True)
-    @MonkeyPatch(storageServer, 'supervdsm', FakeSupervdsm())
-    def test_gluster_replica3_mount_options(self):
+    def test_gluster_replica3_mount_options(self, monkeypatch):
+        monkeypatch.setattr(storageServer, 'supervdsm', FakeSupervdsm())
+        monkeypatch.setattr(gluster_cli, 'exists', lambda: True)
+
         def glusterVolumeInfo(volname=None, volfileServer=None):
-            self.assertEqual(volname, "music")
-            self.assertEqual(volfileServer, "192.168.122.1")
+            assert volname == "music"
+            assert volfileServer == "192.168.122.1"
             return {'music': {'brickCount': '3',
                               'bricks': ['192.168.122.1:/tmp/music',
                                          '192.168.122.2:/tmp/music',
@@ -170,16 +158,17 @@ class TestGlusterFSConnection(VdsmTestCase):
         storageServer.supervdsm.glusterVolumeInfo = glusterVolumeInfo
 
         gluster = GlusterFSConnection(id="id", spec="192.168.122.1:/music")
-        self.assertEqual(gluster.options,
-                         "backup-volfile-servers=192.168.122.2:192.168.122.3")
+        expected_options = "backup-volfile-servers=192.168.122.2:192.168.122.3"
+        assert gluster.options == expected_options
 
-    @MonkeyPatch(gluster_cli, 'exists', lambda: True)
-    @MonkeyPatch(storageServer, 'supervdsm', FakeSupervdsm())
-    def test_server_not_in_volinfo(self):
+    def test_server_not_in_volinfo(self, monkeypatch):
         """
         This test simulates a use case where gluster server provided in the
         path doesn't appear in the volume info.
         """
+        monkeypatch.setattr(storageServer, 'supervdsm', FakeSupervdsm())
+        monkeypatch.setattr(gluster_cli, 'exists', lambda: True)
+
         def glusterVolumeInfo(volname=None, volfileServer=None):
             return {'music': {'brickCount': '3',
                               'bricks': ['192.168.122.5:/tmp/music',
@@ -191,14 +180,15 @@ class TestGlusterFSConnection(VdsmTestCase):
         gluster = GlusterFSConnection(id="id", spec="gluster-server:/music")
         expected_backup_servers = \
             "backup-volfile-servers=192.168.122.5:192.168.122.2:192.168.122.3"
-        self.assertEqual(gluster.options, expected_backup_servers)
+        assert gluster.options == expected_backup_servers
 
-    @MonkeyPatch(gluster_cli, 'exists', lambda: True)
-    @MonkeyPatch(storageServer, 'supervdsm', FakeSupervdsm())
-    def test_duplicate_servers_in_volinfo(self):
+    def test_duplicate_servers_in_volinfo(self, monkeypatch):
         """
         This test verifies that servers list contains no duplicates.
         """
+        monkeypatch.setattr(storageServer, 'supervdsm', FakeSupervdsm())
+        monkeypatch.setattr(gluster_cli, 'exists', lambda: True)
+
         def glusterVolumeInfo(volname=None, volfileServer=None):
             return {'music': {'brickCount': '3',
                               'bricks': ['192.168.122.5:/tmp/music',
@@ -212,11 +202,12 @@ class TestGlusterFSConnection(VdsmTestCase):
         gluster = GlusterFSConnection(id="id", spec="gluster-server:/music")
         expected_backup_servers = \
             "backup-volfile-servers=192.168.122.5:192.168.122.2:192.168.122.3"
-        self.assertEqual(gluster.options, expected_backup_servers)
+        assert gluster.options == expected_backup_servers
 
-    @MonkeyPatch(gluster_cli, 'exists', lambda: True)
-    @MonkeyPatch(storageServer, 'supervdsm', FakeSupervdsm())
-    def test_gluster_and_user_provided_mount_options(self):
+    def test_gluster_and_user_provided_mount_options(self, monkeypatch):
+        monkeypatch.setattr(storageServer, 'supervdsm', FakeSupervdsm())
+        monkeypatch.setattr(gluster_cli, 'exists', lambda: True)
+
         def glusterVolumeInfo(volname=None, volfileServer=None):
             return {'music': {'brickCount': '3',
                               'bricks': ['192.168.122.1:/tmp/music',
@@ -229,20 +220,21 @@ class TestGlusterFSConnection(VdsmTestCase):
                                       options="option1=val1")
         expected_options = \
             "option1=val1,backup-volfile-servers=192.168.122.2:192.168.122.3"
-        self.assertEqual(gluster.options, expected_options)
+        assert gluster.options == expected_options
 
-    @MonkeyPatch(storageServer, 'supervdsm', FakeSupervdsm())
-    def test_gluster_replica1_mount_options(self):
+    def test_gluster_replica1_mount_options(self, monkeypatch):
+        monkeypatch.setattr(storageServer, 'supervdsm', FakeSupervdsm())
+
         def glusterVolumeInfo(volname=None, volfileServer=None):
-            self.assertEqual(volname, "music")
-            self.assertEqual(volfileServer, "192.168.122.1")
+            assert volname == "music"
+            assert volfileServer == "192.168.122.1"
             return {'music': {'brickCount': '1',
                               'bricks': ['192.168.122.1:/tmp/music']}}
 
         storageServer.supervdsm.glusterVolumeInfo = glusterVolumeInfo
 
         gluster = GlusterFSConnection(id="id", spec="192.168.122.1:/music")
-        self.assertEqual(gluster.options, "")
+        assert gluster.options == ""
 
     def test_user_provided_gluster_mount_options(self):
 
@@ -252,12 +244,13 @@ class TestGlusterFSConnection(VdsmTestCase):
         user_options = "backup-volfile-servers=server1:server2"
         gluster = GlusterFSConnection(id="id", spec="192.168.122.1:/music",
                                       options=user_options)
-        self.assertEqual(gluster.options, user_options)
+        assert gluster.options == user_options
 
-    @MonkeyPatch(storageServer, 'supervdsm', FakeSupervdsm())
-    @MonkeyPatch(GlusterFSConnection, 'ALLOWED_REPLICA_COUNTS', ('1', '3'))
-    @permutations([['1'], ['2'], ['3'], ['4']])
-    def test_allowed_gluster_replica_count(self, replica_count):
+    @pytest.mark.parametrize("replica_count", [['1'], ['2'], ['3'], ['4']])
+    def test_allowed_gluster_replica_count(self, monkeypatch, replica_count):
+        monkeypatch.setattr(storageServer, 'supervdsm', FakeSupervdsm())
+        monkeypatch.setattr(
+            GlusterFSConnection, 'ALLOWED_REPLICA_COUNTS', ('1', '3'))
 
         def glusterVolumeInfo(volumeName=None, remoteServer=None):
             return {'music': {'replicaCount': replica_count,
@@ -268,35 +261,40 @@ class TestGlusterFSConnection(VdsmTestCase):
         gluster = GlusterFSConnection(id="id", spec="192.168.122.1:/music")
         gluster.validate()
 
-    @MonkeyPatch(gluster_cli, 'exists', lambda: False)
-    def test_glusterfs_cli_missing(self):
+    def test_glusterfs_cli_missing(self, monkeypatch):
+        monkeypatch.setattr(gluster_cli, 'exists', lambda: False)
+
         gluster = GlusterFSConnection(id="id", spec="192.168.122.1:/music")
-        self.assertEqual(gluster.options, "")
+        assert gluster.options == ""
 
 
-@expandPermutations
-class TestGlusterFSNotAccessibleConnection(VdsmTestCase):
+class TestGlusterFSNotAccessibleConnection:
 
     def glusterVolumeInfo(self, volumeName=None, remoteServer=None):
         raise ge.GlusterCmdExecFailedException()
 
-    @MonkeyPatch(storageServer, 'supervdsm', FakeSupervdsm())
-    @MonkeyPatch(gluster_cli, 'exists', lambda: True)
-    def test_validate(self):
+    def test_validate(self, monkeypatch):
+        monkeypatch.setattr(storageServer, 'supervdsm', FakeSupervdsm())
+        monkeypatch.setattr(gluster_cli, 'exists', lambda: True)
+
         storageServer.supervdsm.glusterVolumeInfo = self.glusterVolumeInfo
 
         gluster = GlusterFSConnection(id="id", spec="192.168.122.1:/music")
         gluster.validate()
 
-    @MonkeyPatch(storageServer, 'supervdsm', FakeSupervdsm())
-    @MonkeyPatch(gluster_cli, 'exists', lambda: True)
-    @permutations([[''], ['backup-volfile-servers=server1:server2']])
-    def test_mount_options(self, userMountOptions):
+    @pytest.mark.parametrize("userMountOptions", [
+        '',
+        'backup-volfile-servers=server1:server2',
+    ])
+    def test_mount_options(self, monkeypatch, userMountOptions):
+        monkeypatch.setattr(storageServer, 'supervdsm', FakeSupervdsm())
+        monkeypatch.setattr(gluster_cli, 'exists', lambda: True)
+
         storageServer.supervdsm.glusterVolumeInfo = self.glusterVolumeInfo
 
         gluster = GlusterFSConnection(id="id", spec="192.168.122.1:/music",
                                       options=userMountOptions)
-        self.assertEqual(gluster.options, userMountOptions)
+        assert gluster.options == userMountOptions
 
 
 def test_prepare_connection_without_initiator_name():
