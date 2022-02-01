oVirt/vdsm

commit 9a2c37ece0cb9d5438c88a3b6ea3c7447ed235a1

storage: don't update device mapper size on SPM during LV extend
When the disk is extended during VM migration, data corruption would
happen if the destination host doesn't update the size of the disk (see
BZ #1883399). To avoid this situation, we refresh the disk on the
destination host first to ensure disk size is correct. If the
destination host doesn't support disk refresh, we stop the disk monitor
which should prevent any further disk extension and also abort disk
refresh on the source host. VM eventually will be paused due
to running out of the disk space.

However, this is not complete solution and the data corruption can
happen in this specific case: when the destination host doesn't
support disk refresh, source host is SPM and disk is extended only
once. Disk extension happens on SPM and lvm updates device mapper when
running LV extend command, so SPM actually sees the extension even if we
don't run LV refresh on the host. Disk monitoring is disabled, so no
further extensions will happen, but the first one already has happened
and is visible by the source host (SPM). If VM writes to this extended
area until migration finishes, the disk corruption will happen, as this
area is not visible on the destination host.

To prevent this, add additional argument "--driverloaded n" to lvm
extend command. This will cause that the extension will not be visible
to the system (SPM host) and thus cannot be used by the VM.
The extension will be visible only when we refresh the disks from vdsm,
as this was an ordinary host.

Change-Id: Ic9ba4d41f79ebd95b00745c2428f7fd797e67113
Bug-Url: https://bugzilla.redhat.com/1983882
Signed-off-by: Vojtěch Juránek <vjuranek@redhat.com>

lib/vdsm/storage/lvm.py
@@ -1673,7 +1673,7 @@ def removeLVs(vgName, lvNames):
         raise se.CannotRemoveLogicalVolume(vgName, str(lvNames), err)
 
 
-def extendLV(vgName, lvName, size_mb):
+def extendLV(vgName, lvName, size_mb, refresh=True):
     # Since this runs only on the SPM, assume that cached vg and lv metadata
     # are correct.
     vg = getVG(vgName)
@@ -1694,6 +1694,8 @@ def extendLV(vgName, lvName, size_mb):
 
     log.info("Extending LV %s/%s to %s megabytes", vgName, lvName, size_mb)
     cmd = ("lvextend",) + LVM_NOBACKUP
+    if not refresh:
+        cmd += ("--driverloaded", "n")
     cmd += ("--size", "%sm" % (size_mb,), "%s/%s" % (vgName, lvName))
     rc, out, err = _lvminfo.cmd(cmd, _lvminfo._getVGDevs((vgName,)))
 

tests/storage/lvm_test.py
@@ -35,6 +35,7 @@ from vdsm.common.units import MiB, GiB
 
 from vdsm.storage import constants as sc
 from vdsm.storage import exception as se
+from vdsm.storage import fsutils
 from vdsm.storage import lvm
 
 import testing
@@ -1435,6 +1436,48 @@ def test_lv_extend_reduce(tmp_storage):
     lvm.reduceLV(vg_name, lv_name, 1024, force=True)
     lv = lvm.getLV(vg_name, lv_name)
     assert int(lv.size) == 1 * GiB
+
+
+@requires_root
+@pytest.mark.root
+def test_lv_extend_with_refresh(tmp_storage):
+    dev_size = 10 * GiB
+    dev = tmp_storage.create_device(dev_size)
+    vg_name = str(uuid.uuid4())
+    lv_name = str(uuid.uuid4())
+    lvm.set_read_only(False)
+    lvm.createVG(vg_name, [dev], "initial-tag", 128)
+    lvm.createLV(vg_name, lv_name, 1024)
+
+    lvm.extendLV(vg_name, lv_name, 2048, refresh=True)
+
+    # LV extension should be visible immediately for the system without
+    # refreshing LV.
+    assert fsutils.size(lvm.lvPath(vg_name, lv_name)) == 2 * GiB
+
+
+@requires_root
+@pytest.mark.root
+def test_lv_extend_without_refresh(tmp_storage):
+    dev_size = 10 * GiB
+    dev = tmp_storage.create_device(dev_size)
+    vg_name = str(uuid.uuid4())
+    lv_name = str(uuid.uuid4())
+    lvm.set_read_only(False)
+    lvm.createVG(vg_name, [dev], "initial-tag", 128)
+    lvm.createLV(vg_name, lv_name, 1024)
+
+    lvm.extendLV(vg_name, lv_name, 2048, refresh=False)
+
+    lv_path = lvm.lvPath(vg_name, lv_name)
+    # LV extension should not be visible to the system at this point and device
+    # should has old size.
+    assert fsutils.size(lv_path) == GiB
+
+    lvm.refreshLVs(vg_name, [lv_name])
+
+    # After LVM refresh, extension should be visible to the system.
+    assert fsutils.size(lv_path) == 2 * GiB
 
 
 @requires_root

lib/vdsm/storage/blockSD.py
@@ -1584,11 +1584,11 @@ class BlockStorageDomain(sd.StorageDomain):
         # It is time to deactivate the master LV now
         lvm.deactivateLVs(self.sdUUID, [MASTERLV])
 
-    def extendVolume(self, volumeUUID, size):
+    def extendVolume(self, volumeUUID, size, refresh=True):
         with self.manifest.metadata_lock:
             self.log.debug("Extending thinly-provisioned LV for volume %s to "
                            "%d MB", volumeUUID, size)
-            lvm.extendLV(self.sdUUID, volumeUUID, size)
+            lvm.extendLV(self.sdUUID, volumeUUID, size, refresh=refresh)
 
     def reduceVolume(self, imgUUID, volUUID, allowActive=False):
         with self.manifest.metadata_lock:

lib/vdsm/storage/sd.py
@@ -1477,7 +1477,7 @@ class StorageDomain(object):
         """
         pass
 
-    def extendVolume(self, volumeUUID, size):
+    def extendVolume(self, volumeUUID, size, refresh=True):
         pass
 
     def reduceVolume(self, imgUUID, volumeUUID, allowActive=False):

lib/vdsm/storage/sp.py
@@ -1477,7 +1477,14 @@ class StoragePool(object):
         # volume apparent size; the virtual disk size seen by the guest is
         # unchanged.
         self._assert_sd_in_pool(sdUUID)
-        sdCache.produce(sdUUID).extendVolume(volumeUUID, size)
+
+        # Extend volume without refreshing its size. If the SPM host see the
+        # new size immediately after extension, this can cause data corruption
+        # during VM migration when the source host is SPM. Volume size will be
+        # refreshed in Vm.after_volume_extension(), which is a callback of disk
+        # extend command.
+        # For more details see https://bugzilla.redhat.com/1983882
+        sdCache.produce(sdUUID).extendVolume(volumeUUID, size, refresh=False)
 
     def reduceVolume(self, sdUUID, imgUUID, volUUID, allowActive=False):
         self._assert_sd_in_pool(sdUUID)

tests/storage/storagefakelib.py
@@ -280,7 +280,7 @@ class FakeLVM(object):
         else:
             return self._getLV(vgName, lvName)
 
-    def extendLV(self, vgName, lvName, size_mb):
+    def extendLV(self, vgName, lvName, size_mb, refresh=True):
         try:
             lv = self.lvmd[(vgName, lvName)]
         except KeyError:

commit 01b6a0c7cfe7d27ca7de96eda33dea57811c16b2

lvm: Suppress warning about disabling device mapper
Each time we extend a disk, we log this warning:

2022-02-01 22:25:48,531+0200 WARN  (mailbox-spm/2) [storage.lvm] Command [...] succeeded with
warnings: ['  WARNING: Activation disabled. No device-mapper interaction will be attempted.']
(lvm:347)

Suppress the warning, it is expected when using "--driverloaded n".

Fixes: 9a2c37ece0cb9d5438c88a3b6ea3c7447ed235a1
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

lib/vdsm/storage/lvm.py
@@ -317,7 +317,9 @@ class LVMRunner(object):
             "WARNING: This metadata update is NOT backed up",
             (r"WARNING: ignoring metadata seqno \d+ on /dev/mapper/\w+ for "
              r"seqno \d+ on /dev/mapper/\w+ for VG \w+"),
-            r"WARNING: Inconsistent metadata found for VG \w+"
+            r"WARNING: Inconsistent metadata found for VG \w+",
+            ("WARNING: Activation disabled. No device-mapper interaction "
+             "will be attempted"),
         ]),
         re.IGNORECASE)
 

tests/storage/lvm_test.py
@@ -711,6 +711,7 @@ def test_suppress_warnings(fake_devices):
   before
   WARNING: This metadata update is NOT backed up.
   WARNING: Combining activation change with other commands is not advised.
+  WARNING: Activation disabled. No device-mapper interaction will be attempted.
   Configuration setting "global/event_activation" unknown.
   WARNING: ignoring metadata seqno 1566 on /dev/mapper/3600a098038304437415d4b6a59684474 for seqno 1567 on /dev/mapper/3600a098038304437415d4b6a59684474 for VG Bug."
   WARNING: Inconsistent metadata found for VG Bug."