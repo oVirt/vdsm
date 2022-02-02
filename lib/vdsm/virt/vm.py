oVirt/vdsm

commit 6a251845f4a237b9c9b78e5d7461bcd9c6184c3b

virt: watermark: use the watermark flag
Use the libvirt block threshold event to raise a flag signaling drives
needing extension. This patch enables the watermark event consumption
for the steady state, aka the simplest VM flow: a VM is run on thinp
storage, and no other management operation is performed.

Following patches will make sure the management operations like
snapshot, live storage migration, live merge works well with the
watermark event.

Please note the usage of the watermark event is still disabled
by default in the configuration.

Change-Id: I23f171607e667c0300374f046829396c0e4c5a27
Signed-off-by: Francesco Romani <fromani@redhat.com>

lib/vdsm/virt/vm.py
@@ -88,6 +88,7 @@ from vdsm.virt import vmdevices
 from vdsm.virt.vmdevices import drivename
 from vdsm.virt.vmdevices import hwclass
 from vdsm.virt.vmdevices.storage import DISK_TYPE, VolumeNotFound, SOURCE_ATTR
+from vdsm.virt.vmdevices.storage import BLOCK_THRESHOLD
 from vdsm.virt.vmpowerdown import VmShutdown, VmReboot
 from vdsm.virt.utils import isVdsmImage, cleanup_guest_socket, is_kvm
 
@@ -1203,7 +1204,8 @@ class Vm(object):
         If this returns True, the periodic system will invoke
         monitor_drives during this periodic cycle.
         """
-        return self._driveMonitorEnabled and bool(self.getChunkedDrives())
+        return (self._driveMonitorEnabled and
+                bool(self.drive_monitor.monitored_drives()))
 
     def monitor_drives(self):
         """
@@ -1212,7 +1214,7 @@ class Vm(object):
         extended = False
 
         try:
-            for drive in self.getChunkedDrives():
+            for drive in self.drive_monitor.monitored_drives():
                 if self.extend_drive_if_needed(drive):
                     extended = True
         except ImprobableResizeRequestError:
@@ -1225,8 +1227,25 @@ class Vm(object):
         Check if a drive should be extended, and start extension flow if
         needed.
 
+        When libvirt BLOCK_THRESHOLD event handling is enabled (
+        irs.enable_block_threshold_event == True), this method acts according
+        the drive.threshold_state:
+
+        - UNSET: the drive needs to register for a new block threshold,
+                 so try to set it.
+        - EXCEEDED: the drive needs extension, try to extend it.
+        - SET: this method should never receive a drive in this state,
+               emit warning and exit.
+
         Return True if started an extension flow, False otherwise.
         """
+
+        if drive.threshold_state == BLOCK_THRESHOLD.SET:
+            self.log.warning(
+                "Unexpected state for drive %s: threshold_state SET",
+                drive.name)
+            return
+
         try:
             capacity, alloc, physical = self._getExtendInfo(drive)
         except libvirt.libvirtError as e:
@@ -1234,15 +1253,27 @@ class Vm(object):
                            drive.name, e)
             return False
 
+        if (drive.chunked and
+                drive.threshold_state == BLOCK_THRESHOLD.UNSET):
+            self.drive_monitor.set_threshold(drive, physical)
+
         if not self._shouldExtendVolume(
                 drive, drive.volumeID, capacity, alloc, physical):
             return False
 
+        # TODO: if the threshold is wrongly set below the current allocation,
+        # for example because of delays in handling the event, or if the VM
+        # writes too fast, we will never receive an event.
+        # We need to set the drive threshold to EXCEEDED both if we receive
+        # one event or if we found that the threshold was exceeded during
+        # the _shouldExtendVolume check.
+
         self.log.info(
             "Requesting extension for volume %s on domain %s (apparent: "
-            "%s, capacity: %s, allocated: %s, physical: %s)",
+            "%s, capacity: %s, allocated: %s, physical: %s "
+            "threshold_state: %s)",
             drive.volumeID, drive.domainID, drive.apparentsize, capacity,
-            alloc, physical)
+            alloc, physical, drive.threshold_state)
 
         self.extendDriveVolume(drive, drive.volumeID, physical, capacity)
         return True
@@ -1371,9 +1402,9 @@ class Vm(object):
             vmDrive = self.findDriveByName(volInfo['name'])
             vmDrive.apparentsize = volSize.apparentsize
             vmDrive.truesize = volSize.truesize
+            self.drive_monitor.set_threshold(vmDrive, volSize.apparentsize)
 
         self._resume_if_needed()
-        self._setWriteWatermarks()
 
     def _resume_if_needed(self):
         try:
@@ -5102,13 +5133,6 @@ class Vm(object):
         """
         self.log.exception("Operation failed")
         return response.error(key, msg)
-
-    def _setWriteWatermarks(self):
-        """
-        Define when to receive an event about high write to guest image
-        Currently unavailable by libvirt.
-        """
-        pass
 
     def handle_failed_post_copy(self, clean_vm=False):
         # After a failed post-copy migration, the VM remains in a paused state

lib/vdsm/clientIF.py
@@ -603,6 +603,10 @@ class clientIF(object):
             elif eventid == libvirt.VIR_DOMAIN_EVENT_ID_DEVICE_REMOVED:
                 device_alias, = args[:-1]
                 v.onDeviceRemoved(device_alias)
+            elif eventid == libvirt.VIR_DOMAIN_EVENT_ID_BLOCK_THRESHOLD:
+                dev, path, threshold, excess = args[:-1]
+                v.drive_monitor.on_block_threshold(
+                    dev, path, threshold, excess)
             else:
                 v.log.debug('unhandled libvirt event (event_name=%s, args=%s)',
                             events.event_name(eventid), args)

lib/vdsm/libvirtconnection.py
@@ -180,7 +180,8 @@ def get(target=None, killOnFailure=True):
                            libvirt.VIR_DOMAIN_EVENT_ID_BLOCK_JOB,
                            libvirt.VIR_DOMAIN_EVENT_ID_WATCHDOG,
                            libvirt.VIR_DOMAIN_EVENT_ID_JOB_COMPLETED,
-                           libvirt.VIR_DOMAIN_EVENT_ID_DEVICE_REMOVED):
+                           libvirt.VIR_DOMAIN_EVENT_ID_DEVICE_REMOVED,
+                           libvirt.VIR_DOMAIN_EVENT_ID_BLOCK_THRESHOLD):
                     conn.domainEventRegisterAny(None,
                                                 ev,
                                                 target.dispatchLibvirtEvents,

lib/vdsm/virt/drivemonitor.py
@@ -19,6 +19,8 @@
 #
 from __future__ import absolute_import
 
+import libvirt
+
 from vdsm.config import config
 from vdsm.virt.vmdevices import storage
 
@@ -40,6 +42,11 @@ class DriveMonitor(object):
         Set the libvirt block threshold on the given drive, enabling
         libvirt to deliver the event when the threshold is crossed.
         Does nothing if the `_events_enabled` attribute is Falsey.
+
+        Call this method when you need to set one initial block threshold
+        (e.g. first time Vdsm monitors one drive), or after one volume
+        extension, or when the top layer changes (after snapshot, after
+        live storage migration completes).
 
         Args:
             drive: A storage.Drive object
@@ -66,9 +73,19 @@ class DriveMonitor(object):
             '(apparentsize %d)',
             threshold, drive.name, apparentsize
         )
-        # TODO: find a good way to expose Vm._dom as public property.
-        # we are running out of names in Vm class.
-        self._vm._dom.setBlockThreshold(drive.name, threshold)
+        try:
+            # TODO: find a good way to expose Vm._dom as public property.
+            # we are running out of names in Vm class.
+            self._vm._dom.setBlockThreshold(drive.name, threshold)
+        except libvirt.libvirtError as exc:
+            # The drive threshold_state can be UNSET or EXCEEDED, and
+            # this ensures that we will attempt to set the threshold later.
+            drive.threshold_state = storage.BLOCK_THRESHOLD.UNSET
+            self._log.error(
+                'Failed to set block threshold on %r (%s): %s',
+                drive.name, drive.path, exc)
+        else:
+            drive.threshold_state = storage.BLOCK_THRESHOLD.SET
 
     def clear_threshold(self, drive, index=None):
         """
@@ -107,7 +124,14 @@ class DriveMonitor(object):
         """
         self._log.info('block threshold %d exceeded on %r (%s)',
                        threshold, dev, path)
-        # for now we log only
+        try:
+            drive = self._vm.findDriveByName(dev)
+        except LookupError:
+            self._log.warning(
+                'Unknown drive %r for vm %s - ignored block threshold event',
+                dev, self._vm.id)
+        else:
+            drive.on_block_threshold(path)
 
     def monitored_drives(self):
         """

tests/virt_drivemonitor_test.py
@@ -32,7 +32,6 @@ from vdsm.virt import drivemonitor
 from vdsm.virt import vm
 from vdsm.virt import vmstatus
 
-from testValidation import xfail
 from testlib import expandPermutations, permutations
 from testlib import make_config
 from testlib import maybefail
@@ -249,7 +248,6 @@ class TestDiskExtensionWithEvents(DiskExtensionTestBase):
     #   -> extend
     # FIXME: already covered by existing cases?
 
-    @xfail('Event support in DriveMonitor not yet implemented')
     def test_extend_using_events(self):
         with make_env(events_enabled=True) as (testvm, dom, drives):
 
@@ -285,7 +283,6 @@ class TestDiskExtensionWithEvents(DiskExtensionTestBase):
             drv = drives[1]
             self.assertEqual(drv.threshold_state, BLOCK_THRESHOLD.SET)
 
-    @xfail('Event support in DriveMonitor not yet implemented')
     @permutations([
         # replicating, threshold
         (False, 1536 * MB),
@@ -341,7 +338,6 @@ class TestDiskExtensionWithEvents(DiskExtensionTestBase):
 
             self.assertFalse(extended)
 
-    @xfail('Event support in DriveMonitor not yet implemented')
     def test_event_received_before_write_completes(self):
         # QEMU submits an event when write is attempted, so it
         # is possible that at the time we receive the event the

commit 36c643cd730e195d3d980d47eebdb35c64c712b3

vm: Remove ancient unused method
VM.getChunkedDrives() is not used since 2017, remove it.

Fixes: 6a251845f4a237b9c9b78e5d7461bcd9c6184c3b
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

lib/vdsm/virt/vm.py
@@ -1280,15 +1280,6 @@ class Vm(object):
         self.log.debug('new rtc offset %s', newTimeOffset)
         self._timeOffset = newTimeOffset
 
-    def getChunkedDrives(self):
-        """
-        Return list of writable chunked drives, or writable non-chunked
-        drives replicating to chunked replica drive.
-        """
-        return [drive for drive in self._devices[hwclass.DISK]
-                if (drive.chunked or drive.replicaChunked) and not
-                drive.readonly]
-
     def _amend_block_info(self, drive, block_info):
         """
         Ammend block info from libvirt in case the drive is not chucked and is

tests/virt/vmoperations_test.py
@@ -40,7 +40,6 @@ from vdsm.virt import saslpasswd2
 from vdsm.virt import virdomain
 from vdsm.virt import vmexitreason
 from vdsm.virt.vmdevices import hwclass
-from vdsm.virt.vmdevices import storage
 
 from monkeypatch import MonkeyPatch, MonkeyPatchScope
 from testlib import XMLTestCase
@@ -465,29 +464,6 @@ class TestVmOperations(XMLTestCase):
         with fake.VM() as testvm:
             testvm._dom = fake.Domain(vmId='testvm')
             assert not response.is_error(testvm.acpiReboot())
-
-    @permutations([
-        # info, expected
-        ({'readonly': True, 'diskType': storage.DISK_TYPE.BLOCK}, []),
-        ({'readonly': True, 'diskType': storage.DISK_TYPE.FILE}, []),
-        ({'readonly': False, 'diskType': storage.DISK_TYPE.FILE}, []),
-        ({'readonly': False, 'diskType': storage.DISK_TYPE.FILE}, []),
-        ({'readonly': False, 'diskType': storage.DISK_TYPE.BLOCK,
-          'format': 'raw'}, []),
-        ({'readonly': False, 'diskType': storage.DISK_TYPE.BLOCK}, ['vda']),
-        ({'readonly': False, 'diskType': storage.DISK_TYPE.FILE,
-          'diskReplicate': {
-              'format': 'cow', 'diskType': storage.DISK_TYPE.BLOCK}
-          },
-         ['vda']),
-    ])
-    def testGetChunkedDrives(self, disk_conf, expected):
-        with fake.VM() as testvm:
-            vda = storage.Drive(self.log, **drive_config(**disk_conf))
-            testvm._devices[hwclass.DISK] = [vda]
-
-            drives = [drive.name for drive in testvm.getChunkedDrives()]
-            assert drives == expected
 
     def test_process_migration_cpusets(self):
         with fake.VM() as testvm: