oVirt/vdsm

commit 84ef2dd05fe1f4dc9e9158d91a7ec84f3c9a4650

fileSD: Do not use qemu-img preallocation=falloc
We used to defer volume preallocation when creating a raw preallocated
volume by supporting initial_size=1. Then when copying the volume, we
used:

    qemu-img convert ... -o preallocation=falloc src dst

Turns out that this does not work well for NFS < 4.2, and can lead to
sanlock timeouts, storage monitoring failures, and VMs becoming
non-responsive. This is also slower than preallocating volumes using dd
that was done in the past.

To fix this, we drop the idea of deferring preallocation. The initial
size sent by engine is ignored now when creating raw preallocated
volume, and we always preallocate the volume at the creation step.

Creating a raw preallocated volume is done in 2 steps:

1. Create sparse image using qemu-img create, since qemu-img allocates
   the first block by writing zeroes.

2. Allocate the rest of the image using fallocate helper, which was
   changed to use efficient and safe method on any NFS version.

When we copy images, we always use the -n option to use the existing
volume:

    qemu-img convert ... -n src dst

This keeps the target volume preallocated. This was already used for
block storage, and now used also for file storage.

When using create=False dstQcow2Compat and backingFormat are ignored, so
these options were removed.

Change-Id: Ifea5d3dc27e69265d23ba561336d5f00b07151f1
https://bugzilla.redhat.com/1850267
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

lib/vdsm/storage/fileVolume.py
@@ -463,9 +463,7 @@ class FileVolume(volume.Volume):
         Specific implementation of _create() for RAW volumes.
         All the exceptions are properly handled and logged in volume.create()
         """
-        if initial_size is None:
-            alloc_size = capacity
-        else:
+        if initial_size is not None:
             if preallocate == sc.SPARSE_VOL:
                 cls.log.error("initial size is not supported for file-based "
                               "sparse volumes")
@@ -478,16 +476,16 @@ class FileVolume(volume.Volume):
                 raise se.InvalidParameterException(
                     "initial size", initial_size)
 
-            # Always allocate at least 4k, so qemu-img can allocated the first
-            # block of the image, helping qemu to probe the alignment later.
-            alloc_size = max(initial_size, sc.BLOCK_SIZE_4K)
+            # In the past engine called with initial_size=0 to deffer
+            # preallocation to qemu-img convert, but we learned that this is a
+            # bad idea on older NFS versions. We ignore initial_size now, and
+            # always allocate the entire image. qemu-img convert is always
+            # using -n so it does not affect image allocation.
+            cls.log.warning("Ignoring initial_size=%s", initial_size)
 
         cls._truncate_volume(vol_path, 0, vol_id, dom)
 
-        cls._allocate_volume(vol_path, alloc_size, preallocate=preallocate)
-
-        if alloc_size < capacity:
-            qemuimg.resize(vol_path, capacity, format=qemuimg.FORMAT.RAW)
+        cls._allocate_volume(vol_path, capacity, preallocate=preallocate)
 
         cls.log.info("Request to create RAW volume %s with capacity = %s",
                      vol_path, capacity)
@@ -547,21 +545,27 @@ class FileVolume(volume.Volume):
 
     @classmethod
     def _allocate_volume(cls, vol_path, size, preallocate):
-        if preallocate == sc.PREALLOCATED_VOL:
-            preallocation = qemuimg.PREALLOCATION.FALLOC
-        else:
-            preallocation = qemuimg.PREALLOCATION.OFF
-
         try:
-            operation = qemuimg.create(
-                vol_path,
-                size=size,
-                format=qemuimg.FORMAT.RAW,
-                preallocation=preallocation)
+            # Always create sparse image, since qemu-img create uses
+            # posix_fallocate() which is inefficient and harmful.
+            op = qemuimg.create(vol_path, size=size, format=qemuimg.FORMAT.RAW)
 
-            with vars.task.abort_callback(operation.abort):
-                with utils.stopwatch("Preallocating volume %s" % vol_path):
-                    operation.run()
+            # This is fast but it can get stuck if storage is inaccessible.
+            with vars.task.abort_callback(op.abort):
+                with utils.stopwatch("Creating image %s" % vol_path):
+                    op.run()
+
+            # If the image is preallocated, allocate the rest of the image
+            # using fallocate helper. qemu-img create always writes zeroes to
+            # the first block so we should skip it during preallocation.
+            if preallocate == sc.PREALLOCATED_VOL:
+                op = fallocate.allocate(vol_path, size - 4096, offset=4096)
+
+                # This is fast on NFS 4.2, GlusterFS, XFS and ext4, but can be
+                # extremely slow on NFS < 4.2, writing zeroes to entire image.
+                with vars.task.abort_callback(op.abort):
+                    with utils.stopwatch("Preallocating volume %s" % vol_path):
+                        op.run()
         except exception.ActionStopped:
             raise
         except Exception:

lib/vdsm/storage/image.py
@@ -441,29 +441,18 @@ class Image:
                     if parentVol is not None:
                         backing = volume.getBackingVolumePath(
                             imgUUID, parentVol.volUUID)
-                        backingFormat = sc.fmt2str(parentVol.getFormat())
                     else:
                         backing = None
-                        backingFormat = None
-
-                    if (destDom.supportsSparseness and
-                            dstVol.getType() == sc.PREALLOCATED_VOL):
-                        preallocation = qemuimg.PREALLOCATION.FALLOC
-                    else:
-                        preallocation = None
 
                     operation = qemuimg.convert(
                         srcVol.getVolumePath(),
                         dstVol.getVolumePath(),
                         srcFormat=srcFormat,
                         dstFormat=dstFormat,
-                        dstQcow2Compat=destDom.qcow2_compat(),
                         backing=backing,
-                        backingFormat=backingFormat,
-                        preallocation=preallocation,
                         unordered_writes=destDom.recommends_unordered_writes(
                             dstVol.getFormat()),
-                        create=not destDom.is_block(),
+                        create=False,
                     )
                     with utils.stopwatch("Copy volume %s"
                                          % srcVol.volUUID):
@@ -738,23 +727,15 @@ class Image:
                 # Start the actual copy image procedure
                 dstVol.prepare(rw=True, setrw=True)
 
-                if (destDom.supportsSparseness and
-                        dstVol.getType() == sc.PREALLOCATED_VOL):
-                    preallocation = qemuimg.PREALLOCATION.FALLOC
-                else:
-                    preallocation = None
-
                 try:
                     operation = qemuimg.convert(
                         volParams['path'],
                         dstVol.getVolumePath(),
                         srcFormat=sc.fmt2str(volParams['volFormat']),
                         dstFormat=sc.fmt2str(dstVolFormat),
-                        dstQcow2Compat=destDom.qcow2_compat(),
-                        preallocation=preallocation,
                         unordered_writes=destDom.recommends_unordered_writes(
                             dstVolFormat),
-                        create=not destDom.is_block(),
+                        create=False,
                     )
                     with utils.stopwatch("Copy volume %s"
                                          % srcVol.volUUID):

lib/vdsm/storage/sdm/api/copy_data.py
@@ -77,13 +77,10 @@ class Job(base.Job):
                         self._dest.path,
                         srcFormat=src_format,
                         dstFormat=dst_format,
-                        dstQcow2Compat=self._dest.qcow2_compat,
                         backing=self._dest.backing_path,
-                        backingFormat=self._dest.backing_qemu_format,
-                        preallocation=self._dest.preallocation,
                         unordered_writes=self._dest
                             .recommends_unordered_writes,
-                        create=not self._dest.volume.is_block())
+                        create=False)
                     self._operation.run()
 
 
@@ -141,26 +138,6 @@ class CopyDataDivEndpoint(properties.Owner):
         if not parent_vol:
             return None
         return volume.getBackingVolumePath(self.img_id, parent_vol.volUUID)
-
-    @property
-    def qcow2_compat(self):
-        dom = sdCache.produce_manifest(self.sd_id)
-        return dom.qcow2_compat()
-
-    @property
-    def backing_qemu_format(self):
-        parent_vol = self.volume.getParentVolume()
-        if not parent_vol:
-            return None
-        return sc.fmt2str(parent_vol.getFormat())
-
-    @property
-    def preallocation(self):
-        dom = sdCache.produce_manifest(self.sd_id)
-        if (dom.supportsSparseness and
-                self.volume.getType() == sc.PREALLOCATED_VOL):
-            return qemuimg.PREALLOCATION.FALLOC
-        return None
 
     @property
     def recommends_unordered_writes(self):

tests/storage/localfssd_test.py
@@ -440,12 +440,9 @@ def test_volume_create_raw_prealloc(user_domain, local_fallocate):
     assert int(actual["truesize"]) == qemu_info['actualsize']
 
 
-@pytest.mark.parametrize("initial_size, actual_size", [
-    (0, sc.BLOCK_SIZE_4K),
-    (INITIAL_VOL_SIZE, INITIAL_VOL_SIZE)
-])
-def test_volume_create_raw_prealloc_with_initial_size(
-        user_domain, local_fallocate, initial_size, actual_size):
+@pytest.mark.parametrize("initial_size", [0, INITIAL_VOL_SIZE])
+def test_volume_create_raw_prealloc_initial_size_ignored(
+        user_domain, local_fallocate, initial_size):
     img_uuid = str(uuid.uuid4())
     vol_uuid = str(uuid.uuid4())
 
@@ -472,7 +469,8 @@ def test_volume_create_raw_prealloc_with_initial_size(
         virtual_size=PREALLOCATED_VOL_SIZE,
         qemu_info=qemu_info)
 
-    assert qemu_info['actualsize'] == actual_size
+    # Initial_size is ignored since we stopped using qemu-img preallocation.
+    assert qemu_info['actualsize'] == PREALLOCATED_VOL_SIZE
 
     # Verify actual volume metadata
     actual = vol.getInfo()

commit 1aa117ac58d0b22406942717b65b0bcb53881b1b

fileVolume: Fix creation of tiny volumes
When creating preallocated volume with size <= 4096, we try to allocate
0 bytes:

    /usr/libexec/vdsm/fallocate --offset 4096 0 /path...

Which fail in fallocate() with EINVAL as expected, and then we raise

    VolumesZeroingError: Cannot zero out volume: /path...

It looks like engine does not handle this error correctly, and the disk
remain locked forever.

Creating a volume of 4k bytes or less is not a real use case, but there
is no technical reason why it should fail. Fixed by skipping the
allocation if we don't have anything to allocate.

Fixes: 84ef2dd05fe1 (fileSD: Do not use qemu-img preallocation=falloc)
Reported-by: Janos Bonic <jpasztor@redhat.com>
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

lib/vdsm/storage/fileVolume.py
@@ -568,7 +568,7 @@ class FileVolume(volume.Volume):
             # If the image is preallocated, allocate the rest of the image
             # using fallocate helper. qemu-img create always writes zeroes to
             # the first block so we should skip it during preallocation.
-            if preallocate == sc.PREALLOCATED_VOL:
+            if size > 4096 and preallocate == sc.PREALLOCATED_VOL:
                 op = fallocate.allocate(vol_path, size - 4096, offset=4096)
 
                 # This is fast on NFS 4.2, GlusterFS, XFS and ext4, but can be