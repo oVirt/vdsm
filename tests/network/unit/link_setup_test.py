594e8f96ed0376f67f81b6c65ff3675603e51da7
Harel Braha <hbraha@redhat.com>
2022-04-07 11:46:18 +0300

    network: use nmstate for bridge options
    the bridge options are currently implemented via sysfs,
    We want to use nmstate for bridge options.

--- tests/network/unit/link_setup_test.py
+++ tests/network/unit/link_setup_test.py
@@ -25,21 +25,14 @@ from vdsm.network.link import setup as linksetup
 
 def test_parse_nets_bridge_opts():
     nets = {
-        'br1': {
-            'custom': {
-                'bridge_opts': 'multicast_router=0 multicast_snooping=0'
-            }
-        },
-        'br2': {
-            'custom': {
-                'bridge_opts': 'multicast_router=1 multicast_snooping=1'
-            }
-        },
+        'br1': 'multicast_router=0 multicast_snooping=0',
+        'br2': 'multicast_router=1 multicast_snooping=1',
     }
     expected = {
-        'br1': {'multicast_router': '0', 'multicast_snooping': '0'},
-        'br2': {'multicast_router': '1', 'multicast_snooping': '1'},
+        'br1': {'multicast-router': 0, 'multicast-snooping': False},
+        'br2': {'multicast-router': 1, 'multicast-snooping': True},
     }
 
-    for name, opts in linksetup.parse_nets_bridge_opts(nets):
-        assert expected[name] == opts
+    for name, opts in nets.items():
+        parsed_opts = linksetup.parse_nets_bridge_opts(opts)
+        assert expected[name] == parsed_opts
