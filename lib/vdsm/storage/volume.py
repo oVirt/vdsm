oVirt/vdsm

commit 2bbe66274524cc009f39d3a5e5577b84144f1171

volume: Be more careful with create=False
Recently we started to use create=False when converting images to block
storage. This was needed as a workaround for qemu-img bug that is now
fixed.

Then we found that qemu-img preallocation is inefficient and cause
trouble with legacy NFS storage, and now we preallocate images with out
fallocate helper.

These changes revealed the fact that we always create the target image
when running qemuimg.convert(). This does not make sense for our use
case, so we switch to create=False for all cases.

Unfortunately, this does not work. We have 2 cases that require
create=True:

1. Raw sparse images when the file system does not support punching
   holes. When qemu-img convert is trying to punch holes in unallocated
   areas it falls back to writing zeroes, which is very slow, and fully
   allocates sparse images.

2. qcow2 with compat=0.10 when volume does not have a parent. Since this
   older qcow2 format does not support zero clusters, qemu-img fall back
   to writing zeroes which is very slow and allocates the entire image.

When qemu-img convert creates a new image, it knows that the image is
zeroed so it can skip unallocated areas.

Here are few examples showing the problem cases:

Coping sparse image to NFS 3

    $ truncate -s 10g /var/tmp/src.raw
    $ truncate -s 10g dst.raw

    $ time qemu-img convert -f raw -O raw -t none -T none -n /var/tmp/src.raw dst.raw

    real    0m50.684s
    user    0m0.034s
    sys     0m0.711s

    $ du -sh dst.raw
    10G     dst.raw

The image became fully allocated and the operation was very slow. If
we use create=True:

    $ time qemu-img convert -f raw -O raw -t none -T none /var/tmp/src.raw dst.raw

    real    0m0.222s
    user    0m0.005s
    sys     0m0.003s

    $ du -sh dst.raw
    4.0K    dst.raw

More correct and 250 times faster.

Copying image to qcow2 compat=0.10:

    $ qemu-img create -f qcow2 -o compat=0.10 dst.qcow2 10g

lib/vdsm/storage/volume.py
@@ -798,6 +798,38 @@ class VolumeManifest(object):
     def getMetaSlot(self):
         raise NotImplementedError
 
+    # Copy volume helpers.
+
+    def requires_create(self):
+        """
+        Return True if we need to use qemuimg.convert(create=True) when this
+        volume is the target image.
+
+        We have 2 cases:
+
+        1. Raw sparse image on filesystem not supporting punching holes (e.g.
+           NFS < 4.2). qemu-img convert will fully allocate the entire image
+           when trying to punch holes in the unallocated areas.
+
+        2. qcow2 compat=0.10 when volume does not have a parent. qemu-img
+           convert will fully allocate the image instead of skipping the
+           unallocated areas.
+           TODO: Remove when qemu-5.1.0 is available.
+           https://bugzilla.redhat.com/1858632
+
+        When qemu-img convert creates the target image it knows that the image
+        is zeroed so it can skip the unallocated areas.
+        """
+        if self.getFormat() == sc.RAW_FORMAT:
+            return self.isSparse()
+        else:
+            puuid = self.getParent()
+            if puuid and puuid != sc.BLANK_UUID:
+                return False
+
+            dom = sdCache.produce(self.sdUUID)
+            return dom.qcow2_compat() == "0.10"
+
 
 class Volume(object):
     log = logging.getLogger('storage.Volume')
@@ -1485,6 +1517,9 @@ class Volume(object):
 
     def setParentTag(self, puuid):
         raise NotImplementedError
+
+    def requires_create(self):
+        return self._manifest.requires_create()
 
 
 class VolumeLease(guarded.AbstractLock):

lib/vdsm/storage/image.py
@@ -441,18 +441,22 @@ class Image:
                     if parentVol is not None:
                         backing = volume.getBackingVolumePath(
                             imgUUID, parentVol.volUUID)
+                        backingFormat = sc.fmt2str(parentVol.getFormat())
                     else:
                         backing = None
+                        backingFormat = None
 
                     operation = qemuimg.convert(
                         srcVol.getVolumePath(),
                         dstVol.getVolumePath(),
                         srcFormat=srcFormat,
                         dstFormat=dstFormat,
+                        dstQcow2Compat=destDom.qcow2_compat(),
                         backing=backing,
+                        backingFormat=backingFormat,
                         unordered_writes=destDom.recommends_unordered_writes(
                             dstVol.getFormat()),
-                        create=False,
+                        create=dstVol.requires_create(),
                     )
                     with utils.stopwatch(
                             "Copy volume {}".format(srcVol.volUUID),
@@ -735,9 +739,10 @@ class Image:
                         dstVol.getVolumePath(),
                         srcFormat=sc.fmt2str(volParams['volFormat']),
                         dstFormat=sc.fmt2str(dstVolFormat),
+                        dstQcow2Compat=destDom.qcow2_compat(),
                         unordered_writes=destDom.recommends_unordered_writes(
                             dstVolFormat),
-                        create=False,
+                        create=dstVol.requires_create(),
                     )
                     with utils.stopwatch(
                             "Copy volume {}".format(srcVol.volUUID),

lib/vdsm/storage/sdm/api/copy_data.py
@@ -79,10 +79,12 @@ class Job(base.Job):
                         self._dest.path,
                         srcFormat=src_format,
                         dstFormat=dst_format,
+                        dstQcow2Compat=self._dest.qcow2_compat,
                         backing=self._dest.backing_path,
+                        backingFormat=self._dest.backing_qemu_format,
                         unordered_writes=self._dest
                             .recommends_unordered_writes,
-                        create=False)
+                        create=self._dest.requires_create)
                     with utils.stopwatch(
                             "Copy volume {}".format(self._source.path),
                             level=logging.INFO,
@@ -146,9 +148,25 @@ class CopyDataDivEndpoint(properties.Owner):
         return volume.getBackingVolumePath(self.img_id, parent_vol.volUUID)
 
     @property
+    def qcow2_compat(self):
+        dom = sdCache.produce_manifest(self.sd_id)
+        return dom.qcow2_compat()
+
+    @property
+    def backing_qemu_format(self):
+        parent_vol = self.volume.getParentVolume()
+        if not parent_vol:
+            return None
+        return sc.fmt2str(parent_vol.getFormat())
+
+    @property
     def recommends_unordered_writes(self):
         dom = sdCache.produce_manifest(self.sd_id)
         return dom.recommends_unordered_writes(self.volume.getFormat())
+
+    @property
+    def requires_create(self):
+        return self.volume.requires_create()
 
     @property
     def volume(self):

tests/storage/sdm_copy_data_test.py
@@ -45,7 +45,6 @@ from storage.storagetestlib import (
     write_qemu_chain,
 )
 
-import testing
 from . import qemuio
 from . import userstorage
 
@@ -68,10 +67,6 @@ from vdsm.storage import workarounds
 from vdsm.storage.sdm.api import copy_data
 
 DEFAULT_SIZE = MiB
-
-
-xfail_block = pytest.mark.xfail(
-    reason="Doesn't work when -n flag is used by qemu-img")
 
 
 @pytest.fixture(
@@ -272,18 +267,8 @@ class TestCopyDataDIV(VdsmTestCase):
 @pytest.mark.parametrize("env_type,qcow2_compat,sd_version", [
     # env_type, src_compat, sd_version
     # Old storage domain, we supported only 0.10
-    pytest.param(
-        'file', '0.10', 3,
-        marks=pytest.mark.xfail(
-            reason="qemu-img allocates entire image",
-            # Test passes on oVirt CI.
-            strict=not testing.on_ovirt_ci())),
-    pytest.param(
-        'block', '0.10', 3,
-        marks=pytest.mark.xfail(
-            reason="qemu-img allocates entire image",
-            # Test passes on oVirt CI.
-            strict=not testing.on_ovirt_ci())),
+    ('file', '0.10', 3),
+    ('block', '0.10', 3),
     # New domain old volume
     ('file', '0.10', 4),
     ('block', '0.10', 4),
@@ -333,7 +318,7 @@ def test_qcow2_compat(
     pytest.param('file', 'cow', 'raw'),
     pytest.param('file', 'cow', 'cow'),
     pytest.param('block', 'raw', 'raw'),
-    pytest.param('block', 'raw', 'cow', marks=xfail_block),
+    pytest.param('block', 'raw', 'cow'),
     pytest.param('block', 'cow', 'raw'),
     pytest.param('block', 'cow', 'cow'),
 ])
@@ -368,13 +353,8 @@ def test_intra_domain_copy(env_type, src_fmt, dst_fmt):
 
 
 @pytest.mark.parametrize("dest_format,sd_version", [
-    (sc.COW_FORMAT, 5),    # compat=1.1.
-    pytest.param(
-        sc.COW_FORMAT, 3,  # compat=0.10.
-        marks=pytest.mark.xfail(
-            reason="qemu-img allocates entire image",
-            # Test passes on oVirt CI.
-            strict=not testing.on_ovirt_ci())),
+    (sc.COW_FORMAT, 5),  # compat=1.1.
+    (sc.COW_FORMAT, 3),  # compat=0.10.
     (sc.RAW_FORMAT, 5),
 ])
 def test_copy_data_collapse(

tests/storage/storagetestlib.py
@@ -237,6 +237,9 @@ class FakeSD(object):
         if self.lvm:
             self.lvm.extendLV(self._manifest.sdUUID, volumeUUID, size)
 
+    def qcow2_compat(self):
+        return self._manifest.qcow2_compat()
+
 
 def make_sd_metadata(sduuid, version=3, dom_class=sd.DATA_DOMAIN, pools=None):
     md = FakeMetadata()

commit 293ac18d9c4498f069361e76ac1a7b4f01e808df

volume: Requires create for qcow2 image without a parent
Previously we requires create=True only for qcow2 images using
compat=0.10 that do not support zero clusters. This issue is not
relevant since RHEL 8.4.

Testing convert of big qcow2 image (8 TiB) show that writing zero
clusters is not fast enough for big images. When qemu-img convert
creates the target image, it knows that the image is zeroed, so it can
skip unallocated areas instead of zeroing them.

Testing copy of 8 TiB empty image shows that without -n the copy is 700
times faster:

    # time qemu-img convert -f qcow2 -O qcow2 -t none -T none -n src dst

    real    2m1.839s
    user    0m10.383s
    sys     0m7.563s

    # time qemu-img convert -f qcow2 -O qcow2 -t none -T none src dst

    real    0m0.171s
    user    0m0.111s
    sys     0m0.011s

With real images we see much lower speedup. Here is an example copy with
a 8 TiB image created with virt-builder:

    # virt-builder fedora-35 \
        --output fedora-35-8t.raw \
        --hostname=fedora-35 \
        --ssh-inject=root \
        --root-password=password:root \
        --selinux-relabel \
        --install=qemu-guest-agent \
        --size=8192G
        ...
                       Output file: /var/tmp/fedora-35-8t.raw
                       Output size: 8192.0G
                     Output format: raw
                Total usable space: 8192.0G
                        Free space: 8133.1G (99%)

    # qemu-img info /var/tmp/fedora-35-8t.raw
    image: /var/tmp/fedora-35-8t.raw
    file format: raw
    virtual size: 8 TiB (8796093022208 bytes)
    disk size: 1.5 GiB

    # qemu-img measure -O qcow2 /var/tmp/fedora-35-8t.raw
    required size: 3228303360
    fully allocated size: 8797435527168

The image was converted to qcow2 format to block volume:

    # time qemu-img convert -f raw -O qcow2 -t none -T none /var/tmp/fedora-35-8t.raw src

    real 0m35.979s
    user 0m3.303s
    sys 0m2.179s

In qcow2 format the image uses 2.2 GiB:

    # qemu-img check src
    No errors were found on the image.
    28746/134217728 = 0.02% allocated, 0.00% fragmented, 0.00% compressed clusters
    Image end offset: 2314469376

Creating new qcow2 image on destination block volume:

    # qemu-img create -f qcow2 dst 8t
    Formatting 'dst', fmt=qcow2 cluster_size=65536 extended_l2=off
    compression_type=zlib size=8796093022208 lazy_refcounts=off
    refcount_bits=16

Copying image from block volume to block volume with -n:

    # time qemu-img convert -f qcow2 -O qcow2 -t none -T none -n src dst

    real 3m7.006s
    user 0m18.817s
    sys 0m11.929s

Same copy without -n (creating new qcow2 image):

    # time qemu-img convert -f qcow2 -O qcow2 -t none -T none src dst

    real 0m59.822s
    user 0m7.632s
    sys 0m5.197s

Change Volume.requires_create() to return True for qcow2 images without
a parent.

Testing creating VM from template with the image tested above shows that
copy time decreased from 280 to 95 seconds (2.9 faster).

Fixes: #221
Fixes: 2bbe66274524 (volume: Be more careful with create=False)

lib/vdsm/storage/volume.py
@@ -850,24 +850,21 @@ class VolumeManifest(object):
            NFS < 4.2). qemu-img convert will fully allocate the entire image
            when trying to punch holes in the unallocated areas.
 
-        2. qcow2 compat=0.10 when volume does not have a parent. qemu-img
-           convert will fully allocate the image instead of skipping the
-           unallocated areas.
-           TODO: Remove when qemu-5.1.0 is available.
-           https://bugzilla.redhat.com/1858632
+        2. qcow2 image when volume does not have a parent. qemu-img convert
+           writes zero clusters for unallocated areas which is very slow with
+           a big image.
 
         When qemu-img convert creates the target image it knows that the image
         is zeroed so it can skip the unallocated areas.
+
+        For qcow2 images, creating a new image during convert creates a sparse
+        image.  This is not a problem since we don't support yet preallocated
+        qcow2 images.
         """
         if self.getFormat() == sc.RAW_FORMAT:
             return self.isSparse()
         else:
-            puuid = self.getParent()
-            if puuid and puuid != sc.BLANK_UUID:
-                return False
-
-            dom = sdCache.produce(self.sdUUID)
-            return dom.qcow2_compat() == "0.10"
+            return self.getParent() == sc.BLANK_UUID
 
     @classmethod
     def zero_initialized(cls):