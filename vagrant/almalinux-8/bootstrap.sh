fa3eca3b8c5718774f6c89ba4928337110070ea3
Nir Soffer <nsoffer@redhat.com>
2022-06-05 23:15:28 +0300

    vagrant: Start vagrant based CI infrastructure
    Vagrant makes it easy to manage virtual machines, and we really need
    virtual machines to run tests.
    
    Starting with almalinux 8.6, since I could not find official centos
    stream or rocky linux boxes in vagrant cloud. Maybe we can use
    euro-linux boxes:
    https://app.vagrantup.com/eurolinux-vagrant
    
    The almalinux-8 box is published at:
    https://app.vagrantup.com/nirs/boxes/vdsm-almalinux-8
    
    Known issues:
    
    - Using my ioprocess-preview and ovirt-imageio-preview instead of
      ovirt-master-snapshot, since it install lot of unneeded repos and mix
      centos and almalinux packages.
    
    - Various network packages are not installed since installing them
      breaks the VM network.
    
    - The base image does not use lvm, so /proc/devices does not report
      device-mapper major number, and some lvmfilter tests fail. We really
      want to run the tests on a system using lvm.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ vagrant/almalinux-8/bootstrap.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+dnf update -y
+
+# TODO: Should really use ovirt-release-master, but it adds lots of unneeded
+# and problematic repos, mixing centos stream packages with almalinux pacakges.
+# For now, use Nir's repos.
+
+dnf copr enable -y nsoffer/ioprocess-preview
+dnf copr enable -y nsoffer/ovirt-imageio-preview
+
+# TODO: Add NetworkManager, nmstate, python3-libmnstate (see
+# automation/check-patch.packages).
+# Installing these pacakges breaks the VM network and vagrant ssh cannot
+# connect.
+
+dnf install -y \
+    autoconf \
+    automake \
+    bash \
+    createrepo \
+    device-mapper-multipath \
+    dnf \
+    dnf-utils \
+    e2fsprogs \
+    gcc \
+    gdb \
+    git \
+    iproute-tc \
+    iscsi-initiator-utils \
+    libguestfs-tools-c \
+    lshw \
+    lsof \
+    lvm2 \
+    make \
+    openssl \
+    ovirt-imageio-client \
+    python3-augeas \
+    python3-blivet \
+    python3-cryptography \
+    python3-dateutil \
+    python3-dbus \
+    python3-decorator \
+    python3-devel \
+    python3-dmidecode \
+    python3-ioprocess \
+    python3-libselinux \
+    python3-libvirt \
+    python3-magic \
+    python3-nose \
+    python3-pip \
+    python3-policycoreutils \
+    python3-pyyaml \
+    python3-requests \
+    python3-sanlock \
+    python3-six \
+    python3-yaml \
+    qemu-img \
+    rpm-build \
+    rpmlint \
+    sanlock \
+    sudo \
+    systemd \
+    systemd-udev \
+    xfsprogs \
