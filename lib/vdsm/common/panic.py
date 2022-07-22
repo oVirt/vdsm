oVirt/vdsm

commit 56ed0796259a69e2f1a653691cf816bf3e08ce5e

panic: Limit time spent shutting down logging
We call panic when we must terminate vdsm and its child processes.
Shutting down logging is useful to get more info about the panic, but it
should not delay termination for unlimited time. A possible reason may
be queuing I/O when logging to non responsive NFS mount or booting from
multipath device.

Use signal.alarm() to interrupt logging.shutdown() after 10 seconds and
kill the process group.

Using signal.alarm() requires registering signal handler, which must be
done from main thread, so we register SIGALRM handler during startup.
The exception raised by the signal handler is raised in the thread
calling panic.panic() if logging.shutdown() times out.

Change-Id: Ib030e75eda0a0f30231c9a8120759ec0e52fb5c8
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

lib/vdsm/common/panic.py
@@ -22,13 +22,17 @@ from __future__ import division
 
 import logging
 import os
+import signal
 import sys
 
 
 def panic(msg):
     try:
         logging.error("Panic: %s", msg, exc_info=True)
+        # Depends on SIGALRM handler register during startup.
+        signal.alarm(10)
         logging.shutdown()
     finally:
+        signal.alarm(0)
         os.killpg(0, 9)
         sys.exit(-3)

lib/vdsm/vdsmd.py
@@ -83,9 +83,14 @@ def serve_clients(log):
             irs.spmStop(
                 irs.getConnectedStoragePoolsList()['poollist'][0])
 
+    def sigalrmHandler(signum, frame):
+        # Used in panic.panic() when shuting down logging, must not log.
+        raise RuntimeError("Alarm timeout")
+
     sigutils.register()
     signal.signal(signal.SIGTERM, sigtermHandler)
     signal.signal(signal.SIGUSR1, sigusr1Handler)
+    signal.signal(signal.SIGALRM, sigalrmHandler)
     zombiereaper.registerSignalHandler()
 
     profile.start()

commit 2817dfe54a71850148fece9f6056e678a0aab185

panic: Fix panic when logging.shutdown hang
We tried to add a timeout using signal.alarm() to make that vdsm will
panic if shutting down logging hangs. However this does not work, since
the alarm is delivered to the main thread instead of the thread setting
the alarm.

What actually happened after we added the alarm was:

1. Panic thread: calls signal.alarm(10)
2. Panic thread: calls logging.shutdown(), call hangs
3. Main thread: crash with "RuntimeError: Alarm timeout"

When we removed zombiereaper, we also removed the SIGALRM signal handler
by mistake, so what happens in current code is:

1. Panic thread: calls signal.alarm(10)
2. Panic thread: calls logging.shutdown(), call hangs
3. Unhandled SIGALRM terminates the process

Replace the broken alarm with a thread waiting on an event.

Fixes: 56ed0796259a (panic: Limit time spent shutting down logging)
Fixes: c0a67a0a3094 (zombiereaper: Reap out last traces of it)
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

lib/vdsm/common/panic.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 Red Hat, Inc.
+# Copyright 2015-2022 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,22 +17,25 @@
 #
 # Refer to the README and COPYING files for full details of the license
 
-from __future__ import absolute_import
-from __future__ import division
-
 import logging
 import os
-import signal
 import sys
+import threading
 
 
 def panic(msg):
-    try:
-        logging.exception("Panic: %s", msg)
-        # Depends on SIGALRM handler register during startup.
-        signal.alarm(10)
-        logging.shutdown()
-    finally:
-        signal.alarm(0)
+    logging.exception("Panic: %s", msg)
+    ready = threading.Event()
+
+    def run():
+        ready.wait(10)
         os.killpg(0, 9)
         sys.exit(-3)
+
+    t = threading.Thread(target=run)
+    t.daemon = True
+    t.start()
+    try:
+        logging.shutdown()
+    finally:
+        ready.set()

lib/vdsm/vdsmd.py
@@ -78,10 +78,6 @@ def serve_clients(log):
             irs.spmStop(
                 irs.getConnectedStoragePoolsList()['poollist'][0])
 
-    def sigalrmHandler(signum, frame):
-        # Used in panic.panic() when shuting down logging, must not log.
-        raise RuntimeError("Alarm timeout")
-
     sigutils.register()
     signal.signal(signal.SIGTERM, sigtermHandler)
     signal.signal(signal.SIGUSR1, sigusr1Handler)