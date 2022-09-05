13028b5067c89d3ba386fc5a9fb1b57edebaffd1
Albert Esteve <aesteve@redhat.com>
2022-09-05 10:33:41 +0200

    contrib: add script to install packages
    Add a script to install packages that were obtained
    in the ci/check-path.packages list.
    
    Update development.md to use the new script to
    install additional dependencies.
    
    Fixes: #43
    Signed-off-by: Albert Esteve <aesteve@redhat.com>

---
+++ contrib/install-pkg.sh
@@ -0,0 +1,53 @@
+#!/bin/sh -e
+
+sudo dnf install -y \
+    autoconf \
+    automake \
+    bash \
+    createrepo \
+    dnf \
+    dnf-utils \
+    e2fsprogs \
+    gcc \
+    gdb \
+    git \
+    iproute-tc \
+    iscsi-initiator-utils \
+    libguestfs-tools-c \
+    lshw \
+    lsof \
+    make \
+    NetworkManager \
+    nmstate \
+    nmstate-plugin-ovsdb \
+    openssl \
+    ovirt-imageio-client \
+    python3-augeas \
+    python3-blivet \
+    python3-cryptography \
+    python3-dateutil \
+    python3-dbus \
+    python3-decorator \
+    python3-devel \
+    python3-dmidecode \
+    python3-ioprocess \
+    python3-libnmstate \
+    python3-libselinux \
+    python3-libvirt \
+    python3-magic \
+    python3-nose \
+    python3-pip \
+    python3-policycoreutils \
+    python3-pyyaml \
+    python3-requests \
+    python3-sanlock \
+    python3-six \
+    python3-yaml \
+    qemu-img \
+    rpm-build \
+    rpmlint \
+    sanlock \
+    sudo \
+    systemd \
+    systemd-udev \
+    xfsprogs
