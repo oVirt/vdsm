75ce82c4cfdcb21619db92830cae926ac7a5f26d
Albert Esteve <aesteve@redhat.com>
2022-09-06 16:34:09 +0200

    reuse: addheader GPL common/*.py
    Add GPL copyright header to common python files.
    
    Signed-off-by: Albert Esteve <aesteve@redhat.com>

--- lib/vdsm/common/units.py
+++ lib/vdsm/common/units.py
@@ -1,9 +1,6 @@
-# Copyright (C) 2019 Red Hat, Inc.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# SPDX-FileCopyrightText: Red Hat, Inc.
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 """
 Constants for file/disk sizes.
 """
