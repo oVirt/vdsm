befa3dedac57b8c31714517e18091206e57df64b
Albert Esteve <aesteve@redhat.com>
2022-09-06 16:52:31 +0200

    reuse: addheader GPL profiling|rpc
    Add GPL copyright header to python files
    in profiling and rpc folders.
    
    Signed-off-by: Albert Esteve <aesteve@redhat.com>

--- lib/vdsm/rpc/bindingjsonrpc.py
+++ lib/vdsm/rpc/bindingjsonrpc.py
@@ -1,18 +1,6 @@
-# VDSM JsonRPC Server
 # Copyright (C) 2012 Adam Litke, IBM Corporation
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+# SPDX-FileCopyrightText: Red Hat, Inc.
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 from __future__ import absolute_import
 from __future__ import division
