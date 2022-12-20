37fc3d16250d194304432c7d61475d299e3e1514
Albert Esteve <aesteve@redhat.com>
2022-12-20 17:01:06 +0100

    ci: use new vdsm-test container
    Update the cis workflow to use the new vdsm-test
    container at quay.io/ovirt/vdsm-test:<distro>.
    
    Modify the test scripts to activate the venv
    before running the tests, otherwise the tests
    will have missing dependencies that are only
    installed in the venv in the new containers.
    
    Signed-off-by: Albert Esteve <aesteve@redhat.com>

--- ci/lint.sh
+++ ci/lint.sh
@@ -3,6 +3,9 @@
 # SPDX-FileCopyrightText: Red Hat, Inc.
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+# Activate the tests venv (for containers only)
+[ -d /venv ] && source /venv/bin/activate
+
 ./autogen.sh --system
 make
 make lint
