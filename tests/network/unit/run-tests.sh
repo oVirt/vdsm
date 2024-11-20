5e4ab156e6f6d568bf38505fa3ba4d62273e2d72
Jean-Louis Dupond <jean-louis@dupond.be>
2024-11-20 11:11:35 +0100

    ci: drop centos 8
    Signed-off-by: Jean-Louis Dupond <jean-louis@dupond.be>

--- tests/network/unit/run-tests.sh
+++ tests/network/unit/run-tests.sh
@@ -5,7 +5,7 @@
 
 source tests/network/common.sh
 
-IMAGE_TAG="${IMAGE_TAG:=centos-8}"
+IMAGE_TAG="${IMAGE_TAG:=centos-9}"
 CONTAINER_IMAGE="${CONTAINER_IMAGE:=$IMAGE_PREFIX-unit}"
 
 function patch_dist_commons {
