b1bb2c0b92234197f0799185aef3b34c9497075b
Pierluigi Lenoci <pierluigilenoci@gmail.com>
2026-04-28 19:36:21 +0200

    common: remove dead atomic_file_write function
    The atomic_file_write function and its tests have no remaining callers
    since commit 86e6768 removed all usages. Remove the dead code and its
    associated tests, along with imports that are no longer needed
    (contextlib.contextmanager, tempfile).
    
    Signed-off-by: Pierluigi Lenoci <pierluigilenoci@gmail.com>

--- tests/common/fileutils_test.py
+++ tests/common/fileutils_test.py
@@ -6,58 +6,9 @@ from __future__ import division
 
 import os
 
-from testlib import VdsmTestCase as TestCaseBase, namedTemporaryDir
+from testlib import VdsmTestCase as TestCaseBase
 
 from vdsm.common import fileutils
-
-
-class TestException(Exception):
-    pass
-
-
-class AtomicFileWriteTest(TestCaseBase):
-
-    def test_exception(self):
-        TEXT = 'foo'
-        with namedTemporaryDir() as tmp_dir:
-            test_file_path = os.path.join(tmp_dir, 'foo.txt')
-            with self.assertRaises(TestException):
-                with fileutils.atomic_file_write(test_file_path, 'w') as f:
-                    f.write(TEXT)
-                    raise TestException()
-            self.assertFalse(os.path.exists(test_file_path))
-            # temporary file was removed
-            self.assertEqual(len(os.listdir(tmp_dir)), 0)
-
-    def test_create_a_new_file(self):
-        TEXT = 'foo'
-        with namedTemporaryDir() as tmp_dir:
-            test_file_path = os.path.join(tmp_dir, 'foo.txt')
-            with fileutils.atomic_file_write(test_file_path, 'w') as f:
-                f.write(TEXT)
-                self.assertFalse(os.path.exists(test_file_path))
-            self._assert_file_contains(test_file_path, TEXT)
-            # temporary file was removed
-            self.assertEqual(len(os.listdir(tmp_dir)), 1)
-
-    def test_edit_file(self):
-        OLD_TEXT = 'foo'
-        NEW_TEXT = 'bar'
-        with namedTemporaryDir() as tmp_dir:
-            test_file_path = os.path.join(tmp_dir, 'foo.txt')
-            with open(test_file_path, 'w') as f:
-                f.write(OLD_TEXT)
-            with fileutils.atomic_file_write(test_file_path, 'w') as f:
-                f.write(NEW_TEXT)
-                self._assert_file_contains(test_file_path, OLD_TEXT)
-            self._assert_file_contains(test_file_path, NEW_TEXT)
-            # temporary file was removed
-            self.assertEqual(len(os.listdir(tmp_dir)), 1)
-
-    def _assert_file_contains(self, path, expected_content):
-        with open(path) as f:
-            content = f.read()
-            self.assertEqual(content, expected_content)
 
 
 class ParseKeyValFileTest(TestCaseBase):
